bffceea9945431f8abb5df7477da183624a6dd7b
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-08-06 18:59:21 +0000

    myuw_api.quarter: simply the logic to exclude caching which will be done in the rest client.

--- myuw_api/quarter.py
+++ myuw_api/quarter.py
@@ -1,81 +1,55 @@
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
-from datetime import datetime, timedelta
-from models import Term
-from restclients.sws_client import SWSClient
+from datetime import datetime
+import restclients.sws_client
 import logging
-import urllib
-import re
 import json
 
 class Quarter:
-    __logger = logging.getLogger('quartermanager')
+    """ The Quarter class encapsulate the access to the Term data """
+
+    __logger = logging.getLogger('myuw_api.quarter')
     __sws_client = SWSClient()
-    __sws_url_base = ''
+
+    class Term:
+        pass
 
     def __init__(self):
-        expiration = datetime.nowtimedelta(1)
+        self.local = Term()
 
     def get_cur_quarter(self):
-        # see if we have local data
-        try:
-            self.local = Term.objects.get(
-                first_day_quarter__lte=datetime.today(),
-                last_final_exam_date__gte=datetime.today()
-                )
-        except ObjectDoesNotExist:
-#            __mock()
-            __refresh_cur_quarter()
-        else:
-            # if the cached date is older than a day, refresh it
-            if (self.local.last_verified__lte=datetime.today()) :
-                __refresh_cur_quarter()
+        __mock()
+#        self.sws_result = __sws_client.get_current_term()
+#        __filter_data()        
         return self.local
 
     def get_next_quarter(self):
-        pass
+        self.sws_result = __sws_client.get_next_term()
+        __filter_data()
+        return self.local
 
     def get_prev_quarter(self):
-        pass
+        self.sws_result = __sws_client.get_previous_term()
+        __filter_data()
+        return self.local
 
-    def __refresh_cur_quarter(self):
-        self.sws_result = __sws_client.get_current_term(__sws_url_base)
-        __refresh_cache()
-
-    def __refresh_next_quarter(self):
-        self.sws_result = __sws_client.get_next_term(__sws_url_base)
-        __refresh_cache()
-
-    def __refresh_prev_quarter(self):
-        self.sws_result = __sws_client.get_previous_term(__sws_url_base)
-        __refresh_cache()
-
-    def __refresh_cache(self):
-        self.local = Term (
-            year = self.sws_result.year,
-            quarter = self.sws_result.quarter,
-            first_day_quarter = self.sws_result.first_day,
-            last_day_instruction = self.sws_result.last_day_of_classes, 
-            aterm_last_date = self.sws_result.last_add_day_a_term,
-            bterm_first_date = self.sws_result.b_term_first_day,
-            last_final_exam_date = self.sws_result.last_final_exam_day,
-            last_verified =  datetime.now()
-            )
-        self.local.save()
+    def __filter_data(self):
+        self.local.year = self.sws_result.year
+        self.local.quarter = self.sws_result.quarter
+        self.local.first_day_quarter = self.sws_result.first_day
+        self.local.last_day_instruction = self.sws_result.last_day_of_classes
+        self.local.aterm_last_date = self.sws_result.a_term_last_day
+        self.local.bterm_first_date = self.sws_result.b_term_first_day
+        self.local.last_final_exam_date = self.sws_result.last_final_exam_day
 
     def __mock(self):
-        # mock data
-        self.local = Term (
-            year = '2012',
-            quarter = '3',
-            first_day_quarter = datetime.date(2012, 6, 18),
-            last_day_instruction = datetime.date(2012, 8, 10),
-            aterm_last_date = datetime.date(2012, 7, 18),
-            bterm_first_date = datetime.date(2012, 6, 19),
-            last_final_exam_date = datetime.date(2012, 8, 17),
-            last_verified =  datetime.now()
-            )
-        self.local.save()
+        self.local.year = '2012'
+        self.local.quarter = 'Summer'
+        self.local.first_day_quarter = datetime.date(2012, 6, 18)
+        self.local.last_day_instruction = datetime.date(2012, 8, 10)
+        self.local.aterm_last_date = datetime.date(2012, 7, 18)
+        self.local.bterm_first_date = datetime.date(2012, 6, 19)
+        self.local.last_final_exam_date = datetime.date(2012, 8, 17)
+
 
      
 
