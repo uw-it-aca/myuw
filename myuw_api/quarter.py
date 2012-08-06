0b934e5a1512db35e29a6ccdb69dfff6be8bb556
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-08-06 22:53:17 +0000

    quarter.py: refactored

--- myuw_api/quarter.py
+++ myuw_api/quarter.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from datetime import datetime
-import restclients.sws_client
+from restclients.sws_client import SWSClient
 import logging
 import json
 
@@ -10,45 +10,39 @@ class Quarter:
     __logger = logging.getLogger('myuw_api.quarter')
     __sws_client = SWSClient()
 
-    class Term:
-        pass
-
-    def __init__(self):
-        self.local = Term()
-
     def get_cur_quarter(self):
-        __mock()
+        return __mock()
 #        self.sws_result = __sws_client.get_current_term()
-#        __filter_data()        
-        return self.local
+#        return __filter_data()
+
 
     def get_next_quarter(self):
         self.sws_result = __sws_client.get_next_term()
-        __filter_data()
-        return self.local
+        return __filter_data()
 
     def get_prev_quarter(self):
         self.sws_result = __sws_client.get_previous_term()
-        __filter_data()
-        return self.local
+        return __filter_data()
 
     def __filter_data(self):
-        self.local.year = self.sws_result.year
-        self.local.quarter = self.sws_result.quarter
-        self.local.first_day_quarter = self.sws_result.first_day
-        self.local.last_day_instruction = self.sws_result.last_day_of_classes
-        self.local.aterm_last_date = self.sws_result.a_term_last_day
-        self.local.bterm_first_date = self.sws_result.b_term_first_day
-        self.local.last_final_exam_date = self.sws_result.last_final_exam_day
+        return {'year': self.sws_result.year,
+                'quarter': self.sws_result.quarter,
+                'first_day_quarter': self.sws_result.first_day,
+                'last_day_instruction': self.sws_result.last_day_of_classes,
+                'aterm_last_date': self.sws_result.a_term_last_day,
+                'bterm_first_date': self.sws_result.b_term_first_day,
+                'last_final_exam_date': self.sws_result.last_final_exam_day
+                }
 
     def __mock(self):
-        self.local.year = '2012'
-        self.local.quarter = 'Summer'
-        self.local.first_day_quarter = datetime.date(2012, 6, 18)
-        self.local.last_day_instruction = datetime.date(2012, 8, 10)
-        self.local.aterm_last_date = datetime.date(2012, 7, 18)
-        self.local.bterm_first_date = datetime.date(2012, 6, 19)
-        self.local.last_final_exam_date = datetime.date(2012, 8, 17)
+        return {'year': '2012',
+                'quarter': 'Summer',
+                'first_day_quarter': datetime.date(2012, 6, 18),
+                'last_day_instruction': datetime.date(2012, 8, 10),
+                'aterm_last_date': datetime.date(2012, 7, 18),
+                'bterm_first_date': datetime.date(2012, 6, 19),
+                'last_final_exam_date': datetime.date(2012, 8, 17)
+                }
 
 
      
