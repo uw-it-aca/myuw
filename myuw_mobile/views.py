72534b3a9f6b99af3c3241f83f9636413ebee2d0
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-08-06 18:57:39 +0000

    myuw_mobile.views: add logic to pass the regid, year, quarter to the template.

--- myuw_mobile/views.py
+++ myuw_mobile/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render_to_response
-from models import Term  
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from myuw_api.quarter import Quarter
+from myuw_api.person import Person
 
 def index(request):
-    return render_to_response('index.html')
+    regid = Person().get_regid ('student1')
+        
+    cur_term = Quarter().get_cur_quarter()
+        
+    context = {'year': cur_term.year,
+               'quarter_name': cur_term.quarter,
+               'regid': regid,
+               'myuw_base_url': ''}
+    return render_to_response('mobile/index.html', 
+                              context, 
+                              context_instance=RequestContext(request))
 
 
