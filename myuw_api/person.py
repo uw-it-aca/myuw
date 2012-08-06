7c07748a65a7a46289530c147cec959a3239409e
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-08-06 22:52:44 +0000

    person.py: revise get_regid method. add get_contact method.

--- myuw_api/person.py
+++ myuw_api/person.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-import restclients.pws_client
+from restclients.pws_client import PWSClient
 import logging
 import json
 
@@ -9,7 +9,14 @@ class Person:
     __logger = logging.getLogger('myuw_api.person')
     __pws_client = PWSClient()
 
-    def get_regid(self, uwnetid):
+    def get_regid(self, netid):
         return '9136CCB8F66711D5BE060004AC494FFE'
-#       self.person = __pws_client.get_person_by_netid()
-#       return self.person.uwnetid
+#       self.person = __pws_client.get_person_by_netid(netid)
+#       return self.person.uwregid
+
+    def get_contact(self, regid):
+#        self.person = __pws_client.get_person_by_regid(netid)
+#        return {'email':
+#                 'phone':
+#                 }        
+        pass
