aecea3f4c9a7126c382b2899c52facaee7c72fc1
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-08-08 22:04:32 +0000

    urls.py: correct a regular expression error in the url pattern.

--- myuw_api/urls.py
+++ myuw_api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
+from myuw_api.views import StudClasScheCurQuarView
 
 urlpatterns = patterns('myuw_spi.views',
-                       url(r'v1/schedule/current/(?P<regid>\s{32})$', 
+                       url(r'v1/schedule/current/(?P<regid>\w{32})$',
                            StudClasScheCurQuarView().run),
                   #    url(r'v1/search/(?P<query>.*)$', SearchView().run),
 )
