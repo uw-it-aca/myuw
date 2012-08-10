bd55403fde51d6a29234567c3f3a230662529326
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-08-10 20:57:56 +0000

    myuw_api.pws_dao.py: implemented get_contact method.

--- myuw_api/pws_dao.py
+++ myuw_api/pws_dao.py
@@ -4,30 +4,43 @@ import logging
 import json
 import re
 
-# private statis
-#pws_client = PWSClient()
+# This module provides the single point access to PWSClient
+pws_client = PWSClient()
+pws_url_base = 'http://127.0.0.1:8080/identity/v1/'
+pws_url_suffix = '/full.json'
+
+uw_directory_url_base = 'https://www.washington.edu/home/peopledir/secure/?method=uwnetid&term='
 
 class Person:
-    """ The Person class encapsulates the interactions with the PWS client """
+    """ The Person class encapsulates the interactions with the PWS person resource """
 
-    logger = logging.getLogger('myuw_api.pws_dao.Person')
+    # static class variables
+    _logger = logging.getLogger('myuw_api.pws_dao.Person')
+    _pws_resource_url = pws_url_base + 'person/'
 
     def get_regid(self, netid):
-        return '9136CCB8F66711D5BE060004AC494FFE'
-#        regid = pws_client.get_person_by_netid(netid).uwregid
-#        if not re.match(r'^[A-F0-9]{32}$', regid, re.I):
-#            raise InvalidRegid("Invalid regid: " + regid)
-#       return regid
+        result = pws_client.get_json(Person._pws_resource_url +
+                                     netid +
+                                     pws_url_suffix)
+        if result:
+            regid = result['UWRegID']
+            if regid and re.match(r'^[A-F0-9]{32}$', regid, re.I):
+                return regid
+        raise Exception('No valid regid: ' + regid)
 
-    def get_contact(self, regid):
-        pass
-#        self.person = pws_client.get_person_by_regid(netid)
-# validate the return attributes 
-#        return {'email':
-#                 'phone':
-#                 }        
+    # parameter id: netid or regid
+    # return {'inDirectory': True/False,
+    #         'directoryUrl': the URL of the UW Directory detail page}
+    def get_contact(self, uid):
+        contact = {'inDirectory': None,
+                   'directoryUrl': None}
+        result = pws_client.get_json(Person._pws_resource_url +
+                                     uid +
+                                     pws_url_suffix)
+        if result:
+            contact['inDirectory'] = result['WhitepagesPublish']
+            if contact['inDirectory'] :
+                contact['directoryUrl'] = uw_directory_url_base + result['UWNetID']
+            return contact
+        raise Exception('Contact not found for ' + uid)
 
-
-class InvalidRegid(RuntimeError):
-    def __init__(self, arg):
-        self.args = arg
