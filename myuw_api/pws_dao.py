f2624cd2f9535b1cde932b8e21b4fe2cf63ddb00
pmichaud@u.washington.edu <pmichaud@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-08-21 22:30:45 +0000

    Moving the myuw pws interface to using restclient.pws

--- myuw_api/pws_dao.py
+++ myuw_api/pws_dao.py
@@ -1,46 +1,44 @@
 from django.conf import settings
-from restclients.pws_client import PWSClient
+from restclients.pws import PWS
 import logging
 import json
 import re
 
 # This module provides the single point access to PWSClient
-pws_client = PWSClient()
-pws_url_base = 'http://127.0.0.1:8080/identity/v1/'
-pws_url_suffix = '/full.json'
-
-uw_directory_url_base = 'https://www.washington.edu/home/peopledir/secure/?method=uwnetid&term='
 
 class Person:
     """ The Person class encapsulates the interactions with the PWS person resource """
 
     # static class variables
     _logger = logging.getLogger('myuw_api.pws_dao.Person')
-    _pws_resource_url = pws_url_base + 'person/'
 
     def get_regid(self, netid):
-        result = pws_client.get_json(Person._pws_resource_url +
-                                     netid +
-                                     pws_url_suffix)
-        if result:
-            regid = result['UWRegID']
-            if regid and re.match(r'^[A-F0-9]{32}$', regid, re.I):
-                return regid
+        pws = PWS()
+        person = pws.get_person_by_netid(netid)
+
+        if person != None:
+            return person.uwregid
+
+
         raise Exception('No valid regid: ' + regid)
 
     # parameter id: netid or regid
     # return {'inDirectory': True/False,
     #         'directoryUrl': the URL of the UW Directory detail page}
     def get_contact(self, uid):
-        contact = {'inDirectory': None,
-                   'directoryUrl': None}
-        result = pws_client.get_json(Person._pws_resource_url +
-                                     uid +
-                                     pws_url_suffix)
-        if result:
-            contact['inDirectory'] = result['WhitepagesPublish']
-            if contact['inDirectory'] :
-                contact['directoryUrl'] = uw_directory_url_base + result['UWNetID']
-            return contact
-        raise Exception('Contact not found for ' + uid)
+        # I don't know what this is, so i'm commenting it out for now
+        # It looks like the only caller is also commented out, so i don't
+        # feel too bad about that.
+        return
+#        contact = {'inDirectory': None,
+#                   'directoryUrl': None}
+#        result = pws_client.get_json(Person._pws_resource_url +
+#                                     uid +
+#                                     pws_url_suffix)
+#        if result:
+#            contact['inDirectory'] = result['WhitepagesPublish']
+#            if contact['inDirectory'] :
+#                contact['directoryUrl'] = uw_directory_url_base + result['UWNetID']
+#            return contact
+#        raise Exception('Contact not found for ' + uid)
 
