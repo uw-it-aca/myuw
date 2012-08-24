5e15ef9785e6a344f35765d9170501755f72cd57
pmichaud@u.washington.edu <pmichaud@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-08-24 22:58:35 +0000

    The start of the over 8 w/ drops - not yet finished

--- myuw_api/test/course/colors.py
+++ myuw_api/test/course/colors.py
@@ -192,3 +192,98 @@ class TestCourseColors(TestCase):
         self.assertEquals(colors[section2.section_label()], 2, "Second section gets the second color")
 
 
+    def test_around_the_top(self):
+        """ This tests that we go over the 8 color limit, and reuse blanks """
+        term = Term()
+        term.year = 2012
+        term.quarter = "autumn"
+
+        person = Person()
+        person.uwnetid = "javerage"
+        person.regid = "00000000000000000000000000000006"
+        schedule = ClassSchedule()
+
+        schedule.term = term
+        schedule.user = person
+        schedule.sections = []
+
+        section = Section()
+        section.term = term
+        section.curriculum_abbr = "MATH"
+        section.course_number = 124
+        section.section_id = "A"
+        section.is_primary_section = True
+        schedule.sections.append(section)
+
+        section2 = Section()
+        section2.term = term
+        section2.curriculum_abbr = "MATH"
+        section2.course_number = 300
+        section2.section_id = "A"
+        section2.is_primary_section = True
+        schedule.sections.append(section2)
+
+        section3 = Section()
+        section3.term = term
+        section3.curriculum_abbr = "MATH"
+        section3.course_number = 301
+        section3.section_id = "A"
+        section3.is_primary_section = True
+        schedule.sections.append(section3)
+
+
+
+        section4 = Section()
+        section4.term = term
+        section4.curriculum_abbr = "MATH"
+        section4.course_number = 302
+        section4.section_id = "A"
+        section4.is_primary_section = True
+        schedule.sections.append(section4)
+
+
+
+        section5 = Section()
+        section5.term = term
+        section5.curriculum_abbr = "MATH"
+        section5.course_number = 303
+        section5.section_id = "A"
+        section5.is_primary_section = True
+        schedule.sections.append(section5)
+
+
+
+        section6 = Section()
+        section6.term = term
+        section6.curriculum_abbr = "MATH"
+        section6.course_number = 304
+        section6.section_id = "A"
+        section6.is_primary_section = True
+        schedule.sections.append(section6)
+
+
+
+        section7 = Section()
+        section7.term = term
+        section7.curriculum_abbr = "MATH"
+        section7.course_number = 307
+        section7.section_id = "A"
+        section7.is_primary_section = True
+        schedule.sections.append(section7)
+
+        sched_dao = ScheduleDAO(person.regid)
+        colors = sched_dao.get_colors_for_schedule(schedule)
+
+
+        schedule.sections = []
+        schedule.sections.append(section)
+        schedule.sections.append(section7)
+
+
+        colors = sched_dao.get_colors_for_schedule(schedule)
+
+        self.assertEquals(colors[section.section_label()], 1, "First course section gets the first color")
+
+        self.assertEquals(colors[section7.section_label()], 7, "7th section gets the 7th color")
+
+
