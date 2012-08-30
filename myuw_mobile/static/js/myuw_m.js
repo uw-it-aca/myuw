b3f356d16994836f63899fcb990678fb01e1b38e
mreeve@u.washington.edu <mreeve@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-08-30 17:56:38 +0000

    Removed extraneous handlebars.js

--- myuw_mobile/static/js/myuw_m.js
+++ myuw_mobile/static/js/myuw_m.js
@@ -13,17 +13,19 @@ $(document).ready(function() {
             if(results !== null){
                 data = results;
 
-                //add indixes
+                //add indixes for the bootstrap accordion effect
                 for (var i=0; i<data.sections.length; i++) {
                     data.sections[i] = $.extend({}, data.sections[i], {index: i+1});
                 }
 
+                //probably extraneous
                 Handlebars.registerHelper("formatTime", function(time) {
                     formatted = time.toString().split(":");
                     formatted[0] = parseInt(formatted[0], 10)
                     return formatted.join(":");
                 });
 
+                //converts 24 hour time to 12 hour
                 Handlebars.registerHelper("formatTimeAMPM", function(time) {
                     formatted = time.toString().split(":");
                     formatted[0] = parseInt(formatted[0], 10)
@@ -36,6 +38,10 @@ $(document).ready(function() {
                     return formatted.join(":");
                 });
 
+                //format names
+                Handlebars.registerHelper("formatName", function(name) {
+                    return name.substr(0,1).toUpperCase()+name.substr(1).toLowerCase()
+                });
 
                 var source   = $("#courses").html();
                 var template = Handlebars.compile(source);
