3fecf03f8211e71581981fd22e13c72a6539ff4e
pmichaud@u.washington.edu <pmichaud@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-08-31 16:55:25 +0000

    Making any url that doesn't start with api go to the index page

--- myuw_mobile/urls.py
+++ myuw_mobile/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from myuw_mobile.views import index
 
 urlpatterns = patterns('myuw_mobile.views',
-    url(r'^$', 'index'),
+    url(r'^(?!api)', 'index'),
 #    url(r'^menu/$', 'menu'),
 #    url(r'^week/$', 'week'),
 )
