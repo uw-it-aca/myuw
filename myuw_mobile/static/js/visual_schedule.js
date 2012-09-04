a3511b8ad5efe082a7b95e2180d4b181f063a1c4
pmichaud@u.washington.edu <pmichaud@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-09-04 18:53:03 +0000

    Starting to pull in the data for the visual schedule

--- myuw_mobile/static/js/visual_schedule.js
+++ myuw_mobile/static/js/visual_schedule.js
@@ -4,11 +4,98 @@ var VisualSchedule = {
     },
 
     render: function() {
+        Handlebars.registerHelper('time_percentage', function(time, start, end) {
+            return parseInt(((time - start) / (end - start)) * 10000, 10) / 100;
+        });
+
         var source   = $("#visual_schedule").html();
         var template = Handlebars.compile(source);
 
         var course_data = WSData.course_data();
-        $("#courselist").html(template(course_data));
+
+        var visual_data = {
+            latest_ending: 0,
+            earliest_start: 24*60,
+            monday: [],
+            tuesday: [],
+            wednesday: [],
+            thursday: [],
+            friday: [],
+            saturday: [],
+            display_hours: [],
+            has_6_days: false
+        };
+
+        for (var index in course_data.sections) {
+            var section = course_data.sections[index];
+
+            for (var meeting_index in section.meetings) {
+                var meeting = section.meetings[meeting_index];
+                if (meeting.days_tbd) {
+                    continue;
+                }
+
+                var start_parts = meeting.start_time.split(":");
+                var start_minutes = parseInt(start_parts[0], 10) * 60 + parseInt(start_parts[1], 10);
+
+
+                var end_parts = meeting.end_time.split(":");
+                var end_minutes = parseInt(end_parts[0], 10) * 60 + parseInt(end_parts[1], 10);
+
+                if (start_minutes < visual_data.earliest_start) {
+                    visual_data.earliest_start = start_minutes;
+                }
+                if (end_minutes > visual_data.latest_ending) {
+                    visual_data.latest_ending = end_minutes;
+                }
+
+                var meeting_info = {
+                    start: start_minutes,
+                    end: end_minutes,
+                    color_id: section.color_id,
+                    curriculum: section.curriculum_abbr,
+                    course_number: section.course_number
+                };
+
+                var days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+                for (var day_index in days) {
+                    var day = days[day_index];
+
+                    if (meeting.meeting_days[day]) {
+                        if (day == "saturday") {
+                            visual_data.has_6_days = true;
+                        }
+                        visual_data[day].push(meeting_info);
+                    }
+                }
+            }
+        }
+
+        // Make it so the start and end times are always on the hour:
+        var start_hour = parseInt(visual_data.earliest_start / 60)
+        if ((start_hour * 60) == visual_data.earliest_start) {
+            visual_data.start_time = (start_hour * 60) - 60;
+        }
+        else {
+            visual_data.start_time = (start_hour * 60);
+        }
+
+        var end_hour = parseInt(visual_data.latest_ending / 60) + 1;
+        if ((end_hour * 60) == visual_data.latest_ending) {
+            visual_data.end_time = (end_hour * 60) - 60;
+        }
+        else {
+            visual_data.end_time = (end_hour * 60);
+        }
+
+        for (var i = visual_data.start_time; i <= visual_data.end_time; i += 60) {
+            visual_data.display_hours.push({
+                hour: (i / 60),
+                position: i
+            });
+        }
+
+        $("#courselist").html(template(visual_data));
 
         source = $("#quarter-visual").html();
         template = Handlebars.compile(source);
