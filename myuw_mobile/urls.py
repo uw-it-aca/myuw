251e40a5dabdaa0f9da0b094d94221f9d897914f
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-09-20 23:04:25 +0000

    Moved myuw_api/views.py to myuw_mobile/views/api.py.

--- myuw_mobile/urls.py
+++ myuw_mobile/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from myuw_mobile.views.page import index, myuw_login
+from myuw_mobile.views.api import StudClasScheCurQuar, TextbookCurQuar, InstructorContact
 
 urlpatterns = patterns('myuw_mobile.views.page',
     url(r'login', 'myuw_login'),
@@ -9,4 +10,11 @@ urlpatterns = patterns('myuw_mobile.views.page',
     url(r'^instructor', 'index'),
     url(r'^links', 'index'),
     url(r'^$', 'index'),
+)
+
+urlpatterns += patterns('myuw_spi.views',
+                       url(r'v1/schedule/current/$', StudClasScheCurQuar().run),
+                       url(r'v1/books/current/$', TextbookCurQuar().run),
+                       url(r'v1/person/(?P<regid>.*)$', InstructorContact().run),
+                  #    url(r'v1/search/(?P<query>.*)$', SearchView().run),
 )
