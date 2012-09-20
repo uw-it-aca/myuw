5d44d46ab226c0ed2da9d5faa62e9082bfc8067c
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-09-20 23:05:38 +0000

    Removed  myuw_api/urls.py.

--- myuw_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from myuw_api.views import StudClasScheCurQuar, TextbookCurQuar, InstructorContact
-
-urlpatterns = patterns('myuw_spi.views',
-                       url(r'v1/schedule/current/$', StudClasScheCurQuar().run),
-                       url(r'v1/books/current/$', TextbookCurQuar().run),
-                       url(r'v1/person/(?P<regid>.*)$', InstructorContact().run),
-                  #    url(r'v1/search/(?P<query>.*)$', SearchView().run),
-)
