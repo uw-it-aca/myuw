6150eb75e7c42e56fe78e68175142a7c05900c17
pmichaud@u.washington.edu <pmichaud@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-09-21 00:23:17 +0000

    Removes demo test

--- myuw_mobile/tests.py
+++ myuw_mobile/tests.py
@@ -1,9 +1,3 @@
 from django.test import TestCase
 from myuw_mobile.dao.test.course.colors import TestCourseColors
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
