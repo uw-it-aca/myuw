6216a049778accb2ddc5edff6c184d0c00a9cccd
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-09-27 22:59:35 +0000

    added log file rotation.

--- myuw_mobile/logger/logfile.py
+++ myuw_mobile/logger/logfile.py
@@ -1,24 +1,20 @@
-from django.conf import settings
-from datetime import datetime
 import logging
+import logging.handlers
 
 """ Specify the log file of the myuw_mobile application """
 
 app_name = 'myuw_mobile'
-logfile_prefix = 'logs/' + app_name + '.'
+log_dir = 'logs/'
+logfile_prefix = log_dir + app_name
 
 logger = logging.getLogger(app_name)
 
-if hasattr(settings, 'MYUW_LOG_FILE'):
-    logfile_prefix = settings.'MYUW_LOG_FILE'
-            
-fhandler = logging.FileHandler(logfile_prefix +
-                               datetime.strftime('%Y%m%d'))
+fhandler = logging.handlers.TimedRotatingFileHandler(logfile_prefix,
+                                                     when='midnight')
 fhandler.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%(levelname)s %(asctime)s %(message)s [%(name)s]')
+formatter = logging.Formatter('%(levelname)s %(asctime)s %(user)s %(message)s [%(name)s]')
+                              )
 fhandler.setFormatter(formatter)
 
 logger.addHandler(fhandler)
-
-    
