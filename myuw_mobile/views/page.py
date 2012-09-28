bf83ec105e5a3213e616fb3accbc4d07a8e6b1cc
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-09-28 22:43:00 +0000

    Changed to pass UserService to QuarterDao.

--- myuw_mobile/views/page.py
+++ myuw_mobile/views/page.py
@@ -1,93 +1,45 @@
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import get_object_or_404, render_to_response, redirect
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
+import logging
 from django.conf import settings
 from restclients.gws import GWS
-import logging
-from myuw_mobile.user import UserService
 from myuw_mobile.dao.sws import Quarter as QuarterDao
-from myuw_mobile.dao.pws import Person as PersonDao
+from myuw_mobile.logger.timer import Timer
+from myuw_mobile.user import UserService
 
 logger = logging.getLogger('myuw_mobile.views.page')
 
-gws = GWS()
-
 #@mobile_template('{mobile/}index.html')
 def index(request):
+    timer = Timer()
     context = {'year': None,
                'quarter': None,
-               'home_url': '',
+               'home_url': '/mobile',
                'err': None}
 
-    netid = get_netid_from_session(request)
-    if PersonDao().get_regid(netid):
-        request.session["user_netid"] = netid
+    if not request.is_secure():
+        context['err'] = 'Not https, abort!'
     else:
-        context['err'] = 'Invalid netid'
+        user_service = UserService(request)
+        netid = user_service.get_user()
 
-    try:
-        cur_term = QuarterDao().get_cur_quarter()
-    except Exception, message:
-        logger.error(netid + " get_cur_quarter --> " + message)
-        context['err'] = 'Failed to get quarter '
-    else:
-        context['year'] = cur_term.year
-        context['quarter'] = cur_term.quarter
+        if not netid:
+            context['err'] = 'Invalid netid!'
+        else:
+            request.session["user_netid"] = netid
+
+            cur_term = QuarterDao(user_service).get_cur_quarter()
+            if not cur_term:
+                context['err'] = 'No current quarter data!'
+            else:
+                context['year'] = cur_term.year
+                context['quarter'] = cur_term.quarter
+
+        logger.info("index time=%s", timer.get_elapsed,
+                    user_service.get_log_user_info())
 
     return render_to_response('index.html',
                               context,
                               context_instance=RequestContext(request))
-
-def myuw_login(request):
-    netid = get_netid_from_session(request)
-    if PersonDao().is_student(netid):
-        return redirect("myuw_mobile.views.page.index")
-
-    if hasattr(settings, "MYUW_USER_SERVLET_URL"):
-        return redirect(settings.MYUW_USER_SERVLET_URL)
-    else:
-        return redirect("https://myuw.washington.edu/servlet/user")
-
-
-def support(request):
-    user_service = UserService(request.session)
-    user_service.get_user()
-    # Do the group auth here.
-
-    if not hasattr(settings, "MYUW_ADMIN_GROUP"):
-        print "You must have a group in GWS defined as your admin group."
-        print 'Configure that using MYUW_ADMIN_GROUP="u_foo_bar"'
-        raise Exception("missing config")
-
-
-    actual_user = user_service.get_original_user()
-    members = gws.get_effective_members(settings.MYUW_ADMIN_GROUP)
-
-    is_admin = False
-
-    for member in members:
-        if member.uwnetid == actual_user:
-            is_admin = True
-            break
-
-    if is_admin == False:
-        return  HttpResponseRedirect("/mobile")
-
-    if "override_as" in request.POST:
-        user_service.set_override_user(request.POST["override_as"])
-
-    if "clear_override" in request.POST:
-        user_service.clear_override()
-
-    context = {
-        'original_user': user_service.get_original_user(),
-        'override_user': user_service.get_override_user(),
-    }
-
-    return render_to_response('support.html',
-                              context,
-                              context_instance=RequestContext(request))
-
-def get_netid_from_session(request): 
-    return UserService(request.session).get_user()
 
