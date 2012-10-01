59990f924eef47ed6ae11e74bc5d1b7fdd2a9822
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-10-01 00:43:59 +0000

    Added the user data and method needed by logging.

--- myuw_mobile/user.py
+++ myuw_mobile/user.py
@@ -1,23 +1,62 @@
+from django.http import HttpRequest
 from django.conf import settings
 from myuw_mobile.models import User
+import logging
 
 class UserService:
     _session = None
+    _logger = logging.getLogger('myuw_mobile.user.UserService')
 
-    def __init__(self, session):
-        self._session = session
+    def __init__(self, request):
+        self._session = request.session
+        self._log_data = {'clientip':request.META['HTTP_X_FORWARDED_FOR'],
+                          'user': None,
+                          'useragent': request.META['HTTP_USER_AGENT'],
+                          'path': request.get_full_path() }
+
+    def get_log_user_info(self):
+        """
+        Return a dictionary of user, accessed path, and client information for logging
+        """
+        self._log_data['user'] = get_userid_for_log() 
+        return self._log_data
+    
+    def get_userid_for_log(self):
+        """
+        Return <actual user netid> acting_as: <override user netid> if
+        the user is acting as someone else, otherwise <actual user netid>
+        """
+        override_userid = self.get_override_user()
+        actual_userid = self.get_original_user()
+        if override_userid:
+            log_userid = actual_userid + ' acting_as: ' + override_userid
+        else:
+            log_userid = actual_userid
+        return log_userid
 
     def get_user(self):
         override = self.get_override_user()
-        if override is not None and len(override) > 0:
+        if override and len(override) > 0:
             return override
 
         actual = self.get_original_user()
-        if actual is None or len(actual) == 0:
-            self._get_logged_in_user()
-            actual = self.get_original_user()
-
+        if not actual or len(actual) == 0:
+            return self._get_authenticated_user()
         return actual
+
+    def _get_authenticated_user(self):
+        if settings.DEBUG:
+            netid = 'javerage'
+        else:
+            netid = request.user.username
+
+        if netid
+            self.clear_override()
+            self.set_user(netid)
+        else:
+            self._logger.error("_get_authenticated_user no valid netid!",
+                               get_logging_user_info())
+        return netid
 
     def get_original_user(self):
         if "_us_user" in self._session:
@@ -36,21 +75,9 @@ class UserService:
     def clear_override(self):
         self._session["_us_override"] = None
 
-    def _get_logged_in_user(self):
-        if settings.DEBUG:
-            netid = 'javerage'
-        else:
-            netid = request.user.username
-
-        if netid is None:
-            raise("Must have a logged in user when DEBUG is off")
-
-        self.clear_override()
-        self.set_user(netid)
-
-    # the get_user / get_original_user / get_override_user should all really
-    # be returning user models.  But, i don't want to be serializing that
-    # data for each request.  So for now:
+    # the get_user / get_original_user / get_override_user 
+    # should all really be returning user models.  But, i don't want 
+    # to be serializing that data for each request. So for now:
     def get_user_for_netid(self, netid):
         in_db = User.objects.filter(uwnetid=netid)
 
