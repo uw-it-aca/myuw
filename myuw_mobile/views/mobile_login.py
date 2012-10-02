d51e82080d278ac765e8df5067ec9b2e92f48325
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-10-02 20:50:45 +0000

    Added logging.

--- myuw_mobile/views/mobile_login.py
+++ myuw_mobile/views/mobile_login.py
@@ -6,17 +6,28 @@ from myuw_mobile.user import UserService
 from myuw_mobile.dao.pws import Person as PersonDao
 from myuw_mobile.logger.timer import Timer
 
-logger = logging.getLogger('myuw_mobile.views.user_login')
-
 def user_login(request):
+    timer = Timer()
+    logger = logging.getLogger('myuw_mobile.views.user_login')
+
     user_service = UserService(request)
     netid = user_service.get_user()
 
     if not netid:
+        logger.info("user_login invalid netid, abort! time=%s",
+                    timer.get_elapsed(),
+                    user_service.get_log_user_info())
         return #a static error page 
 
     if PersonDao(user_service).is_student():
+        logger.info("user_login to mobile page. time=%s",
+                    timer.get_elapsed(),
+                    user_service.get_log_user_info())
         return redirect("myuw_mobile.views.page.index")
+
+    logger.info("user_login to desktop page. time=%s",
+                timer.get_elapsed(),
+                user_service.get_log_user_info())
 
     if hasattr(settings, "MYUW_USER_SERVLET_URL"):
         return redirect(settings.MYUW_USER_SERVLET_URL)
