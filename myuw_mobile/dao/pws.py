243f5a9ef4bd03d300ab7e31586418c55a72bf52
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-10-02 00:19:37 +0000

    Added response time logging of the PWS calls.

--- myuw_mobile/dao/pws.py
+++ myuw_mobile/dao/pws.py
@@ -4,6 +4,7 @@ import logging
 import sys
 from restclients.pws import PWS
 from myuw_mobile.user import UserService
+from myuw_mobile.logger.timer import Timer
 
 pws = PWS()
 
@@ -16,11 +17,12 @@ class Person:
     # static class variables
     _logger = logging.getLogger('myuw_mobile.dao.pws.Person')
 
-    def __init__(self, user_svc):
-        self._user_svc = user_svc
-        self._userid = user_svc.get_user()
+    def __init__(self, user_service):
+        self._user_service = user_service
+        self._userid = user_service.get_user()
 
     def get_person_by_netid(self):
+        timer = Timer()
         try:
             return pws.get_person_by_netid(self._userid)
         except Exception, message:
@@ -28,8 +30,12 @@ class Person:
             traceback.print_exc()
             Person._logger.error("get_person_by_netid %s %s", 
                                  Exception, message,
-                                 self._user_svc.get_log_user_info())
-            return None
+                                 self._user_service.get_log_user_info())
+        finally:
+            Person._logger.info("PWS get_person_by_netid time=%s", 
+                                timer.get_elapsed(),
+                                self._user_service.get_log_user_info())
+        return None
 
     def is_student(self):
         return self.get_person_by_netid().is_student
@@ -39,6 +45,7 @@ class Person:
 
     def get_contact(self, regid):
         contact = None
+        timer = Timer()
         try:
             contact = pws.get_contact(regid)
         except Exception, message:
@@ -46,7 +53,11 @@ class Person:
             traceback.print_exc()
             Person._logger.error("get_contact for %s: %s %s", 
                                  regid, Exception, message,
-                                 self._user_svc.get_log_user_info())
+                                 self._user_service.get_log_user_info())
+        finally:
+            Person._logger.info("PWS get_contact time=%s", 
+                                timer.get_elapsed(),
+                                self._user_service.get_log_user_info())
 
         if contact and not contact["WhitepagesPublish"] :
             affiliations = contact["PersonAffiliations"]
