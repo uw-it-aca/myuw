664f6c29d19aa074c642df9b23b2f564cf81b610
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-10-04 21:41:39 +0000

    No longer needs to pass in user service in the param.

--- myuw_mobile/logger/util.py
+++ myuw_mobile/logger/util.py
@@ -2,36 +2,36 @@ from myuw_mobile.user import UserService
 from myuw_mobile.logger.timer import Timer
 import logging
 
-def log_success_response(logger, user_service, timer):
-    logger.info("%s - fulfilled. time=%.2f",
-                user_service.get_logging_userid(),
+def log_success_response(logger, timer):
+    logger.info("%s - fulfilled. Time=%.2f",
+                UserService().get_logging_userid(),
                 timer.get_elapsed())
 
-def log_data_not_found_response(logger, user_service, timer):
-    logger.warning("%s - data not found. time=%.2f",
-                user_service.get_logging_userid(),
+def log_data_not_found_response(logger, timer):
+    logger.warning("%s - data not found. Time=%.2f",
+                UserService().get_logging_userid(),
                 timer.get_elapsed())
 
-def log_invalid_netid_response(logger, user_service, timer):
-    logger.error("%s - invalid netid, abort! time=%.2f",
-                user_service.get_logging_userid(),
+def log_invalid_netid_response(logger, timer):
+    logger.error("%s - invalid netid, abort! Time=%.2f",
+                UserService().get_logging_userid(),
                 timer.get_elapsed())
 
-def log_invalid_regid_response(logger, user_service, timer):
-    logger.error("%s - invalid regid, abort! time=%.2f",
-                user_service.get_logging_userid(),
+def log_invalid_regid_response(logger, timer):
+    logger.error("%s - invalid regid, abort! Time=%.2f",
+                UserService().get_logging_userid(),
                 timer.get_elapsed())
 
-def log_resp_time(logger, user_service, action, timer):
-    logger.error("%s - %s. time=%.2f",
-                 user_service.get_logging_userid(),
-                 action,
-                 timer.get_elapsed())
-
-def log_exception(logger, user_service, action, message):
+def log_exception(logger, action, message):
     logger.error("%s - %s =>Exception: %s",
-                 user_service.get_logging_userid(),
+                 UserService().get_logging_userid(),
                  action,
                  message)
+
+def log_resp_time(logger, action, timer):
+    logger.error("%s - %s fulfilled. Time=%.2f",
+                 UserService().get_logging_userid(),
+                 action,
+                 timer.get_elapsed())
 
 
