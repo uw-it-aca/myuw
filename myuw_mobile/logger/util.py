3c2d0ad43e2ad208416797cddfd83b5db3a8b9aa
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-10-04 05:45:56 +0000

    truncate the time value to two decimal places.

--- myuw_mobile/logger/util.py
+++ myuw_mobile/logger/util.py
@@ -3,30 +3,35 @@ from myuw_mobile.logger.timer import Timer
 import logging
 
 def log_success_response(logger, user_service, timer):
-    logger.info("%s - fulfilled. time=%f",
+    logger.info("%s - fulfilled. time=%.2f",
                 user_service.get_logging_userid(),
                 timer.get_elapsed())
 
 def log_data_not_found_response(logger, user_service, timer):
-    logger.warning("%s - data not found. time=%f",
+    logger.warning("%s - data not found. time=%.2f",
                 user_service.get_logging_userid(),
                 timer.get_elapsed())
 
 def log_invalid_netid_response(logger, user_service, timer):
-    logger.error("%s - invalid netid, abort! time=%f",
+    logger.error("%s - invalid netid, abort! time=%.2f",
                 user_service.get_logging_userid(),
                 timer.get_elapsed())
 
 def log_invalid_regid_response(logger, user_service, timer):
-    logger.error("%s - invalid regid, abort! time=%f",
+    logger.error("%s - invalid regid, abort! time=%.2f",
                 user_service.get_logging_userid(),
                 timer.get_elapsed())
 
-def log_exception(logger, user_service, message, timer):
-    logger.error("%s - Exception: %s. time=%f",
+def log_resp_time(logger, user_service, action, timer):
+    logger.error("%s - %s. time=%.2f",
                  user_service.get_logging_userid(),
-                 message,
+                 action,
                  timer.get_elapsed())
 
+def log_exception(logger, user_service, action, message):
+    logger.error("%s - %s =>Exception: %s",
+                 user_service.get_logging_userid(),
+                 action,
+                 message)
 
 
