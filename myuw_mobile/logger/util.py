74272aedeaf93be5af356878067f15871e527d5b
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-10-04 22:24:36 +0000

    truncate the time value to three decimal places.

--- myuw_mobile/logger/util.py
+++ myuw_mobile/logger/util.py
@@ -3,22 +3,22 @@ from myuw_mobile.logger.timer import Timer
 import logging
 
 def log_success_response(logger, timer):
-    logger.info("%s - fulfilled. Time=%.2f milliseconds",
+    logger.info("%s - fulfilled. Time=%.3f milliseconds",
                 UserService().get_logging_userid(),
                 timer.get_elapsed())
 
 def log_data_not_found_response(logger, timer):
-    logger.warning("%s - data not found. Time=%.2f milliseconds",
+    logger.warning("%s - data not found. Time=%.3f milliseconds",
                 UserService().get_logging_userid(),
                 timer.get_elapsed())
 
 def log_invalid_netid_response(logger, timer):
-    logger.error("%s - invalid netid, abort! Time=%.2f milliseconds",
+    logger.error("%s - invalid netid, abort! Time=%.3f milliseconds",
                 UserService().get_logging_userid(),
                 timer.get_elapsed())
 
 def log_invalid_regid_response(logger, timer):
-    logger.error("%s - invalid regid, abort! Time=%.2f milliseconds",
+    logger.error("%s - invalid regid, abort! Time=%.3f milliseconds",
                 UserService().get_logging_userid(),
                 timer.get_elapsed())
 
@@ -29,7 +29,7 @@ def log_exception(logger, action, message):
                  message)
 
 def log_resp_time(logger, action, timer):
-    logger.info("%s - %s fulfilled. Time=%.2f milliseconds",
+    logger.info("%s - %s fulfilled. Time=%.3f milliseconds",
                 UserService().get_logging_userid(),
                 action,
                 timer.get_elapsed())
