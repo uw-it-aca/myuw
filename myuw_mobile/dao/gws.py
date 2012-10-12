f8d8892336e93e1739c9f04523b21c109bfd56ab
pmichaud@u.washington.edu <pmichaud@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-10-12 21:50:16 +0000

    Reverting commit 443

--- myuw_mobile/dao/gws.py
+++ myuw_mobile/dao/gws.py
@@ -3,10 +3,9 @@ import traceback
 import logging
 import sys
 from restclients.gws import GWS
-from myuw_mobile.dao.sws import Schedule
+from myuw_mobile.user import UserService
 from myuw_mobile.logger.timer import Timer
 from myuw_mobile.logger.logback import log_resp_time, log_exception
-from myuw_mobile.user import UserService
 
 class Member:
     """
@@ -16,9 +15,6 @@ class Member:
 
     # static class variables
     _logger = logging.getLogger('myuw_mobile.dao.gws.Member')
-
-    def __init__(self):
-        self.schedule = Schedule().get_cur_quarter_schedule()
 
     def _is_member(self, groupid):
         """
@@ -44,43 +40,22 @@ class Member:
         """
         Return True if the user is an UW Seattle student
         in the current quarter
-        Note:
-        As uw_affiliation only maintains one affiliation from SDB
-        we get campus information from the registered sections
         """
-        if self.schedule and len(self.schedule.sections) > 0:
-            for section in self.schedule.sections:
-                if section.course_campus == 'Seattle':
-                    return True
-        return False
+        return self._is_member('uw_affiliation_seattle-student')
 
     def is_bothell_student(self):
         """
         Return True if the user is an UW Bothell student
         in the current quarter
-        Note:
-        As uw_affiliation only maintains one affiliation from SDB
-        we get campus information from the registered sections
         """
-        if self.schedule and len(self.schedule.sections) > 0:
-            for section in self.schedule.sections:
-                if section.course_campus == 'Bothell':
-                    return True
-        return False
+        return self._is_member('uw_affiliation_bothell-student')
 
     def is_tacoma_student(self):
         """
         Return True if the user is an UW Tacoma student
         in the current quarter
-        Note:
-        As uw_affiliation only maintains one affiliation from SDB
-        we get campus information from the registered sections
         """
-        if self.schedule and len(self.schedule.sections) > 0:
-            for section in self.schedule.sections:
-                if section.course_campus == 'Tacoma':
-                    return True
-        return False
+        return self._is_member('uw_affiliation_tacoma-student')
 
     def is_current_grad_student(self):
         """
