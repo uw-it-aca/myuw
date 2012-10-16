f858d2f0ba63aff01728b5be084ef9037f970b26
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-10-16 22:42:14 +0000

    Switch to use the new Affiliation class for identity. Add a log_affiliation function for logging link visits.

--- myuw_mobile/logger/logresp.py
+++ myuw_mobile/logger/logresp.py
@@ -1,17 +1,20 @@
-from myuw_mobile.dao.gws import Member
-from myuw_mobile.logger.logback import log_time
+from myuw_mobile.dao.affiliation import Affiliation
+from myuw_mobile.logger.logback import log_info, log_time
 
 def log_response_time(logger, message, timer):
     log_time(logger, message, timer) 
 
 def log_success_response(logger, timer):
+    log_time(logger, 'fulfilled', timer) 
+
+def log_success_response_with_affiliation(logger, timer):
     log_time(logger, 
              get_identity() + 'fulfilled', 
              timer) 
 
 def log_data_not_found_response(logger, timer):
-    log_time(logger,
-             get_identity() + ' data not found',
+    log_time(logger, 
+             ' data not found',
              timer)
 
 def log_invalid_netid_response(logger, timer):
@@ -20,30 +23,33 @@ def log_invalid_netid_response(logger, timer):
 def log_invalid_regid_response(logger, timer):
     log_time(logger, 'invalid regid, abort', timer)
 
+def log_affiliation(logger, message):
+    log_info(logger, get_identity() + message)
+
 def get_identity():
     """
     Return "(Affiliations: <affiliations>, <campus codes>)"
     """
     res = "(Affiliations:"
     no_affiliation_lengthmark = len(res)
-    member = Member()
-    if member.is_grad_student():
+    affi = Affiliation().get_all()
+    if affi["grad"]:
         res += ' Grad'
-    if member.is_undergrad_student():
+    if affi["undergrad"]:
         res += ' Undergrad'
-    if member.is_pce_student():
+    if affi["pce"]:
         res += ' Pce'
-    if member.is_student_employee():
+    if affi["stud_employee"]:
         res += ' StudEmployee'
     if len(res) == no_affiliation_lengthmark:
         res += 'None'
     res += ', Campuses:'
     no_campus_lengthmark = len(res)
-    if member.is_seattle_student():
+    if affi["seattle"]:
         res += ' Seattle'
-    if member.is_bothell_student():
+    if affi["bothell"]:
         res += ' Bothell'
-    if member.is_tacoma_student():
+    if affi["tacoma"]:
         res += ' Tacoma'
     if len(res) == no_campus_lengthmark:
         res += 'None'
