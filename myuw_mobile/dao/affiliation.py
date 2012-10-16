ed0e95f2bd034b6afae468592efbc106c71d308e
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-10-16 22:34:52 +0000

    create a class that combines the affiliation data from gws.Member and sws.Schedule.

---
+++ myuw_mobile/dao/affiliation.py
@@ -0,0 +1,59 @@
+import logging
+from sws import Schedule
+from gws import Member
+
+class Affiliation:
+    """
+    This class encapsulate the access of the term data
+    """
+    _logger = logging.getLogger('myuw_mobile.dao.affiliation.Affiliation')
+
+    def __init__(self):
+        self.enrolled_campuses = Schedule().get_cur_quarter_campuses()
+        self.member = Member()
+
+    def get_all(self):
+        """
+        return a dictionary of affiliations
+        """
+        return {"grad": self.member.is_grad_student(),
+                "undergrad": self.member.is_undergrad_student(),
+                "pce": self.member.is_pce_student(),
+                "stud_employee": self.member.is_student_employee(),
+                "seattle": self._is_seattle_student(),
+                "bothell": self._is_bothell_student(), 
+                "tacoma": self._is_tacoma_student()
+                }
+
+    def _is_seattle_student(self):
+        """
+        Return True if the user is an UW Seattle student
+        in the current quarter
+        Note:
+        As the UW Affiliation group only knows about one campus,
+        we also use registered sections in the current quarter
+        to determine the campuses.
+        """
+        return self.member.is_seattle_student() or self.enrolled_campuses["seattle"]
+
+    def _is_bothell_student(self):
+        """
+        Return True if the user is an UW Bothell student
+        in the current quarter
+        Note:
+        As the UW Affiliation group only knows about one campus,
+        we also use registered sections in the current quarter
+        to determine the campuses.
+        """
+        return self.member.is_bothell_student() or self.enrolled_campuses["bothell"]
+
+    def _is_tacoma_student(self):
+        """
+        Return True if the user is an UW Tacoma student
+        in the current quarter
+        Note:
+        As the UW Affiliation group only knows about one campus,
+        we also use registered sections in the current quarter
+        to determine the campuses.
+        """
+        return self.member.is_tacoma_student() or self.enrolled_campuses["tacoma"]
