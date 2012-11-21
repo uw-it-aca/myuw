616bd7c5ff8ba5fe5dd190a459b93980613a3ad4
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-11-21 00:04:15 +0000

    Fix bug: must specify the port number.

--- myuw_mobile/restclients/dao_implementation/hfs.py
+++ myuw_mobile/restclients/dao_implementation/hfs.py
@@ -1,6 +1,8 @@
 from os.path import dirname
 from restclients.dao_implementation.mock import get_mockdata_url
 from restclients.dao_implementation.live import get_con_pool, get_live_url
+import logging
+from myuw_mobile.logger.logback import log_info
 
 class File(object):
     """
@@ -23,6 +25,7 @@ class Live(object):
     This DAO provides real data.
     Access is restricted to localhost.
     """
+    logger = logging.getLogger('myuw_mobile.restclients.dao_implementation.hfs.Live')
     pool = None
 
     def getURL(self, url, headers):
@@ -31,8 +34,11 @@ class Live(object):
         :param url:
             in the format of "hfs/servlet/hfservices?sn=<student number>"
         """
-        host = 'http://localhost/'
-        if Live.pool == None:
-            Live.pool = get_con_pool(host, None, None)
+        host = 'http://localhost:80/'
+        if Live.pool is None:
+            Live.pool = get_con_pool(host, None, None,
+                                     socket_timeout=5.0,
+                                     max_pool_size=5)
+        log_info(Live.logger, Live.pool)
         return get_live_url (Live.pool, 'GET',
                              host, url, headers=headers)
