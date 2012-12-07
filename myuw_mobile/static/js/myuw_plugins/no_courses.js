9ee176c1affb7baa23aaa58064d288513b10d17c
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2012-12-07 22:56:10 +0000

    No longer need for using partial for no course.

--- myuw_mobile/static/js/myuw_plugins/no_courses.js
+++ myuw_mobile/static/js/myuw_plugins/no_courses.js
@@ -6,17 +6,21 @@
         var settings = $.extend({}, {
             "which_quarter_or_term" : "this quarter",
 	    "present_addi_links" : true,
+	    "visual": "",
         }, inputs);
 
         return this.each(function() {
             var source   = $("#no-courses").html();
             var template = Handlebars.compile(source);
-	    Handlebars.registerPartial("no-course-msg", 
-				       $("#no-course-msg").html());
+//	    Handlebars.registerPartial("no-course-msg", 
+//				       $("#no-course-msg").html());
             $(this).html(template(settings));
 	    
-	    $("#addi_links").addi_course_links({
-		"registered_for_the_quarter" : false });
+	    if (settings.present_addi_links) {
+		$("#addi_links").addi_course_links({
+		    "visual": settings.visual,
+		    "registered_for_the_quarter" : false });
+	    }
         });
     };
 })( jQuery );
