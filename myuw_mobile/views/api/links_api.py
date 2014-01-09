3f64fbefe5901cfc4c273399a6b0213299a8cf95
pmichaud@u.washington.edu <pmichaud@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-01-09 20:34:21 +0000

    Moving away from old userservice api

--- myuw_mobile/views/api/links_api.py
+++ myuw_mobile/views/api/links_api.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.utils import simplejson as json
 import logging
 from myuw_mobile.views.rest_dispatch import RESTDispatch, data_not_found
+from myuw_mobile.models import User
 from myuw_mobile.dao.links import Link
 from userservice.user import UserService
 from myuw_mobile.logger.timer import Timer
@@ -12,14 +13,14 @@ class QuickLinks(RESTDispatch):
     """
     Performs actions on resource at /api/v1/links/.
     """
-    
+
     def GET(self, request):
         """
         GET returns 200 with textbooks for the current quarter
         """
         timer = Timer()
         logger = logging.getLogger('myuw_mobile.views.links_api.QuickLinks.GET')
-        user = UserService().get_user_model()
+        user = self._get_user_model(UserService().get_user())
         links = Link().get_links_for_user(user)
         if not links:
             log_data_not_found_response(logger, timer)
@@ -46,8 +47,19 @@ class QuickLinks(RESTDispatch):
         for link in links:
             link_lookup[link["id"]] = link["is_on"]
 
-        user = UserService().get_user_model()
+        user = self._get_user_model(UserService().get_user())
         Link().save_link_preferences_for_user(link_lookup, user)
         log_success_response(logger, timer)
         return HttpResponse("")
 
+    def _get_user_model(self, netid):
+        in_db = User.objects.filter(uwnetid=netid)
+
+        if len(in_db) > 0:
+            return in_db[0]
+
+        new = User()
+        new.uwnetid = netid
+        new.save()
+
+        return new
