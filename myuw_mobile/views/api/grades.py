3979dd581e0542f5bbe8425d111bce3c177d9219
devights@u.washington.edu <devights@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-02-06 00:24:20 +0000

    Removing canvas grades for MUWM-1106

--- myuw_mobile/views/api/grades.py
+++ myuw_mobile/views/api/grades.py
@@ -4,11 +4,6 @@ from myuw_mobile.views.rest_dispatch import RESTDispatch
 from myuw_mobile.dao.sws import Quarter, Schedule
 from restclients.sws import SWS
 from restclients.pws import PWS
-from restclients.canvas.quizzes import Quizzes
-from restclients.canvas.submissions import Submissions
-from restclients.canvas.assignments import Assignments
-from restclients.canvas.enrollments import Enrollments
-from restclients.canvas import Canvas
 from operator import itemgetter
 from userservice.user import UserService
 
@@ -42,8 +37,6 @@ class Grades(RESTDispatch):
         username = UserService().get_user()
         regid = PWS().get_person_by_netid(username).uwregid
 
-        canvas_grades = self._get_canvas_grades_for_schedule(schedule, regid)
-
         final_grades = SWS().grades_for_regid_and_term(regid, term)
 
         grade_by_section_label = {}
@@ -59,8 +52,6 @@ class Grades(RESTDispatch):
             color = colors[section_label]
 
             section_data["color_id"] = color
-
-            self._add_canvas_grades_for_section(canvas_grades, section_data, section_label)
 
             if section_label in grade_by_section_label:
                 grade_data = grade_by_section_label[section_label]
@@ -94,75 +85,4 @@ class Grades(RESTDispatch):
                 "source_name": source_name,
                 "data": data
             })
-    def _get_canvas_grades_for_schedule(self, schedule, regid):
-        canvas = Canvas()
-        grades = {}
-        for section in schedule.sections:
-            grades[section.section_label()] = self._get_canvas_grades_section_sis_id(canvas.generate_section_id(section), regid)
-        return grades
-        
-        
-    def _get_canvas_grades_section_sis_id(self, section_sis_id, regid):
-        sub_cli = Submissions(as_user=regid)
-        submissions = []
-        try:
-            submissions = sub_cli.get_submissions_multiple_assignments_by_sis_id(False, section_sis_id)
-        except:
-            pass
-        assign_cli = Assignments(as_user=regid)
-        assignments = []
-        try:
-            assignments = assign_cli.get_assignments_by_sis_id(section_sis_id)
-        except:
-            pass
-        quiz_cli = Quizzes(as_user=regid)
-        quizzes = []
-        try:
-            quizzes = quiz_cli.get_quizzes_by_sis_id(section_sis_id)
-        except:
-            pass
 
-        assignments_data = []
-        submission_map = {}
-        for submission in submissions:
-            submission_map[submission.assignment_id] = submission
-        for assignment in assignments:
-            assign = {}
-            assign['name'] = assignment.name
-            assign['max_points'] = assignment.points_possible
-            assign['url'] = assignment.html_url
-            if assignment.assignment_id in submission_map:
-                assign['score'] = submission_map[assignment.assignment_id].grade
-            assignments_data.append(assign)
-        
-        quizzes_data = []
-        for quiz in quizzes:
-            quiz_data = {}
-            quiz_data['name'] = quiz.title
-            quiz_data['url'] = quiz.html_url
-            if quiz.quiz_id in submission_map:
-                quiz_data['score'] = submission_map[quiz.quiz_id].grade
-            quizzes_data.append(quiz_data)
-
-        return { "canvas_assignments": assignments_data, "canvas_quizzes": quizzes_data  }
-
-    def _add_canvas_grades_for_section(self, canvas_grades, section_data, section_label):
-        
-
-        if not "assignments" in section_data:
-            section_data["assignments"] = []
-
-        section_data["assignments"].append({
-            "source_id": "canvas_assignments",
-            "source_name": "Canvas Assignments",
-            "data": [{'assignments': canvas_grades[section_label]["canvas_assignments"]}]
-        })
-
-        
-
-        section_data["assignments"].append({
-            "source_id": "canvas_quizzes",
-            "source_name": "Canvas Quizzes",
-            "data": [{'assignments':canvas_grades[section_label]["canvas_quizzes"]}]
-        })
-            
