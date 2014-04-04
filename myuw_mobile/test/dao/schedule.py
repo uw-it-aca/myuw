7c3139f2aaae31b2e38e897b474cf71062a1e89f
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-04-04 23:14:03 +0000

    Added untitest for dao.schedule.

---
+++ myuw_mobile/test/dao/schedule.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.conf import settings
+
+from myuw_mobile.dao.schedule import _get_schedule
+from myuw_mobile.dao.schedule import has_summer_quarter_section
+from myuw_mobile.dao.schedule import filter_schedule_sections_by_summer_term
+from restclients.models import ClassSchedule, Term, Section, Person
+
+class TestSchedule(TestCase):
+    def test_has_summer_quarter_section(self):
+        regid = "9136CCB8F66711D5BE060004AC494FFE"
+        term = Term()
+        term.year = 2012
+        term.quarter = "summer"
+        schedule = _get_schedule(regid, term)
+        self.assertTrue(has_summer_quarter_section(schedule))
+
+        term = Term()
+        term.year = 2012
+        term.quarter = "autumn"
+        schedule = _get_schedule(regid, term)
+        self.assertFalse(has_summer_quarter_section(schedule))
+
+class TestSchedule(TestCase):
+    def test_filter_schedule_sections_by_summer_term(self):
+        regid = "9136CCB8F66711D5BE060004AC494FFE"
+        term = Term()
+        term.year = 2013
+        term.quarter = "summer"
+        schedule = _get_schedule(regid, term)
+        #it has a B-term section
+        for section in schedule.sections:
+            self.assertFalse(section.summer_term=="A-term")
+
+        filter_schedule_sections_by_summer_term(schedule, "A-term")
+        # the B-term section no longer exists
+        for section in schedule.sections:
+            self.assertFalse(section.summer_term=="B-term")
+            self.assertTrue(section.summer_term=="Full-term")
+
