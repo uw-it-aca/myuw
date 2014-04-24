59912483a1c589d02d69b2de5274208cc0b1d7a7
devights@u.washington.edu <devights@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-04-24 18:47:25 +0000

    Keying off of COMPRESS_PRECOMPILERS as COMPRESS_ENABLED is independent of server side less compilation

--- myuw_mobile/context_processors.py
+++ myuw_mobile/context_processors.py
@@ -3,10 +3,8 @@ from django.conf import settings
 def has_less_compiled(request):
     """ See if django-compressor is being used to precompile less
     """
-    if settings.COMPRESS_ENABLED:
-        return {'has_less_compiled': True}
-    else:
-        return {'has_less_compiled': False}
+    key = getattr(settings, "COMPRESS_PRECOMPILERS", None)
+    return {'has_less_compiled': key != ()}
 
 
 def less_not_compiled(request):
