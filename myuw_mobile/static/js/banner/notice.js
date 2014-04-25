2db3dcbf3b331363ca1cb26d21123e6ccab278f8
devights@u.washington.edu <devights@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-04-25 21:08:34 +0000

    Updating notice banner/reg card to support new JSON format

--- myuw_mobile/static/js/banner/notice.js
+++ myuw_mobile/static/js/banner/notice.js
@@ -1,9 +1,7 @@
 var NoticeBanner = {
     render: function (notice_data) {
-
-
         var source = $("#notice_banner").html();
         var template = Handlebars.compile(source);
-        return template();
+        return template(notice_data);
     }
 };
--- myuw_mobile/static/js/card/reg_status.js
+++ myuw_mobile/static/js/card/reg_status.js
@@ -1,14 +1,23 @@
 var RegStatusCard = {
     render: function (notice_data) {
-        var notices = [];
-        if (notice_data.Registration !== undefined) {
-            notices = notice_data.Registration.notices;
-        }
-
+        var reg_notices = RegStatusCard._get_registration_notices(notice_data);
         var source = $("#reg_status_card").html();
         var template = Handlebars.compile(source);
-        return template({ 
-            reg_notices: notices 
+        return template({"reg_notices": reg_notices});
+    },
+
+    _get_registration_notices: function (notice_data) {
+        reg_notices = [];
+        $.each(notice_data, function (group, notices) {
+            if (group !== "total_unread"){
+                notice_list = notices["notices"];
+                $.each(notice_list, function (key, notice) {
+                    if (notice.category === "Registration") {
+                        reg_notices.push(notice);
+                    }
+                });
+            }
         });
+        return reg_notices;
     }
 };
