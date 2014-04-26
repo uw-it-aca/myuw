113c306c23e76008007fca4ddc32e0a80b6eeadf
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-04-26 01:48:15 +0000

    Resolve MUWM-1262.

--- myuw_mobile/static/js/banner/notice.js
+++ myuw_mobile/static/js/banner/notice.js
@@ -1,7 +1,27 @@
 var NoticeBanner = {
     render: function (notice_data) {
+        var unread_categories = NoticeBanner._get_categories(notice_data);
         var source = $("#notice_banner").html();
         var template = Handlebars.compile(source);
-        return template(notice_data);
+        return template({
+            "total_unread": notice_data.total_unread,
+            "categories": unread_categories.sort()
+        });
+    },
+    _get_categories: function (notice_data) {
+        categories = [];
+        $.each(notice_data, function (group, notices) {
+            if (group !== "total_unread"){
+                notice_list = notices["notices"];
+                $.each(notice_list, function (key, notice) {
+                    if (!notice.is_read &&  
+                        $.inArray(notice.category, categories) == -1) {
+                        categories.push(notice.category);
+                    }
+                });
+            }
+        });
+        return categories;
     }
 };
+
