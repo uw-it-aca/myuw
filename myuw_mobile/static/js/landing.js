de694e11bfe1729f3430b08712f97b50a6e7bece
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-05-03 00:18:46 +0000

    Move filter_notices_by_category function from myuw_m.js to landing.js.

--- myuw_mobile/static/js/landing.js
+++ myuw_mobile/static/js/landing.js
@@ -14,7 +14,7 @@ var Landing = {
             notice_banner = NoticeBanner.render(notice_data);
         }
 
-        reg_notices = filter_notices_by_category('Registration', notice_data);
+        reg_notices = Landing.filter_notices_by_category('Registration', notice_data);
         var reg_status_card = ''
         if (reg_notices.length > 0) {
             reg_status_card = RegStatusCard.render(reg_notices);
@@ -25,5 +25,20 @@ var Landing = {
             reg_status_card: reg_status_card
         }));
 
+    },
+
+
+    // Filter non-holds notices by the given category
+    // Return a list of notices
+    filter_notices_by_category: function (category, notice_data) {
+        reg_notices = notice_data.today.notices.concat(
+            notice_data.week.notices, 
+            notice_data.future.notices);
+        reg_notices = reg_notices.filter(function(notice) {
+            if (notice.category === category) {
+                return true;
+            }
+        });
+        return reg_notices;
     }
 };
