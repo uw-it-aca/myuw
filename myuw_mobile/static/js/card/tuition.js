0f3354011b7381532c1d807029f5dc7754bb8e11
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-05-09 22:26:17 +0000

    Added for MUWM-1305

---
+++ myuw_mobile/static/js/card/tuition.js
@@ -0,0 +1,9 @@
+var TuitionCard = {
+    render: function (fina_notices) {
+        var source = $("#tuition_card").html();
+        var template = Handlebars.compile(source);
+        return template({"balances": WSData.tuition_data(),
+                         "fina_notices": fina_notices});
+    },
+
+};
