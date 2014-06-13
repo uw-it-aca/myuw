2db59b574eb953f548fb38594f911bde0756d52e
devights@u.washington.edu <devights@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-06-13 21:25:28 +0000

    Adding tuition due date/days until due to tuition card

--- myuw_mobile/static/js/card/tuition.js
+++ myuw_mobile/static/js/card/tuition.js
@@ -1,10 +1,34 @@
 var TuitionCard = {
     render: function (tuition_data) {
-        var template_data = tuition_data;
+        var template_data = tuition_data,
+            tuition_due_notice,
+            due_date;
         template_data['pce_tuition_dup'] = Notices.get_notices_for_tag("pce_tuition_dup");
+
+        tuition_due_notice = Notices.get_notices_for_tag("tuition_balance")[0];
+        for (var i = 0; i < tuition_due_notice.attributes.length; i += 1){
+            if (tuition_due_notice.attributes[i].name === "Date"){
+                due_date = new Date(tuition_due_notice.attributes[i].value.replace(/-/g, "/"));
+            }
+        }
+        if (due_date !== undefined) {
+            template_data['tuition_date'] = TuitionCard._format_date(due_date);
+            template_data['tuition_date_offset'] = TuitionCard._days_from_today(due_date);
+        }
+
         var source = $("#tuition_card").html();
         var template = Handlebars.compile(source);
         return template(tuition_data);
     },
+
+    _format_date: function (date) {
+        var date_string = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear().toString().substr(2,2);
+        return date_string;
+    },
+
+    _days_from_today: function (date) {
+        var today = new Date()
+        return Math.ceil((date - today) / (1000*60*60*24));
+    }
 
 };
