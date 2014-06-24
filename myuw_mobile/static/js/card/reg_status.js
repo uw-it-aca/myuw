2c81578558eac4f0c48c823972602e6619a5ef7d
charlon@u.washington.edu <charlon@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-06-24 18:11:07 +0000

    Adds todo classes to registration card.

--- myuw_mobile/static/js/card/reg_status.js
+++ myuw_mobile/static/js/card/reg_status.js
@@ -3,6 +3,12 @@ var RegStatusCard = {
         var source = $("#reg_status_card").html();
         var template = Handlebars.compile(source);
         return template({"reg_notices": reg_notices});
+        
+        // handle show more resources
+        $("#reg_show_resources").bind("click", function(ev) {
+            console.log("show more was clicked");
+        });
+        
     },
 
 };
