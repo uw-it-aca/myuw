d816be4b0f3005d216a3a5879e9242ae7391fe52
charlon@u.washington.edu <charlon@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-06-25 19:19:46 +0000

    Bootstrap grid cleanup.

--- myuw_mobile/static/js/card/reg_status.js
+++ myuw_mobile/static/js/card/reg_status.js
@@ -3,12 +3,7 @@ var RegStatusCard = {
         var source = $("#reg_status_card").html();
         var template = Handlebars.compile(source);
         return template({"reg_notices": reg_notices});
-        
-        // handle show more resources
-        $("#reg_show_resources").bind("click", function(ev) {
-            console.log("show more was clicked");
-        });
-        
     },
-
 };
+
+    
