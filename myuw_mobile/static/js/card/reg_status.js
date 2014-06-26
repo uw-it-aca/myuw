ba00300453bb66d8dfc247e83e91db0ce9f86f5f
charlon@u.washington.edu <charlon@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-06-26 20:10:04 +0000

    Wire up animated show and hide disclosure divs.

--- myuw_mobile/static/js/card/reg_status.js
+++ myuw_mobile/static/js/card/reg_status.js
@@ -2,8 +2,32 @@ var RegStatusCard = {
     render: function (reg_notices) {
         var source = $("#reg_status_card").html();
         var template = Handlebars.compile(source);
-        $('body').on('click', '#reg_show_resources', function () {
-            console.log('click');
+        
+        
+        // show registration resources
+        $('body').on('click', '#reg_show_resources', function (ev) {
+            
+            ev.preventDefault();
+            
+            $("#reg_resources").toggleClass("slide-show");
+            
+            if ($(this).text() == "Show more...")
+               $(this).text("Show less...")
+            else
+               $(this).text("Show more...");
+        });
+        
+        // show hold details
+        $('body').on('click', '#reg_show_holds', function (ev) {
+            
+            ev.preventDefault();
+            
+            $("#reg_holds").toggleClass("slide-show");
+            
+            if ($(this).text() == "Show details")
+               $(this).text("Hide details")
+            else
+               $(this).text("Show details");
         });
 
         return template({"reg_notices": reg_notices});
