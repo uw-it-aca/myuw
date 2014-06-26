a6a8b7820abc4e96c44139ded78c983fbd8c9b15
devights@u.washington.edu <devights@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-06-26 19:24:03 +0000

    Adding click event for show/hide link

--- myuw_mobile/static/js/card/reg_status.js
+++ myuw_mobile/static/js/card/reg_status.js
@@ -2,6 +2,10 @@ var RegStatusCard = {
     render: function (reg_notices) {
         var source = $("#reg_status_card").html();
         var template = Handlebars.compile(source);
+        $('body').on('click', '#reg_show_resources', function () {
+            console.log('click');
+        });
+
         return template({"reg_notices": reg_notices});
     },
 };
