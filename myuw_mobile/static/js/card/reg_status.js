905e0e08c2e6a2ed1c1eda295969b6368eb7d47f
charlon@u.washington.edu <charlon@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-06-27 17:28:25 +0000

    Fixes category header syntax and link alignment.

--- myuw_mobile/static/js/card/reg_status.js
+++ myuw_mobile/static/js/card/reg_status.js
@@ -1,8 +1,8 @@
 var RegStatusCard = {
     render: function (reg_notices) {
+        
         var source = $("#reg_status_card").html();
         var template = Handlebars.compile(source);
-        
         
         // show registration resources
         $('body').on('click', '#reg_show_resources', function (ev) {
@@ -11,10 +11,10 @@ var RegStatusCard = {
             
             $("#reg_resources").toggleClass("slide-show");
             
-            if ($(this).text() == "Show more...")
-               $(this).text("Show less...")
+            if ($("#reg_show_resources").text() == "Show more...")
+               $("#reg_show_resources").text("Show less...")
             else
-               $(this).text("Show more...");
+               $("#reg_show_resources").text("Show more...");
         });
         
         // show hold details
@@ -24,10 +24,11 @@ var RegStatusCard = {
             
             $("#reg_holds").toggleClass("slide-show");
             
-            if ($(this).text() == "Show details")
-               $(this).text("Hide details")
+            if ($("#reg_show_holds").text() == "Show details")
+               $("#reg_show_holds").text("Hide details")
             else
-               $(this).text("Show details");
+               $("#reg_show_holds").text("Show details");
+            
         });
 
         return template({"reg_notices": reg_notices});
