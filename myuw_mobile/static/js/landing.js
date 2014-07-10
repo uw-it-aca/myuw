ad35321b9c5423d1e83232c53947c39ccc981d34
devights@u.washington.edu <devights@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-07-10 22:22:54 +0000

    Restoring notice banner on landing page

--- myuw_mobile/static/js/landing.js
+++ myuw_mobile/static/js/landing.js
@@ -4,8 +4,14 @@ var Landing = {
         Landing.make_html();
     },
 
-    make_html: function() {
+    make_html: function () {
         $('html,body').animate({scrollTop: 0}, 'fast');
+        var landing_source = $("#landing").html();
+        var template = Handlebars.compile(landing_source);
+         $("#main-content").html(template());
+
+        WSData.fetch_notice_data(Landing.render_notice_banner);
+
         var cards = [RegStatusCard,
                      VisualScheduleCard,
                      CourseCard,
@@ -15,7 +21,18 @@ var Landing = {
                      HfsCard,
                      LibraryCard];
 
-        Cards.load_cards_in_order(cards, $("#main-content"));
+        Cards.load_cards_in_order(cards, $("#landing_content"));
+
+
     },
+
+    render_notice_banner: function () {
+        var notice_banner = '',
+            notice_data = WSData.notice_data();
+        if (Notices.get_total_unread() > 0) {
+            notice_banner = NoticeBanner.render(notice_data);
+        }
+        $("#notice_banner_location").html(notice_banner);
+    }
 
 };
