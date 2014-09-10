584641154c31310284c2ab05fad6130792ddef99
pmichaud@u.washington.edu <pmichaud@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-09-10 00:07:27 +0000

    MUWM-1926, MUWM-1927, MUWM-1931, MUWM-1888

---
+++ myuw_mobile/userservice_validation.py
@@ -0,0 +1,14 @@
+import re
+
+def validate(username):
+    if len(username) == 0:
+        return "No override user supplied"
+
+    if username != username.lower():
+        return "Usernames must be all lowercase"
+
+    re_personal_netid = re.compile(r'^[a-z][a-z0-9]{0,7}$', re.I)
+    if not re_personal_netid.match(username):
+        return "Username not a valid netid (starts with a letter, then 0-7 letters or numbers)"
+
+    return None
