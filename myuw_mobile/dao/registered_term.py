db211cede78c5f8f224b34345593d840e01e1d49
pmichaud@u.washington.edu <pmichaud@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-09-10 20:11:49 +0000

    MUWM-1950

--- myuw_mobile/dao/registered_term.py
+++ myuw_mobile/dao/registered_term.py
@@ -16,6 +16,13 @@ FULL_TERM = "F"
 A_TERM = "A"
 B_TERM = "B"
 
+FULL_TERM_CREDITS = "FC"
+A_TERM_CREDITS = "AC"
+B_TERM_CREDITS = "BC"
+
+FULL_TERM_SECTIONS = "FS"
+A_TERM_SECTIONS = "AS"
+B_TERM_SECTIONS = "BS"
 
 def get_current_summer_term_in_schedule(schedule):
     """
@@ -64,15 +71,15 @@ def _get_registered_future_quarters(next_quar_sche,
             
             if sumr_tms[A_TERM] or sumr_tms[FULL_TERM] and sumr_tms[B_TERM]:
                 terms.append(_get_future_term_json(next_quarter,
-                                                   "A-term"))
+                                                   "A-term", sumr_tms))
 
             if sumr_tms[B_TERM] or sumr_tms[FULL_TERM] and sumr_tms[A_TERM]:
                 terms.append(_get_future_term_json(next_quarter,
-                                                   "B-term"))
+                                                   "B-term", sumr_tms))
 
             if sumr_tms[FULL_TERM] and not sumr_tms[A_TERM] and not sumr_tms[B_TERM]:
                 terms.append(_get_future_term_json(next_quarter,
-                                                   "Full-term"))
+                                                   "Full-term", sumr_tms))
         else:
             terms.append(_get_future_term_json(next_quarter,""))
 
@@ -81,15 +88,27 @@ def _get_registered_future_quarters(next_quar_sche,
     return terms
 
 
-def _get_future_term_json(term, summer_term):
+def _get_future_term_json(term, summer_term, summer_term_data=None):
     return_json = term.json_data()
     return_json["summer_term"] = summer_term
     url = "/" + str(term.year) + "," + term.quarter
     if len(summer_term) > 0:
         url = url + "," + summer_term.lower()
+
+        if summer_term == "A-term":
+            return_json["credits"] = str(summer_term_data[FULL_TERM_CREDITS] + summer_term_data[A_TERM_CREDITS])
+            return_json["section_count"] = summer_term_data[FULL_TERM_SECTIONS] + summer_term_data[A_TERM_SECTIONS]
+        elif summer_term == "B-term":
+            return_json["credits"] = str(summer_term_data[FULL_TERM_CREDITS] + summer_term_data[B_TERM_CREDITS])
+            return_json["section_count"] = summer_term_data[FULL_TERM_SECTIONS] + summer_term_data[B_TERM_SECTIONS]
+        elif summer_term == "Full-term":
+            return_json["credits"] = str(summer_term_data[FULL_TERM_CREDITS])
+            return_json["section_count"] = summer_term_data[FULL_TERM_SECTIONS]
+    else:
+        return_json["credits"] = str(term.credits)
+        return_json["section_count"] = term.section_count
+
     return_json["url"] = url
-    return_json["credits"] = str(term.credits)
-    return_json["section_count"] = term.section_count
     return return_json
 
 
@@ -101,14 +120,26 @@ def _get_registered_summer_terms(registered_summer_sections):
         FULL_TERM : False,
         A_TERM : False,
         B_TERM : False,
+        FULL_TERM_CREDITS: 0,
+        A_TERM_CREDITS: 0,
+        B_TERM_CREDITS: 0,
+        FULL_TERM_SECTIONS: 0,
+        A_TERM_SECTIONS: 0,
+        B_TERM_SECTIONS: 0,
         }
     for section in registered_summer_sections:
         if is_full_summer_term(section.summer_term):
             data[FULL_TERM] = True
+            data[FULL_TERM_SECTIONS] = data[FULL_TERM_SECTIONS] + 1
+            data[FULL_TERM_CREDITS] = data[FULL_TERM_CREDITS] + section.student_credits
         elif is_a_term(section.summer_term):
             data[A_TERM] = True
+            data[A_TERM_SECTIONS] = data[A_TERM_SECTIONS] + 1
+            data[A_TERM_CREDITS] = data[A_TERM_CREDITS] + section.student_credits
         elif is_b_term(section.summer_term):
             data[B_TERM] = True
+            data[B_TERM_SECTIONS] = data[B_TERM_SECTIONS] + 1
+            data[B_TERM_CREDITS] = data[B_TERM_CREDITS] + section.student_credits
         else:
             pass
     return data
