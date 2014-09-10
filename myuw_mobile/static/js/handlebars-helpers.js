c3b049ab55d31961cdf2d0aac78afb4dd77e9bb9
pmichaud@u.washington.edu <pmichaud@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-09-10 15:46:01 +0000

    MUWM-1899

--- myuw_mobile/static/js/handlebars-helpers.js
+++ myuw_mobile/static/js/handlebars-helpers.js
@@ -239,4 +239,10 @@ Handlebars.registerHelper('show_final_card_day', function(list, start_time, end_
     return new Handlebars.SafeString(FinalExamCard.day_template({ meetings: list, start_time: start_time, end_time: end_time, term: term }));
 });
 
+Handlebars.registerHelper('pluralize', function(number, single, plural) {
+    if (number === 1) {
+        return single;
+    }
+    return plural;
+});
 
