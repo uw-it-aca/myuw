dc6b8169c30a7dd3f7c610c839a777c28eb1391e
devights@u.washington.edu <devights@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-09-16 22:22:38 +0000

    Hiding notices

--- myuw_mobile/views/api/notices.py
+++ myuw_mobile/views/api/notices.py
@@ -19,10 +19,14 @@ class Notices(RESTDispatch):
         """
         timer = Timer()
         logger = logging.getLogger(__name__)
-        notices = get_notices_for_current_user()
-        notice_json = self._get_json(notices)
-        logger.debug(notice_json)
-        log_success_response(logger, timer)
+
+        # MYUW NOTICE REMOVAL
+        # notices = get_notices_for_current_user()
+        # notice_json = self._get_json(notices)
+        # logger.debug(notice_json)
+        # log_success_response(logger, timer)
+        notice_json = []
+
         return HttpResponse(json.dumps(notice_json))
 
 
