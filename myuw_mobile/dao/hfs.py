4da023b04f17e871e9eb74e61a211963106854cc
pmichaud@u.washington.edu <pmichaud@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-09-18 16:58:26 +0000

    Rolling back that last commit - going to move forward w/ the new approach

--- myuw_mobile/dao/hfs.py
+++ myuw_mobile/dao/hfs.py
@@ -5,15 +5,10 @@ the student account balances (MyUW HFS) web service.
 
 import logging
 import traceback
-from myuw_mobile.logger.timer import Timer
-from myuw_mobile.restclients.hfs_accounts import HfsAccounts
-from myuw_mobile.dao.pws import get_student_number, get_employee_id
 from restclients.hfs.idcard import get_hfs_accounts
-from restclients.models.hfs import StudentHuskyCardAccout, ResidentDiningAccount
 from restclients.exceptions import DataFailureException
-from myuw_mobile.logger.logback import log_exception, log_resp_time, log_info
+from myuw_mobile.logger.logback import log_exception
 from myuw_mobile.dao.pws import get_netid_of_current_user
-from restclients.models.hfs import HfsAccouts as AccountsModel
 
 
 logger = logging.getLogger(__name__)
@@ -35,56 +30,7 @@ def get_account_balances_by_uwnetid(uwnetid):
                      id,
                       traceback.format_exc())
     return None
-
-
-def get_legacy_data():
-    student_number = get_student_number()
-    if student_number is None:
-        student_number = "0000000"
-
-    employee_id = get_employee_id()
-    if employee_id is None:
-        employee_id = "000000000"
-
-    timer = Timer()
-    try:
-        account_data = HfsAccounts().get_balances(student_number,
-                                                  employee_id)
-
-        if account_data is not None:
-            log_info(logger, account_data.json_data())
-            return account_data
-    except Exception as ex:
-        log_exception(logger,
-                     'HfsAccounts.get_balances',
-                      traceback.format_exc())
-    finally:
-        log_resp_time(logger,
-                     'HfsAccounts.get_balances',
-                      timer)
-    return None
-
-
+        
 
 def get_account_balances_for_current_user():
-    # XXX - MUWM-1982
-    # Falling back to the old myuw code until we get our network issue sorted out
-    #new_values = get_account_balances_by_uwnetid(get_netid_of_current_user())
-
-    old_values = get_legacy_data()
-
-    old_values_obj = AccountsModel()
-    print old_values.husky_card, old_values.residence_hall_dining
-
-    if old_values.husky_card:
-        old_values_obj.student_husky_card = StudentHuskyCardAccout()
-        old_values_obj.student_husky_card.balance = old_values.husky_card
-        old_values_obj.student_husky_card.last_updated = old_values.asof_datetime
-
-    if old_values.residence_hall_dining:
-        old_values_obj.resident_dining = ResidentDiningAccount()
-        old_values_obj.resident_dining.balance = old_values.residence_hall_dining
-        old_values_obj.resident_dining.last_updated = old_values.asof_datetime
-
-
-    return old_values_obj
+    return get_account_balances_by_uwnetid(get_netid_of_current_user())
