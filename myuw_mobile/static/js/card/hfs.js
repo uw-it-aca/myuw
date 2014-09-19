40fe26d2b4c389ca6938650ecf52173fd3603234
fanglin@u.washington.edu <fanglin@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-09-19 17:25:13 +0000

    Removed code no-long needed in hfs.js.

--- myuw_mobile/static/js/card/hfs.js
+++ myuw_mobile/static/js/card/hfs.js
@@ -22,20 +22,10 @@ var HfsCard = {
             template_data = {hfs_data: null};
         } else {
             template_data = {
-                hfs_data: hfs_data,
-                is_only_one_card: HfsCard.is_only_one_card(hfs_data),
-                is_total_two_cards: HfsCard.is_total_two_cards(hfs_data),
+                hfs_data: hfs_data
             };
         }
         HfsCard.dom_target.html(template(template_data));
-    },
-
-    is_only_one_card: function(hfs_data) {
-        return (hfs_data.student_husky_card && !hfs_data.employee_husky_card && !hfs_data.resident_dining || !hfs_data.student_husky_card && hfs_data.employee_husky_card && !hfs_data.resident_dining || !hfs_data.student_husky_card && !hfs_data.employee_husky_card && hfs_data.resident_dining);
-    },
-
-    is_total_two_cards: function(hfs_data) {
-        return (hfs_data.student_husky_card && hfs_data.employee_husky_card && !hfs_data.resident_dining || !hfs_data.student_husky_card && hfs_data.employee_husky_card && hfs_data.resident_dining || hfs_data.student_husky_card && !hfs_data.employee_husky_card && hfs_data.resident_dining);
     },
 
     _has_all_data: function () {
