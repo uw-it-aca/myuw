a5ef955e6b1616265568727e28d21791db913a3a
charlon@u.washington.edu <charlon@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-09-25 18:06:23 +0000

    Moves google analytics to bottom script and using django settings.

--- myuw_mobile/context_processors.py
+++ myuw_mobile/context_processors.py
@@ -6,6 +6,14 @@ def has_less_compiled(request):
     key = getattr(settings, "COMPRESS_PRECOMPILERS", None)
     return {'has_less_compiled': key != ()}
 
-
 def less_not_compiled(request):
     return { }
+
+
+def has_google_analytics(request):
+
+    ga_key = getattr(settings, 'GOOGLE_ANALYTICS_KEY', False)
+    return {
+        'GOOGLE_ANALYTICS_KEY': ga_key,
+        'has_google_analytics': ga_key
+    }
