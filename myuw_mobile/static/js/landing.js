3500d29f0ce3158d515f5b17a49b374ea602262f
devights@u.washington.edu <devights@u.washington.edu@41fae6fc-1e32-4298-8953-7c58a95c8a02>
2014-10-13 18:58:22 +0000

    Removes duplicate Tuition card for MUWM-2133

--- myuw_mobile/static/js/landing.js
+++ myuw_mobile/static/js/landing.js
@@ -23,7 +23,6 @@ var Landing = {
                      HfsCard,
                      TuitionCard,
                      LibraryCard,
-                     TuitionCard,
                      RegStatusCard
                     ];
         
