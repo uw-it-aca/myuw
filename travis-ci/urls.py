15782ffd7e6463c4610cc3df2384517f50ab0842
Patrick Michaud <pmichaud@uw.edu>
2014-11-26 11:50:00 -0800

    Adding travis ci configuration, the license, and a gitignore

---
+++ travis-ci/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^mobile/', include('myuw_mobile.urls')),
+)
