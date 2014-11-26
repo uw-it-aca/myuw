59e6b96c36270ea5ff7eaa4877d237e12c56d785
Stephen De Vight <devights@gmail.com>
2014-11-26 13:48:43 -0800

    fixing registered_term tests

--- myuw_mobile/test/dao/registered_term.py
+++ myuw_mobile/test/dao/registered_term.py
@@ -19,9 +19,9 @@ class TestRegisteredTerm(TestCase):
             term = _get_term_by_year_and_quarter(2013, "summer")
             schedule = _get_schedule(regid, term)
             data = _get_registered_summer_terms(schedule.sections)
-            self.assertFalse(data["A"])
+            self.assertFalse(data["F"])
             self.assertTrue(data["B"])
-            self.assertTrue(data["F"])
+            self.assertTrue(data["A"])
 
 
     def test_must_displayed_separately(self):
@@ -41,21 +41,21 @@ class TestRegisteredTerm(TestCase):
             regid = "9136CCB8F66711D5BE060004AC494FFE"
             term1 = _get_term_by_year_and_quarter(2013, "summer")
             schedule1 = _get_schedule(regid, term1)
-            self.assertEqual(len(schedule1.sections), 5)
+            self.assertEqual(len(schedule1.sections), 2)
 
             term2 = _get_term_by_year_and_quarter(2013, "autumn")
             schedule2 = _get_schedule(regid, term2)
-            self.assertEqual(len(schedule2.sections), 5)
+            self.assertEqual(len(schedule2.sections), 1)
 
             terms = _get_registered_future_quarters(schedule1, schedule2)
             self.assertTrue(len(terms)==3)
             self.assertTrue(terms[0]['year'] == 2013)
             self.assertEqual(terms[0]['quarter'], "Summer")
-            self.assertEqual(terms[0]['summer_term'], "A-term")
+            self.assertEqual(terms[0]['summer_term'], "a-term")
 
             self.assertTrue(terms[1]['year'] == 2013)
             self.assertEqual(terms[1]['quarter'], "Summer")
-            self.assertEqual(terms[1]['summer_term'], "B-term")
+            self.assertEqual(terms[1]['summer_term'], "b-term")
 
             self.assertTrue(terms[2]['year'] == 2013)
             self.assertEqual(terms[2]['quarter'], "Autumn")
