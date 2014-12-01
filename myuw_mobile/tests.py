d7117cfbcb213e708c5f929f65e9270be6f5b0f2
Stephen De Vight <devights@gmail.com>
2014-12-01 13:33:58 -0800

    Adding tests for myuw userservice validation

--- myuw_mobile/tests.py
+++ myuw_mobile/tests.py
@@ -5,3 +5,4 @@ from myuw_mobile.test.dao.schedule import TestSchedule
 from myuw_mobile.test.dao.notice import TestNotce
 from myuw_mobile.test.models import TestUserNotices
 from myuw_mobile.test.dao.term import TestTerm
+from myuw_mobile.test.userservice_validation import TestValidation
