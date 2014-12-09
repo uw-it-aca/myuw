e6969a0fc00b7611244cb9ad167cd2a6edcc7a0c
Patrick Michaud <pmichaud@uw.edu>
2014-12-09 11:55:00 -0800

    Fixes the context processor's less check

--- myuw_mobile/test/context_processors.py
+++ myuw_mobile/test/context_processors.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
+from django.conf import settings
 from myuw_mobile.context_processors import has_less_compiled, has_google_analytics
 
 class TestContextProcessors(TestCase):
     def test_less_none(self):
         with self.settings(COMPRESS_PRECOMPILERS=None):
+            del settings.COMPRESS_PRECOMPILERS
             values = has_less_compiled(None)
             self.assertEquals(values["has_less_compiled"], False)
 
@@ -20,5 +22,5 @@ class TestContextProcessors(TestCase):
     def test_less_has_less_precompiler(self):
         with self.settings(COMPRESS_PRECOMPILERS=(('text/coffeescript', 'coffee --compile --stdio'), ('text/foobar', 'path.to.MyPrecompilerFilter'), ('text/less', 'lessc {infile} {outfile}'),)):
             values = has_less_compiled(None)
-            self.assertEquals(values["has_less_compiled"], False)
+            self.assertEquals(values["has_less_compiled"], True)
 
