a3b8986086acbbd4d35545e80bef4b549963c351
Stephen De Vight <devights@gmail.com>
2014-12-16 12:09:22 -0800

    Fixing test/pep8

--- myuw_mobile/test/api/profile.py
+++ myuw_mobile/test/api/profile.py
@@ -6,17 +6,18 @@ from myuw_mobile.test.api import missing_url, get_user, get_user_pass
 from django.test.utils import override_settings
 import json
 
+
 @override_settings(RESTCLIENTS_SWS_DAO_CLASS='restclients.dao_implementation.sws.File',
-                   MIDDLEWARE_CLASSES = (
-                                'django.contrib.sessions.middleware.SessionMiddleware',
-                                'django.middleware.common.CommonMiddleware',
-                                'django.middleware.csrf.CsrfViewMiddleware',
-                                'django.contrib.auth.middleware.AuthenticationMiddleware',
-                                'django.contrib.auth.middleware.RemoteUserMiddleware',
-                                'django.contrib.messages.middleware.MessageMiddleware',
-                                'django.middleware.clickjacking.XFrameOptionsMiddleware',
-                                'userservice.user.UserServiceMiddleware',
-                                ),
+                   MIDDLEWARE_CLASSES=(
+                       'django.contrib.sessions.middleware.SessionMiddleware',
+                       'django.middleware.common.CommonMiddleware',
+                       'django.middleware.csrf.CsrfViewMiddleware',
+                       'django.contrib.auth.middleware.AuthenticationMiddleware',
+                       'django.contrib.auth.middleware.RemoteUserMiddleware',
+                       'django.contrib.messages.middleware.MessageMiddleware',
+                       'django.middleware.clickjacking.XFrameOptionsMiddleware',
+                       'userservice.user.UserServiceMiddleware',
+                       ),
                    AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
                    )
 class TestProfile(TestCase):
@@ -36,6 +37,5 @@ class TestProfile(TestCase):
         self.assertEquals(data["first_name"], "John Joseph")
         self.assertEquals(data["last_name"], "Average")
         self.assertEquals(data["local_address"]["street_line1"], "4634 26th Ave NE")
-        self.assertEquals(data["local_address"]["zip_code"], "98105")
+        self.assertEquals(data["local_address"]["zip_code"], "98105-4566")
         self.assertEquals(data["student_number"], "1033334")
-
