211d91d8de2799dbf03ca6fa79abfd5a04fef60f
Patrick Michaud <pmichaud@uw.edu>
2014-12-16 15:37:30 -0800

    Adding a setup.py, just to get this installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MyUW',
+      version='4.0',
+      description='',
+      install_requires=[] # uh... move requirements.txt into here
+     )
