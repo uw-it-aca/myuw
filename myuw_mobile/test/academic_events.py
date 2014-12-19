7c20ef3c1e6d9aa416074c3e4b60233876ce3d64
Patrick Michaud <pmichaud@uw.edu>
2014-12-19 14:33:25 -0800

    Unit tests for future and past filters

--- myuw_mobile/test/academic_events.py
+++ myuw_mobile/test/academic_events.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from myuw_mobile.views.api.academic_events import AcademicEvents
 from icalendar import Calendar, Event
 from datetime import datetime, date
+from django.test.client import RequestFactory
 
 class TestAcademicEvents(TestCase):
     def test_parsers(self):
@@ -33,3 +34,36 @@ class TestAcademicEvents(TestCase):
         self.assertEquals(year, '2018')
         self.assertEquals(quarter, 'Winter')
 
+    def test_filter_past(self):
+        request = RequestFactory().get("/")
+        request.session = {}
+        request.session["myuw_override_date"] = "2011-01-01"
+
+        past = Event()
+        past.add('dtstart', date(2010, 01, 01))
+        past.add('dtend', date(2010, 12, 31))
+        past['summary'] = "Past Event"
+
+        events = AcademicEvents().filter_past_events(request, [past])
+        self.assertEquals(len(events), 0)
+
+        request.session["myuw_override_date"] = "2010-12-31"
+        events = AcademicEvents().filter_past_events(request, [past])
+        self.assertEquals(len(events), 1)
+
+    def test_filter_future(self):
+        request = RequestFactory().get("/")
+        request.session = {}
+        request.session["myuw_override_date"] = "2012-12-01"
+
+        past = Event()
+        past.add('dtstart', date(2014, 01, 01))
+        past.add('dtend', date(2014, 12, 31))
+        past['summary'] = "Future Event"
+
+        events = AcademicEvents().filter_too_future_events(request, [past])
+        self.assertEquals(len(events), 0)
+
+        request.session["myuw_override_date"] = "2013-01-01"
+        events = AcademicEvents().filter_too_future_events(request, [past])
+        self.assertEquals(len(events), 1)
