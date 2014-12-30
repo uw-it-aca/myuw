ae0ea1ed730df6124dae88822f25ccb883c10ce6
Patrick Michaud <pmichaud@uw.edu>
2014-12-30 10:42:56 -0800

    MUWM-2399

--- myuw_mobile/static/js/academic_calendar.js
+++ myuw_mobile/static/js/academic_calendar.js
@@ -13,6 +13,8 @@ var AcademicCalendar = {
         var template = Handlebars.compile(source);
         var grouped = AcademicCalendar.group_events_by_term(events);
         $("#main-content").html(template({terms: grouped}));
+
+        AcademicCalendar.add_events();
     },
 
     group_events_by_term: function(events) {
@@ -44,5 +46,29 @@ var AcademicCalendar = {
         }
 
         return groups;
+    },
+
+    filter_all: function() {
+        $(".myuw-selected-button").removeClass("myuw-selected-button");
+        $("#myuw-event-filter-all").addClass("myuw-selected-button");
+        $(".acal-page-event").show();
+    },
+    filter_breaks: function() {
+        $(".myuw-selected-button").removeClass("myuw-selected-button");
+        $("#myuw-event-filter-breaks").addClass("myuw-selected-button");
+        $(".acal-page-event").hide();
+        $(".acal-page-event-break").show();
+    },
+    filter_classes: function() {
+        $(".myuw-selected-button").removeClass("myuw-selected-button");
+        $("#myuw-event-filter-classes").addClass("myuw-selected-button");
+        $(".acal-page-event").hide();
+        $(".acal-page-event-class").show();
+    },
+
+    add_events: function() {
+        $("#myuw-event-filter-all").click(AcademicCalendar.filter_all);
+        $("#myuw-event-filter-breaks").click(AcademicCalendar.filter_breaks);
+        $("#myuw-event-filter-classes").click(AcademicCalendar.filter_classes);
     }
 };
