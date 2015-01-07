520be3535c8d17a1a8692f5da12ae9b8f9ada9b3
Patrick Michaud <pmichaud@uw.edu>
2015-01-07 09:16:03 -0800

    Adding a snazzy message.

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@
 
 from distutils.core import setup
 
+print "\n\n\n\n************************************************************"
+print "         If you're installing MyUW,  you must also install"
+print "               each dependency in requirements.txt"
+print "************************************************************\n\n\n\n"
+
 setup(name='MyUW',
       version='4.0',
       description='',
