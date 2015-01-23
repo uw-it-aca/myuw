80a7970d1a956a4396cb8cc38c207c3af918eddb
mstone121 <mstone121@gmail.com>
2015-01-23 15:22:09 -0800

    Added override date checking to display date page.

--- myuw_mobile/test/views/display_dates.py
+++ myuw_mobile/test/views/display_dates.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from unittest2 import skipIf
 from myuw_mobile.test.api import missing_url, get_user, get_user_pass
 from django.test.utils import override_settings
+from myuw_mobile.dao.term import get_current_term
 import json
 
 @override_settings(RESTCLIENTS_SWS_DAO_CLASS='restclients.dao_implementation.sws.File',
@@ -32,10 +33,16 @@ class TestDisplayDatesPage(TestCase):
         self.client.login(username='javerage', password=get_user_pass('javerage'))
 
         response = self.client.post(url, {'date':'2013-06-07', 'myuw_after_submission':'no', 'myuw_after_reg':'yes'})
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.status_code, 200)    
+        self.assertEqual(self.client.session['myuw_override_date'], '2013-06-07')
 
-        response = self.client.post(url, {'date':'', 'myuw_after_reg':'blah'})
-        self.assertEquals(response.status_code, 200)
+        response = self.client.post(url, {'date':'2013-02-07', 'myuw_after_reg':'blah'})
+        self.assertEquals(response.status_code, 200)    
+        self.assertEqual(self.client.session['myuw_override_date'], '2013-02-07')
+
+        response = self.client.post(url, {'date':'2012-11-07'})
+        self.assertEquals(response.status_code, 200)    
+        self.assertEqual(self.client.session['myuw_override_date'], '2012-11-07')
 
     @skipIf(missing_url("myuw_mobile.views.display_dates.override"), "display dates override urls not configured")
     @override_settings(USERSERVICE_ADMIN_GROUP='test group')
@@ -45,13 +52,7 @@ class TestDisplayDatesPage(TestCase):
         get_user('javerage')
         self.client.login(username='javerage', password=get_user_pass('javerage'))
 
-        e = False
-        try:
+        with self.assertRaises(Exception):
             self.client.get(url)
-        except Exception as e:
-            ex = e
-
-        self.assertTrue(e)
-        self.assertEquals(e.message, 'Missing USERSERVICE_ADMIN_GROUP in settings')
 
         
