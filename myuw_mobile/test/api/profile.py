ad36d9fbecc03c887a456c27c8957029760af64f
mstone121 <mstone121@gmail.com>
2015-01-26 17:36:51 -0800

    Fixed error in logback.py (now log messages have actual content) and added error user test to api/profile.py test

--- myuw_mobile/test/api/profile.py
+++ myuw_mobile/test/api/profile.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.core.urlresolvers import reverse
+from django.test.utils import override_settings
 from unittest2 import skipIf
 from myuw_mobile.test.api import missing_url, get_user, get_user_pass
-from django.test.utils import override_settings
+from myuw_mobile.dao.student_profile import __name__ as student_profile_dao_name
+from logging.handlers import MemoryHandler
+import logging
 import json
 
 
@@ -39,3 +42,17 @@ class TestProfile(TestCase):
         self.assertEquals(data["local_address"]["street_line1"], "4634 26th Ave NE")
         self.assertEquals(data["local_address"]["zip_code"], "98105-4566")
         self.assertEquals(data["student_number"], "1033334")
+
+    @skipIf(missing_url("myuw_home"), "myuw urls not configured")
+    def test_missing_regid(self):
+        logger = logging.getLogger(student_profile_dao_name)
+        handler = MemoryHandler(100, flushLevel=60)
+        logger.addHandler(handler)
+        
+        url = reverse("myuw_profile_api")
+        get_user('err_user')
+        self.client.login(username='err_user', password=get_user_pass('err_user'))
+        self.client.get(url)
+
+        for log in handler.buffer:
+            self.assertIn('in get_profile_of_current_user', log.msg)
