cabf2ba4709596ec2cce07cb55b9c955b94a721a
Stephen De Vight <devights@gmail.com>
2015-03-02 17:41:44 -0800

    Modifying event tests to support new mock data

--- myuw_mobile/test/dao/calendar.py
+++ myuw_mobile/test/dao/calendar.py
@@ -10,29 +10,31 @@ from datetime import date
 from myuw_mobile.dao.calendar import get_events
 
 
-
 class TestCalendar(TestCase):
-    def test_get_events(self):
+    def test_far_future(self):
         now = date(2013, 04, 15)
-        cal = {'organic': None}
-        event_response = get_events([cal], now)
-        self.assertEqual(len(event_response['active_cals']), 0)
-        self.assertEqual(len(event_response['events']), 4)
-
-
-    def test_past_30(self):
-        now = date(2013, 04, 15)
-        cal = {'past30d': None}
+        cal = {'far_future': None}
         event_response = get_events([cal], now)
         self.assertEqual(len(event_response['active_cals']), 0)
         self.assertEqual(len(event_response['events']), 0)
 
-
-    def test_active(self):
+    def test_past_events(self):
         now = date(2013, 04, 15)
-        cal = {'within30d': None}
+        cal = {'past': None}
         event_response = get_events([cal], now)
-        print event_response
-        self.assertTrue('within30d' in event_response['active_cals'])
-        self.assertEqual(event_response['active_cals']['within30d']['count'], 5)
+        self.assertEqual(len(event_response['active_cals']), 0)
         self.assertEqual(len(event_response['events']), 0)
+
+    def test_future(self):
+        now = date(2013, 04, 15)
+        cal = {'future_1': None}
+        event_response = get_events([cal], now)
+        self.assertEqual(len(event_response['active_cals']), 1)
+        self.assertEqual(len(event_response['events']), 0)
+
+    def test_current(self):
+        now = date(2013, 04, 15)
+        cal = {'5_current': None}
+        event_response = get_events([cal], now)
+        self.assertEqual(len(event_response['active_cals']), 0)
+        self.assertEqual(len(event_response['events']), 5)
