4d8f43103c423354039678400a97571dab47cb9a
Patrick Michaud <pmichaud@uw.edu>
2015-03-04 14:58:24 -0800

    MUWM-2608

--- myuw_mobile/static/js/banner/profile.js
+++ myuw_mobile/static/js/banner/profile.js
@@ -4,6 +4,7 @@ var Profile = {
         var template = Handlebars.compile(source);
         $("#profile").html(template(WSData.profile_data()));
         $("#toggle_my_profile").attr("title", $("#profile_toggle_hidden").text());
+        $("#toggle_my_profile").attr("aria-label", $("#profile_toggle_hidden").text());
         Profile.add_events();
     },
 
@@ -17,6 +18,7 @@ var Profile = {
                 $("#my_profile").attr('aria-hidden', 'false');
                 $("#my_profile").show();
                 $("#toggle_my_profile").attr("title", $("#profile_toggle_displayed").text());
+                $("#toggle_my_profile").attr("aria-label", $("#profile_toggle_displayed").text());
                 WSData.log_interaction("show_my_profile");
             }
             else {
@@ -25,6 +27,7 @@ var Profile = {
 
                 setTimeout(function() {
                     $("#toggle_my_profile").attr("title", $("#profile_toggle_hidden").text());
+                    $("#toggle_my_profile").attr("aria-label", $("#profile_toggle_hidden").text());
                     $("#my_profile_arrow").attr('class', 'fa fa-chevron-down');
                     $("#my_profile").hide();
                 }, 900);
