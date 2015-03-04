eef783855ee6a5d8184fc5fd5c688f02123a53a4
Patrick Michaud <pmichaud@uw.edu>
2015-03-04 10:51:55 -0800

    pep8 fixes

--- myuw_mobile/util/cache_implementation.py
+++ myuw_mobile/util/cache_implementation.py
@@ -4,6 +4,7 @@ import re
 FOUR_HOURS = 60 * 60 * 4
 ONE_MONTH = 60 * 60 * 24 * 30
 
+
 class MyUWCache(TimedCache):
     def getCache(self, service, url, headers):
         if "sws" == service:
@@ -20,7 +21,6 @@ class MyUWCache(TimedCache):
         if re.match('^/student/v5/term', url):
             return self._response_from_cache('sws', url, headers, ONE_MONTH)
         return self._default('sws', url, headers)
-
 
     def _default(self, service, url, headers):
         return self._response_from_cache(service, url, headers, FOUR_HOURS)
