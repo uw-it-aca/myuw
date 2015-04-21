300e9f37473f80b247868934789c88cfb44651a3
Stephen De Vight <devights@gmail.com>
2015-04-20 18:30:06 -0600

    pep8 fixes

--- myuw_mobile/dao/iasystem.py
+++ myuw_mobile/dao/iasystem.py
@@ -2,27 +2,28 @@ from restclients.iasystem import evaluation
 from myuw_mobile.dao.student_profile import get_profile_of_current_user
 from restclients.exceptions import DataFailureException
 from restclients.pws import PWS
-import simplejson as json
-
-
 
 
 def get_evaluations_by_section(section):
     profile = get_profile_of_current_user()
-    return _get_evaluations_by_section_and_student(section, profile.student_number)
+    return _get_evaluations_by_section_and_student(section,
+                                                   profile.student_number)
+
 
 def _get_evaluations_by_section_and_student(section, student_id):
     try:
+        search_params = {'year': section.term.year,
+                         'term_name': section.term.quarter.capitalize(),
+                         'curriculum_abbreviation': section.curriculum_abbr,
+                         'course_number': section.course_number,
+                         'section_id': section.section_id,
+                         'student_id': student_id}
         return evaluation.search_evaluations(section.course_campus.lower(),
-                                             year=section.term.year,
-                                             term_name=section.term.quarter.capitalize(),
-                                             curriculum_abbreviation=section.curriculum_abbr,
-                                             course_number=section.course_number,
-                                             section_id=section.section_id,
-                                             student_id=student_id)
+                                             **search_params)
 
     except DataFailureException:
         return None
+
 
 def json_for_evaluation(evaluations):
     json_data = []
@@ -35,7 +36,6 @@ def json_for_evaluation(evaluations):
         eval_json['url'] = evaluation.eval_url
         eval_json['is_online'] = evaluation.eval_is_online
         json_data.append(eval_json)
-
 
     return json_data
 
--- myuw_mobile/views/api/base_schedule.py
+++ myuw_mobile/views/api/base_schedule.py
@@ -14,7 +14,8 @@ from myuw_mobile.dao.registered_term import get_current_summer_term_in_schedule
 from myuw_mobile.logger.logresp import log_data_not_found_response
 from myuw_mobile.logger.logresp import log_success_response
 from myuw_mobile.views.rest_dispatch import RESTDispatch, data_not_found
-from myuw_mobile.dao.iasystem import get_evaluations_by_section, json_for_evaluation
+from myuw_mobile.dao.iasystem import get_evaluations_by_section,\
+    json_for_evaluation
 
 
 class StudClasSche(RESTDispatch):
@@ -82,7 +83,8 @@ def load_schedule(schedule, summer_term=""):
 
         evaluation_data = get_evaluations_by_section(section)
         if evaluation_data is not None:
-             section_data["evaluation_data"] = json_for_evaluation(evaluation_data)
+            section_data["evaluation_data"] = \
+                json_for_evaluation(evaluation_data)
 
         if section.section_label() in canvas_data_by_course_id:
             enrollment = canvas_data_by_course_id[section.section_label()]
