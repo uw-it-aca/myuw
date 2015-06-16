0cf17e01ac2686db8857165c4edc9acae118c86a
Stephen De Vight <devights@gmail.com>
2015-06-16 16:52:17 -0700

    adding notices to thank you card

--- myuw_mobile/static/js/card/new_student/ns_thank_you.js
+++ myuw_mobile/static/js/card/new_student/ns_thank_you.js
@@ -2,12 +2,30 @@ var ThankYouCard = {
     name: 'ThankYouCard',
     dom_target: undefined,
     render_init: function() {
-        ThankYouCard.dom_target.html(ThankYouCard.render());
+        WSData.fetch_notice_data(ThankYouCard.render_upon_data,ThankYouCard.render_error);
+    },
+        render_upon_data: function() {
+        if (!ThankYouCard._has_all_data()) {
+            return;
+        }
+        ThankYouCard._render();
     },
 
-    render: function () {
+    _has_all_data: function () {
+        if (WSData.notice_data()) {
+            return true;
+        }
+        return false;
+    },
+
+    _render: function () {
         var source = $("#ns_thank_you").html();
         var template = Handlebars.compile(source);
-        return template({});
+        var notices = Notices.get_notices_for_tag("checklist_thankyou");
+
+        ThankYouCard.dom_target.html(template({'notices': notices}));
+    },
+    render_error: function () {
+        ThankYouCard.dom_target.html(CardWithError.render(ThankYouCard.name));
     },
 };
