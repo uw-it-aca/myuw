a2b600ef7b078d37ae6ef8e2398aec438300fe7e
Stephen De Vight <devights@gmail.com>
2015-06-17 16:13:37 -0700

    adding mock data and notice fetching to summer/efs card

--- myuw_mobile/static/js/card/new_student/ns_summer_efs.js
+++ myuw_mobile/static/js/card/new_student/ns_summer_efs.js
@@ -1,13 +1,32 @@
 var SummerEFSCard = {
     name: 'SummerEFSCard',
     dom_target: undefined,
-    render_init: function() {
-        SummerEFSCard.dom_target.html(SummerEFSCard.render());
+    render_init: function () {
+        WSData.fetch_notice_data(SummerEFSCard.render_upon_data, SummerEFSCard.render_error);
+    },
+    render_upon_data: function () {
+        if (!SummerEFSCard._has_all_data()) {
+            return;
+        }
+        SummerEFSCard._render();
     },
 
-    render: function () {
+    _has_all_data: function () {
+        if (WSData.notice_data()) {
+            return true;
+        }
+        return false;
+    },
+
+    _render: function () {
         var source = $("#ns_summer_efs").html();
         var template = Handlebars.compile(source);
-        return template({});
+        var notices = Notices.get_notices_for_tag("checklist_summerreg");
+        console.log(notices);
+
+        SummerEFSCard.dom_target.html(template({'notices': notices}));
+    },
+    render_error: function () {
+        SummerEFSCard.dom_target.html(CardWithError.render(SummerEFSCard.name));
     },
 };
