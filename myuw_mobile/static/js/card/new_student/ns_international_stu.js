43c405f5a71bdc5dcc0687c8522e864ec9e8f00a
Stephen De Vight <devights@gmail.com>
2015-06-16 17:50:28 -0700

    adding notice to international card

--- myuw_mobile/static/js/card/new_student/ns_international_stu.js
+++ myuw_mobile/static/js/card/new_student/ns_international_stu.js
@@ -1,13 +1,33 @@
 var InternationalStuCard = {
     name: 'InternationalStuCard',
     dom_target: undefined,
+
     render_init: function() {
-        InternationalStuCard.dom_target.html(InternationalStuCard.render());
+        WSData.fetch_notice_data(InternationalStuCard.render_upon_data,InternationalStuCard.render_error);
     },
 
-    render: function () {
+    render_upon_data: function() {
+        if (!InternationalStuCard._has_all_data()) {
+            return;
+        }
+        InternationalStuCard._render();
+    },
+
+    _has_all_data: function () {
+        if (WSData.notice_data()) {
+            return true;
+        }
+        return false;
+    },
+
+    _render: function () {
         var source = $("#ns_international_stu").html();
         var template = Handlebars.compile(source);
-        return template({});
+        var notices = Notices.get_notices_for_tag("checklist_ISS");
+
+        InternationalStuCard.dom_target.html(template({'notices': notices}));
+    },
+    render_error: function () {
+        InternationalStuCard.dom_target.html(CardWithError.render(InternationalStuCard.name));
     },
 };
