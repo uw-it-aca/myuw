4c5d8f8d2245e843a5d376bcbff8f84b40164cc7
Stephen De Vight <devights@gmail.com>
2015-06-17 16:59:59 -0700

    adding to register card notices

--- myuw_mobile/static/js/card/new_student/ns_to_register.js
+++ myuw_mobile/static/js/card/new_student/ns_to_register.js
@@ -1,13 +1,43 @@
 var ToRegisterCard = {
     name: 'ToRegisterCard',
     dom_target: undefined,
-    render_init: function() {
-        ToRegisterCard.dom_target.html(ToRegisterCard.render());
+    render_init: function () {
+        WSData.fetch_notice_data(ToRegisterCard.render_upon_data, ToRegisterCard.render_error);
+    },
+    render_upon_data: function () {
+        if (!ToRegisterCard._has_all_data()) {
+            return;
+        }
+        ToRegisterCard._render();
     },
 
-    render: function () {
+    _has_all_data: function () {
+        if (WSData.notice_data()) {
+            return true;
+        }
+        return false;
+    },
+
+    _render: function () {
         var source = $("#ns_to_register").html();
         var template = Handlebars.compile(source);
-        return template({});
+        var no_orient = Notices.get_notices_for_tag("checklist_no_orient");
+        var post_orient = Notices.get_notices_for_tag("checklist_orient_after");
+        var pre_iss = Notices.get_notices_for_tag("checklist_iss_before");
+        var post_iss = Notices.get_notices_for_tag("checklist_iss_after");
+        var pre_measles = Notices.get_notices_for_tag("checklist_measles_before");
+        var post_measles = Notices.get_notices_for_tag("checklist_measles_after");
+        var pre_orient = Notices.get_notices_for_tag("checklist_orient_before");
+
+        ToRegisterCard.dom_target.html(template({'no_orient': no_orient,
+                                                   'post_orient': post_orient,
+                                                   'pre_iss': pre_iss,
+                                                   'post_iss': post_iss,
+                                                   'pre_measles': pre_measles,
+                                                   'post_measles': post_measles,
+                                                   'pre_orient': pre_orient}));
+    },
+    render_error: function () {
+        ToRegisterCard.dom_target.html(CardWithError.render(ToRegisterCard.name));
     },
 };
