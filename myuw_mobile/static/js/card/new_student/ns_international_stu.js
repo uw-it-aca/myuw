cef97c24012127d73e0301fcc18b2ec3da2511b9
Stephen De Vight <devights@gmail.com>
2015-06-17 17:11:07 -0700

    display logic for international student card

--- myuw_mobile/static/js/card/new_student/ns_international_stu.js
+++ myuw_mobile/static/js/card/new_student/ns_international_stu.js
@@ -23,9 +23,12 @@ var InternationalStuCard = {
     _render: function () {
         var source = $("#ns_international_stu").html();
         var template = Handlebars.compile(source);
-        var notices = Notices.get_notices_for_tag("checklist_ISS");
-
-        InternationalStuCard.dom_target.html(template({'notices': notices}));
+        var notices = Notices.get_notices_for_tag("checklist_fiuts");
+        if (notices.length > 0){
+            InternationalStuCard.dom_target.html(template({'notices': notices}));
+        } else {
+            InternationalStuCard.dom_target.hide()
+        }
     },
     render_error: function () {
         InternationalStuCard.dom_target.html(CardWithError.render(InternationalStuCard.name));
