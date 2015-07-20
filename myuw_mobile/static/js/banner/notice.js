c7cadcab8218cc337b7e8eee0f59b5a4c346a2b5
Stephen De Vight <devights@gmail.com>
2015-07-20 16:54:53 -0700

    static critical notice category order

--- myuw_mobile/static/js/banner/notice.js
+++ myuw_mobile/static/js/banner/notice.js
@@ -8,19 +8,40 @@ var NoticeBanner = {
 
     render: function () {
         var notice_data = WSData.notice_data();
-        Notices.get_categorized_critical_notices();
 
         if (notice_data.length > 0) {
             var source = $("#notice_banner").html();
             var template = Handlebars.compile(source);
+            var notices = Notices.get_categorized_critical_notices();
+            var sorted_notices = NoticeBanner._sort_categories(notices);
 
             var html = template({
                 "total_unread": Notices.get_total_unread(),
-                "categorized_critical":
-                    Notices.get_categorized_critical_notices()
+                "categorized_critical": sorted_notices
             });
             NoticeBanner.dom_target.html(html);
         }
+    },
+
+    _sort_categories: function (notices) {
+        var category_order = ['Holds',
+        'Fees & Finances',
+        'Graduation',
+        'Academics',
+        'Registration',
+        'Insurance',
+        'Legal',
+        'Visa'];
+        var sorted_notices = [];
+        $.each(category_order, function(idx, category){
+            if (category in notices) {
+                sorted_notices.push(notices[category]);
+            }
+
+        });
+        return sorted_notices;
+
+
     }
 };
 
