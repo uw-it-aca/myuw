bec9b9d7605ebdd87b0aa568b9465970aca9518b
Patrick Michaud <pmichaud@uw.edu>
2015-08-28 15:24:43 -0700

    Resolves MUWM-3300

--- myuw/static/js/landing.js
+++ myuw/static/js/landing.js
@@ -81,6 +81,7 @@ var Landing = {
     _load_mobile_cards: function() {
         Landing._reset_content_divs();
         var mobile_cards = [
+            EmpFacStudentCard,
             FinalExamCard,
             GradeCard,
             FutureQuarterCardA,
