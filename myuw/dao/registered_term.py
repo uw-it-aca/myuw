f99be64e3e3333fdf2d64a66ef2282c07701327f
Fang Lin <fanglin@uw.edu>
2015-11-17 16:03:12 -0800

    Revert "Revert "merged develop""
    This reverts commit 973a9b2f1596fdb8d0bee9f35a2d73fa2eee9c37.

--- myuw/dao/registered_term.py
+++ myuw/dao/registered_term.py
@@ -4,6 +4,11 @@ This module encapsulates the access of the term data
 """
 
 import logging
+from datetime import datetime, timedelta
+from django.utils import timezone
+from myuw.models import SeenRegistration
+from myuw.models import SeenRegistration
+from myuw.dao import get_user_model
 from myuw.dao.term import is_a_term, is_b_term, is_full_summer_term
 from myuw.dao.term import get_current_summer_term, get_comparison_date
 from myuw.dao.term import get_quarter, get_current_quarter
@@ -12,9 +17,12 @@ from myuw.dao.schedule import get_next_autumn_quarter_schedule
 from myuw.dao.schedule import has_summer_quarter_section
 from myuw.dao.schedule import get_current_quarter_schedule
 from myuw.dao import get_user_model
-from myuw.models import SeenRegistration
-from django.utils import timezone
-from datetime import datetime, timedelta
+from myuw.dao.term import get_current_summer_term,\
+    get_comparison_date, get_specific_term, get_current_quarter,\
+    is_a_term, is_b_term, is_full_summer_term
+from myuw.dao.schedule import has_summer_quarter_section,\
+    get_current_quarter_schedule, get_next_quarter_schedule,\
+    get_next_autumn_quarter_schedule
 
 
 logger = logging.getLogger(__name__)
