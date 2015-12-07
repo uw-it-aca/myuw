ce05634f0e32285712df8813a41849b075414dd5
Fang Lin <fanglin@uw.edu>
2015-12-07 11:06:44 -0800

    Resolve MUWM-3407

--- myuw/test/cache.py
+++ myuw/test/cache.py
@@ -62,21 +62,51 @@ class TestCustomCachePolicy(TestCase):
 
             cache_entry = CacheEntryTimed.objects.get(
                 service="sws", url="/student/v5/term/1014,summer.json")
-            # Cached response is returned after 29 days
+            # Cached response is returned after 6 days
             orig_time_saved = cache_entry.time_saved
-            cache_entry.time_saved = orig_time_saved - timedelta(days=29)
+            cache_entry.time_saved = orig_time_saved - timedelta(
+                minutes=(60*24*7-1))
             cache_entry.save()
 
             response = cache.getCache(
                 'sws', '/student/v5/term/1014,summer.json', {})
             self.assertNotEquals(response, None)
 
-            # Cached response is not returned after 31 days
-            cache_entry.time_saved = orig_time_saved - timedelta(days=31)
+            # Cached response is not returned after 7 days
+            cache_entry.time_saved = orig_time_saved - timedelta(days=7)
             cache_entry.save()
 
             response = cache.getCache(
-                'sws', '/student/v5/term/1014,summer.json', {})
+                'sws', '/student/v5/term/current.json', {})
+            self.assertEquals(response, None)
+
+            response = cache.getCache(
+                'sws', '/student/v5/term/current.json', {})
+            self.assertEquals(response, None)
+            cache.processResponse(
+                "sws", "/student/v5/term/current.json", ok_response)
+            response = cache.getCache(
+                'sws', '/student/v5/term/current.json', {})
+            self.assertEquals(response["response"].data, 'xx')
+
+            cache_entry = CacheEntryTimed.objects.get(
+                service="sws", url="/student/v5/term/current.json")
+            # Cached response is returned after 6 days
+            orig_time_saved = cache_entry.time_saved
+            cache_entry.time_saved = orig_time_saved - timedelta(
+                minutes=(60*24-1))
+            cache_entry.save()
+
+            response = cache.getCache(
+                'sws', '/student/v5/term/current.json', {})
+            self.assertNotEquals(response, None)
+
+            # Cached response is not returned after 7 days
+            cache_entry.time_saved = orig_time_saved - timedelta(days=1)
+            cache_entry.save()
+
+            response = cache.getCache(
+                'sws', '/student/v5/term/current.json', {})
             self.assertEquals(response, None)
 
     def test_myplan_default(self):
@@ -90,6 +120,106 @@ class TestCustomCachePolicy(TestCase):
             self.assertEquals(response, None)
             cache.processResponse("myplan", "/api/plan/xx", ok_response)
             response = cache.getCache('myplan', '/api/plan/xx', {})
+
+            cache_entry = CacheEntryTimed.objects.get(
+                service="myplan", url="/api/plan/xx")
+            orig_time_saved = cache_entry.time_saved
+            cache_entry.time_saved = (orig_time_saved -
+                                      timedelta(seconds=5))
+            cache_entry.save()
+            response = cache.getCache('myplan', '/api/plan/xx', {})
+            self.assertEquals(response, None)
+
+    def test_registration_default(self):
+        with self.settings(RESTCLIENTS_DAO_CACHE_CLASS=CACHE):
+            cache = MyUWCache()
+            ok_response = MockHTTP()
+            ok_response.status = 200
+            ok_response.data = "xx"
+
+            response = cache.getCache('sws', '/student/v5/registration/xx', {})
+            self.assertEquals(response, None)
+            cache.processResponse(
+                "sws", "/student/v5/registration/xx", ok_response)
+            response = cache.getCache(
+                'sws', '/student/v5/registration/xx', {})
+
+            cache_entry = CacheEntryTimed.objects.get(
+                service="sws", url="/student/v5/registration/xx")
+            orig_time_saved = cache_entry.time_saved
+            cache_entry.time_saved = (orig_time_saved -
+                                      timedelta(minutes=15))
+            cache_entry.save()
+            response = cache.getCache('sws', '/student/v5/registration/xx', {})
+            self.assertEquals(response, None)
+
+    def test_course_default(self):
+        with self.settings(RESTCLIENTS_DAO_CACHE_CLASS=CACHE):
+            cache = MyUWCache()
+            ok_response = MockHTTP()
+            ok_response.status = 200
+            ok_response.data = "xx"
+
+            response = cache.getCache('sws', '/student/v5/course/xx', {})
+            self.assertEquals(response, None)
+            cache.processResponse(
+                "sws", "/student/v5/course/xx", ok_response)
+            response = cache.getCache(
+                'sws', '/student/v5/course/xx', {})
+
+            cache_entry = CacheEntryTimed.objects.get(
+                service="sws", url="/student/v5/course/xx")
+            orig_time_saved = cache_entry.time_saved
+            cache_entry.time_saved = (orig_time_saved -
+                                      timedelta(minutes=60))
+            cache_entry.save()
+            response = cache.getCache('sws', '/student/v5/course/xx', {})
+            self.assertEquals(response, None)
+
+    def test_enrollment_default(self):
+        with self.settings(RESTCLIENTS_DAO_CACHE_CLASS=CACHE):
+            cache = MyUWCache()
+            ok_response = MockHTTP()
+            ok_response.status = 200
+            ok_response.data = "xx"
+
+            response = cache.getCache('sws', '/student/v5/enrollment/xx', {})
+            self.assertEquals(response, None)
+            cache.processResponse(
+                "sws", "/student/v5/enrollment/xx", ok_response)
+            response = cache.getCache(
+                'sws', '/student/v5/enrollment/xx', {})
+
+            cache_entry = CacheEntryTimed.objects.get(
+                service="sws", url="/student/v5/enrollment/xx")
+            orig_time_saved = cache_entry.time_saved
+            cache_entry.time_saved = (orig_time_saved -
+                                      timedelta(minutes=60))
+            cache_entry.save()
+            response = cache.getCache('sws', '/student/v5/enrollment/xx', {})
+            self.assertEquals(response, None)
+
+    def test_notice_default(self):
+        with self.settings(RESTCLIENTS_DAO_CACHE_CLASS=CACHE):
+            cache = MyUWCache()
+            ok_response = MockHTTP()
+            ok_response.status = 200
+            ok_response.data = "xx"
+
+            response = cache.getCache('sws', '/student/v5/notice/xx', {})
+            self.assertEquals(response, None)
+            cache.processResponse(
+                "sws", "/student/v5/notice/xx", ok_response)
+            response = cache.getCache(
+                'sws', '/student/v5/notice/xx', {})
+
+            cache_entry = CacheEntryTimed.objects.get(
+                service="sws", url="/student/v5/notice/xx")
+            orig_time_saved = cache_entry.time_saved
+            cache_entry.time_saved = (orig_time_saved -
+                                      timedelta(minutes=60))
+            cache_entry.save()
+            response = cache.getCache('sws', '/student/v5/notice/xx', {})
             self.assertEquals(response, None)
 
     def test_default_policies(self):
--- myuw/util/cache_implementation.py
+++ myuw/util/cache_implementation.py
@@ -1,28 +1,45 @@
-from restclients.cache_implementation import TimedCache
 import re
+from restclients.cache_implementation import TimedCache
 
+
+FIVE_SECONDS = 5
+FIFTEEN_MINS = 60 * 15
+ONE_HOUR = 60 * 60
 FOUR_HOURS = 60 * 60 * 4
-ONE_MONTH = 60 * 60 * 24 * 30
+ONE_DAY = 60 * 60 * 24
+ONE_WEEK = 60 * 60 * 24 * 7
+
+
+def get_myuw_sws_cache_time(url):
+    if re.match('^/student/v5/term/current', url):
+        return ONE_DAY
+
+    if re.match('^/student/v5/term', url):
+        return ONE_WEEK
+
+    if re.match('^/student/v5/course', url) or\
+            re.match('^/student/v5/enrollment', url) or\
+            re.match('^/student/v5/notice', url):
+        return ONE_HOUR
+
+    if re.match('^/student/v5/registration', url):
+        return FIFTEEN_MINS
+
+    # person, AccountBalance
+    return FOUR_HOURS
 
 
 class MyUWCache(TimedCache):
+
     def getCache(self, service, url, headers):
+        if "myplan" == service:
+            return self._response_from_cache(
+                service, url, headers, FIVE_SECONDS)
+
         if "sws" == service:
-            return self._getSWScache(url, headers)
-        elif "myplan" == service:
-            return self._getMyPlanCache(url, headers)
+            return self._response_from_cache(
+                service, url, headers, get_myuw_sws_cache_time(url))
 
-        return self._default(service, url, headers)
-
-    def _getMyPlanCache(self, url, headers):
-        return None
-
-    def _getSWScache(self, url, headers):
-        if re.match('^/student/v5/term', url):
-            return self._response_from_cache('sws', url, headers, ONE_MONTH)
-        return self._default('sws', url, headers)
-
-    def _default(self, service, url, headers):
         return self._response_from_cache(service, url, headers, FOUR_HOURS)
 
     def processResponse(self, service, url, response):
