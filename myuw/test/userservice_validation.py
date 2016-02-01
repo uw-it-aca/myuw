3c79c8500796fbfccdde5b9f215c15237779ea20
Patrick Michaud <pmichaud@uw.edu>
2016-02-01 14:35:28 -0800

    Adding transformation support + shib validation

--- myuw/test/userservice_validation.py
+++ myuw/test/userservice_validation.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from myuw.userservice_validation import validate
+from myuw.userservice_validation import validate, validate_shib, transform
 
 
 class TestValidation(TestCase):
@@ -16,3 +16,16 @@ class TestValidation(TestCase):
         self.assertEquals(validate("99invalid"), invalid_string)
         self.assertEquals(validate("thisisfartoolongtobeanetid"),
                           invalid_string)
+
+    def test_shib_validation(self):
+        invalid_string = ("Username not a valid netid (starts with a letter, "
+                          "then 0-7 letters or numbers)")
+        self.assertEquals(validate_shib("javerage"), invalid_string)
+        self.assertEquals(validate_shib("javerage@washington.edu"), None)
+
+    def test_transform(self):
+        self.assertEquals(transform("JAVERAGE"), "javerage@washington.edu")
+        self.assertEquals(transform("javerage@washington.edu"),
+                          "javerage@washington.edu")
+        self.assertEquals(transform("javerage@gmail.com"),
+                          "javerage@gmail.com")
--- myuw/userservice_validation.py
+++ myuw/userservice_validation.py
@@ -2,15 +2,31 @@ import re
 
 
 def validate(username):
+    return _validate(username, r'^[a-z][a-z0-9]{0,7}$')
+
+
+def validate_shib(username):
+    return _validate(username, r'^[a-z][a-z0-9]{0,7}@washington.edu$')
+
+
+def _validate(username, regex):
     if len(username) == 0:
         return "No override user supplied"
 
     if username != username.lower():
         return "Usernames must be all lowercase"
 
-    re_personal_netid = re.compile(r'^[a-z][a-z0-9]{0,7}$', re.I)
+    re_personal_netid = re.compile(regex)
     if not re_personal_netid.match(username):
         return ("Username not a valid netid (starts with a letter, "
                 "then 0-7 letters or numbers)")
 
     return None
+
+
+def transform(username):
+    start = username.lower()
+    if re.match(".*@.*", start):
+        return start
+
+    return "%s@washington.edu" % start
