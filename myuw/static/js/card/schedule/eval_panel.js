5184ea54ad387207616db11b30dd842f613a337d
Fang Lin <fanglin@uw.edu>
2016-02-02 10:12:50 -0800

    Correct eval_data_err condition logic in eval_panel.js.

--- myuw/static/js/card/schedule/eval_panel.js
+++ myuw/static/js/card/schedule/eval_panel.js
@@ -5,13 +5,13 @@ var CourseEvalPanel = {
         var eval_data = (fetched_eval? WSData.iasystem_data(): null);
         var index = c_section.index;
         
-        // has valid eval data
+        // Determine if we have valid eval data for the section
         var has_valid_eval = (eval_data && eval_data.sections && eval_data.sections.length > 0 && eval_data.sections[index] && eval_data.sections[index].evaluation_data && eval_data.sections[index].evaluation_data.length > 0 ? true : false);
         c_section.has_eval = has_valid_eval;
         c_section.evals = (has_valid_eval ? eval_data.sections[index].evaluation_data : null);
 
-        // failed to fetch the eval data for the section
-        var eval_data_err = (!has_valid_eval &&  eval_data.sections[index].evaluation_data === null ? true :false);
+        // Determine if there was an err when fetching the section eval data
+        var eval_data_err = (fetched_eval && !has_valid_eval && eval_data && eval_data.sections[index].evaluation_data === null ? true :false);
         c_section.eval_data_err = eval_data_err;
 
         var source = $("#course_eval_panel").html();
