1d2523796189505ebb1665676f38f64c802462c8
Fang Lin <fanglin@uw.edu>
2016-08-11 17:02:57 -0700

    fix MUWM-3710

--- myuw/static/js/card/no_course.js
+++ myuw/static/js/card/no_course.js
@@ -5,7 +5,11 @@ var CardWithNoCourse = {
         if ($(".no_courses_dupe_blocker").length) {
             return "";
         }
-        var term_str = (term === 'current' ? 'Current Quarter' : titilizeTerm(term));
+        var term_str = (term === 'current'
+                        ? (window.card_display_dates.is_summer
+                           ? titilizeTerm(window.card_display_dates.current_summer_term)
+                           : 'Current Quarter')
+                        : titilizeTerm(term));
         var source   = $("#card_with_no_course").html();
         var template = Handlebars.compile(source);
         var raw = template({term: term_str});
