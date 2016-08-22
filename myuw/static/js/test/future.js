69d533a74bf469cbc80db88cb328dee510fb169d
Patrick Michaud <pmichaud@uw.edu>
2016-08-22 14:20:09 -0700

    Dropping the test w/ the future page method

--- myuw/static/js/test/future.js
+++ myuw/static/js/test/future.js
@@ -3,12 +3,10 @@ var Card = require("../card/schedule/visual.js");
 var jsdom = require('jsdom');
 var Handlebars = require("../../vendor/js/handlebars-v2.0.0.js");
 
-var doc = jsdom.jsdom('<html><head></head><body><script type="text/x-handlebars-template" id="future"><div class="container future-quarter-container"></div></script></body></html>'),
+var doc = jsdom.jsdom('<html></html>'),
     window = doc.parentWindow;
 
 var $ = require('jquery')(window);
-
-
 var assert = require("assert");
 
 describe("Always shows the visual schedule", function() {
@@ -16,11 +14,6 @@ describe("Always shows the visual schedule", function() {
     global.window = window;
     global.Handlebars = Handlebars;
     global.VisualScheduleCard = Card.VisualScheduleCard;
-    // Just mocked out - otherwise it calls WSData
-    global.NoticeBanner = { render_init: function() { } };
-    global.CourseCards = {};
-    global.TextbookCard = {};
-    global.Cards = { load_cards_in_order: function() { } };
     window.user = {};
     window.user.student = true;
 
@@ -41,12 +34,4 @@ describe("Always shows the visual schedule", function() {
         Card.VisualScheduleCard.force_visual_schedule_display();
         assert.equal(true, Card.VisualScheduleCard.should_display_card());
     });
-
-    it("should be set after the future quarter page renders", function() {
-        window.card_display_dates.is_before_last_day_of_classes = false;
-        window.force_visual_schedule_display = false;
-        Future.FutureQuarter.make_html();
-        assert.equal(true, Card.VisualScheduleCard.should_display_card());
-    });
-
 });
