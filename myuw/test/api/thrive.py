f0930dd9b4d4f5e02ab3246f602909f77b7c8152
mike seibel <mikes@washington.edu>
2016-09-14 16:48:18 -0700

    updates tests with new thrive content columns

--- myuw/test/api/thrive.py
+++ myuw/test/api/thrive.py
@@ -8,12 +8,14 @@ class TestApiThrive(MyuwApiTest):
     def get_thrive_response(self):
         return self.get_response_by_reverse('myuw_thrive_api')
 
-    def test_javerage_email(self):
+    def test_jnew_thrive(self):
         self.set_user('jnew')
+        self.set_date('2013-09-28')
         response = self.get_thrive_response()
         self.assertEquals(response.status_code, 200)
         data = json.loads(response.content)
-        self.assertEquals(data["title"], "Fail Forward")
+        self.assertEquals(data["week_label"], "Week 1")
+        self.assertEquals(data["title"], "All the Time in the World?")
 
         self.set_date('2015-02-12')
         response = self.get_thrive_response()
