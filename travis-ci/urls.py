b402be15c3e0e13bb8a74066c7342de770483b36
Patrick Michaud <pmichaud@uw.edu>
2016-10-31 08:48:09 -0700

    Travis-ci: move away from url patterns

--- travis-ci/urls.py
+++ travis-ci/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^', include('myuw.urls')),
-)
+]
