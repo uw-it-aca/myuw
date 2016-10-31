d47d39e41f79bf928ae73a7c13587b3c60f173c6
Patrick Michaud <pmichaud@uw.edu>
2016-10-31 08:53:01 -0700

    Add the newer template config

--- travis-ci/settings.py
+++ travis-ci/settings.py
@@ -22,6 +22,23 @@ SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 
 ALLOWED_HOSTS = []
 
