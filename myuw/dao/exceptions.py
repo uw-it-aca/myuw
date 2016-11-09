e67c6977ed22cef15cbeacafa43e7d80772d9d9a
mike seibel <mikes@washington.edu>
2016-11-08 16:05:05 -0800

    removes unused exception from test

--- myuw/dao/exceptions.py
+++ myuw/dao/exceptions.py
@@ -6,8 +6,3 @@ Contains the custom exceptions used by the restclients.
 class EmailServiceUrlException(Exception):
     """Unhandled email domain or malformed email address"""
     pass
-
-
-class UnsupportedAffiliationException(Exception):
-    """User not associated with supported affiliation"""
-    pass
