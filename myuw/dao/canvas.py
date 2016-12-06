31138dab317db61ebb05a160c802d287a8575365
mike seibel <mikes@washington.edu>
2016-12-06 09:48:12 -0800

    leaner active canvas course fetch

--- myuw/dao/canvas.py
+++ myuw/dao/canvas.py
@@ -1,59 +1,78 @@
 import logging
-from restclients.canvas.enrollments import Enrollments as CanvasEnrollments
-from restclients.canvas.sections import Sections
 from restclients.canvas.courses import Courses
 from myuw.dao.pws import get_regid_of_current_user
 from myuw.logger.timer import Timer
-from myuw.logger.logback import log_resp_time, log_exception
+from myuw.logger.logback import log_resp_time
+import re
 
 
 logger = logging.getLogger(__name__)
 
 
 def get_canvas_enrolled_courses():
-    """
-    Returns calendar information for the current term.
-    Raises: DataFailureException
-    """
     timer = Timer()
     try:
-        regid = get_regid_of_current_user()
-        return get_indexed_data_for_regid(regid)
-    except AttributeError:
-        # If course is not in canvas, skip
-        return []
+        return Courses().get_courses_for_regid(
+            get_regid_of_current_user(),
+            {'enrollment_type': 'student',
+             'enrollment_state': 'active',
+             'include': ['sections', 'term']},
+            verbatim=True)
     finally:
         log_resp_time(logger,
                       'get_canvas_enrolled_courses',
                       timer)
 
 
-def get_indexed_data_for_regid(regid):
-    return _indexed_by_course_id(
-        CanvasEnrollments().get_enrollments_for_regid(
-            regid,
-            {'state': "active",
-             'as_user': CanvasEnrollments().sis_user_id(regid)})
-        )
+class CanvasCourses(object):
+    def __init__(self):
+        """
+        Returns active course information for the current user.
+        Raises: DataFailureException
+        """
+        self.courses_dict = {}
+        self.courses = get_canvas_enrolled_courses()
+        for course in self.courses:
+            course.sections_dict = {self._sws_id_from_course_code(
+                course, s.name): s for s in course.sections}
+            self.courses_dict[self._sws_id_from_course_code(
+                course, course.code)] = course
 
+    def get_url_for_section(self, sws_section_id):
+        course = self._course_from_sws_section_id(sws_section_id)
+        return course.course_url if course else None
 
-def get_indexed_by_decrosslisted(by_primary, sws_sections):
-    for section in sws_sections:
-        base_id = section.section_label()
-        alternate_id = None
+    def get_name_for_section(self, sws_section_id):
+        course = self._course_from_sws_section_id(sws_section_id)
+        return course.name if course else ''
+
+    def _course_from_sws_section_id(self, sws_section_id):
         try:
-            sis_id = section.canvas_section_sis_id()
-            canvas_section = Sections().get_section_by_sis_id(sis_id)
-            primary_course = Courses().get_course(canvas_section.course_id)
-            alternate_id = primary_course.sws_course_id()
-        except Exception as ex:
-            # primary section doesn't have canvas_section_sis_id
-            alternate_id = base_id
+            return self.courses_dict[sws_section_id]
+        except KeyError:
+            for course_id, course in self.courses_dict.iteritems():
+                try:
+                    if course.sections_dict[sws_section_id]:
+                        return course
+                except KeyError:
+                    pass
 
-        if base_id not in by_primary:
-            if alternate_id in by_primary:
-                by_primary[base_id] = by_primary[alternate_id]
-    return by_primary
+        return None
+
+    def _sws_id_from_course_code(self, course, course_code):
+        term = course.term.name.split(' ')
+        code = re.match(r'^(.*) ([0-9]+) ([0-9A-Z]+)( \([a-z]+ [0-9]+\))?$',
+                        course_code)
+        if code:
+            return "%s,%s,%s,%s/%s" % (
+                term[1], term[0].lower(), code.group(1), code.group(2),
+                code.group(3)) if code else ''
+
+        return course.name
+
+
+def canvas_courses_prefetch():
+    return [get_canvas_enrolled_courses]
 
 
 def _indexed_by_course_id(enrollments):
