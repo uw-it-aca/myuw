b12457c6ad92a2ba64f834811084aae3236bb547
Patrick Michaud <pmichaud@uw.edu>
2016-12-08 15:25:01 -0800

    Next restclients version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'unittest2',
         'AuthZ-Group',
         'python-binary-memcached',
-        'UW-RestClients==1.4a1',
+        'UW-RestClients==1.4a2',
         'Django-SupportTools',
         'django_mobileesp',
         'django_client_logger',
