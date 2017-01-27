25eed756d55236c2ba46643df378709e0bd143b6
mike seibel <mikes@washington.edu>
2017-01-27 15:18:04 -0800

    adds final exam confirmation link

--- myuw/static/js/card/instructor_schedule/course_sche_panel.js
+++ myuw/static/js/card/instructor_schedule/course_sche_panel.js
@@ -3,6 +3,7 @@ var InstructorCourseSchePanel = {
     render: function (c_section) {
         var source = $("#instructor_course_sche_panel").html();
         var template = Handlebars.compile(source);
+        c_section.netid = window.user.netid
         var raw = template(c_section);
         $('#instructor_sche_on_course_card' + c_section.index).html(raw);
     }
