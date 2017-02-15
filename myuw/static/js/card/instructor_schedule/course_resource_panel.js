c3e5cfd2575362b567396f072426bfbf152e7337
Patrick Michaud <pmichaud@uw.edu>
2017-02-15 15:34:37 -0800

    Making the class list a popup
    Also logging the interaction

--- myuw/static/js/card/instructor_schedule/course_resource_panel.js
+++ myuw/static/js/card/instructor_schedule/course_resource_panel.js
@@ -37,5 +37,20 @@ var InstructorCourseResourcePanel = {
             course_id = course_id.replace(/[^a-z0-9]/gi, '_');
             WSData.log_interaction("open_course_canvas_website_"+course_id, term);
         });
+
+        $(".course_class_list", panel).on("click", function(ev) {
+            var width = 800;
+            var height = 400;
+
+            var left = window.screenX + 200;
+            var top = window.screenY + 200;
+
+            window.open(ev.target.href, '_blank', 'width='+width+',height='+height+',left='+left+',top='+top);
+
+            var course_id = ev.currentTarget.getAttribute("rel");
+            course_id = course_id.replace(/[^a-z0-9]/gi, '_');
+            WSData.log_interaction("open_course_classlist_"+course_id, term);
+            return false;
+        });
     }
 };
