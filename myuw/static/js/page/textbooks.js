d51ba3309c53e2a14a30580681228816050fffff
mike seibel <mikes@washington.edu>
2017-03-01 10:17:32 -0800

    conditionally adds nav links to textbook page

--- myuw/static/js/page/textbooks.js
+++ myuw/static/js/page/textbooks.js
@@ -1,4 +1,8 @@
 var RenderPage = function () {
+    if (myuwFeatureEnabled('instructor_schedule')) {
+        $("#app_navigation").show();
+    }
+
     TextBooks.show_books(window.textbook_data.term,
                          window.textbook_data.textbook ? window.textbook_data.textbook.length : undefined);
 };
