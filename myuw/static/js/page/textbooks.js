a59f8fe69fc5c06dc78fb11312157b2f671b58fd
mike seibel <mikes@washington.edu>
2017-03-01 10:13:20 -0800

    Revert "conditionally adds nav links to textbook page"
    This reverts commit cda678f41d9f4376ac46e4bb1d95a204f95a18ec.

--- myuw/static/js/page/textbooks.js
+++ myuw/static/js/page/textbooks.js
@@ -1,8 +1,4 @@
 var RenderPage = function () {
-    if (myuwFeatureEnabled('instructor_schedule')) {
-        $("#app_navigation").show();
-    }
-
     TextBooks.show_books(window.textbook_data.term,
                          window.textbook_data.textbook ? window.textbook_data.textbook.length : undefined);
 };
