a437e141e949cb9695c936c417cab89d2356bad7
Fang Lin <fanglin@uw.edu>
2017-03-01 20:27:16 -0800

    latest restclient

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'unittest2',
         'AuthZ-Group',
         'python-binary-memcached',
-        'UW-RestClients==1.6.1',
+        'UW-RestClients==1.6.a7',
         'Django-SupportTools',
         'django_mobileesp',
         'django_client_logger',
