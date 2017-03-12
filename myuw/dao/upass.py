fb3eef240726b787ad3f3ff95d04548a21c6f7ba
Fang Lin <fanglin@uw.edu>
2017-03-11 16:15:16 -0800

    set is_student and is_employee when upass is not current.

--- myuw/dao/upass.py
+++ myuw/dao/upass.py
@@ -1,31 +1,24 @@
 """
-This class encapsulates the interactions with
-the UPass web service.
+This class encapsulates the interactions with the UPass web service.
 """
 
 import logging
+import re
 from restclients.upass import get_upass_status
-from myuw.logger.timer import Timer
-from myuw.logger.logback import log_resp_time
 from myuw.dao import get_netid_of_current_user
+from myuw.dao.gws import is_student, is_employee, is_student_employee
 
 
 logger = logging.getLogger(__name__)
-
-
-def get_upass_by_current_user():
-    netid = get_netid_of_current_user()
-    return get_upass_by_netid(netid)
 
 
 def get_upass_by_netid(netid):
     """
     returns upass status for a netid
     """
-    timer = Timer()
-    try:
-        return get_upass_status(netid)
-    finally:
-        log_resp_time(logger,
-                      'get_upass_status',
-                      timer)
+    status = get_upass_status(netid)
+    status.is_employee = (status.is_employee or
+                          (is_employee() and not is_student_employee()))
+    status.is_student = (status.is_student or is_student())
+
+    return status
--- myuw/test/dao/upass.py
+++ myuw/test/dao/upass.py
@@ -1,15 +1,26 @@
 from django.test import TestCase
 from myuw.dao.upass import get_upass_by_netid
-from myuw.test import get_request
+from myuw.test import fdao_upass_override, get_request_with_user
 
 
+@fdao_upass_override
 class TestUPassDao(TestCase):
-    def setUp(self):
-        get_request()
 
     def test_get_by_netid(self):
-        status = get_upass_by_netid("javerage")
-
-        self.assertFalse(status.is_staff)
+        get_request_with_user("jnew")
+        status = get_upass_by_netid("jnew")
+        self.assertFalse(status.is_current)
+        self.assertFalse(status.is_employee)
         self.assertTrue(status.is_student)
-        self.assertTrue(status.is_active)
+
+        get_request_with_user("botgrad")
+        status = get_upass_by_netid("botgrad")
+        self.assertTrue(status.is_current)
+        self.assertFalse(status.is_employee)
+        self.assertTrue(status.is_student)
+
+        get_request_with_user("staff")
+        status = get_upass_by_netid("staff")
+        self.assertFalse(status.is_current)
+        self.assertTrue(status.is_employee)
+        self.assertFalse(status.is_student)
