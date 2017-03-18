24a9f85e5897ab0c5867e8f4a371736b4c98e60f
Fang Lin <fanglin@uw.edu>
2017-03-17 17:16:18 -0700

    update upass url path

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'unittest2',
         'AuthZ-Group',
         'python-binary-memcached',
-        'UW-RestClients==1.7.a1',
+        'UW-RestClients==1.7.a2',
         'Django-SupportTools',
         'django_mobileesp',
         'django_client_logger',
