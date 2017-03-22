a87e91363eb4a9f6dee3c047170776616f7a8c68
Stephen De Vight <devights@gmail.com>
2017-03-22 16:51:04 -0700

    making key optional

--- myuw/dao/mailman.py
+++ myuw/dao/mailman.py
@@ -162,7 +162,7 @@ def get_section_email_lists(section,
                 if total_secondaries > 1:
                     json_data["secondary_combined_list"] =\
                         get_section_secondary_combined_list(section)
-    json_data["has_lists"] = (json_data["has_secondary_lists"] or
+    json_data["has_lists"] = (json_data.get("has_secondary_lists") or
                               json_data["section_list"]["list_exists"])
     return json_data
 
