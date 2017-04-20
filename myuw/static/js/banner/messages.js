a7b7bd3d481ae2ecd3a29c35f1909ac42098cbfd
Stephen De Vight <devights@gmail.com>
2017-04-19 17:34:39 -0700

    adding message banner and associated js

---
+++ myuw/static/js/banner/messages.js
@@ -0,0 +1,23 @@
+var MessageBanner = {
+    dom_target: undefined,
+
+    render_init: function(dom_taget) {
+        MessageBanner.dom_target  = dom_taget;
+        WSData.fetch_message_data(MessageBanner.render);
+    },
+
+    render: function () {
+        var message_data = WSData.message_data();
+
+        if (message_data.length > 0) {
+            var source = $("#message_banner").html();
+            var template = Handlebars.compile(source);
+
+            var html = template({
+                "messages": message_data
+            });
+            MessageBanner.dom_target.html(html);
+        }
+    }
+};
+
