06b1e88f5393ecbb3c9aafc4014ac26bb44a05e2
Stephen De Vight <devights@gmail.com>
2017-04-28 17:13:46 -0700

    adding profile and medicine cards

--- myuw/static/js/page/accounts.js
+++ myuw/static/js/page/accounts.js
@@ -1,5 +1,5 @@
 var RenderPage = function () {
     CommonLoading.render_init();
     $("#app_navigation").show();
-    Cards.load_cards_in_order([AccountsCard], $("#account_card_content"));
+    Cards.load_cards_in_order([ProfileCard, AccountsCard, MedicineAccountsCard], $("#account_card_content"));
 };
