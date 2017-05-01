9b4b129b26b5d2a5d0a00525eda4c2498ce8e158
Fang Lin <fanglin@uw.edu>
2017-04-30 20:53:49 -0700

    update uw_sws version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'AuthZ-Group',
         'python-binary-memcached',
         'UW-RestClients>=2.2,<3.0',
-        'UW-RestClients-SWS>=0.5.8,<1.0',
+        'UW-RestClients-SWS>=0.5.9,<1.0',
         'UW-RestClients-PWS>=0.5,<1.0',
         'UW-RestClients-HFS>=0.5,<1.0',
         'UW-RestClients-GWS>=0.2,<1.0',
