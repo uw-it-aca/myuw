26b1078e3c8c47e30de55a213efff95cd126e714
Stephen De Vight <devights@gmail.com>
2017-05-03 16:52:36 -0700

    adding data to account card

--- myuw/static/js/card/accounts/account_card.js
+++ myuw/static/js/card/accounts/account_card.js
@@ -3,13 +3,13 @@ var AccountsCard = {
     dom_target: undefined,
 
     render_init: function() {
-        AccountsCard._render();
+        WSData.fetch_profile_data(AccountsCard._render);
     },
 
     _render: function() {
         var source   = $("#accounts_card").html();
         var template = Handlebars.compile(source);
-        var compiled = template({"card_name": AccountsCard.name});
+        var compiled = template(WSData.profile_data().password);
         AccountsCard.dom_target.html(compiled);
     }
 };
