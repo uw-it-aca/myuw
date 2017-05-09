63c30c11469ec97d6a81c34ed277aa98b091835e
Stephen De Vight <devights@gmail.com>
2017-05-09 16:37:12 -0700

    switching card order when user has medicine acct

--- myuw/static/js/page/accounts.js
+++ myuw/static/js/page/accounts.js
@@ -1,5 +1,25 @@
 var RenderPage = function () {
     CommonLoading.render_init();
     $("#app_navigation").show();
-    Cards.load_cards_in_order([MedicineAccountsCard, AccountsCard], $("#account_card_content"));
+    WSData.fetch_profile_data(RenderAfterAjax, RenderAfterAjaxError);
+
 };
+
+var RenderAfterAjax = function () {
+    var profile_data = WSData.profile_data();
+    _render_cards(profile_data.password.has_active_med_pw);
+
+};
+
+var RenderAfterAjaxError = function () {
+    _render_cards(false)
+};
+
+var _render_cards = function(has_medicine) {
+    // will ultimately have more than acct and med acct cards here
+    var card_order = [AccountsCard];
+    if(has_medicine){
+        card_order = [MedicineAccountsCard, AccountsCard];
+    }
+    Cards.load_cards_in_order(card_order, $("#account_card_content"));
+}
