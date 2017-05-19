8ae4bbe4352eed1ed234e8cfd7906d9bd52216c3
Charlon Palacay <charlon@gmail.com>
2017-05-19 11:20:29 -0700

    Adds accounts mini to mobile landing.

--- myuw/static/js/landing.js
+++ myuw/static/js/landing.js
@@ -83,6 +83,7 @@ var Landing = {
     _load_mobile_cards: function() {
         Landing._reset_content_divs();
         var mobile_cards = [
+            LibraryCardMini,
             ThriveCard,
             FinalExamCard,
             GradeCard,
