f9ed6f56556938b49b247ba81c2f9c1ae57d4bc8
Charlon Palacay <charlon@gmail.com>
2017-05-22 14:45:27 -0700

    Adjust content padding for fixed positioned banners.

--- myuw/static/js/banner/messages.js
+++ myuw/static/js/banner/messages.js
@@ -17,9 +17,31 @@ var MessageBanner = {
                 "messages": message_data
             });
             MessageBanner.dom_target.html(html);
+
+            MessageBanner.adjust_banner_padding();
+
+            $(window).resize(function(){
+                MessageBanner.adjust_banner_padding();
+            });
+
         } else {
             MessageBanner.dom_target.hide();
         }
+    },
+
+    adjust_banner_padding: function () {
+
+        var viewport_width = $(window).width();
+
+        // set top padding based on height of message banner container
+        // should be for tablet and desktop
+        if (viewport_width > 1024) {
+            var sHeight = $(".myuw-banner").prop('scrollHeight');
+            $(".myuw-body .myuw-wrapper").css("padding-top", sHeight);
+        }
+        else {
+            $(".myuw-body .myuw-wrapper").css("padding-top", "0px");
+        }
+
     }
 };
-
