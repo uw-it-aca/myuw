9ec34775783db697a54f0f3b63b8868072a255a2
mike seibel <mikes@washington.edu>
2017-05-22 16:23:55 -0700

    repurposes old profile card into common profile card

--- myuw/static/js/card/profile/profile.js
+++ myuw/static/js/card/profile/profile.js
@@ -1,17 +1,36 @@
-var ProfileCard = {
-    name: 'ProfileCard',
+var CommonProfileCard = {
+    name: 'CommonProfileCard',
     dom_target: undefined,
 
     render_init: function() {
-        WSData.fetch_profile_data(ProfileCard._render);
+        WSData.fetch_directory_data(CommonProfileCard.render_upon_data);
+    },
+
+    render_upon_data: function () {
+        if (!CommonProfileCard._has_all_data()) {
+            return;
+        }
+
+        CommonProfileCard._render();
+    },
+
+    _has_all_data: function () {
+        if (WSData.directory_data()) {
+            return true;
+        }
+        return false;
     },
 
     _render: function() {
-        var profile_data = WSData.profile_data();
-        var source   = $("#profile_card").html();
+        var directory_data = WSData.directory_data();
+        var source   = $("#common_profile_card").html();
         var template = Handlebars.compile(source);
-        profile_data.card_name = ProfileCard.name;
-        var compiled = template(profile_data);
-        ProfileCard.dom_target.html(compiled);
+        directory_data.card_name = CommonProfileCard.name;
+        var compiled = template({
+            display_name: directory_data.display_name,
+            full_name: directory_data.full_name,
+            has_preferred: (directory_data.display_name !== directory_data.full_name)
+        });
+        CommonProfileCard.dom_target.html(compiled);
     }
 };
