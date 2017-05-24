f19bd5a249f0e05f597a60489e12484208f83e23
Patrick Michaud <pmichaud@uw.edu>
2017-05-24 11:27:28 -0700

    Dropping some old page title code

--- myuw/static/js/page/landing.js
+++ myuw/static/js/page/landing.js
@@ -1,10 +1,4 @@
 var RenderPage = function () {
     Landing.render(null, null);
     $("#landing").addClass("active");
-    document.title = window.page_titles.landing;
-
-    $("#landing").removeClass("active");
-    $("#nav_course_list").removeClass("active");
-    $("#nav_visual_schedule").removeClass("active");
-    $("#nav_finabala").removeClass("active");
 };
