171765e2d7ba026f2a280cb2399333659ca92062
Fang Lin <fanglin@uw.edu>
2017-06-05 17:43:25 -0700

    bug fix: adding quicklinks in context at right time

--- myuw/views/index.py
+++ myuw/views/index.py
@@ -1,14 +1,7 @@
 from myuw.util.page_view import page_view
 from myuw.views.page import page
-from myuw.views.teaching import _add_quicklink_context
 
 
 @page_view
-def index(request, year=None, quarter=None, summer_term=None):
-    context = {
-        "year": year,
-        "quarter": quarter,
-        "summer_term": summer_term
-    }
-    _add_quicklink_context(request, context)
-    return page(request, context, template='index.html')
+def index(request):
+    return page(request, add_quicklink_context=True)
