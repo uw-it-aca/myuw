fb1296b042520d03bad1cf8f851ddd15ae308a50
Fang Lin <fanglin@uw.edu>
2017-08-25 14:15:16 -0700

    log myuw.context_processors.has_google_analytics

--- myuw/context_processors.py
+++ myuw/context_processors.py
@@ -1,4 +1,8 @@
-from django.conf import settings
+import logging
+from commonconf import settings
+
+
+logger = logging.getLogger(__name__)
 
 
 def has_less_compiled(request):
@@ -18,8 +22,8 @@ def less_not_compiled(request):
 
 
 def has_google_analytics(request):
-
     ga_key = getattr(settings, 'GOOGLE_ANALYTICS_KEY', False)
+    logger.info("has_google_analytics: %s", (ga_key is not False))
     return {
         'GOOGLE_ANALYTICS_KEY': ga_key,
         'has_google_analytics': ga_key
