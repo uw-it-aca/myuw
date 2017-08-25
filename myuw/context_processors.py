0f68dd265a1104d9da80ddaaa8e339c7aecf0c4b
Fang Lin <fanglin@uw.edu>
2017-08-25 14:38:43 -0700

    clean up context_processors

--- myuw/context_processors.py
+++ myuw/context_processors.py
@@ -1,25 +1,13 @@
-from django.conf import settings
+import logging
+from commonconf import settings
 
 
-def has_less_compiled(request):
-    """ See if django-compressor is being used to precompile less
-    """
-    key = getattr(settings, "COMPRESS_PRECOMPILERS", ())
-
-    has_less_precompiler = False
-    for entry in key:
-        if entry[0] == "text/less":
-            has_less_precompiler = True
-    return {'has_less_compiled': has_less_precompiler}
-
-
-def less_not_compiled(request):
-    return {}
+logger = logging.getLogger(__name__)
 
 
 def has_google_analytics(request):
-
     ga_key = getattr(settings, 'GOOGLE_ANALYTICS_KEY', False)
+    logger.info("has_google_analytics: %s", (ga_key is not False))
     return {
         'GOOGLE_ANALYTICS_KEY': ga_key,
         'has_google_analytics': ga_key
