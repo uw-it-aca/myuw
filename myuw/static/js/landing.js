580b86f12cfe4d81cd05e4f37abcccc6b83e66a1
Fang Lin <fanglin@uw.edu>
2017-09-04 11:57:25 -0700

    Resolve MUWM-3866

--- myuw/static/js/landing.js
+++ myuw/static/js/landing.js
@@ -43,6 +43,7 @@ var Landing = {
         Landing._reset_content_divs();
         var desktop_body_cards = [
             ThriveCard,
+            OutageCard,
             GradeCard,
             FutureQuarterCardA,
             ThankYouCard,
@@ -67,10 +68,6 @@ var Landing = {
             AcadCalSnippet,
             EventsCard
         ];
-        // Add in outage card if we can't get the SWS/term resource
-        if(window.webservice_outage){
-            desktop_body_cards.unshift(OutageCard);
-        }
         Cards.load_cards_in_order(desktop_body_cards, $("#landing_content_cards"));
         Cards.load_cards_in_order(desktop_sidebar_cards, $("#landing_accounts_cards"));
     },
@@ -80,6 +77,7 @@ var Landing = {
         var mobile_cards = [
             QuickLinksCard,
             ThriveCard,
+            OutageCard,
             GradeCard,
             FutureQuarterCardA,
             ThankYouCard,
@@ -100,10 +98,6 @@ var Landing = {
         ];
         if(window.user.staff_employee && !(window.user.student || window.user.instructor)) {
             mobile_cards.unshift(HRPayrollCard);
-        }
-        // Add in outage card if we can't get the SWS/term resource
-        if(window.webservice_outage){
-            mobile_cards.unshift(OutageCard);
         }
         Cards.load_cards_in_order(mobile_cards, $("#landing_content_cards"));
     },
--- myuw/static/js/myuw_m.js
+++ myuw/static/js/myuw_m.js
@@ -195,6 +195,7 @@ var renderedCardOnce = function(card_name) {
 
 var resetCardRenderCalled = function() {
     multi_res_card_render_called = {};
+    OutageCard.reset();
 };
 
 var toggle_card_disclosure = function(card, div_toggled, a_expose, a_hide, label) {
