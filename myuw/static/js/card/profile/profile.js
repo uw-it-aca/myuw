b99f47cf876fee1eb25e79b138b6fd1854e5b08a
Fang Lin <fanglin@uw.edu>
2017-09-05 14:21:16 -0700

    similar error on the CommonProfileCard.

--- myuw/static/js/card/profile/profile.js
+++ myuw/static/js/card/profile/profile.js
@@ -2,20 +2,37 @@ var CommonProfileCard = {
     name: 'CommonProfileCard',
     dom_target: undefined,
 
+    hide_card: function() {
+        if (window.user.employee) {
+            return false;
+        }
+        return true;
+    },
+
+
     render_init: function() {
+        if (CommonProfileCard.hide_card()) {
+            $("#CommonProfileCard").hide();
+            return;
+        }
         WSData.fetch_directory_data(CommonProfileCard.render_upon_data);
     },
 
     render_upon_data: function () {
-        if (!CommonProfileCard._has_all_data()) {
+        if (WSData.directory_data()) {
+            CommonProfileCard._render();
             return;
         }
-
-        CommonProfileCard._render();
+        var dir_info_err = WSData._directory_error_status;
+        if (dir_info_err === 404) {
+            $("#CommonProfileCard").hide();
+            return;
+        }
+        var raw = CardWithError.render("Profile Card");
+        CommonProfileCard.dom_target.html(raw);
     },
 
     _has_all_data: function () {
-        if (WSData.directory_data()) {
             return true;
         }
         return false;
