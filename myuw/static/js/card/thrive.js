e4076e554cd6315ce04b292eed518c51a109ce53
Fang Lin <fanglin@uw.edu>
2017-09-11 12:39:41 -0700

    Log link clicks on Thrive card

--- myuw/static/js/card/thrive.js
+++ myuw/static/js/card/thrive.js
@@ -1,6 +1,7 @@
 var ThriveCard = {
     name: 'ThriveCard',
     dom_target: undefined,
+    target_group: undefined,
 
     hide_card: function() {
         if (window.user.fyp ||
@@ -34,17 +35,31 @@ var ThriveCard = {
         var thrive = WSData.thrive_data();
         if (window.user.fyp) {
             thrive.target_fyp = true;
+            ThriveCard.target_group = '_fyp';
         }
         if (window.user.aut_transfer) {
             thrive.target_aut_transfer = true;
+            ThriveCard.target_group = '_au_xfer';
         }
         if (window.user.win_transfer) {
             thrive.target_win_transfer = true;
+            ThriveCard.target_group = '_wi_xfer';
         }
         var source = $("#thrive_card").html();
         var template = Handlebars.compile(source);
         ThriveCard.dom_target.html(template(thrive));
-        LogUtils.cardLoaded(ThriveCard.name, ThriveCard.dom_target);
+        var name = ThriveCard.name + ThriveCard.target_group;
+        LogUtils.cardLoaded(name, ThriveCard.dom_target);
+        ThriveCard.add_events();
+    },
+
+    add_events: function(term) {
+        $(".what-is-thrive").on("click", function(ev) {
+            WSData.log_interaction("click_what_is_thrive" + ThriveCard.target_group);
+        });
+        $(".view-thrive-msg").on("click", function(ev) {
+            WSData.log_interaction("click_view_thrive_msg" + ThriveCard.target_group);
+        });
     },
 
     render_error: function () {
