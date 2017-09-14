61ab3fe8f8be61036b2dbb4b4e6d093c5d2b66e9
Stephen De Vight <devights@gmail.com>
2017-09-13 18:26:48 -0700

    more progress on support functions

--- myuw/dao/visual_schedule.py
+++ myuw/dao/visual_schedule.py
@@ -1,4 +1,5 @@
 from dateutil.relativedelta import *
+from datetime import timedelta
 
 def get_visual_schedule(schedule):
 
@@ -25,9 +26,64 @@ def get_periods_from_bounds(bounds):
         # periods[start_week] = period
         # start_week += 1
 
+def _add_sections_to_weeks(sections, weeks):
+    for week in weeks:
+        for section in sections:
+            if section.start_date <= week.end_date and section.end_date >= week.start_date:
+                week.sections.append(section)
+    return weeks
+
+def _consolidate_weeks(weeks):
+    consolidated_weeks = []
+
+    i = 0
+    while i < len(weeks):
+        if _section_lists_are_same(weeks[i].sections, weeks[i+1].sections):
+            weeks[i].end_date = weeks[i+1].end_date
+            del weeks[i+1]
+        i += 1
+
+    return consolidated_weeks
 
 
 
+def _section_lists_are_same(list1, list2):
+    if len(list1) is not len(list2):
+        return False
+
+    for l1_section in list1:
+        found_match = False
+        for l2_section in list2:
+            if _sections_are_same(l1_section, l2_section):
+                found_match = True
+        if not found_match:
+            return False
+    return True
+
+
+def _sections_are_same(section1, section2):
+    return (section1.curriculum_abbr == section2.curriculum_abbr) \
+           and (section1.course_number == section2.course_number) \
+           and (section1.section_id == section2.section_id)
+
+
+
+def _get_weeks_from_bounds(bounds):
+    start, end = bounds
+    periods = []
+
+    # weeks between start>end dates, including first day
+    schedule_length = ((end-start).days + 1)/7
+
+    while schedule_length > 0:
+        period = SchedulePeriod()
+        period.start_date = start
+        period.end_date = (start + timedelta(days=6))
+        periods.append(period)
+        start = (start + timedelta(days=7))
+        schedule_length -= 1
+
+    return periods
 
 def get_schedule_bounds(schedule):
     start = None
@@ -70,9 +126,12 @@ def _add_dates_to_sections(schedule):
 
 
 class SchedulePeriod():
-    start_date = None
-    end_date = None
-    sections = []
+    def __init__(self):
+        self.start_date = None
+        self.end_date = None
+        self.sections = []
+        self.is_boundary_period = False
+
 
 
 
--- myuw/test/dao/visual_schedule.py
+++ myuw/test/dao/visual_schedule.py
@@ -1,13 +1,15 @@
 from django.test import TestCase
 from restclients_core.exceptions import DataFailureException
-from uw_sws.models import Term
+from uw_sws.models import Term, Section
 from myuw.dao.term import get_term_from_quarter_string
 from myuw.dao.registration import _get_schedule
-from myuw.dao.visual_schedule import get_visual_schedule, get_schedule_bounds, _add_dates_to_sections
+from myuw.dao.visual_schedule import get_visual_schedule,\
+    get_schedule_bounds, _add_dates_to_sections, _get_weeks_from_bounds,\
+    _add_sections_to_weeks, _section_lists_are_same, _sections_are_same
 from myuw.test import fdao_sws_override, fdao_pws_override,\
     get_request
 import datetime
-
+import copy
 
 
 @fdao_pws_override
@@ -28,12 +30,88 @@ class TestVisualSchedule(TestCase):
 
     def test_get_bounds(self):
         regid = "9136CCB8F66711D5BE060004AC494FFE"
-        term = get_term_from_quarter_string("2013,summer")
+        term = get_term_from_quarter_string("2013,spring")
 
         schedule = _get_schedule(regid, term)
 
         schedule = _add_dates_to_sections(schedule)
 
-        periods = get_schedule_bounds(schedule)
-        self.assertEqual(periods[0], datetime.date(2013, 6, 23))
-        self.assertEqual(periods[1], datetime.date(2013, 8, 24))
+        bounds = get_schedule_bounds(schedule)
+        self.assertEqual(bounds[0], datetime.date(2013, 3, 31))
+        self.assertEqual(bounds[1], datetime.date(2013, 6, 8))
+
+
+    def test_get_weeks(self):
+        bounds = [datetime.date(2013, 3, 17), datetime.date(2013, 3, 31)]
+
+        weeks = _get_weeks_from_bounds(bounds)
+
+        self.assertEqual(len(weeks), 2)
+        self.assertEqual(weeks[0].end_date, datetime.date(2013, 3, 23))
+        self.assertEqual(weeks[1].start_date, datetime.date(2013, 3, 24))
+
+
+    def test_add_schedule_to_weeks(self):
+        regid = "9136CCB8F66711D5BE060004AC494FFE"
+        term = get_term_from_quarter_string("2013,spring")
+
+        schedule = _get_schedule(regid, term)
+        schedule = _add_dates_to_sections(schedule)
+        bounds = get_schedule_bounds(schedule)
+        weeks = _get_weeks_from_bounds(bounds)
+        weeks = _add_sections_to_weeks(schedule.sections, weeks)
+
+        for week in weeks:
+            self.assertEqual(len(week.sections), 5)
+
+
+    def test_sections_are_same(self):
+        section1 = Section()
+        section1.curriculum_abbr = 'ASD'
+        section1.course_number = 123
+        section1.section_id = 'A'
+
+        section2 = Section()
+        section2.curriculum_abbr = 'ASD'
+        section2.course_number = 123
+        section2.section_id = 'A'
+
+        self.assertTrue(_sections_are_same(section1, section2))
+
+        section2.section_id = 'B'
+        self.assertFalse(_sections_are_same(section1, section2))
+
+
+    def test_section_lists_are_same(self):
+        regid = "9136CCB8F66711D5BE060004AC494FFE"
+        term = get_term_from_quarter_string("2013,spring")
+
+        schedule = _get_schedule(regid, term)
+        schedule = _add_dates_to_sections(schedule)
+
+        list1 = copy.deepcopy(schedule.sections)
+        list2 = copy.deepcopy(schedule.sections)
+        list3 = copy.deepcopy(schedule.sections)
+
+        self.assertTrue(_section_lists_are_same(list1,
+                                                list2))
+
+        del list1[0]
+        self.assertFalse(_section_lists_are_same(list1,
+                                                 list2))
+        list3[0].course_number = 999
+        self.assertFalse(_section_lists_are_same(list2,
+                                                 list3))
+
+
+    def test_consolidate_weeks(self):
+        regid = "9136CCB8F66711D5BE060004AC494FFE"
+        term = get_term_from_quarter_string("2013,spring")
+
+        schedule = _get_schedule(regid, term)
+        schedule = _add_dates_to_sections(schedule)
+        bounds = get_schedule_bounds(schedule)
+        weeks = _get_weeks_from_bounds(bounds)
+        weeks = _add_sections_to_weeks(schedule.sections, weeks)
+
+        # consolidated =
