15214e8f9a4014e3c9313ad4107a3e10765ed7e3
Stephen De Vight <devights@gmail.com>
2017-09-26 12:59:54 -0700

    fixing test

--- myuw/test/dao/category_links.py
+++ myuw/test/dao/category_links.py
@@ -10,7 +10,7 @@ class TestCategoryLinks(TestCase):
 
     def test_get_all_links(self):
         all_links = Res_Links.get_all_links()
-        self.assertEquals(len(all_links), 156)
+        self.assertEquals(len(all_links), 201)
         val = URLValidator()
         for link in all_links:
             try:
