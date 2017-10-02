bbb9d406648761fe467038e98e7cf706cdbb90bd
Fang Lin <fanglin@uw.edu>
2017-10-02 13:14:22 -0700

    pick the rest-sws change

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'AuthZ-Group',
         'python-binary-memcached',
         'UW-RestClients-Core<1.0,>=0.9.3',
-        'UW-RestClients-SWS<2.0,>=1.4.3',
+        'UW-RestClients-SWS<2.0,>=1.4.4',
         'UW-RestClients-PWS<1.0,>=0.6',
         'UW-RestClients-HFS<1.0',
         'UW-RestClients-GWS<1.0',
