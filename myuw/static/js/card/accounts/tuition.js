4d556da66c3fcc2082a22ca6e2d23b1defb850e6
Fang Lin <fanglin@uw.edu>
2017-10-20 12:52:15 -0700

    Tuition card display order change. Add Related resources

--- myuw/static/js/card/accounts/tuition.js
+++ myuw/static/js/card/accounts/tuition.js
@@ -67,6 +67,10 @@ var TuitionCard = {
             template_data.pce_accbalance = 0;
         }
 
+        template_data.is_tacoma = window.user.tacoma_affil;
+        template_data.is_bothell = window.user.bothell_affil;
+        template_data.is_seattle = window.user.seattle_affil;
+
         has_credit_values = TuitionCard.process_tuition(template_data.tuition_accbalance);
         template_data.plain_tuition = has_credit_values.tuition;
         template_data.is_credit = has_credit_values.is_credit;
