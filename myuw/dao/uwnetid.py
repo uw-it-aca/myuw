06be06bc62248330d0fa2f852cb9f8e45be30aeb
Fang Lin <fanglin@uw.edu>
2017-10-27 15:45:49 -0700

    reorg dao.uwnetid, add 2fa support

--- myuw/dao/uwnetid.py
+++ myuw/dao/uwnetid.py
@@ -4,9 +4,13 @@ with the UW Netid Web Service
 """
 
 import logging
-from uw_uwnetid.subscription_60 import is_current_clinician
-from myuw.dao import get_netid_of_current_user
 from restclients_core.exceptions import DataFailureException
+from uw_uwnetid.models import Subscription
+from uw_uwnetid.subscription import get_netid_subscriptions
+from uw_uwnetid.subscription_60 import select_kerberos,\
+    has_clinician_in_permits
+from uw_uwnetid.subscription_64 import select_2fa
+from myuw.dao import get_netid_of_current_user
 
 
 logger = logging.getLogger(__name__)
@@ -18,9 +22,38 @@ def is_clinician():
     a member of the UW Med Center Workforce
     """
     try:
-        return is_current_clinician(get_netid_of_current_user())
+        subs = select_kerberos(get_subscriptions())
+        if subs:
+            return has_clinician_in_permits(subs.permits)
+        return False
     except DataFailureException as ex:
         if ex.status == 404:
             return False
         else:
             raise
+
+
+def is_2fa_permitted():
+    """
+    Return True if the current user netid is 2fa permitted
+    """
+    try:
+        status = select_2fa(get_subscriptions())
+        return status and status.permitted
+    except DataFailureException as ex:
+        if ex.status == 404:
+            return False
+        else:
+            raise
+
+
+def get_subscriptions():
+    return get_netid_subscriptions(
+        get_netid_of_current_user(),
+        [Subscription.SUBS_CODE_KERBEROS, Subscription.SUBS_CODE_2FA])
+
+
+def uwnetid_prefetch():
+    def _method(request):
+        return get_subscriptions()
+    return [_method]
--- myuw/test/dao/uwnetid.py
+++ myuw/test/dao/uwnetid.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from myuw.dao.uwnetid import is_clinician, is_faculty
+from myuw.dao.uwnetid import is_clinician, is_2fa_permitted, get_subscriptions
 from myuw.test import (fdao_uwnetid_override,
                        get_request, get_request_with_user)
 
@@ -9,10 +9,25 @@ class TestUWNetid(TestCase):
     def setUp(self):
         get_request()
 
-    def test_is_faculty(self):
+    def test_get_subscriptions(self):
+        get_request_with_user('javerage')
+        subs = get_subscriptions()
+        self.assertIsNotNone(subs)
+        self.assertEqual(len(subs), 2)
+
+    def test_is_2fa_permitted(self):
         get_request_with_user('bill')
-        self.assertTrue(is_faculty())
+        self.assertTrue(is_2fa_permitted())
+        self.assertFalse(is_clinician())
+
+        get_request_with_user('javerage')
+        self.assertTrue(is_2fa_permitted())
+        self.assertFalse(is_clinician())
+
+        get_request_with_user('eight')
+        self.assertFalse(is_2fa_permitted())
+        self.assertTrue(is_clinician())
 
     def test_is_clinician(self):
-        get_request_with_user('eight')
+        get_request_with_user('staff')
         self.assertTrue(is_clinician())
