153cbe88fd875b5ef274eb7c4d2751e12672a37a
Ethan Turner <ezturner@washington.edu>
2017-11-14 10:50:52 -0800

    Adding tests

--- myuw/test/dao/test_applications.py
+++ myuw/test/dao/test_applications.py
@@ -1,7 +1,36 @@
 from unittest import TestCase
 
+from uw_sdbmyuw.util import fdao_sdbmyuw_override
+from uw_sws.util import fdao_sws_override
 
-class TestApplications(TestCase):
+from myuw.dao.applications import get_applications
+from myuw.test.api import MyuwApiTest
+
+
+@fdao_sws_override
+@fdao_sdbmyuw_override
+class TestApplications(MyuwApiTest):
 
     def test_applications(self):
-        pass
+        self.set_netid("japplicant")
+
+        applications = get_applications()
+
+        self.assertEqual(len(applications), 3)
+
+        seattle_application = None
+        bothell_application = None
+        tacoma_application = None
+
+        for application in applications:
+            if application.is_seattle:
+                seattle_application = application
+            elif application.is_tacoma:
+                tacoma_application = application
+            elif application.is_bothell:
+                bothell_application = application
+
+        self.assertIsNotNone(seattle_application)
+        self.assertIsNotNone(bothell_application)
+        self.assertIsNotNone(tacoma_application)
+
