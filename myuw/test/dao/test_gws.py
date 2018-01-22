5bcf45d79649554b91e331bb5089b67f1127ea1c
Fang Lin <fanglin@uw.edu>
2018-01-22 11:48:11 -0800

    fix admin group bug in previous gws change

--- myuw/test/dao/test_gws.py
+++ myuw/test/dao/test_gws.py
@@ -4,8 +4,10 @@ from myuw.dao.gws import get_groups, is_seattle_student,\
     is_bothell_student, is_tacoma_student, is_current_graduate_student,\
     is_grad_student, is_undergrad_student, is_student, is_pce_student,\
     is_student_employee, is_faculty, is_employee, is_staff_employee,\
-    is_applicant, is_grad_c2, is_undergrad_c2
+    is_applicant, is_grad_c2, is_undergrad_c2, is_in_admin_group
 from myuw.test import fdao_gws_override, get_request_with_user
+
+GROUP_BACKEND='authz_group.authz_implementation.all_ok.AllOK'
 
 
 @fdao_gws_override
@@ -34,3 +36,14 @@ class TestPwsDao(TestCase):
         get_request_with_user('jinter')
         grs = get_groups('jinter')
         self.assertTrue(is_grad_c2())
+
+    def test_is_in_admin_group(self):
+        with self.settings(AUTHZ_GROUP_BACKEND=GROUP_BACKEND):
+            with self.settings(MYUW_ADMIN_GROUP='x'):
+                self.assertTrue(is_in_admin_group('MYUW_ADMIN_GROUP'))
+
+            with self.settings(USERSERVICE_ADMIN_GROUP='x'):
+                self.assertTrue(is_in_admin_group('USERSERVICE_ADMIN_GROUP'))
+
+            with self.settings(RESTCLIENTS_ADMIN_GROUP='x'):
+                self.assertTrue(is_in_admin_group('RESTCLIENTS_ADMIN_GROUP'))
