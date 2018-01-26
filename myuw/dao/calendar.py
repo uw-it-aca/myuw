dc14f402ecf2ad917f78848eca0e554f401ff0e4
Jim Laney <jlaney@uw.edu>
2018-01-26 15:46:00 -0800

    adds forward-compatible urllib imports

--- myuw/dao/calendar.py
+++ myuw/dao/calendar.py
@@ -1,7 +1,10 @@
 import re
 import pytz
 from datetime import timedelta, datetime, time
-from urllib import quote_plus, urlencode
+try:
+    from urllib.parse import quote_plus, urlencode
+except ImportError:
+    from urllib import quote_plus, urlencode
 from django.utils import timezone
 from restclients_core.exceptions import DataFailureException
 from uw_trumba import get_calendar_by_name
