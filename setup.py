3ac0c3c5f46d6e546c8b67072ab212cdb6ac001f
Jim Laney <jlaney@uw.edu>
2018-02-02 10:34:29 -0800

    update to django 1.11.10

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
-        'Django==1.10.5',
+        'Django==1.11.10',
         'ordereddict',
         'simplejson',
         'django-compressor==2.2',
