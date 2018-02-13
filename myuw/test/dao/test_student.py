f49256f89cbd7e14901139443524ac0b8ed0f48e
Ethan Turner <ezturner@washington.edu>
2018-02-12 17:07:29 -0800

    Finalizing resource

--- myuw/test/dao/test_student.py
+++ myuw/test/dao/test_student.py
@@ -1,7 +1,7 @@
 from unittest2 import TestCase
 from uw_sws.models import Term
 
-from myuw.dao.student import get_class_standings, get_majors
+from myuw.dao.student import get_class_standings, get_majors, get_student_status
 
 javerage_regid = '9136CCB8F66711D5BE060004AC494FFE'
 test_regid = '9136CCB8F66711D5BE060024ACA9CFDE'
@@ -31,43 +31,52 @@ winter_2014.quarter = Term.WINTER
 class TestStudentDAO(TestCase):
 
     def test_get_majors(self):
-        majors = get_majors(javerage_regid)
-
-        self.assertIn("standings", majors)
-        self.assertIn("current", majors)
-        self.assertIn("rollup", majors)
-
-        self.assertEquals(majors['current']['FullName'],
-                          "App & Comp Math Sci (Social & Behav Sci)")
-
-        self.assertEquals(majors['rollup']['FullName'],
-                          "App & Comp Math Sci (Social & Behav Sci)")
-
-        term_majors = majors['majors']
-
-        self.assertEquals(term_majors[spring_2013]['FullName'],
-                          "App & Comp Math Sci (Social & Behav Sci)")
-
-        self.assertEquals(term_majors[autumn_2013]['FullName'],
-                          "American Sign Language")
-
-        self.assertEquals(term_majors[winter_2014]['FullName'],
-                          "English")
-
         majors = get_majors(test_regid)
 
         self.assertNotIn('current', majors)
 
-        self.assertEquals(majors['rollup']['FullName'],
+        self.assertEquals(majors['rollup'][0].full_name,
+                          "Test Major")
+
+        majors = get_majors(javerage_regid)
+
+        self.assertIn("majors", majors)
+        self.assertIn("current", majors)
+        self.assertIn("rollup", majors)
+
+        self.assertEquals(majors['current'][0].full_name,
+                          "App & Comp Math Sci (Social & Behav Sci)")
+
+        self.assertEquals(majors['rollup'][0].full_name,
+                          "App & Comp Math Sci (Social & Behav Sci)")
+
+        term_majors = majors['majors']
+
+        self.assertEquals(term_majors[spring_2013][0].full_name,
+                          "App & Comp Math Sci (Social & Behav Sci)")
+
+        self.assertEquals(term_majors[autumn_2013][0].full_name,
+                          "Computer Science")
+
+        self.assertEquals(term_majors[winter_2014][0].full_name,
+                          "English")
+
+        majors = get_majors(testalicious_regid)
+
+        self.assertEquals(majors['rollup'][0].full_name,
                           "App & Comp Math Sci (Social & Behav Sci)")
 
     def test_get_class_standing(self):
 
         class_standings = get_class_standings(javerage_regid)
 
-        self.assertIn(class_standings, "standings")
-        self.assertIn(class_standings, "current")
-        self.assertIn(class_standings, "rollup")
+        self.assertIn("class_level", class_standings)
+        self.assertIn("current", class_standings)
+        self.assertIn("rollup", class_standings)
 
     def test_get_student_status(self):
-        pass
+
+        student_status = get_student_status(javerage_regid)
+
+        self.assertIn("majors", student_status)
+        self.assertIn("class_level", student_status)
