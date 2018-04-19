a04ad75c723c98b74c579c2c2c1f0539c6310b88
Fang Lin <fanglin@uw.edu>
2018-04-19 13:22:53 -0700

    check quick link-control

--- myuw/static/js/test/quicklinks_card.js
+++ myuw/static/js/test/quicklinks_card.js
@@ -40,7 +40,12 @@ describe('QuickLinksCard', function(){
                      url: "http://search.lib.uw.edu/account",
                      label: "Your Library Account"}
                 ],
-                recent_links: [],
+                recent_links: [
+                    {added: false,
+                     id: "1",
+                     url: "https://notify.uw.edu/",
+                     label: "Notify.UW"}
+                ],
                 custom_links: [
                     {id: "83",
                      url: "https://wiki.cac.washington.edu/",
@@ -51,17 +56,30 @@ describe('QuickLinksCard', function(){
                 ],
             };
             QuickLinksCard.render_init();
-            assert.equal(QuickLinksCard.dom_target.find('.myuw-qlinks-item').length, 7);
+            assert.equal(QuickLinksCard.dom_target.find('.myuw-qlinks-item').length, 8);
+
+            // default_links
             assert.equal(QuickLinksCard.dom_target.find('.myuw-qlinks-item')[0].innerHTML, '<a href="http://canvas.uw.edu/" title="Canvas LMS" data-notrack="" target="_blank"><span>Canvas LMS</span></a>');
+            assert.ok(QuickLinksCard.dom_target.find('.link-controls')[0].innerHTML.includes("Remove Canvas LMS link from Quick Links list"))
+
             assert.equal(QuickLinksCard.dom_target.find('.myuw-qlinks-item')[1].innerHTML, '<a href="https://myplan.uw.edu" title="MyPlan" data-notrack="" target="_blank"><span>MyPlan</span></a>');
 
+            // custom_links
             assert.ok(QuickLinksCard.dom_target.find('.myuw-qlinks-item')[2].innerHTML.includes("custom-link-83"));
             assert.ok(QuickLinksCard.dom_target.find('.myuw-qlinks-item')[3].innerHTML.includes("custom-link-130"));
+            assert.ok(QuickLinksCard.dom_target.find('.link-controls')[3].innerHTML.includes("edit-link"))
+            assert.ok(QuickLinksCard.dom_target.find('.link-controls')[3].innerHTML.includes("remove-link"))
 
+            // recent_links
+            assert.equal(QuickLinksCard.dom_target.find('.myuw-qlinks-item')[4].innerHTML, '<a href="https://notify.uw.edu/" title="Notify.UW" target="_blank"><span>Notify.UW</span></a>');
+            assert.ok(QuickLinksCard.dom_target.find('.link-controls')[4].innerHTML.includes("Save Notify.UW link to your Quick Links list"))
+            
+            // popular_links
+            assert.equal(QuickLinksCard.dom_target.find('.myuw-qlinks-item')[5].innerHTML, '<a href="https://itconnect.uw.edu" title="IT Connect" target="_blank"><span>IT Connect</span></a>');
+            assert.ok(QuickLinksCard.dom_target.find('.link-controls')[5].innerHTML.includes("Save IT Connect link to your Quick Links list"))
 
-            assert.equal(QuickLinksCard.dom_target.find('.myuw-qlinks-item')[4].innerHTML, '<a href="https://itconnect.uw.edu" title="IT Connect" target="_blank"><span>IT Connect</span></a>');
-            assert.equal(QuickLinksCard.dom_target.find('.myuw-qlinks-item')[5].innerHTML, '<a href="http://hr.uw.edu/jobs/" title="UW Jobs" target="_blank"><span>UW Jobs</span></a>');
-            assert.equal(QuickLinksCard.dom_target.find('.myuw-qlinks-item')[6].innerHTML, '<a href="http://search.lib.uw.edu/account" title="Your Library Account" target="_blank"><span>Your Library Account</span></a>');
+            assert.equal(QuickLinksCard.dom_target.find('.myuw-qlinks-item')[6].innerHTML, '<a href="http://hr.uw.edu/jobs/" title="UW Jobs" target="_blank"><span>UW Jobs</span></a>');
+            assert.equal(QuickLinksCard.dom_target.find('.myuw-qlinks-item')[7].innerHTML, '<a href="http://search.lib.uw.edu/account" title="Your Library Account" target="_blank"><span>Your Library Account</span></a>');
         });
     });
 });
