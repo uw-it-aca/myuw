a191da3d64b3234a3e8892f337430818253337f5
Fang Lin <fanglin@uw.edu>
2018-04-23 15:23:20 -0700

    another way to change excution order

--- myuw/static/js/test/card_quicklinks.js
+++ myuw/static/js/test/card_quicklinks.js
@@ -1,6 +1,6 @@
 var Global = require("./global.js");
 
-describe('QuickLinksCard', function(){
+describe('Card: QuickLinksCard', function(){
 
     before(function () {
         var render_id = 'render_quick_links_card';
