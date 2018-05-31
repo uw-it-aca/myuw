ac46cad4935d52f6205fc9da97476d3c2a5e8526
Fang Lin <fanglin@uw.edu>
2018-05-31 12:31:27 -0700

    prevent local setting affect tests

--- myuw/test/util/test_settings.py
+++ myuw/test/util/test_settings.py
@@ -7,19 +7,29 @@ from myuw.util.settings import get_calendar_time_zone,\
     get_disable_actions_when_override
 
 
+legacy_url = "https://myuw.washington.edu/"
+
+
 class TestSetting(TestCase):
 
     def test_default(self):
-        self.assertEqual(get_calendar_time_zone(), 'America/Los_Angeles')
-        self.assertIsNone(get_mailman_courserequest_recipient())
-        self.assertIsNone(get_google_search_key())
-        self.assertEqual(get_legacy_url(),
-                         "https://myuw.washington.edu/servlet/user")
-        self.assertEqual(get_logout_url(), "/user_logout")
-        self.assertEqual(get_myuwclass_url(), "myuwclass.asp?cid=")
-        self.assertEqual(get_myuw_admin_group(),
-                         'u_astratst_myuw_test-support-admin')
-        self.assertEqual(get_myuw_override_group(),
-                         'u_astratst_myuw_test-support-impersonate')
-        self.assertEqual(get_myuw_astra_group_stem(), 'u_astratst_myuw')
-        self.assertEqual(get_disable_actions_when_override(), True)
+        with self.settings(TRUMBA_CALENDAR_TIMEZONE='America/Los_Angeles',
+                           MAILMAN_COURSEREQUEST_RECIPIENT=None,
+                           GOOGLE_SEARCH_KEY=None,
+                           MYUW_USER_SERVLET_URL=legacy_url,
+                           LOGOUT_URL="/user_logout",
+                           MYUWCLASS="myuwclass.asp?cid=",
+                           MYUW_ADMIN_GROUP='admin',
+                           MYUW_OVERRIDE_GROUP='impersonate',
+                           MYUW_ASTRA_GROUP_STEM='u_astratst_myuw',
+                           MYUW_DISABLE_ACTIONS_WHEN_OVERRIDE=True):
+            self.assertEqual(get_calendar_time_zone(), 'America/Los_Angeles')
+            self.assertIsNone(get_mailman_courserequest_recipient())
+            self.assertIsNone(get_google_search_key())
+            self.assertEqual(get_legacy_url(), legacy_url)
+            self.assertEqual(get_logout_url(), "/user_logout")
+            self.assertEqual(get_myuwclass_url(), "myuwclass.asp?cid=")
+            self.assertEqual(get_myuw_admin_group(), 'admin')
+            self.assertEqual(get_myuw_override_group(), 'impersonate')
+            self.assertEqual(get_myuw_astra_group_stem(), 'u_astratst_myuw')
+            self.assertEqual(get_disable_actions_when_override(), True)
