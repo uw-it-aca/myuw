09c6e04919a23f83e424bc28501de9eaf0ed5001
Fang Lin <fanglin@uw.edu>
2018-06-20 11:43:32 -0700

    handle the case where section type is null

--- myuw/static/js/card/instructor_schedule/course_sche_panel.js
+++ myuw/static/js/card/instructor_schedule/course_sche_panel.js
@@ -4,9 +4,16 @@ var InstructorCourseSchePanel = {
         var source = $("#instructor_course_sche_panel").html();
         var template = Handlebars.compile(source);
         c_section.netid = window.user.netid;
-        for (i = 0; i < c_section.meetings.length; i++) {
-            if (c_section.meetings[i].type !== c_section.section_type) {
-                c_section.meetings[i].display_type = true;
+        if (c_section.meetings.length > 0) {
+            if (!c_section.section_type) {
+                c_section.section_type = c_section.meetings[0].type;
+            }
+            for (i = 0; i < c_section.meetings.length; i++) {
+                if (!c_section.wont_meet &&
+                    !c_section.no_meeting &&
+                    c_section.meetings[i].type !== c_section.section_type) {
+                    c_section.meetings[i].display_type = true;
+                }
             }
         }
         var raw = template(c_section);
