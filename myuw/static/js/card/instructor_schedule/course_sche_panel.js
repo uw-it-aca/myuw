d192d431cbd27aaadd348ef2d75e70cbd8dceefb
Fang Lin <fanglin@uw.edu>
2018-06-20 11:25:07 -0700

    Resolve MUWM-4175

--- myuw/static/js/card/instructor_schedule/course_sche_panel.js
+++ myuw/static/js/card/instructor_schedule/course_sche_panel.js
@@ -4,6 +4,11 @@ var InstructorCourseSchePanel = {
         var source = $("#instructor_course_sche_panel").html();
         var template = Handlebars.compile(source);
         c_section.netid = window.user.netid;
+        for (i = 0; i < c_section.meetings.length; i++) {
+            if (c_section.meetings[i].type !== c_section.section_type) {
+                c_section.meetings[i].display_type = true;
+            }
+        }
         var raw = template(c_section);
         $('#instructor_sche_on_course_card' + c_section.index).html(raw);
     }
--- myuw/static/js/card/schedule/course_sche_panel.js
+++ myuw/static/js/card/schedule/course_sche_panel.js
@@ -3,6 +3,12 @@ var CourseSchePanel = {
     render: function (c_section) {
         var source = $("#course_sche_panel").html();
         var template = Handlebars.compile(source);
+         c_section.netid = window.user.netid;
+        for (i = 0; i < c_section.meetings.length; i++) {
+            if (c_section.meetings[i].type !== c_section.section_type) {
+                c_section.meetings[i].display_type = true;
+            }
+        }
         var raw = template(c_section);
         $('#sche_on_course_card' + c_section.index).html(raw);
     }
