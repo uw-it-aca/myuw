b689da059c22c03953cdeeb7bafccaebff5112ec
Fang Lin <fanglin@uw.edu>
2018-06-26 09:54:43 -0700

    enhanece the performance of is_instructor function.

--- myuw/dao/instructor.py
+++ myuw/dao/instructor.py
@@ -1,7 +1,7 @@
 import logging
 from restclients_core.exceptions import DataFailureException
 from myuw.models import SeenInstructor
-from myuw.dao.instructor_schedule import get_instructor_sections
+from uw_sws.section import get_last_section_by_instructor_and_terms
 from myuw.dao.pws import get_person_of_current_user
 from myuw.dao.term import get_prev_num_terms,\
     get_term_before, get_previous_quarter
@@ -22,34 +22,32 @@ def is_instructor(request):
     if hasattr(request, "myuw_is_instructor"):
         return request.myuw_is_instructor
 
+    person = get_person_of_current_user(request)
+    user_netid = person.uwnetid
+    if is_seen_instructor(user_netid):
+        request.myuw_is_instructor = True
+        return True
+
     request.myuw_is_instructor = False
-    try:
-        person = get_person_of_current_user(request)
-        user_netid = person.uwnetid
-        if is_seen_instructor(user_netid):
-            request.myuw_is_instructor = True
-            return True
+    term = get_term_before(get_previous_quarter(request))
+    section = get_last_section_by_instructor_and_terms(
+        person, term, 4, transcriptable_course='all',
+        delete_flag=['active', 'suspended'])
 
-        term = get_term_before(get_previous_quarter(request))
-        sections = get_instructor_sections(person,
-                                           term,
-                                           future_terms=4,
-                                           include_secondaries=False)
-        if len(sections) > 0:
-            request.myuw_is_instructor = True
-
-            quarter = sections[-1].term.quarter
-            year = sections[-1].term.year
+    if section:
+        request.myuw_is_instructor = True
+        quarter = section.term.quarter
+        year = section.term.year
+        try:
             add_seen_instructor(user_netid, year, quarter)
-    except DataFailureException as err:
-        if err.status != 404:
-            raise
+        except Exception as ex:
+            logger.error("add_seen_instructor(%s, %s, %s) ==> %s",
+                         user_netid, year, quarter, ex)
     return request.myuw_is_instructor
 
 
 def is_seen_instructor(uwnetid):
-    qset = SeenInstructor.objects.filter(uwnetid=uwnetid)
-    return qset.count() > 0
+    return SeenInstructor.objects.filter(uwnetid=uwnetid).exists()
 
 
 def add_seen_instructor(netid, year, quarter):
@@ -58,10 +56,5 @@ def add_seen_instructor(netid, year, quarter):
                                             year=year)
 
 
-def remove_seen_instructors_for_prior_terms(term):
-    term_4th_prev = get_prev_num_terms(term, 4)[0]
-    remove_seen_instructors_for_prior_years(term_4th_prev.year)
-
-
-def remove_seen_instructors_for_prior_years(year):
-    SeenInstructor.objects.filter(year__lte=year).delete()
+def remove_seen_instructors_yrs_before(year):
+    SeenInstructor.objects.filter(year__lt=year).delete()
--- myuw/test/dao/test_instructor.py
+++ myuw/test/dao/test_instructor.py
@@ -1,57 +1,51 @@
 from django.test import TransactionTestCase
 from myuw.dao.instructor import is_instructor, add_seen_instructor,\
-    remove_seen_instructors_for_prior_terms, is_seen_instructor,\
-    remove_seen_instructors_for_prior_years
+    is_seen_instructor, remove_seen_instructors_yrs_before
 from myuw.dao.term import get_specific_term
 from myuw.test import get_request_with_user, get_request_with_date
 
 
 class TestSeenInstructor(TransactionTestCase):
     def test_non_instructorness(self):
-        self.assertEqual(is_seen_instructor('bill'), False)
+        self.assertFalse(is_seen_instructor('bill'))
 
     def test_is_seen_instructor(self):
         add_seen_instructor('bill', 2012, "autumn")
-        self.assertEqual(is_seen_instructor('bill'), True)
+        self.assertTrue(is_seen_instructor('bill'))
 
-    def test_remove_instructors_prior_terms(self):
-        add_seen_instructor('bill', 2012, "summer")
-        term = get_specific_term(2013, "autumn")
-        remove_seen_instructors_for_prior_terms(term)
-        req = get_request_with_user('bill',
-                                    get_request_with_date("2013-04-10"))
-        self.assertEqual(is_seen_instructor('bill'), False)
-
-        add_seen_instructor('bill', 2013, "summer")
-        term = get_specific_term(2013, "autumn")
-        remove_seen_instructors_for_prior_terms(term)
-        self.assertEqual(is_seen_instructor('bill'), True)
-        self.assertTrue(is_instructor(req))
-
-    def test_remove_instructors_prior_years(self):
+    def test_remove_seen_instructors_yrs_before(self):
         add_seen_instructor('bill', 2012, "autumn")
-        req = get_request_with_user('bill',
-                                    get_request_with_date("2013-04-10"))
-        self.assertEqual(is_seen_instructor('bill'), True)
+        self.assertTrue(is_seen_instructor('bill'))
+        remove_seen_instructors_yrs_before(2013)
+        self.assertFalse(is_seen_instructor('bill'))
 
-        remove_seen_instructors_for_prior_years(2013)
-        self.assertEqual(is_seen_instructor('bill'), False)
+        add_seen_instructor('bill', 2013, "winter")
+        remove_seen_instructors_yrs_before(2013)
+        self.assertTrue(is_seen_instructor('bill'))
+
+    def test_instructor_3_term_before(self):
+        req = get_request_with_user('bill',
+                                    get_request_with_date("2014-04-10"))
+        self.assertFalse(hasattr(req, "myuw_is_instructor"))
+        self.assertFalse(is_instructor(req))
+        self.assertFalse(req.myuw_is_instructor)
 
     def test_is_instructor(self):
-        request = get_request_with_user('bill',
-                                        get_request_with_date("2013-04-10"))
-        self.assertFalse(hasattr(request, "myuw_is_instructor"))
-        self.assertTrue(is_instructor(request))
-        self.assertTrue(hasattr(request, "myuw_is_instructor"))
+        req = get_request_with_user('bill',
+                                    get_request_with_date("2013-04-10"))
+        self.assertTrue(is_instructor(req))
+        # get the one cached in the req
+        self.assertTrue(is_instructor(req))
 
-        request = get_request_with_user('billsea',
-                                        get_request_with_date("2013-04-10"))
-        self.assertTrue(is_instructor(request))
+        # taught in last term
+        req = get_request_with_user('billsea',
+                                    get_request_with_date("2014-01-10"))
+        self.assertTrue(is_instructor(req))
 
-        request = get_request_with_user('billseata',
-                                        get_request_with_date("2013-04-10"))
-        self.assertTrue(is_instructor(request))
+        req = get_request_with_user('billseata',
+                                    get_request_with_date("2013-04-10"))
+        self.assertTrue(is_instructor(req))
 
-        request = get_request_with_user('billpce',
-                                        get_request_with_date("2013-04-10"))
-        self.assertTrue(is_instructor(request))
+        req = get_request_with_user('billpce',
+                                    get_request_with_date("2013-04-10"))
+        self.assertTrue(is_instructor(req))
