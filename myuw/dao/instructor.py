537682c9275fea384d4001c7bb7c1b52d071fb2c
Fang Lin <fanglin@uw.edu>
2018-07-05 15:38:37 -0700

    Check past 6 years of teaching records

--- myuw/dao/instructor.py
+++ myuw/dao/instructor.py
@@ -1,8 +1,10 @@
 import logging
 from myuw.models import Instructor
 from uw_sws.section import get_last_section_by_instructor_and_terms
+from myuw.dao import is_using_file_dao
 from myuw.dao.pws import get_person_of_current_user
-from myuw.dao.term import get_term_before, get_previous_quarter
+from myuw.dao.term import get_term_before, get_previous_quarter,\
+    get_current_quarter
 from myuw.dao.user import get_user_model
 
 logger = logging.getLogger(__name__)
@@ -35,10 +37,20 @@ def is_instructor(request):
 
 
 def get_most_recent_sectionref_by_instructor(request):
-    term = get_term_before(get_previous_quarter(request))
+    if is_using_file_dao():
+        term = get_term_before(get_previous_quarter(request))
+        future_term = 4
+    else:
+        cur_term = get_current_quarter(request)
+        term = get_specific_term(cur_term.year - 6, cur_term.quarter)
+        if cur_term.quarter == 'spring':
+            future_term = 26
+        else:
+            future_term = 25
+
     person = get_person_of_current_user(request)
     return get_last_section_by_instructor_and_terms(
-        person, term, 4,
+        person, term, future_term,
         transcriptable_course='all',
         delete_flag=['active', 'suspended'])
 
