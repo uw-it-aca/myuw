589718f31e837c08e4a1d744c8b1f16d53a874c3
Fang Lin <fanglin@uw.edu>
2018-07-06 16:46:40 -0700

    fix test

--- myuw/test/api/test_instructor_schedule.py
+++ myuw/test/api/test_instructor_schedule.py
@@ -217,7 +217,7 @@ class TestInstructorSection(MyuwApiTest):
                                         get_request_with_date("2013-10-01"))
         resp = InstScheCurQuar().get(request)
         data = json.loads(resp.content)
-        self.assertEqual(len(data['sections']), 1)
+        self.assertEqual(len(data['sections']), 2)
         self.assertEqual(data['sections'][0]['current_enrollment'], 1)
         self.assertEqual(data['sections'][0]['enrollment_student_name'],
                          "Student1, Jake Average")
