b8eaa09236089f557fb0733b62df7ae3436b0b0c
Fang Lin <fanglin@uw.edu>
2018-07-20 14:19:15 -0700

    fix syntax err

--- myuw/static/js/card/accounts/hr_payroll_card.js
+++ myuw/static/js/card/accounts/hr_payroll_card.js
@@ -7,9 +7,9 @@ var HRPayrollCard = {
         var template = Handlebars.compile(source);
         var data = {
             card_name: HRPayrollCard.name,
-            is_seattle: window.user.seattle_emp),
-            is_bothell: window.user.bothell_emp),
-            is_tacoma: window.user.tacoma_emp),
+            is_seattle: window.user.seattle_emp,
+            is_bothell: window.user.bothell_emp,
+            is_tacoma: window.user.tacoma_emp,
             is_faculty: window.user.faculty,
             is_stud_employee: window.user.stud_employee,
             truncate_view: (window.user.retiree || window.user.past_employee)
