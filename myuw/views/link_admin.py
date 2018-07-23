8215a0bfc7b98d16d65efe93aa5c493172f12a40
Fang Lin <fanglin@uw.edu>
2018-07-23 14:00:44 -0700

    make intl_stud an affiliation choice.

--- myuw/views/link_admin.py
+++ myuw/views/link_admin.py
@@ -32,10 +32,6 @@ def popular_links(request, page):
             if 'pce' in request.POST and request.POST['pce'] == "pce":
                 create_args['pce'] = True
 
-            if ('intl_stud' in request.POST and
-                    request.POST['intl_stud'] == "intl_stud"):
-                create_args['intl_stud'] = True
-
             PopularLink.objects.create(**create_args)
             logger.info("popular link added.  user: %s, link: %s" %
                         (uwnetid, request.POST['url']))
@@ -55,7 +51,7 @@ def popular_links(request, page):
         existing_lookup.add(link.url)
     kwargs = {}
     filter_kwargs = {}
-    for field in ('campus', 'affiliation', 'pce', 'intl_stud'):
+    for field in ('campus', 'affiliation', 'pce'):
         if field in request.GET:
             value = request.GET[field]
             kwargs['is_'+value] = True
