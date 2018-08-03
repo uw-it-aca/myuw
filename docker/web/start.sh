2d5a5b618c0f14ac96f93c89b2f9295029762142
Ethan Turner <ezturner@uw.edu>
2018-08-03 16:36:04 -0700

    Switching to python3

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -5,12 +5,12 @@ rm -rf /run/httpd/* /tmp/httpd*
 
 # Check if we're the leader using ElasticBeanstalk's methods
 if [ -f /tmp/is_leader ]; then
-    python manage.py migrate
+    python3 manage.py migrate
 fi
 
 pip install -r requirements.txt
 rm -rf /static/
-python manage.py collectstatic
+python3 manage.py collectstatic
 
 # Start Apache server in foreground
 exec /usr/sbin/apachectl -DFOREGROUND
