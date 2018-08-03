924fcd5e32a39a1d527f08e7750a6a5b5d10c3a9
Ethan Turner <ezturner@uw.edu>
2018-08-03 12:06:52 -0700

    Modifying startup script to run migrations as part of startup

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -2,8 +2,15 @@
 # Remove any existing httpd data
 rm -rf /run/httpd/* /tmp/httpd*
 
+
+# Check if we're the leader using ElasticBeanstalk's methods
+if [ -f /tmp/is_leader ]; then
+    python manage.py migrate
+fi
+
 pip install -r requirements.txt
 rm -rf /static/
 python manage.py collectstatic
+
 # Start Apache server in foreground
 exec /usr/sbin/apachectl -DFOREGROUND
