d3517955a0564e3055100c4f1543eae6bf0e9c16
Ethan Turner <ezturner@uw.edu>
2018-08-03 15:18:53 -0700

    Adjusting docker setup to add cache and disable CloudWatch logging

--- docker/settings.py
+++ docker/settings.py
@@ -86,8 +86,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
@@ -98,6 +96,7 @@ STATICFILES_FINDERS = (
 COMPRESS_ENABLED = False
 COMPRESS_ROOT = "/static/"
 STATIC_ROOT = "/static/"
+STATIC_URL = '/static/'
 
 TEMPLATES = [
     {
@@ -158,115 +157,117 @@ from boto3.session import Session
 # logging configfrom boto3.session import Session
 boto3_session = Session(region_name='us-west-2')
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
+
+if os.getenv('LOG', 'none') == "CloudWatch":
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'filters': {
+            'require_debug_false': {
+                '()': 'django.utils.log.RequireDebugFalse'
+            }
+        },
+        'formatters': {
+            'myuw': {
+                'format': '%(levelname)-4s %(asctime)s %(message)s [%(name)s]',
+                'datefmt': '%d %H:%M:%S',
+            },
+            'aws': {
+                # you can add specific format for aws here
+                'format': u"%(asctime)s [%(levelname)-8s] %(message)s",
+                'datefmt': "%Y-%m-%d %H:%M:%S"
+            }
+        },
+        'handlers': {
+            'watchtower': {
+                'level': 'DEBUG',
+                'class': 'watchtower.CloudWatchLogHandler',
+                        'boto3_session': boto3_session,
+                        'log_group': os.getenv("CW_GROUP", ""),
+                        'stream_name': os.getenv("CW_STREAM", "localhost"),
+                'formatter': 'aws',
+            },
+        },
+        'loggers': {
+            'django.request': {
+                'handlers': ['watchtower'],
+                'level': 'ERROR',
+                'propagate': True,
+            },
+            'restclients_core': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+            'rc_django': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+            'uw_sws': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+            'uw_iasystem': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+            'myuw.util.performance': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+            'myuw.views.choose': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+            'myuw.views.api.banner_message': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+            'myuw.views.api.resources.pin': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+            'myuw.views.api.instructor_section_display': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+            'myuw.views.logger': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+            'myuw.views.api.notices.seen': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+            'myuw': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+            'propagate': True,
+            },
+            'card': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+            'link': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+            'session': {
+                'handlers': ['watchtower'],
+                'level': 'INFO',
+                'propagate': True,
+            },
         }
-    },
-    'formatters': {
-        'myuw': {
-            'format': '%(levelname)-4s %(asctime)s %(message)s [%(name)s]',
-            'datefmt': '%d %H:%M:%S',
-        },
-        'aws': {
-            # you can add specific format for aws here
-            'format': u"%(asctime)s [%(levelname)-8s] %(message)s",
-            'datefmt': "%Y-%m-%d %H:%M:%S"
-        }
-    },
-    'handlers': {
-        'watchtower': {
-            'level': 'DEBUG',
-            'class': 'watchtower.CloudWatchLogHandler',
-                     'boto3_session': boto3_session,
-                     'log_group': os.getenv("CW_GROUP", ""),
-                     'stream_name': os.getenv("CW_STREAM", "localhost"),
-            'formatter': 'aws',
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['watchtower'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'restclients_core': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        'rc_django': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': False,
-         },
-        'uw_sws': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        'uw_iasystem': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        'myuw.util.performance': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        'myuw.views.choose': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'myuw.views.api.banner_message': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'myuw.views.api.resources.pin': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'myuw.views.api.instructor_section_display': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'myuw.views.logger': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'myuw.views.api.notices.seen': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'myuw': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-           'propagate': True,
-        },
-        'card': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'link': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'session': {
-            'handlers': ['watchtower'],
-            'level': 'INFO',
-            'propagate': True,
-        },
     }
-}
