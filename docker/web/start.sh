4f4c3e791923b8313ed43f462f8bb9938b61499b
Ethan Turner <ezturner@uw.edu>
2018-08-05 16:55:59 -0700

    Switching dependency versions

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -9,6 +9,11 @@ if [ -f /tmp/is_leader ]; then
 fi
 
 pip install -r requirements.txt
+pip uninstall -y uw-restclients-sws
+pip install -e git+https://github.com/uw-it-aca/uw-restclients-sws@bug/mock-memcached-error#egg=uw-restclients-sws
+pip uninstall -y uw-restclients-django-utils
+pip install -e git+https://github.com/uw-it-aca/uw-restclients-django-utils@bug/mock-memcached-error#egg=uw-restclients-django-utils
+
 rm -rf /static/
 python manage.py collectstatic
 
