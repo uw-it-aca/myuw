7145942b3db7809d68291951bc0f4d50082355cb
Ethan Turner <ezturner@washington.edu>
2018-08-14 11:35:25 -0700

    Fixing sytnax error

--- myuw/util/__init__.py
+++ myuw/util/__init__.py
@@ -1,26 +1,27 @@
 from threading import Thread
-import gc
 from time import sleep
+import gc
+import json
+import logging
 
 logger = logging.getLogger(__name__)
 
 
 def check_memory():
-  while(True):
-    objects = gc.get_objects()
-    obj_count = {}
+    while(True):
+        objects = gc.get_objects()
+        obj_count = {}
 
-    for obj in objects:
-      if type(obj) in obj_count:
-        obj_count[type(obj)] += 1
-      
-    logger.log(json.dumps(obj_count, indent=4, sort_keys=True)
-    thread.sleep(60)
+        for obj in objects:
+            if type(obj) in obj_count:
+                obj_count[type(obj)] += 1
 
+        logger.log(json.dumps(obj_count, indent=4, sort_keys=True))
+        sleep(60)
 
 
 def start_memory_thread():
-  thread = Thread(target=check_memory)
-  thread.start()
+    thread = Thread(target=check_memory)
+    thread.start()
 
 start_memory_thread()
