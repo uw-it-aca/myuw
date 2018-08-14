bdcd73f69f7cf382f1d3c1a2de8d5bac869a1986
Ethan Turner <ezturner@washington.edu>
2018-08-14 12:17:25 -0700

    Profiling object size

--- myuw/util/__init__.py
+++ myuw/util/__init__.py
@@ -3,6 +3,7 @@ from time import sleep
 import gc
 import json
 import logging
+import sys
 
 logger = logging.getLogger(__name__)
 
@@ -14,9 +15,10 @@ def check_memory():
 
         for obj in objects:
             if str(type(obj)) in obj_count:
-                obj_count[str(type(obj))] += 1
+                obj_count[str(type(obj))][0] += 1 
+                obj_count[str(type(obj))][0] +=  sys.getsizeof(obj) 
             else:
-                obj_count[str(type(obj))] = 1
+                obj_count[str(type(obj))] = [1, sys.getsizeof(obj)]
 
         logger.error(json.dumps(obj_count, indent=4, sort_keys=True))
         sleep(5)
