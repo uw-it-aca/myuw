78fee1bd1ead2de6a6d9a9e5b0dd59bb70fcb2f8
Fang Lin <fanglin@uw.edu>
2018-08-24 10:55:01 -0700

    simplify command

--- myuw/management/commands/load_section_status_changes.py
+++ myuw/management/commands/load_section_status_changes.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from myuw.event. section_status import SectionStatusProcessor
-from aws_message.gather import Gather, GatherException
+from aws_message.gather import Gather
 
 
 class Command(BaseCommand):
@@ -9,7 +9,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             Gather(processor=SectionStatusProcessor()).gather_events()
-        except GatherException as err:
-            raise CommandError(err)
         except Exception as err:
-            raise CommandError('FAIL: %s' % (err))
+            raise CommandError("Cause: %s" % err)
