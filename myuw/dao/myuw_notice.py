8ef317d20ba6831ac3e78e59ebff49396990851d
Fang Lin <fanglin@uw.edu>
2018-08-30 16:22:43 -0700

    MUWM-4347: exclude campus from affiliation matching

--- myuw/dao/myuw_notice.py
+++ myuw/dao/myuw_notice.py
@@ -1,44 +1,42 @@
 from myuw.dao.affiliation import get_all_affiliations
 from myuw.models.myuw_notice import MyuwNotice
-from myuw.dao.term import get_comparison_datetime
+from myuw.dao.term import get_comparison_datetime_with_tz
 
 
 def get_myuw_notices_for_user(request):
-    date = get_comparison_datetime(request)
+    date = get_comparison_datetime_with_tz(request)
     fetched_notices = MyuwNotice.objects.filter(start__lte=date)
     active_notices = []
+
     for notice in fetched_notices:
+
         if notice.end is None:
             active_notices.append(notice)
             continue
         if notice.end is not None:
-            end = notice.end.replace(tzinfo=None)
-            if end >= date:
+            if notice.end >= date:
                 active_notices.append(notice)
 
     affiliations = get_all_affiliations(request)
 
-    campus_notices = []
-    for notice in active_notices:
-        # If campus is not set show to all campuses
-        if not(notice.is_bothell or notice.is_seattle or notice.is_tacoma):
-            campus_notices.append(notice)
-            pass
-        if notice.is_seattle and affiliations['seattle']:
-            campus_notices.append(notice)
-            pass
-        if notice.is_tacoma and affiliations['tacoma']:
-            campus_notices.append(notice)
-            pass
-        if notice.is_bothell and affiliations['bothell']:
-            campus_notices.append(notice)
-            pass
-
     user_notices = []
-    for notice in campus_notices:
-        # If affiliation not set show to all affiliations
-        if not(notice.is_alumni or notice.is_applicant or notice.is_grad or
-                notice.is_grad_c2 or notice.is_pce or notice.is_student or
+    for notice in active_notices:
+
+        # If campus is not set show to all campuses
+        if (not (notice.is_bothell or
+                 notice.is_seattle or
+                 notice.is_tacoma) or
+                notice.is_seattle and affiliations['seattle'] or
+                notice.is_bothell and affiliations['bothell'] or
+                notice.is_tacoma and affiliations['tacoma']):
+            campus_matched = True
+        else:
+            continue
+
+        # If no affiliation is required
+        if not (notice.is_alumni or notice.is_applicant or
+                notice.is_grad or notice.is_grad_c2 or
+                notice.is_pce or notice.is_student or
                 notice.is_undergrad or notice.is_undergrad_c2 or
                 notice.is_fyp or notice.is_past_student or
                 notice.is_clinician or notice.is_employee or
@@ -48,14 +46,20 @@ def get_myuw_notices_for_user(request):
                 notice.is_intl_stud):
             user_notices.append(notice)
             continue
+
         if notice.is_past_student and affiliations["past_stud"]:
             user_notices.append(notice)
             continue
+
         for key in affiliations:
+            # exclude campuses
+            if key == 'seattle' or key == 'bothell' or key == 'tacoma':
+                continue
             try:
                 if getattr(notice, "is_" + key) and affiliations[key]:
                     user_notices.append(notice)
                     continue
             except AttributeError:
                 pass
+
     return user_notices
--- myuw/test/dao/myuw_notice.py
+++ myuw/test/dao/myuw_notice.py
@@ -1,28 +1,38 @@
 # -*- coding: utf8 -*-
-from django.test import TestCase
+from django.test import TransactionTestCase
 from datetime import datetime
+import pytz
+from django.utils import timezone
 from myuw.dao.myuw_notice import get_myuw_notices_for_user
 from myuw.dao.notice_mapping import categorize_notices
 from myuw.test import get_request_with_user, get_request_with_date
 from myuw.models.myuw_notice import MyuwNotice
 
 
-class TestMyuwNotice(TestCase):
+def get_datetime_with_tz(year, month, day, hour):
+    local_tz = timezone.get_current_timezone()
+    return local_tz.localize(
+        datetime(year, month, day, hour, 0, 0)).astimezone(pytz.utc)
+
+
+class TestMyuwNotice(TransactionTestCase):
+
     def test_by_date(self):
         notice = MyuwNotice(title="Foo",
                             content="Notice Content",
                             notice_type="Banner",
                             notice_category="Student",
-                            start=datetime(2018, 5, 8, 10, 0, 0),
-                            end=datetime(2018, 5, 10, 10, 0, 0),
+                            start=get_datetime_with_tz(2018, 5, 8, 10),
+                            end=get_datetime_with_tz(2018, 5, 10, 10),
                             is_seattle=True)
         notice.save()
+        self.assertIsNotNone(str(notice))
         notice = MyuwNotice(title="Bar",
                             content="Notice Content Two",
                             notice_type="Banner",
                             notice_category="Student",
-                            start=datetime(2018, 5, 12, 10, 0, 0),
-                            end=datetime(2018, 5, 20, 10, 0, 0),
+                            start=get_datetime_with_tz(2018, 5, 12, 10),
+                            end=get_datetime_with_tz(2018, 5, 20, 10),
                             is_seattle=True)
         notice.save()
 
@@ -37,16 +47,16 @@ class TestMyuwNotice(TestCase):
                             content="Notice Content Three",
                             notice_type="Banner",
                             notice_category="Student",
-                            start=datetime(2018, 6, 8, 10, 0, 0),
-                            end=datetime(2018, 6, 10, 10, 0, 0),
+                            start=get_datetime_with_tz(2018, 6, 8, 10),
+                            end=get_datetime_with_tz(2018, 6, 10, 10),
                             is_seattle=True)
         notice.save()
         notice = MyuwNotice(title="Alert",
                             content="Notice Content Four",
                             notice_type="Banner",
                             notice_category="Student",
-                            start=datetime(2018, 6, 8, 10, 0, 0),
-                            end=datetime(2018, 6, 20, 10, 0, 0),
+                            start=get_datetime_with_tz(2018, 6, 8, 10),
+                            end=get_datetime_with_tz(2018, 6, 20, 10),
                             is_bothell=True)
         notice.save()
         request = get_request_with_date("2018-06-09")
@@ -60,8 +70,8 @@ class TestMyuwNotice(TestCase):
                             content="Notice Content Four",
                             notice_type="Banner",
                             notice_category="Student",
-                            start=datetime(2018, 6, 8, 10, 0, 0),
-                            end=datetime(2018, 6, 20, 10, 0, 0))
+                            start=get_datetime_with_tz(2018, 6, 8, 10),
+                            end=get_datetime_with_tz(2018, 6, 20, 10))
         notice.save()
         request = get_request_with_date("2018-06-09")
         get_request_with_user('javerage', request)
@@ -74,52 +84,60 @@ class TestMyuwNotice(TestCase):
                             content="Notice Content Four",
                             notice_type="Banner",
                             notice_category="Student",
-                            start=datetime(2018, 6, 8, 10, 0, 0),
-                            end=datetime(2018, 6, 20, 10, 0, 0),
+                            start=get_datetime_with_tz(2018, 6, 8, 10),
+                            end=get_datetime_with_tz(2018, 6, 20, 10),
                             is_instructor=True)
         notice.save()
-        notice = MyuwNotice(title="Test",
+        notice = MyuwNotice(title="All Student",
                             content="Notice Content Five",
                             notice_type="Banner",
                             notice_category="Student",
-                            start=datetime(2018, 6, 8, 10, 0, 0),
-                            end=datetime(2018, 6, 20, 10, 0, 0),
+                            start=get_datetime_with_tz(2018, 6, 8, 10),
+                            end=get_datetime_with_tz(2018, 6, 20, 10),
                             is_student=True)
         notice.save()
-        notice = MyuwNotice(title="Test Intl",
+        notice = MyuwNotice(title="Seattle Intl",
                             content="Notice Content Five",
                             notice_type="Banner",
                             notice_category="Student",
-                            start=datetime(2018, 6, 8, 10, 0, 0),
-                            end=datetime(2018, 6, 20, 10, 0, 0),
+                            start=get_datetime_with_tz(2018, 6, 8, 10),
+                            end=get_datetime_with_tz(2018, 6, 20, 10),
+                            is_seattle=True,
                             is_intl_stud=True)
         notice.save()
         request = get_request_with_date("2018-06-09")
         get_request_with_user('javerage', request)
         notices = get_myuw_notices_for_user(request)
         self.assertEqual(len(notices), 1)
-        self.assertEqual(notices[0].title, "Test")
+        self.assertEqual(notices[0].title, "All Student")
 
         request = get_request_with_date("2018-06-09")
         get_request_with_user('jinter', request)
         notices = get_myuw_notices_for_user(request)
         self.assertEqual(len(notices), 2)
-        self.assertEqual(notices[1].title, "Test Intl")
+        self.assertEqual(notices[0].title, "All Student")
+        self.assertEqual(notices[1].title, "Seattle Intl")
+
+        request = get_request_with_date("2018-06-09")
+        get_request_with_user('jbothell', request)
+        notices = get_myuw_notices_for_user(request)
+        self.assertEqual(len(notices), 1)
+        self.assertEqual(notices[0].title, "All Student")
 
     def test_no_affil(self):
             notice = MyuwNotice(title="Alert",
                                 content="Notice Content Four",
                                 notice_type="Banner",
                                 notice_category="Student",
-                                start=datetime(2018, 6, 8, 10, 0, 0),
-                                end=datetime(2018, 6, 20, 10, 0, 0))
+                                start=get_datetime_with_tz(2018, 6, 8, 10),
+                                end=get_datetime_with_tz(2018, 6, 20, 10))
             notice.save()
             notice = MyuwNotice(title="Test",
                                 content="Notice Content Five",
                                 notice_type="Banner",
                                 notice_category="Student",
-                                start=datetime(2018, 6, 8, 10, 0, 0),
-                                end=datetime(2018, 6, 20, 10, 0, 0),
+                                start=get_datetime_with_tz(2018, 6, 8, 10),
+                                end=get_datetime_with_tz(2018, 6, 20, 10),
                                 is_student=True)
             notice.save()
             request = get_request_with_date("2018-06-09")
@@ -132,8 +150,8 @@ class TestMyuwNotice(TestCase):
                             content="Notice Content Five",
                             notice_type="Banner",
                             notice_category="MyUWNotice",
-                            start=datetime(2018, 6, 8, 10, 0, 0),
-                            end=datetime(2018, 6, 20, 10, 0, 0),
+                            start=get_datetime_with_tz(2018, 6, 8, 10),
+                            end=get_datetime_with_tz(2018, 6, 20, 10),
                             is_student=True)
         categorized = categorize_notices([notice])
         self.assertEqual(len(categorized), 1)
