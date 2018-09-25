4248179b95d53f19dc8a697b5b4c2e820dd95c46
Fang Lin <fanglin@uw.edu>
2018-09-24 17:13:50 -0700

    Resolve MUWM-4360

--- myuw/dao/registration.py
+++ myuw/dao/registration.py
@@ -32,8 +32,8 @@ def get_schedule_by_term(request, term):
         student_schedule = get_schedule_by_regid_and_term(
             regid,
             term,
-            non_time_schedule_instructors=non_tsp,
             per_section_prefetch_callback=myuw_section_prefetch,
+            include_instructor_not_on_time_schedule=non_tsp,
             transcriptable_course="all")
         set_course_display_pref(request, student_schedule)
         request.id = student_schedule
