0929c483925d0588a3bf6c8596a12545cc4ed0e7
Fang Lin <fanglin@uw.edu>
2018-10-18 11:49:22 -0700

    Add django_user_agents in TEMPLATES tag library

--- travis-ci/settings.py
+++ travis-ci/settings.py
@@ -17,6 +17,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django_user_agents.utils.django_user_agents',
             ],
         },
     },
