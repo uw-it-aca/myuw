e4cc665815bd735aa1d114b39a6bdfa175cdcd8b
Fang Lin <fanglin@uw.edu>
2018-11-12 12:56:52 -0800

    update Django version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
-        'Django>=2.0.8',
+        'Django>=2.0.8,<2.1',
         'ordereddict',
         'django-compressor==2.2',
         'beautifulsoup4',
