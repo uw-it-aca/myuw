8da4a27e666af9e3de1d61a655e010c8703ce543
Ethan Turner <ezturner@washington.edu>
2018-12-19 18:19:16 -0800

    Adding debugging

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -4,10 +4,11 @@ rm -rf /run/httpd/* /tmp/httpd*
 
 source "/app/bin/activate"
 export DATABASE_NAME=`echo $BRANCH | sed 's/-/_/g' `
+echo $DATABASE_NAME
 
 if [ "$DB" = "mysql" ] && [ "$ENV" = "dev" ]
 then
-  mysql -u $DATABASE_USERNAME -p$DATABASE_PASSWORD -h $DATABASE_HOSTNAME --execute="create database `echo $DATABASE_NAME | sed 's/-/_/g' ` "
+  mysql -u $DATABASE_USERNAME -p$DATABASE_PASSWORD -h $DATABASE_HOSTNAME --execute="create database $DATABASE_NAME "
 fi
 
 python3 manage.py migrate
