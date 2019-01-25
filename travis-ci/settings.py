ddea22cf43c2f0fae50bc0caf2b44263c32c35f4
Fang Lin <fanglin@uw.edu>
2019-01-24 17:39:01 -0800

    compress HTML with django-htmlmin

--- travis-ci/settings.py
+++ travis-ci/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.gzip.GZipMiddleware',
+    'htmlmin.middleware.HtmlMinifyMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
