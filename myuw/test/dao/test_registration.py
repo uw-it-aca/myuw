82d52da632721bb1f6928f435948978692bcd95d
Stephen De Vight <devights@gmail.com>
2019-03-25 13:09:31 -0700

    fixing test

--- myuw/test/dao/test_registration.py
+++ myuw/test/dao/test_registration.py
@@ -65,11 +65,4 @@ class TestRegistrationsDao(TestCase):
                          "2014,winter,PHYS,122/A")
         self.assertEqual(len(schedule.sections[2].meetings), 2)
         self.assertEqual(len(schedule.sections[2].meetings[0].instructors), 1)
-        instructor = schedule.sections[2].meetings[0].instructors[1]
-        self.assertEqual(instructor.display_name, u'BOTHELL GRADUATE STUDENT')
-
-        instructor = schedule.sections[2].meetings[0].instructors[2]
-        self.assertEqual(instructor.display_name, u'J. Average Student')
-
-        instructor = schedule.sections[2].meetings[0].instructors[3]
-        self.assertEqual(len(schedule.sections[2].meetings[1].instructors), 4)
+        self.assertEqual(len(schedule.sections[2].meetings[1].instructors), 1)
