479923aaeca6687837b7916d69f1f0894c4f6450
Fang Lin <fanglin@uw.edu>
2019-09-10 10:56:53 -0700

    Resolve MUWM-3989

--- myuw/views/api/base_schedule.py
+++ myuw/views/api/base_schedule.py
@@ -117,7 +117,7 @@ def load_schedule(request, schedule, summer_term=""):
             if len(pce_sections) > 0 and\
                     section.section_label() in pce_sections:
                 pce_course = pce_sections.get(section.section_label())
-                section_data["on_standby"] = pce_course.standby()
+                section_data["on_standby"] = pce_course.is_standby_status()
                 group_independent_start = irregular_start_end(
                     schedule.term, pce_course, section.summer_term)
                 if group_independent_start:
