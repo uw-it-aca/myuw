afaad001ecff4ec879f647db0613d3d4d0be3f62
Ethan Turner <ezturner@uw.edu>
2019-12-05 16:07:31 -0800

    Fixing pep8 errors

--- myuw/context_processors.py
+++ myuw/context_processors.py
@@ -1,7 +1,9 @@
 # Determins if the requesting device is a native hybrid app (android/ios)
 
+
 def is_hybrid(request):
     
     return {
-        'is_hybrid': 'HTTP_MYUW_HYBRID' in request.META  
+        'is_hybrid': 'HTTP_MYUW_HYBRID' in request.META
     }
+
