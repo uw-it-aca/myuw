0bd09a2296e3cdac2c6ccf7264cf900e2d74db04
Fang Lin <fanglin@uw.edu>
2020-01-06 16:35:34 -0800

    use bulk delete

--- myuw/management/commands/clear_expired_sessions.py
+++ myuw/management/commands/clear_expired_sessions.py
@@ -3,9 +3,9 @@ The django clearsessions commend internally calls:
    cls.get_model_class().objects.filter(
        expire_date__lt=timezone.now()).delete()
 which could lock the DB table for a long time when
- having a large number of records to delete.
+having a large number of records to delete.
 
-To prevent the job running forever, we only delete 40000
+To prevent the job running forever, we only delete a limit number of
 expired django sessions in a single run
 """
 
@@ -15,17 +15,16 @@ from django.contrib.sessions.models import Session
 from django.utils import timezone
 
 logger = logging.getLogger(__name__)
-DEL_SIZE = 40000
+DEL_SIZE = 10000
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        i = 0
-        for record in Session.objects.filter(expire_date__lt=timezone.now()):
-            i += 1
-            if i <= DEL_SIZE:
-                try:
-                    record.delete()
-                except Exception as ex:
-                    logger.error("{} {}".format(record, str(ex)))
+        try:
+            qs = Session.objects.filter(
+                expire_date__lt=timezone.now())[0:DEL_SIZE]
+            if qs.exists():
+                qs.delete()
+        except Exception as ex:
+            logger.error(str(ex))
--- myuw/test/management/commands/test_clear_expired_sessions.py
+++ myuw/test/management/commands/test_clear_expired_sessions.py
@@ -1,11 +1,8 @@
 from django.test import TestCase
 from django.core.management import call_command
-from django.core.management.base import CommandError
+
 
 class TestClearSessions(TestCase):
 
     def test_run(self):
-        try:
-            call_command('clear_expired_sessions')
-        except CommandError as err:
-            self.assertFalse(str(err))
+        call_command('clear_expired_sessions')
