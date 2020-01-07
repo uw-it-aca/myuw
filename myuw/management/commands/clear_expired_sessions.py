80cb3c83fcada35c44bca84276d66afaff94da50
Fang Lin <fanglin@uw.edu>
2020-01-06 16:55:30 -0800

    log the deletion time

--- myuw/management/commands/clear_expired_sessions.py
+++ myuw/management/commands/clear_expired_sessions.py
@@ -13,6 +13,7 @@ import logging
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.sessions.models import Session
 from django.utils import timezone
+from myuw.logger.timer import Timer
 
 logger = logging.getLogger(__name__)
 DEL_SIZE = 10000
@@ -22,9 +23,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
+            timer = Timer()
             qs = Session.objects.filter(
                 expire_date__lt=timezone.now())[0:DEL_SIZE]
             if qs.exists():
                 qs.delete()
+            logger.info(
+                "clear_expired_sessions, Time={} seconds".format(
+                    timer.get_elapsed()))
         except Exception as ex:
             logger.error(str(ex))
