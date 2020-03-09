9bb2dcb454789e9834db4fc0c579a8514210872b
Fang Lin <fanglin@uw.edu>
2020-03-09 13:47:38 -0700

    pin lxml at 4.2.6

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
         'ua-parser',
         'user-agents',
         'django-user-agents',
-        'lxml==4.4.2'
+        'lxml==4.2.6'
     ],
     license='Apache License, Version 2.0',
     description=('MyUW is the portal of the University of Washington'),
