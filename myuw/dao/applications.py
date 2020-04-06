83061a9f11cb8be02054247e6bd3fe1d008a0366
Fang Lin <fanglin@uw.edu>
2020-04-06 16:34:06 -0700

    Resolve fix/MUWM-4361

--- myuw/dao/applications.py
+++ myuw/dao/applications.py
@@ -1,11 +1,18 @@
+import logging
 from uw_sdbmyuw import get_app_status
+from myuw.dao import get_userids
 from myuw.dao.pws import get_student_system_key_of_current_user
+
+logger = logging.getLogger(__name__)
 
 
 def get_applications(request):
-    applications = get_app_status(
-        get_student_system_key_of_current_user(request))
     response = []
-    for application in applications:
-        response.append(application.json_data())
+    system_key = get_student_system_key_of_current_user(request)
+    if system_key is not None:
+        applications = get_app_status(system_key)
+        for application in applications:
+            response.append(application.json_data())
+    else:
+        logger.error("Missing system key. {}".format(get_userids()))
     return response
