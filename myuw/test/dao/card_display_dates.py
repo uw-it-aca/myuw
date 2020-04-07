b54fcfe218a77e4efa98f81c81c299c3628cfa79
Fang Lin <fanglin@uw.edu>
2020-04-06 18:27:44 -0700

    Resolve MUWM-3430

--- myuw/test/dao/card_display_dates.py
+++ myuw/test/dao/card_display_dates.py
@@ -18,6 +18,11 @@ class TestDisplayValues(TestCase):
     def setUp(self):
         get_request()
 
+    def get_last_term_is_none(self, date):
+        now_request = get_request_with_date("2012-01-01")
+        values = get_card_visibilty_date_values(now_request)
+        self.assertIsNone(values.get("last_term"))
+
     def get_visibility_for_date(self, date):
         now_request = get_request_with_date(date)
         values = get_card_visibilty_date_values(now_request)
