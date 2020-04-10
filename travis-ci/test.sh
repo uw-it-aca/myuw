d406d5701144f633007e7461f632355714e9f842
mike seibel <mikes@washington.edu>
2020-04-10 12:19:21 -0700

    container tests: ruby

--- travis-ci/test.sh
+++ travis-ci/test.sh
@@ -12,7 +12,7 @@ source bin/activate
 
 # install test tooling
 pip install pycodestyle coverage
-apt-get install -y nodejs npm
+apt-get install -y nodejs npm ruby-full
 npm install -g jshint
 npm install -g jquery
 npm install -g moment
