5a8ecfceb4df212d2f19932a42c589e5f3413bbb
mike seibel <mikes@washington.edu>
2020-05-20 13:03:51 -0700

    fixup urls, dev pod resource/scaling

--- docker/urls.py
+++ docker/urls.py
@@ -1,10 +1,10 @@
+from .base_urls import *
 from django.urls import include, re_path
 
 
-urlpatterns = [
-    re_path(r'^saml/', include('uw_saml.urls')),
+urlpatterns += [
     re_path(r'^support', include('userservice.urls')),
-    re_path(r'^logging', include('django_client_logger.urls')),
-    re_path(r'^logging', include('rc_django.urls')),
+    re_path(r'^restclients/', include('rc_django.urls')),
+    re_path(r'^logging/', include('django_client_logger.urls')),
     re_path(r'^', include('myuw.urls')),
 ]
