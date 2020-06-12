c37c30e6ef2e9ef6e28f4f0512fecee94c6586dd
Fang Lin <fanglin@uw.edu>
2020-06-11 17:43:54 -0700

    use optional arg

--- myuw/management/commands/memcache.py
+++ myuw/management/commands/memcache.py
@@ -7,12 +7,13 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('action', type=str,
-                            help="param1: flush|stats (default)")
+        parser.add_argument("-f", "--flush",
+                            action="store_true", default=False,
+                            help="Flush the cache")
 
     def handle(self, *args, **options):
-        action = options['action']
+        flush = options["flush"]
         client = MyUWMemcachedCache().client
         logger.info("Stats {}".format(client.stats()))
-        if action == "flush":
-            logger.info("Flush all successful: {}".format(client.flush_all()))
+        if flush:
+            logger.info("Flushed: {}".format(client.flush_all()))
