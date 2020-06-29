ef8bfd33392baf1d65bf76ef6635420e065553cb
Fang Lin <fanglin@uw.edu>
2020-06-29 16:31:21 -0700

    enable specify scope

--- myuw/management/commands/del_user_session.py
+++ myuw/management/commands/del_user_session.py
@@ -6,7 +6,10 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('netid', type=str, help="param1: uwnetid")
+        parser.add_argument('scope', type=str, help="param2: idtoken|all",
+                            default=SCOPE_IDTOKEN)
 
     def handle(self, *args, **options):
         netid = options['netid']
-        delete_sessions(netid, SCOPE_IDTOKEN)
+        scope = options['scope']
+        delete_sessions(netid, scope)
