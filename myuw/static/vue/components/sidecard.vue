1318cbc36bed8a11b83549e02d0c4ff328a6b50c
Sarthak Singh <ss269@uw.edu>
2020-08-03 09:21:41 -0700

    Showing off some of the vue features

---
+++ myuw/static/vue/components/sidecard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="side card">
+    <h3>{{ message }}</h3>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'I am a sidecard',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+$coolColor: lime;
+
+.hello {
+  margin-bottom: 1.5rem;
+}
+</style>
--- myuw/static/vue/entry/home.js
+++ myuw/static/vue/entry/home.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Home from '../pages/home.vue'
+import SideCard from '../components/sidecard.vue'
 
 Vue.component('home-page-component', Home)
+Vue.component('side-card-component', SideCard)
 
 new Vue({ el: '#home' })
