b3f7b132702f06fe2e7b9d8556da44158426103c
Sarthak Singh <ss269@uw.edu>
2020-08-07 11:27:52 -0700

    Hollowed out he app

--- myuw/static/js/error410.js
+++
@@ -1,9 +0,0 @@
-var Error410 = {
-
-    render: function() {
-        var source = $("#future_410_error").html();
-        var template = Handlebars.compile(source);
-        var raw = template({});
-        $("#main-content").html(raw);
-    }
-};
