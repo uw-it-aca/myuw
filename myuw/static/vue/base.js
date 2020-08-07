6070056b7580b07437614005d569262afb118586
Sarthak Singh <ss269@uw.edu>
2020-08-07 15:33:12 -0700

    Integrated bootstrap4 and bootstrap vue into the pipeline

---
+++ myuw/static/vue/base.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import '../css/bootstrap-theming.scss'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+export default Vue;
--- webpack.config.js
+++ webpack.config.js
@@ -26,28 +26,9 @@ module.exports = {
     ],
   },
   entry: {
-      base: [
-        "./myuw/static/vendor/css/bootstrap-3.3.5.min.css",
-        "./myuw/static/vendor/css/font-awesome-4.7.0.min.css",
-        "./myuw/static/vendor/css/user-fonts.css",
-        "./myuw/static/css/mobile.less",
-        "./myuw/static/css/desktop.less",
-        "./myuw/static/css/typography.less",
-        "./myuw/static/css/buttons.less",
-        "./myuw/static/css/course_card.less",
-        "./myuw/static/css/boilerplate.less",
-        "./myuw/static/css/tabs.less",
-        "./myuw/static/css/dropmenus.less",
-        "./myuw/static/css/ratings.less",
-        "./myuw/static/css/cards.less",
-        "./myuw/static/css/components.less",
-        "./myuw/static/css/photo_list.less",
-        "./myuw/static/css/textbooks.less",
-        "./myuw/static/css/huskyx.less",
-        "./myuw/static/css/resources.less",
-        "./myuw/static/css/notices.less",
+      index: [
+        "./myuw/static/vue/index.js"
       ],
-      base_hybrid: "./myuw/static/css/hybrid.less",
   },
   output: {
       path: path.resolve('../static/myuw/'),
