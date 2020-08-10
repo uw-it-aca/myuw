f2c8cefd184a53b8b3a423218c3101b35f69d8f3
Charlon Palacay <charlon@gmail.com>
2020-08-10 13:45:36 -0700

    Fixes header container.

--- myuw/static/vue/containers/boilerplate.vue
+++ myuw/static/vue/containers/boilerplate.vue
@@ -1,62 +1,58 @@
 <template>
   <div>
-   
-   <!--<myuw-header :disable_actions="disable_actions" :logout_url="logout_url" />-->
+  
+    <header>
+      <div id="actions_disabled_banner" v-if="disable_actions">
+        <strong>YOU ARE CURRENTLY OVERRIDING AS ANOTHER USER</strong>. Overriding
+        is read-only and no actions will be saved. &nbsp;&nbsp;
+        <a href="/support" style="font-weight: normal; color: #003399;">
+          Back to MyUW Support tool
+        </a>
+      </div>
 
-    <div>
-    <div id="actions_disabled_banner" v-if="disable_actions">
-      <strong>YOU ARE CURRENTLY OVERRIDING AS ANOTHER USER</strong>. Overriding
-      is read-only and no actions will be saved. &nbsp;&nbsp;
-      <a href="/support" style="font-weight: normal; color: #003399;">
-        Back to MyUW Support tool
-      </a>
-    </div>
+      <b-collapse id="app_search">
+        this is search
+      </b-collapse>
 
-    <b-collapse id="app_search">
-      this is search
-    </b-collapse>
+      <div class="myuw-thin-bar">
+        <b-navbar class="p-0" type="dark">
+          <b-navbar-nav>
+            <b-nav-item href="/profile/">
+              <b-icon icon="person-fill"></b-icon> {{ user.netid }}
+            </b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item
+              v-if="user.email_error"
+              href="https://itconnect.uw.edu/connect/email/"
+              title="UW email services"
+            >
+              <b-icon icon="exclamation-triangle"></b-icon> Email error
+            </b-nav-item>
+            <b-nav-item
+              v-else
+              :href="user.email_forward_url"
+              title="Open your email in new tab"
+            >
+              <b-icon icon="envelope-fill"></b-icon> Email
+            </b-nav-item>
+            <b-nav-text v-b-toggle.app_search aria-label="Open search area">
+              <b-icon icon="search"></b-icon> Search
+            </b-nav-text>
+            <b-nav-item :href="logout_url">
+              <b-icon icon="box-arrow-right"></b-icon> Sign Out
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-navbar>
+      </div>
 
-    <div class="myuw-thin-bar">
-      <b-navbar class="p-0" type="dark">
-        <b-navbar-nav>
-          <b-nav-item href="/profile/">
-            <b-icon icon="person-fill"></b-icon> {{ user.netid }}
-          </b-nav-item>
-        </b-navbar-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item
-            v-if="user.email_error"
-            href="https://itconnect.uw.edu/connect/email/"
-            title="UW email services"
-          >
-            <b-icon icon="exclamation-triangle"></b-icon> Email error
-          </b-nav-item>
-          <b-nav-item
-            v-else
-            :href="user.email_forward_url"
-            title="Open your email in new tab"
-          >
-            <b-icon icon="envelope-fill"></b-icon> Email
-          </b-nav-item>
-          <b-nav-text v-b-toggle.app_search aria-label="Open search area">
-            <b-icon icon="search"></b-icon> Search
-          </b-nav-text>
-          <b-nav-item :href="logout_url">
-            <b-icon icon="box-arrow-right"></b-icon> Sign Out
-          </b-nav-item>
-        </b-navbar-nav>
-      </b-navbar>
-    </div>
-
-    <div class="myuw-navigation">
-      <b-navbar toggleable="lg" type="dark">
-        <b-navbar-brand href="#"><b-navbar-toggle target="nav-collapse" size="sm"></b-navbar-toggle> MyUW</b-navbar-brand>
-      </b-navbar>
-    </div>
-
-    
-    
-  </div>
+      <div class="myuw-navigation">
+        <b-navbar toggleable="lg" type="dark">
+          <b-navbar-brand href="#"><b-navbar-toggle target="nav-collapse" size="sm"></b-navbar-toggle> MyUW</b-navbar-brand>
+        </b-navbar>
+      </div>
+      
+    </header>
 
     <b-collapse visible id="nav-collapse" is-nav>
       <ul>
