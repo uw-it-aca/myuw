ffd407b49e775716adbfd31fe92c101f596dfbb6
Sarthak Singh <ss269@uw.edu>
2020-08-18 15:48:17 -0700

    Added the applicant cards

---
+++ myuw/static/vue/components/index/cards/applicant/bothell.vue
@@ -0,0 +1,94 @@
+<template>
+  <uw-card :loaded="isReady">
+    <template v-if="applicantData.is_returning" #card-heading>
+      <h3>Your Returning Student Application</h3>
+    </template>
+    <template v-else #card-heading>
+      <h3>
+        Your Bothell Application for
+        {{ ucfirst(applicantData.quarter) }} {{ applicantData.year }}
+      </h3>
+    </template>
+    <template v-if="applicantData.is_returning" #card-body>
+      <h4>
+        For application status, contact the UW Bothell Office of Admissions
+      </h4>
+      <b-container>
+        <b-row>
+          <b-col>Email</b-col>
+          <b-col><a href="mailto:uwbifo@uw.edu">uwbifo@uw.edu</a></b-col>
+        </b-row>
+        <b-row>
+          <b-col>Phone</b-col>
+          <b-col>(425) 352-5000</b-col>
+        </b-row>
+        <b-row>
+          <b-col>Mail</b-col>
+          <b-col>10909 NE 185th St <br>Bothell, WA 98011<br>Box 358500</b-col>
+        </b-row>
+      </b-container>
+      <h4> Resources for Bothell Applicants</h4>
+      <ul>
+        <li>
+          <a href="http://www.uwb.edu/financialaid"
+             target="_blank" aria-label="Student Financial Aid"
+          >
+            Learn all about student financial aid and scholarships
+          </a>
+        </li>
+        <li>
+          <a href="http://www.uwb.edu/studentaffairs"
+             target="_blank" aria-label="Student Resources"
+          >
+            Check out the student resources for student life,
+            career services, and more
+          </a>
+        </li>
+        <li>
+          <a href="http://www.uwb.edu/calendars"
+             target="_blank" aria-label="UW Bothell Academic calendars"
+          >
+            View the UW Bothell Academic calendars
+          </a>
+        </li>
+      </ul>
+    </template>
+    <template v-else #card-body>
+      <a href="https://admissions.uwb.edu/apply/status"
+         aria-label="Your application status details"
+      >
+        View your {{ applicantData.type }} application status and checklist
+      </a>
+      <p>
+        If you have questions regarding your application,
+        please contact UW Bothell Office of Admissions at
+        <a href="mailto:uwbinfo@uw.edu">uwbinfo@uw.edu</a>
+      </p>
+    </template>
+  </uw-card>
+</template>
+
+<script>
+import Card from '../../../../containers/card.vue';
+
+export default {
+  components: {
+    'uw-card': Card,
+  },
+  props: {
+    applicantData: {
+      type: [Object, Boolean],
+      default: false,
+    },
+    isReady: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    ucfirst: (s) => s.replace(/^([a-z])/, (c) => c.toUpperCase()),
+  },
+};
+</script>
+
+<style scoped></style>
--- myuw/static/vue/index.js
+++ myuw/static/vue/index.js
@@ -6,14 +6,17 @@ import Notices from './components/index/cards/notices.vue';
 import InterStudent from './components/index/cards/international/student.vue';
 import NewInterStudent from 
   './components/index/cards/international/new_student.vue';
+import Applicant from './components/index/cards/applicant/applicant.vue';
 
 import notices from './store/notices';
 import hfs from './store/hfs';
 import library from './store/library';
+import applicant from './store/applicant';
 
 vueConf.store.registerModule('notices', notices);
 vueConf.store.registerModule('hfs', hfs);
 vueConf.store.registerModule('library', library);
+vueConf.store.registerModule('applicant', applicant);
 
 vueConf.store.state['termData'] = window.term_data;
 vueConf.store.state['pageTitle'] = 'Home';
@@ -23,6 +26,7 @@ Vue.component('myuw-banner-summaries', Summaries);
 Vue.component('myuw-notice-card', Notices);
 Vue.component('myuw-international-student', InterStudent);
 Vue.component('myuw-new-international-student', NewInterStudent);
+Vue.component('myuw-applicant', Applicant);
 
 new Vue({
   ...vueConf,
