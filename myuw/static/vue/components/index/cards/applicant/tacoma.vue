95dec9a70d7a253953bfd494c4a70e3ae0a0721d
Sarthak Singh <ss269@uw.edu>
2020-08-18 15:48:17 -0700

    Added the applicant cards

---
+++ myuw/static/vue/components/index/cards/applicant/tacoma.vue
@@ -0,0 +1,189 @@
+<template>
+  <uw-card :loaded="isReady">
+    <template v-if="applicantData.is_returning" #card-heading>
+      <h3>Your Returning Student Application</h3>
+    </template>
+    <template v-else #card-heading>
+      <h3>
+        Your Tacoma Application for
+        {{ ucfirst(applicantData.quarter) }} {{ applicantData.year }}
+      </h3>
+    </template>
+    <template v-if="applicantData.is_returning" #card-body>
+      <h4>
+        For application status, contact the UW Tacoma Office of Registrar
+      </h4>
+      <b-container>
+        <b-row>
+          <b-col>Email</b-col>
+          <b-col><a href="mailto:reguwt@uw.edu">reguwt@uw.edu</a></b-col>
+        </b-row>
+        <b-row>
+          <b-col>Phone</b-col>
+          <b-col>(253) 692-4913</b-col>
+        </b-row>
+        <b-row>
+          <b-col>Fax</b-col>
+          <b-col>(253) 692-4414</b-col>
+        </b-row>
+        <b-row>
+          <b-col>Location</b-col>
+          <b-col>MAT 253</b-col>
+        </b-row>
+        <b-row>
+          <b-col>Mail</b-col>
+          <b-col>
+            University of Washington Tacoma
+            <br>1900 Commerce Street
+            <br>Tacoma, WA 98402-3100
+          </b-col>
+        </b-row>
+      </b-container>
+      <h4>Resources for Tacoma Applicants</h4>
+      <ul>
+        <li>
+          <a href="http://www.tacoma.uw.edu/node/36610"
+             target="_blank" aria-label="Financial Aid"
+          >
+            Learn all about student financial aid
+          </a>
+        </li>
+        <li>
+          <a href="http://www.tacoma.uw.edu/node/41373" target="_blank"
+             aria-label="Financial aid key dates and deadlines"
+          >
+            Financial aid key dates and deadlines
+          </a>
+        </li>
+        <li>
+          <a href="http://www.tacoma.uw.edu/node/41353"
+             target="_blank" aria-label="UW Tacoma Academic calendars"
+          >
+            View the UW Tacoma Academic calendars
+          </a>
+        </li>
+      </ul>
+    </template>
+    <template v-else #card-body>
+      <a href="https://sdb.admin.uw.edu/admissions/uwnetid/appstatus.asp"
+         aria-label="Your application status details"
+      >
+        View your {{ applicantData.type }} application status
+      </a>
+      <h4>Resources for Tacoma Applicants</h4>
+      <h5>ADMISSIONS</h5>
+      <ul>
+        <li>
+          <a
+            href="http://www.tacoma.uw.edu/admissions/important-dates-deadlines-all-applicants"
+            target="_blank"
+            aria-label="UW Tacoma Admission: Important dates &amp; deadlines"
+          >
+            Important dates &amp; deadlines
+          </a>
+        </li>
+      </ul>
+      <h5>FINANCES</h5>
+      <ul>
+        <li>
+          <a href="http://www.tacoma.uw.edu/node/20240"
+             target="_blank" aria-label="Applying for a financial Aid"
+          >
+            Applying for a financial Aid
+          </a>
+        </li>
+        <li>
+          <a href="http://www.tacoma.uw.edu/node/41373"
+             target="_blank"
+             aria-label="Financial aid key dates and deadlines"
+          >
+            Financial aid key dates and deadlines
+          </a>
+        </li>
+      </ul>
+      <h5>STUDENT LIFE</h5>
+      <ul>
+        <li>
+          <a href="http://www.tacoma.uw.edu/admissions/visit"
+             target="_blank" aria-label="Tacoma Campus Tours"
+          >
+            Plan your visit: Tacoma campus tours
+          </a>
+        </li>
+        <li>
+          <a href="http://www.tacoma.uw.edu/node/38753"
+             target="_blank" aria-label="Student Housing"
+          >
+            Learn about student Housing
+          </a>
+        </li>
+        <li>
+          Check out
+          <a
+            href="http://www.tacoma.uw.edu/new-student-family-programs/new-student-family-programs"
+            target="_blank" aria-label="New Student programs"
+          >
+            New Student programs
+          </a>
+          and
+          <a href="http://www.tacoma.uw.edu/drsuwt"
+             target="_blank"
+             aria-label="Disability Resources for Students (DRS)"
+          >
+            Disability Resources for Students (DRS)
+          </a>
+        </li>
+      </ul>
+      <h5>IF ADMITTED</h5>
+      <ul>
+        <li>
+          <a href="http://www.tacoma.uw.edu/admissions/i-was-admitted-now-what"
+             target="_blank" aria-label="Next Steps for Admitted Students"
+          >
+            Next steps for Admitted students
+          </a>
+        </li>
+        <li>
+          <a href="http://www.tacoma.uw.edu/node/41496"
+             target="_blank" aria-label="New Student Orientation"
+          >
+            Sign up for a New student orientation
+          </a>
+        </li>
+        <li v-if="applicantData.is_international">
+          <a href="http://www.tacoma.uw.edu/iss/to-do-list"
+             target="_blank" aria-label="Int’l student to-do list"
+          >
+            View the Int’l student to-do list from International Student
+            and Scholar Services (ISSS)
+          </a>
+        </li>
+      </ul>
+    </template>
+  </uw-card>
+</template>
+
+<script>
+import Card from '../../../../containers/card.vue';
+
+export default {
+  components: {
+    'uw-card': Card,
+  },
+  props: {
+    applicantData: {
+      type: [Object, Boolean],
+      default: false,
+    },
+    isReady: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    ucfirst: (s) => s.replace(/^([a-z])/, (c) => c.toUpperCase()),
+  },
+};
+</script>
+
+<style scoped></style>
