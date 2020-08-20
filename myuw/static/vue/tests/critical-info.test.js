c0cad83e28b45fe6d1a09763bb1670c2e1badeb8
Sarthak Singh <ss269@uw.edu>
2020-08-20 15:55:40 -0700

    Added tests for Critical Info Card

---
+++ myuw/static/vue/tests/critical-info.test.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import {mount, shallowMount, createLocalVue} from '@vue/test-utils';
+import BootstrapVue from 'bootstrap-vue';
+import Vuex from 'vuex';
+import notices from '../store/notices';
+import CriticalInfoCard from '../components/index/cards/critical-info.vue';
+
+import mockNotices from './mock_data/notices.json';
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
+localVue.use(Vuex);
+
+jest.mock('axios');
+
+describe('Critical Info Card', () => {
+  let store;
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      modules: {
+        notices,
+      },
+      state: {
+        user: {
+          affiliations: {
+            student: true,
+          }
+        }
+      }
+    });
+  });
+
+  it('Check the filter function - default', async () => {
+    axios.get.mockResolvedValue({data: mockNotices});
+    const wrapper = mount(CriticalInfoCard, {store, localVue});
+    // It takes like 10 ms to process the mock data through fetch postProcess
+    await new Promise((r) => setTimeout(r, 10));
+    expect(wrapper.vm.isReady).toBeTruthy();
+
+    expect(wrapper.vm.notices).toHaveLength(1);
+    expect(wrapper.vm.isResident).toBeTruthy();
+    expect(wrapper.vm.hasRegisterNotices).toBeTruthy();
+  });
+
+  it('Check the filter function - not resident', async () => {
+    mockNotices[20].attributes[0].value = 3;
+    axios.get.mockResolvedValue({data: mockNotices});
+    const wrapper = mount(CriticalInfoCard, {store, localVue});
+    // It takes like 10 ms to process the mock data through fetch postProcess
+    await new Promise((r) => setTimeout(r, 10));
+    expect(wrapper.vm.isReady).toBeTruthy();
+
+    expect(wrapper.vm.notices).toHaveLength(1);
+    expect(wrapper.vm.isResident).toBeFalsy();
+    expect(wrapper.vm.hasRegisterNotices).toBeTruthy();
+  });
+});
