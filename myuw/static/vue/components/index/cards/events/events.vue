fbba4d6ad46647783fcf2c4001a1d92ba77b90a7
Sarthak Singh <ss269@uw.edu>
2020-08-25 09:32:07 -0700

    More fixes to events card

--- myuw/static/vue/components/index/cards/events/events.vue
+++ myuw/static/vue/components/index/cards/events/events.vue
@@ -1,5 +1,5 @@
 <template>
-  <uw-card v-if="(shownEvents && shownEvents.length > 0) || futureCalCount > 0"
+  <uw-card v-if="(shownEvents.length > 0) || futureCalCount > 0"
            :loaded="isReady" :errored="isErrored"
   >
     <template #card-heading>
@@ -7,7 +7,7 @@
         Events
       </h3>
     </template>
-    <template v-if="shownEvents && shownEvents.length > 0" #card-body>
+    <template v-if="shownEvents.length > 0" #card-body>
       <div>Showing events in the next 14 days.</div>
       <uw-list-events :events="shownEvents" />
     </template>
@@ -19,7 +19,7 @@
         from {{ futureCalLinks.length }} calendars in the next 30 days.
         <ul>
           <li v-for="(event, i) in futureCalLinks" :key="i">
-            <a :href="event.url">{{ event.title }}</a>
+            <a :href="event.base_url">{{ event.title }}</a>
           </li>
         </ul>
       </div>
@@ -29,20 +29,20 @@
         {{ futureCalCount > 1 ? 'events' : 'event' }}
         from
         <span v-for="(event, i) in futureCalLinks" :key="i">
-          <a :href="event.url">{{ event.title }}</a>
+          <a :href="event.base_url">{{ event.title }}</a>
         </span>
         in the next 30 days.
       </div>
     </template>
     <!-- v-if condition is common with #card-footer -->
-    <template v-if="hiddenEvents && hiddenEvents.length > 0" #card-disclosure>
+    <template v-if="hiddenEvents.length > 0" #card-disclosure>
       <b-collapse id="hidden_events_collapse" v-model="isOpen">
         <uw-list-events :events="hiddenEvents" />
         <div v-if="calLinks.length > 1">
           See all events from:
           <ul>
             <li v-for="(event, i) in calLinks" :key="i">
-              <a :href="event.url">{{ event.title }}</a>
+              <a :href="event.base_url">{{ event.title }}</a>
             </li>
           </ul>
         </div>
@@ -54,7 +54,7 @@
       </b-collapse>
     </template>
     <!-- Open disclosure -->
-    <template v-else #card-disclosure>
+    <template v-else-if="calLinks.length > 0" #card-disclosure>
       <div v-if="calLinks.length > 1">
         See all events from:
         <ul>
@@ -69,7 +69,7 @@
         </a> calendar.
       </div>
     </template>
-    <template v-if="hiddenEvents && hiddenEvents.length > 0" #card-footer>
+    <template v-if="hiddenEvents.length > 0" #card-footer>
       <button v-if="!isOpen" v-b-toggle.hidden_events_collapse
               :aria-label="`Show ${hiddenEvents.length} more ${
                 hiddenEvents.length > 1 ? 'events' : 'event'}`"
@@ -102,11 +102,11 @@ export default {
   },
   computed: {
     ...mapState({
-      shownEvents: (state) => state.events.value.shownEvents,
-      hiddenEvents: (state) => state.events.value.hiddenEvents,
+      shownEvents: (state) => state.events.value.shownEvents || [],
+      hiddenEvents: (state) => state.events.value.hiddenEvents || [],
       futureCalCount: (state) => state.events.value.futureCalCount,
-      futureCalLinks: (state) => state.events.value.futureCalLinks,
-      calLinks: (state) => state.events.value.calLinks,
+      futureCalLinks: (state) => state.events.value.futureCalLinks || [],
+      calLinks: (state) => state.events.value.calLinks || [],
     }),
     ...mapGetters('events', {
       isReady: 'isReady',
