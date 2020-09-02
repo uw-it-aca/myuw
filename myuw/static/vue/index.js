77b85811c01f55ceaba8c22f25ec21a4c1e3d340
Sarthak Singh <ss269@uw.edu>
2020-09-02 13:03:22 -0700

    Layed the foundation for the reg status card

--- myuw/static/vue/index.js
+++ myuw/static/vue/index.js
@@ -34,6 +34,7 @@ import VisualSchedule from './components/cards/schedule/visual-schedule.vue';
 import HuskyExperience from './components/cards/husky-experience.vue'
 import Outage from './components/cards/outage.vue'
 import Quicklinks from './components/index/cards/quicklinks/quicklinks.vue'
+import RegStatus from './components/index/cards/reg_status/status.vue';
 
 // Stores
 import notices from './store/notices';
@@ -45,6 +46,8 @@ import events from './store/events';
 import courses from './store/courses';
 import schedule from './store/schedule';
 import hx_toolkit from './store/hx_toolkit';
+import oquarter from './store/oquarter';
+import profile from './store/profile';
 
 vueConf.store.registerModule('notices', notices);
 vueConf.store.registerModule('hfs', hfs);
@@ -55,6 +58,9 @@ vueConf.store.registerModule('courses', courses);
 vueConf.store.registerModule('schedule', schedule);
 vueConf.store.registerModule('hx_toolkit', hx_toolkit);
 vueConf.store.registerModule('quicklinks', quicklinks);
+vueConf.store.registerModule('oquarter', oquarter);
+vueConf.store.registerModule('profile', profile);
+
 
 vueConf.store.commit('addVarToState', {
   name: 'termData',
@@ -99,6 +105,7 @@ Vue.component('myuw-visual-schedule', VisualSchedule);
 Vue.component('myuw-husky-experience', HuskyExperience);
 Vue.component('myuw-outage', Outage);
 Vue.component('myuw-quicklinks', Quicklinks);
+Vue.component('myuw-reg-status', RegStatus);
 
 new Vue({
   ...vueConf,
