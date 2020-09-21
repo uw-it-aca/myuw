1348a03bf4c506261ff1980647adf8d989a78e30
Sarthak Singh <ss269@uw.edu>
2020-09-21 08:56:00 -0700

    finaid complete

--- myuw/static/vue/components/index/cards/registration/finaid.vue
+++ myuw/static/vue/components/index/cards/registration/finaid.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>finaid</div>
+  <div>
+    <h4>Financial Aid</h4>
+    <ul>
+      <li v-for="(notice, i) in finAidNotices" :key="i">
+        <button
+          v-b-toggle="`finAid-${notice.id_hash}-collapse`"
+        >
+          {{notice.short_content}}
+        </button>
+        <b-collapse
+          :id="`finAid-${notice.id_hash}-collapse`"
+          v-model="collapseOpen"
+          v-html="notice.notice_content"
+        />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    finAidNotices: {
+      type: Array,
+      required: true,
+    }
+  },
+  data: function() {
+    return {
+      collapseOpen: true,
+    }
+  },
 }
 </script>
 
