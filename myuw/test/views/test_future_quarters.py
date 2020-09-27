214c56478cbf9c5a5b9f0762f8b5f1e00ea6b136
Fang Lin <fanglin@uw.edu>
2020-09-26 21:53:39 -0700

    add test of future_quarters

---
+++ myuw/test/views/test_future_quarters.py
@@ -0,0 +1,22 @@
+from unittest import skipIf
+from django.urls import reverse
+from myuw.test.api import MyuwApiTest, missing_url
+
+
+class TestFutureQuarterPage(MyuwApiTest):
+
+    @skipIf(missing_url("myuw_future_quarters_page",
+                        kwargs={"quarter": "2013,summer,a-term"}),
+            "myuw urls not configured")
+    def test_student_summer_aterm(self):
+        url = reverse("myuw_future_quarters_page",
+                      kwargs={"quarter": "2013,summer,a-term"})
+        print(url)
+        self.set_user("javerage")
+        response = self.client.get(
+            url,
+            HTTP_USER_AGENT="Lynx/2.8.2rel.1 libwww-FM/2.14")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['future_term'], "2013,summer,a-term")
+        self.assertEqual(response.context['term_data']['summer_term'],
+                         "a-term")
