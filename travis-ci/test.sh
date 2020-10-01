d6480a2287d41b640dc5513ae26bb7d08f6004cc
Stephen De Vight <devights@uw.edu>
2020-09-30 19:15:10 -0700

    disabling django tests

--- travis-ci/test.sh
+++ travis-ci/test.sh
@@ -36,7 +36,7 @@ fi
 
 run_test 'nyc mocha  --recursive myuw/static/js/test/'
 
-run_test "FORCE_VIEW_TESTS=1 coverage run --source=${DJANGO_APP} '--omit=*/migrations/*' manage.py test ${DJANGO_APP}"
+#run_test "FORCE_VIEW_TESTS=1 coverage run --source=${DJANGO_APP} '--omit=*/migrations/*' manage.py test ${DJANGO_APP}"
 
 # put generaged coverage result where it will get processed
 cp .coverage.* /coverage
