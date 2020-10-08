2f7c61aeb161881652de57fa9f0c2faa86ae7c07
Fang Lin <fanglin@uw.edu>
2020-10-08 14:31:27 -0700

    store in under vuex

--- myuw/static/vue/tests/summer-efs.test.js
+++ myuw/static/vue/tests/summer-efs.test.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 import {mount, shallowMount, createLocalVue} from '@vue/test-utils';
 import BootstrapVue from 'bootstrap-vue';
 import Vuex from 'vuex';
-import notices from '../store/notices';
-import SummerEFSCard from '../components/index/cards/new_student/summer-efs.vue';
+import notices from '../vuex/store/notices';
+import SummerEFSCard from
+  '../components/index/cards/new_student/summer-efs.vue';
 
 import mockNotices from './mock_data/notices.json';
 
