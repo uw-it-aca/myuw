ef291b539a16d13baa3ca71647c5a8dcb6f7fae3
Charlon Palacay <charlon@gmail.com>
2020-10-14 16:37:23 -0700

    Adds Academics page and boilerplate setup.

---
+++ myuw/static/vue/academics.js
@@ -0,0 +1,50 @@
+import {Vue, vueConf} from './base.js';
+
+// bootstrap vue plugins
+import {
+  AlertPlugin,
+  BadgePlugin,
+  ButtonPlugin,
+  CardPlugin,
+  CollapsePlugin,
+  FormPlugin,
+  FormGroupPlugin,
+  FormInputPlugin,
+  FormSelectPlugin,
+  LayoutPlugin,
+  LinkPlugin,
+  NavPlugin,
+  SpinnerPlugin,
+  TabsPlugin,
+  VBTogglePlugin,
+} from 'bootstrap-vue';
+
+import Boilerplate from './components/_templates/boilerplate.vue';
+
+vueConf.store.commit('addVarToState', {
+  name: 'pageTitle',
+  value: 'Academics',
+});
+
+// bootstrap-vue components as plugins
+Vue.use(AlertPlugin);
+Vue.use(BadgePlugin);
+Vue.use(ButtonPlugin);
+Vue.use(CardPlugin);
+Vue.use(CollapsePlugin);
+Vue.use(FormPlugin);
+Vue.use(FormGroupPlugin);
+Vue.use(FormInputPlugin);
+Vue.use(FormSelectPlugin);
+Vue.use(LayoutPlugin);
+Vue.use(LinkPlugin);
+Vue.use(NavPlugin);
+Vue.use(SpinnerPlugin);
+Vue.use(TabsPlugin);
+Vue.use(VBTogglePlugin);
+
+Vue.component('myuw-boilerplate', Boilerplate);
+
+new Vue({
+  ...vueConf,
+});
