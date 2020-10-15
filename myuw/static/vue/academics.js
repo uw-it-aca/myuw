6cbdea4c1e64d79148adc8086985ec03bc9041a3
Fang Lin <fanglin@uw.edu>
2020-10-15 11:18:03 -0700

    add academics page cards

--- myuw/static/vue/academics.js
+++ myuw/static/vue/academics.js
@@ -21,6 +21,35 @@ import {
 
 import Boilerplate from './components/_templates/boilerplate.vue';
 
+// common components
+import Grades from './components/_common/grades.vue';
+import Outage from './components/_common/outage.vue';
+import VisualSchedule from './components/_common/visual_schedule/schedule.vue';
+import Textbooks from './components/_common/textbooks.vue';
+import FutureQuarterCards from './components/_common/future-quarter.vue';
+
+// academics components
+// import CourseCards from './components/academics/schedule/course-cards.vue';
+
+// stores
+import oquarter from './vuex/store/oquarter';
+import textbooks from './vuex/store/textbooks';
+import studSchedule from './vuex/store/stud_schedule';
+import visual_schedule from './vuex/store/visual_schedule';
+
+vueConf.store.registerModule('oquarter', oquarter);
+vueConf.store.registerModule('stud_schedule', studSchedule);
+vueConf.store.registerModule('textbooks', textbooks);
+vueConf.store.registerModule('visual_schedule', visual_schedule);
+
+vueConf.store.commit('addVarToState', {
+  name: 'termData',
+  value: window.term_data,
+});
+vueConf.store.commit('addVarToState', {
+  name: 'cardDisplayDates',
+  value: JSON.parse(document.getElementById('card_display_dates').innerHTML),
+});
 vueConf.store.commit('addVarToState', {
   name: 'pageTitle',
   value: 'Academics',
@@ -44,6 +73,14 @@ Vue.use(TabsPlugin);
 Vue.use(VBTogglePlugin);
 
 Vue.component('myuw-boilerplate', Boilerplate);
+
+// Vue.component('myuw-course-cards', CourseCards);
+Vue.component('myuw-grades', Grades);
+Vue.component('myuw-outage', Outage);
+Vue.component('myuw-future-quarter-cards', FutureQuarterCards);
+Vue.component('myuw-visual-schedule', VisualSchedule);
+Vue.component('myuw-textbooks', Textbooks);
+
 
 new Vue({
   ...vueConf,
