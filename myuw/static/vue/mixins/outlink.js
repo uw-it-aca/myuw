e031dde5482e30de3ad8dd7820a1047d892a53ec
Sarthak Singh <ss269@uw.edu>
2020-10-15 16:59:10 -0700

    Fixed bug with baseURI causing the outlink mixin to not recognize internal links

--- myuw/static/vue/mixins/outlink.js
+++ myuw/static/vue/mixins/outlink.js
@@ -3,11 +3,11 @@ export default {
     if (this.$el.querySelectorAll) {
       let links = this.$el.querySelectorAll('a:not(.out-link):not(.in-link)');
       links.forEach((link) => {
-        if (link.href.includes(document.baseURI)) {
+        if (link.href.includes(document.location.origin)) {
           link.classList.add('in-link');
         } else {
           let label = link.innerText;
-          link.href = `${document.baseURI}out?u=${
+          link.href = `${document.location.origin}/out?u=${
             encodeURI(link.href)
           }&l=${encodeURI(label)}`;
           link.classList.add('out-link');
