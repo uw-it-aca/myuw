57c17d3e44e51ab214cd6e0787f5591190edca9f
Jonathan Beaubien <beaubj@uw.edu>
2020-10-16 12:32:23 -0700

    Got rid of classes, updated format, fixed leaves headers

--- myuw/static/vue/components/academics/grad-status.vue
+++ myuw/static/vue/components/academics/grad-status.vue
@@ -1,78 +1,73 @@
 <template>
   <uw-card
     v-if="showCard"
-    :loaded="isReady" :errored="isErrored"
+    :loaded="isReady"
+    :errored="isErrored"
     :errored-show="showError"
   >
     <template #card-heading>
-      <h3 class="text-dark-beige">
-        Graduate Request Status
-      </h3>
+      <h3 class="text-dark-beige">Graduate Request Status</h3>
     </template>
     <template #card-body>
       <div v-if="petitions">
-        <div id="petition-reqs" class="card-badge-container">
+        <div id="petition-reqs">
           <h4>Petition Requests</h4>
 
-          <ul
-            v-for="(petition, index) in petitions"
-            :key="index" class="card_list"
-          >
+          <ul v-for="(petition, index) in petitions" :key="index">
             <li>
-              <div class="card-badge clearfix">
-                <h5>{{ petition.description }}</h5>
-                <ul class="recommendation-list">
-                  <li v-if="petition.dept_recommend" class="clearfix">
-                    <div class="pull-left">
-                      <span class="card-badge-label">
-                        Department Recommendation
-                      </span>
-                    </div>
+              <h5>{{ petition.description }}</h5>
+              <ul>
+                <li v-if="petition.dept_recommend">
+                  <div>
+                    <span> Department Recommendation </span>
+                  </div>
+                  <span>
                     <span>
-                      <span class="card-badge-value">
-                        {{ petition.dept_recommend }}
-                      </span>
+                      {{ petition.dept_recommend }}
                     </span>
-                  </li>
+                  </span>
+                </li>
 
-                  <li v-if="petition.gradschool_decision" class="clearfix">
-                    <div class="pull-left">
-                      <span class="card-badge-label">
-                        Graduate School Decision
-                      </span>
-                    </div>
-                    <span class="card-badge-value">
-                      {{ petition.gradschool_decision }}
-                    </span>
-                  </li>
-                </ul>
-              </div>
+                <li v-if="petition.gradschool_decision">
+                  <div>
+                    <span> Graduate School Decision </span>
+                  </div>
+                  <span>
+                    {{ petition.gradschool_decision }}
+                  </span>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
       </div>
 
       <div v-if="leaves">
-        <div id="leave-reqs" class="card-badge-container">
+        <div id="leave-reqs">
           <h4>Leave Requests</h4>
 
-          <ul v-for="(leave, index) in leaves" :key="index" class="card_list">
+          <ul v-for="(leave, index) in leaves" :key="index">
             <li>
-              <div class="card-badge clearfix">
-                <h5 v-for="(term, termIndex) in leave.terms" :key="termIndex"><span v-if="termIndex > 0">,</span><span>{{ term.quarter + ' ' + term.year }} Leave</span></h5>
-                <div class="pull-left">
-                  <span class="card-badge-label">Status</span>
-                </div>
-                <span class="card-badge-value">
-                  <span v-if="leave.status === 'Approved'">
-                    Approved<br>
-                    <a target="_blank" href="https://apps.grad.uw.edu/mgp-stu/uwnetid/default.aspx" class="pull-right" style="font-weight: normal;" data-linklabel="MyGrad Payment Portal">Pay Your Fee To Confirm</a>
-                  </span>
-                  <span v-else>
-                    {{ leave.status }}
-                  </span>
-                </span>
-              </div>
+              <h5>
+                <template v-for="(term, termIndex) in leave.terms"
+                  ><span :key="termIndex" v-if="termIndex > 0">, </span
+                  >{{ term.quarter + " " + term.year }}</template
+                >
+                Leave
+              </h5>
+              <div>Status</div>
+              <span v-if="leave.status === 'Approved'">
+                Approved<br />
+                <a
+                  target="_blank"
+                  href="https://apps.grad.uw.edu/mgp-stu/uwnetid/default.aspx"
+                  data-linklabel="MyGrad Payment Portal"
+                  >Pay Your Fee To Confirm</a
+                >
+              </span>
+              <span v-else>
+                {{ leave.status }}
+              </span>
             </li>
           </ul>
         </div>
@@ -80,43 +75,52 @@
 
       <div v-if="degrees">
         <h4>Degree and Exam Requests</h4>
-        <ul v-for="(degree, index) in degrees" :key="index" class="card_list">
+        <ul v-for="(degree, index) in degrees" :key="index">
           <li>
-            <div class="card-badge clearfix">
-              <h5>{{ degree.req_type }}, {{ degree.target_award_quarter }} {{ degree.target_award_year }}</h5>
-              <div class="degree-title">
-                {{ degree.degree_title }}
-              </div>
-              <div class="pull-left">
-                <span class="card-badge-label">Status</span>
-              </div>
-              <span class="card-badge-value">{{ degree.status }}</span>
+            <h5>
+              {{ degree.req_type }}, {{ degree.target_award_quarter }}
+              {{ degree.target_award_year }}
+            </h5>
+            <div>
+              {{ degree.degree_title }}
             </div>
+            <div>
+              <span>Status</span>
+            </div>
+            <span>{{ degree.status }}</span>
           </li>
         </ul>
       </div>
 
-      <div class="clearfix">
-        <div class="card-badge-action pull-right">
-          <a href="https://grad.uw.edu/for-students-and-post-docs/mygrad-program/" data-linklabel="MyGrad" target="_blank">Go to MyGrad</a>
-        </div>
+      <div>
+        <a
+          href="https://grad.uw.edu/for-students-and-post-docs/mygrad-program/"
+          data-linklabel="MyGrad"
+          target="_blank"
+          >Go to MyGrad</a
+        >
       </div>
     </template>
     <template #card-error>
-      An error occurred and MyUW cannot load your graduate
-      request information right now. In the meantime, try the
-      <a href="https://grad.uw.edu/for-students-and-post-docs/mygrad-program/" data-linklabel="MyGrad" target="_blank">MyGrad program page</a>.
+      An error occurred and MyUW cannot load your graduate request information
+      right now. In the meantime, try the
+      <a
+        href="https://grad.uw.edu/for-students-and-post-docs/mygrad-program/"
+        data-linklabel="MyGrad"
+        target="_blank"
+        >MyGrad program page</a
+      >.
     </template>
   </uw-card>
 </template>
 
 <script>
-import {mapGetters, mapState, mapActions} from 'vuex';
-import Card from '../_templates/card.vue';
+import { mapGetters, mapState, mapActions } from "vuex";
+import Card from "../_templates/card.vue";
 
 export default {
   components: {
-    'uw-card': Card,
+    "uw-card": Card,
   },
   computed: {
     ...mapState({
@@ -124,38 +128,36 @@ export default {
         const petitionList = state.grad.value.petitions;
         if (petitionList) {
           petitionList.forEach((petition) => {
-            if (petition.dept_recommend === 'Pending' ||
-                petition.dept_recommend === 'Withdraw') {
+            if (
+              petition.dept_recommend === "Pending" ||
+              petition.dept_recommend === "Withdraw"
+            ) {
               petition.gradschool_decision = null;
             }
-            if (petition.gradschool_decision === 'Approved') {
+            if (petition.gradschool_decision === "Approved") {
               petition.dept_recommend = null;
             }
           });
-          return petitionList;
         }
+        return petitionList;
       },
       leaves: (state) => state.grad.value.leaves,
       degrees: (state) => state.grad.value.degrees,
-      is_grad: (state) => state.user.affiliations.grad,
+      isGrad: (state) => state.user.affiliations.grad,
     }),
-    ...mapGetters('grad', [
-      'isReady',
-      'isErrored',
-      'statusCode',
-    ]),
-    showError: function() {
+    ...mapGetters("grad", ["isReady", "isErrored", "statusCode"]),
+    showError: function () {
       return this.statusCode !== 404;
     },
-    showCard: function() {
-      return !this.isReady || (this.leaves || this.petitions || this.degrees);
+    showCard: function () {
+      return !this.isReady || this.isGrad && (this.leaves || this.petitions || this.degrees);
     },
   },
   created() {
     this.fetch();
   },
   methods: {
-    ...mapActions('grad', ['fetch']),
+    ...mapActions("grad", ["fetch"]),
   },
 };
 </script>
