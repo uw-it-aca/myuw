bc8447f46fd3f7436b9b59286e369dbf44f0e968
Jonathan Beaubien <beaubj@uw.edu>
2020-10-16 13:45:17 -0700

    Last error hopefully

--- myuw/static/vue/components/academics/grad-status.vue
+++ myuw/static/vue/components/academics/grad-status.vue
@@ -144,7 +144,7 @@ export default {
     ...mapGetters('grad', [
       'isReady',
       'isErrored',
-      'statusCode'
+      'statusCode',
     ]),
     showError: function() {
       return this.statusCode !== 404;
