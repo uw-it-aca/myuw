5b98d2f15b479bf8f0bd711360ff3eb5e33c0c18
Sarthak Singh <ss269@uw.edu>
2020-10-16 14:27:58 -0700

    Fixed MUWM-4776 finals tab time when there are no finals

--- myuw/static/vue/components/_common/visual_schedule/schedule-tab.vue
+++ myuw/static/vue/components/_common/visual_schedule/schedule-tab.vue
@@ -180,9 +180,7 @@ export default {
   },
   computed: {
     ...mapState({
-      quarterLastDate: (state) => dayjs(
-          state.termData.lastDay, 'dddd, MMMM D, YYYY',
-      ),
+      quarterLastDate: (state) => dayjs(state.termData.lastDay),
       today: (state) => dayjs(state.termData.todayDate),
     }),
   },
