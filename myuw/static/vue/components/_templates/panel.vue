7de7d732784c882df251074e7adf84af5f6f3ddb
Jonathan Beaubien <beaubj@uw.edu>
2020-10-21 13:06:45 -0700

    Added panel for sections of content that don't need a card and finished sidebar-links card

---
+++ myuw/static/vue/components/_templates/panel.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="(mobileOnly && $mq === 'mobile') || !mobileOnly">
+    <b-container v-if="loaded">
+      <slot name="panel-body" />
+    </b-container>
+    <b-container v-else>
+      <div class="d-flex justify-content-center">
+        <b-spinner small variant="muted" class="my-auto" label="Loading..." />
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loaded: {
+      type: Boolean,
+      default: false,
+    },
+    mobileOnly: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data: function() {
+    return {};
+  },
+};
+</script>
