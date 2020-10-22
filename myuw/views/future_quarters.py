fe3b3ec3144d16cf98f07c2ea57e5fd5b8417ecb
Fang Lin <fanglin@uw.edu>
2020-10-22 14:42:38 -0700

    expand term_data

--- myuw/views/future_quarters.py
+++ myuw/views/future_quarters.py
@@ -8,13 +8,15 @@ def future_quarters(request, quarter):
     term_label = quarter.split(",")
     term = get_specific_term(term_label[0], term_label[1])
     term_data = {
+        "url": quarter,
         "year": term.year,
         "quarter": term.quarter,
         'summer_term': term_label[2] if len(term_label) == 3 else "",
         "first_day_quarter": term.first_day_quarter,
         "last_day_instruction": term.last_day_instruction,
         "aterm_last_date": term.aterm_last_date,
-        "bterm_first_date": term.bterm_first_date
+        "bterm_first_date": term.bterm_first_date,
+        "last_final_exam_date ": term.last_final_exam_date
     }
     context = {
         'future_term': quarter,
