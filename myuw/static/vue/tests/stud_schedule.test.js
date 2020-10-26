aa8090bf5f9b54e3275233ab06685c43e3766f1b
Fang Lin <fanglin@uw.edu>
2020-10-26 12:52:16 -0700

    import Vuex

--- myuw/static/vue/tests/stud_schedule.test.js
+++ myuw/static/vue/tests/stud_schedule.test.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import {mount} from '@vue/test-utils';
+import Vuex from 'vuex';
 import {expectAction} from './helper';
 import {statusOptions} from '../vuex/store/model_builder';
 import stud_schedule from '../vuex/store/stud_schedule';
+import {expectAction} from './helper';
 import mockCoursesJaverage2013Spring from
   './mock_data/stud_schedule/javerage2013Spring.json';
 import mockCoursesJeos2013Spring from
