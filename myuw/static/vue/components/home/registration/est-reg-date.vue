e5cbabb3af7ab69c6bf2e256a995bc928587b95a
Charlon Palacay <charlon@gmail.com>
2020-10-28 15:00:03 -0700

    Fixes styling on future quarter page. Disabled broken encode sans google font.

--- myuw/static/vue/components/home/registration/est-reg-date.vue
+++ myuw/static/vue/components/home/registration/est-reg-date.vue
@@ -1,10 +1,14 @@
 <template>
   <div v-if="estRegData.estRegDate" class="mb-4">
-    <div v-if="estRegData.isMy1stRegDay"
-         class="alert alert-primary" role="alert"
+    <b-alert
+      v-if="estRegData.isMy1stRegDay"
+      show
+      variant="primary"
+      class="myuw-text-md"
     >
       Registration opens at 6:00AM
-    </div>
+    </b-alert>
+
     <div v-else class="d-flex align-items-center">
       <h4 class="h6 m-0 text-dark font-weight-bold flex-fill"
           aria-label="Estimated Registration Date"
