9f147dc62c9683d78fcfa8cb9e1d6c2db1977720
Sarthak Singh <ss269@uw.edu>
2020-10-29 16:03:28 -0700

    Renamed eval to evalObj as eval is resorved word in vue

--- myuw/static/vue/components/_common/course/course-eval.vue
+++ myuw/static/vue/components/_common/course/course-eval.vue
@@ -6,15 +6,18 @@
       (at 11:59PM), and only take a few minutes to complete.
     </p>
 
-    <div v-for="(eval, index) in evalData"
+    <div v-for="(evalObj, index) in evalData"
       :key="`${section.id}-eval-${index}`">
-      <template v-if="eval.is_multi_instr">
+      <template v-if="evalObj.is_multi_instr">
         <!-- evaluation is on the course -->
-        <a :href="eval.url" target="_blank">
-          {{ section.curriculum_abbr }} {{ section.course_number }} {{ section.section_id }} Evaluation</span>
+        <a :href="evalObj.url" target="_blank">
+          {{ section.curriculum_abbr }}
+          {{ section.course_number }}
+          {{ section.section_id }}
+          Evaluation
         </a>
         <ul>
-          <li v-for="(instructor, index) in eval.instructors"
+          <li v-for="(instructor, index) in evalObj.instructors"
             :key="`${section.id}-eval-inst-${index}`">
             <span>
               {{ instructor.instructor_name }}
@@ -27,9 +30,9 @@
       </template>
       <template v-else>
         <ul>
-          <li v-for="(instructor, index) in eval.instructors"
+          <li v-for="(instructor, index) in evalObj.instructors"
             :key="`${section.id}-eval-inst-${index}`">
-            <a :href="eval.url" target="_blank">
+            <a :href="evalObj.url" target="_blank">
               {{ titleCaseName(instructor.dinstructor_name) }}
             </a>
             <span v-if="instructor.instructor_title.length > 0">
