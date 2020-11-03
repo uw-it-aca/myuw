a25e9bf1e2b4b64fbd0438ca9ba3b4e72b182fca
Charlon Palacay <charlon@gmail.com>
2020-11-03 15:19:12 -0800

    Applies style pattern to course details components.

--- myuw/static/vue/components/_common/course/resources.vue
+++ myuw/static/vue/components/_common/course/resources.vue
@@ -1,49 +1,55 @@
 <template>
-  <b-col>
-    <h5 class="h6 font-weight-bold text-danger">Course Resources</h5>
-    <ul class="list-unstyled myuw-text-md">
-      <li v-if="section.class_website_url" class="mb-1">
-        <a
-          :href="section.class_website_url"
-          :label="`${
-            section.curriculum_abbr
-          } ${section.course_number} Course Website`"
-        >
-          Course Website
-        </a>
-      </li>
-      <li v-if="section.lib_subj_guide" class="mb-1">
-        <a
-          :href="section.lib_subj_guide"
-          :label="`${
-            section.curriculum_abbr
-          } ${section.course_number} Library Research Guides`"
-        >
-          Library Research Guides
-        </a>
-      </li>
-      <li v-if="section.canvas_url" class="mb-1">
-        <a
-          :href="section.canvas_url"
-          :label="`${
-            section.curriculum_abbr
-          } ${section.course_number} Course Canvas`"
-        >
-          Course Canvas
-        </a>
-      </li>
-      <li v-if="section.sln" class="mb-1">
-        <a
-          :href="slnHref"
-          :label="`${
-            section.curriculum_abbr
-          } ${section.course_number} Course Canvas`"
-        >
-          Textbooks
-        </a>
-      </li>
-    </ul>
-  </b-col>
+  <div class="d-flex">
+    <h5 :class="[!showRowHeading ? 'sr-only' : '']"
+        class="w-25 h6 font-weight-bold text-danger"
+    >
+      Course Resources
+    </h5>
+    <div class="flex-fill">
+      <ul class="list-unstyled myuw-text-md">
+        <li v-if="section.class_website_url" class="mb-1">
+          <a
+            :href="section.class_website_url"
+            :label="`${
+              section.curriculum_abbr
+            } ${section.course_number} Course Website`"
+          >
+            Course Website
+          </a>
+        </li>
+        <li v-if="section.lib_subj_guide" class="mb-1">
+          <a
+            :href="section.lib_subj_guide"
+            :label="`${
+              section.curriculum_abbr
+            } ${section.course_number} Library Research Guides`"
+          >
+            Library Research Guides
+          </a>
+        </li>
+        <li v-if="section.canvas_url" class="mb-1">
+          <a
+            :href="section.canvas_url"
+            :label="`${
+              section.curriculum_abbr
+            } ${section.course_number} Course Canvas`"
+          >
+            Course Canvas
+          </a>
+        </li>
+        <li v-if="section.sln" class="mb-1">
+          <a
+            :href="slnHref"
+            :label="`${
+              section.curriculum_abbr
+            } ${section.course_number} Course Canvas`"
+          >
+            Textbooks
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -56,6 +62,10 @@ export default {
     course: {
       type: Object,
       required: true,
+    },
+    showRowHeading: {
+      type: Boolean,
+      default: false,
     },
   },
   computed: {
