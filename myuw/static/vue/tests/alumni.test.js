ade53ca4ce7849e80802a8b61d03abf69caa9bde
Fang Lin <fanglin@uw.edu>
2020-11-06 20:26:14 -0800

    MUWM-4765: add tests

---
+++ myuw/static/vue/tests/alumni.test.js
@@ -0,0 +1,65 @@
+import { shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
+import {createLocalVue} from './helper';
+import AlumniCard from '../components/home/alumni.vue';
+import UwCard from '../components/_templates/card.vue';
+const localVue = createLocalVue(Vuex);
+
+describe('User Is an Alumni', () => {
+  let store;
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      state: {
+        user: {
+          affiliations: {
+            alumni: true,
+          }
+        }
+      }
+    });
+  });
+
+  it('Display Alumni Card', () => {
+    const wrapper = shallowMount(AlumniCard, { store, localVue });
+    expect(
+      wrapper.findComponent(UwCard).exists()
+    ).toBe(true);
+    expect(
+      wrapper.findAll('a').at(0).text()
+    ).toBe('Alumni News, Events and Services');
+    expect(
+      wrapper.findAll('a').at(1).text()
+    ).toBe('Columns Magazine');
+    expect(
+      wrapper.findAll('a').at(2).text()
+    ).toBe('Learn about the UWAA');
+    expect(
+      wrapper.findAll('h3').at(0).text()
+    ).toBe('Alumni and Alumni Association');
+    expect(
+      wrapper.findAll('h4').at(0).text()
+    ).toBe('University of Washington Alumni Association (UWAA)');
+  });
+
+  describe('User Not an Alumni', () => {
+    let store;
+  
+    beforeEach(() => {
+      store = new Vuex.Store({
+        state: {
+          user: {
+            affiliations: {
+              alumni: false,
+            }
+          }
+        }
+      });
+    });
+  
+    it('Hide Alumni Card', () => {
+      const wrapper = shallowMount(AlumniCard, { store, localVue });
+      expect(wrapper.findComponent(UwCard).exists()).toBe(false);
+    });
+  });
+});
