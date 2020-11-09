ba7c3789e8cb1d64226923022dcd7f9ca380f54e
Fang Lin <fanglin@uw.edu>
2020-11-09 11:40:48 -0800

    MUWM-4765: test

--- myuw/static/vue/tests/alumni.test.js
+++ myuw/static/vue/tests/alumni.test.js
@@ -26,20 +26,29 @@ describe('User Is an Alumni', () => {
       wrapper.findComponent(UwCard).exists()
     ).toBe(true);
     expect(
-      wrapper.findAll('a').at(0).text()
-    ).toBe('Alumni News, Events and Services');
-    expect(
-      wrapper.findAll('a').at(1).text()
-    ).toBe('Columns Magazine');
-    expect(
-      wrapper.findAll('a').at(2).text()
-    ).toBe('Learn about the UWAA');
-    expect(
       wrapper.findAll('h3').at(0).text()
     ).toBe('Alumni and Alumni Association');
     expect(
       wrapper.findAll('h4').at(0).text()
     ).toBe('University of Washington Alumni Association (UWAA)');
+    expect(
+      wrapper.findAll('a').at(0).text()
+    ).toBe('Alumni News, Events and Services');
+    expect(
+      wrapper.findAll('a').at(0).attributes().hrep
+    ).toBe('https://www.washington.edu/alumni');
+    expect(
+      wrapper.findAll('a').at(1).text()
+    ).toBe('Columns Magazine');
+    expect(
+      wrapper.findAll('a').at(1).attributes().hrep
+    ).toBe('https://magazine.washington.edu/');
+    expect(
+      wrapper.findAll('a').at(2).text()
+    ).toBe('Learn about the UWAA');
+    expect(
+      wrapper.findAll('a').at(2).attributes().hrep
+    ).toBe('https://www.washington.edu/cms/alumni/membership/');
   });
 
   describe('User Not an Alumni', () => {
