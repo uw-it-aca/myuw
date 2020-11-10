438a7796c6966802c0930a53675ce997a839dee8
Charlon Palacay <charlon@gmail.com>
2020-11-09 22:16:42 -0800

    Updates message container padding.

--- myuw/static/vue/components/_templates/boilerplate/messages.vue
+++ myuw/static/vue/components/_templates/boilerplate/messages.vue
@@ -7,14 +7,14 @@
       <h2 class="sr-only">
         Announcements
       </h2>
-      <div v-if="bannerMessages.length > 0" id="message_banner_location">
-        <div v-for="(message, i) in bannerMessages" id="messages" :key="i">
-          <div class="message px-3 py-2">
-            {{ message }}
-          </div>
+      <template v-if="bannerMessages.length > 0" id="message_banner_location">
+        <div v-for="(message, i) in bannerMessages" id="messages" :key="i"
+             class="message px-3 py-2"
+        >
+          {{ message }}
         </div>
-      </div>
-      <div v-if="displayOnboardMessage">
+      </template>
+      <div v-if="displayOnboardMessage" class="px-3 py-2">
         New here?
         <b-link v-b-modal.tourModal class="text-white">
           <u>See MyUW at a glance</u>
