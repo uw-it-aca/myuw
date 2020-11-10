5f66a85c6a229e8e493b6b0cabfdc9833ac790ec
Nicholas Falco <ncfalco@uw.edu>
2020-11-10 11:31:59 -0600

    eslint

--- myuw/static/vue/components/home/former_student/ctnu-edu.vue
+++ myuw/static/vue/components/home/former_student/ctnu-edu.vue
@@ -9,15 +9,20 @@
       </h3>
     </template>
     <template #card-body>
-	    <p>Whether you want to update your skills or delve into a new field, UW Professional &amp; Continuing Education can help.</p>
-	    <p><a href="https://www.pce.uw.edu" target="_blank" data-linklabel="PCE Info">About PCE</a></p>
-	    <h4 class="myuw-card-heading">Available programs</h4>
-	    <ul class="unstyled-list">
-		    <li><a href="https://www.pce.uw.edu/career-accelerator-certificates" target="_blank" data-linklabel="PCE Career Accelerator Programs">Career Accelerator Certificate Programs</a></li>
-		    <li><a href="https://www.pce.uw.edu/certificates" target="_blank" data-linklabel="PCE Certificate Programs">Certificate Programs</a></li>
-		    <li><a href="https://www.pce.uw.edu/degrees" target="_blank" data-linklabel="PCE Degree Programs">Degree Programs</a></li>
-		    <li><a href="https://www.pce.uw.edu/courses" target="_blank" data-linklabel="PCE Courses">Courses</a></li>
-	    </ul>
+      <p>
+        Whether you want to update your skills or delve into a new field,
+        UW Professional &amp; Continuing Education can help.
+      </p>
+      <p><a href="https://www.pce.uw.edu" target="_blank" data-linklabel="PCE Info">About PCE</a></p>
+      <h4 class="myuw-card-heading">
+        Available programs
+      </h4>
+      <ul class="unstyled-list">
+        <li><a href="https://www.pce.uw.edu/career-accelerator-certificates" target="_blank" data-linklabel="PCE Career Accelerator Programs">Career Accelerator Certificate Programs</a></li>
+        <li><a href="https://www.pce.uw.edu/certificates" target="_blank" data-linklabel="PCE Certificate Programs">Certificate Programs</a></li>
+        <li><a href="https://www.pce.uw.edu/degrees" target="_blank" data-linklabel="PCE Degree Programs">Degree Programs</a></li>
+        <li><a href="https://www.pce.uw.edu/courses" target="_blank" data-linklabel="PCE Courses">Courses</a></li>
+      </ul>
     </template>
   </uw-card>
 </template>
