cd17931f8d87c4746432b219a0ae510792e73f5b
Charlon Palacay <charlon@gmail.com>
2020-11-12 10:01:22 -0800

    Styles retiree card.

--- myuw/static/vue/components/home/former_employee/retiree.vue
+++ myuw/static/vue/components/home/former_employee/retiree.vue
@@ -4,19 +4,21 @@
     :loaded="true"
   >
     <template #card-heading>
-      <h3>
+      <h3 class="h4 mb-3 text-dark-beige myuw-font-encode-sans">
         UW Retirement Association (UWRA)
       </h3>
     </template>
     <template #card-body>
-      <p>
+      <p class="myuw-text-md">
         The UWRA informs, connects, and advocates for UW retirees and
         pre-retirees. The UWRA encourages continued engagement with
         the UW and fosters a purposeful retirement through educational
         and social programs.
       </p>
-      <ul>
-        <li><a href="http://www.washington.edu/uwra/" target="_blank" data-linklabel="UW Retirement Association">Learn more about UWRA</a></li>
+      <ul class="list-unstyled mb-0 myuw-text-md">
+        <li>
+          <a href="http://www.washington.edu/uwra/" target="_blank" data-linklabel="UW Retirement Association">Learn more about UWRA</a>
+        </li>
       </ul>
     </template>
   </uw-card>
@@ -36,6 +38,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-</style>
