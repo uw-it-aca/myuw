11359e186e1672414bd5e0548806d8a48338c1bc
Fang Lin <fanglin@uw.edu>
2020-11-13 14:36:46 -0800

    MUWM-4844: handle section with linked secondaries

---
+++ myuw/static/vue/components/home/inst_course_summary/section-group.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <uw-section :section="section">
+      <slot />
+    </uw-section>
+  </div>
+</template>
+
+<script>
+import Section from './section.vue';
+
+export default {
+  components: {
+    'uw-section': Section,
+  },
+  props: {
+    mobileOnly: {
+      type: Boolean,
+      default: false,
+    },
+    section: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
