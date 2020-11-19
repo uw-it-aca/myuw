59bf04869a96991fe19320fbc4dae8cdb35014aa
Fang Lin <fanglin@uw.edu>
2020-11-19 14:19:59 -0800

    focus on content

--- myuw/static/vue/tests/inst-summary.test.js
+++ myuw/static/vue/tests/inst-summary.test.js
@@ -36,7 +36,7 @@ localVue.component('font-awesome-icon', FontAwesomeIcon);
 
 jest.mock('axios');
 
-describe('Instructor Schedule Summary', () => {
+describe('Instructor Teaching Summary', () => {
   let store;
 
   beforeEach(() => {
@@ -63,57 +63,7 @@ describe('Instructor Schedule Summary', () => {
     });
   });
 
-  it('Check status on fetch success', async () => {
-    axios.get.mockResolvedValue(
-      {data: mockNoCourse2013Summer, status: 200}
-    );
-
-    const getters = {
-      isReadyTagged: () => false,
-      isFetchingTagged: () => false,
-    };
-
-    return expectAction(
-      inst_schedule.actions.fetch, null, inst_schedule.state, getters, [
-        {type: 'setStatus', payload: statusOptions[1]},
-        {type: 'setValue', payload: mockNoCourse2013Summer},
-        {type: 'setStatus', payload: statusOptions[0]},
-      ]);
-  });
-
-  it('Check status on fetch failure', () => {
-    axios.get.mockResolvedValue(
-      Promise.reject({response: {status: 404}}));
-    const getters = {
-      isReadyTagged: () => false,
-      isFetchingTagged: () => false,
-    };
-    return expectAction(
-      inst_schedule.actions.fetch, null, inst_schedule.state, getters, [
-        {type: 'setStatus', payload: statusOptions[1]},
-        {type: 'setStatus', payload: statusOptions[2]},
-      ]);
-  });
-
-  it ('Check postProcess - billsea 2013 spring', () => {
-    axios.get.mockResolvedValue(
-        {data: mockBillsea2013Spring, status: 200}
-    );
-
-    const getters = {
-      isReadyTagged: () => false,
-      isFetchingTagged: () => false,
-    };
-
-    return expectAction(
-      inst_schedule.actions.fetch, null, inst_schedule.state, getters, [
-        {type: 'setStatus', payload: statusOptions[1]},
-        {type: 'setValue', payload: mockBillsea2013Spring},
-        {type: 'setStatus', payload: statusOptions[0]},
-      ]);
-  });
-
-  it('Basic Mount', async () => {
+  it('Display the card for billsea 2013 spring', async () => {
     axios.get.mockImplementation((url) => {
       const urlData = {
         '/api/v1/instructor_schedule/current': mockBillsea2013Spring,
