f0ed66e94c9e2a27867d9d94809a2681bda44a5c
Charlon Palacay <charlon@gmail.com>
2020-11-23 09:30:07 -0800

    Adds .prettierrc for formatting rules and updates max-len to 100.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,40 +5,41 @@ module.exports = {
     // (i.e are we running in a browser environment or a node.js env)
     node: true,
     browser: true,
-    "jest/globals": true,
+    'jest/globals': true
   },
-  plugins: ["jest"],
+  plugins: ['jest'],
   extends: [
     // use the recommended rule set for both plain javascript and vue
-    "eslint:recommended",
-    "plugin:vue/recommended",
-    "plugin:jest/recommended",
-    "prettier",
-    "prettier/vue",
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/vue'
   ],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint'
   },
   rules: {
-    // we should always disable console logs and debugging in production
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-unused-vars": "off",
+    // global rules
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'off',
+    'max-len': [2, { code: 100, tabWidth: 4, ignoreUrls: true, ignoreStrings: true }],
 
-    "max-len": ["error", { ignoreUrls: true, ignoreStrings: true }],
-    "jest/no-disabled-tests": "warn",
-    "jest/no-focused-tests": "error",
-    "jest/no-identical-title": "error",
-    "jest/prefer-to-have-length": "warn",
-    "jest/valid-expect": "error",
-    "jest/expect-expect": [
-      "error",
+    // jest rules
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'jest/expect-expect': [
+      'error',
       {
-        assertFunctionNames: ["expect*"],
-      },
+        assertFunctionNames: ['expect*']
+      }
     ],
-    // TODO: This is not ideal and needs to fixed at some point
-    "vue/no-v-html": "off",
-    "vue/max-attributes-per-line": "off",
-  },
-};
+    // vue rules
+    'vue/no-v-html': 'off',
+    'vue/max-attributes-per-line': 'off'
+  }
+}
