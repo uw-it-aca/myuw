0316ccd211de7aebea88f49fb7da280a9928bed8
Fang Lin <fanglin@uw.edu>
2020-12-08 21:45:27 -0800

    Add content table component

--- myuw/static/vue/components/classlist/class-student-list.vue
+++ myuw/static/vue/components/classlist/class-student-list.vue
@@ -28,6 +28,80 @@
         :year="sectionData.year"
         :current-student-majors="sectionData.sections[0].current_student_majors"
       />
+      <div id="classlist_controls">
+        <div role="tablist" aria-label="Views">
+          <div>
+            <a id="list_view"
+               href="#" role="tab"
+               aria-controls="classlist_table_view"
+               aria-selected="true"
+            >
+              <i class="fa fa-table" aria-hidden="true" />Table
+            </a>
+          </div>
+
+          <div>
+            <a id="grid_view"
+               href="#" role="tab"
+               aria-controls="classlist_photogrid_view"
+               aria-selected="false"
+            >
+              <i class="fa fa-user-circle-o" aria-hidden="true" />Photo Grid
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <div id="class-list-sort-controls">
+            <label for="sort_list">Sort: </label>
+            <select id="sort_list" class="">
+              <option value="surname,first_name" selected="selected">
+                Last Name
+              </option>
+              <option value="first_name,surname">
+                First Name
+              </option>
+              <option value="netid">
+                UW NetID
+              </option>
+              <option value="class_code">
+                Class
+              </option>
+              <option value="credits">
+                Credits
+              </option>
+
+              <option v-if="sectionData.sections[0].has_linked_sections"
+                      value="linked_sections"
+              >
+                Secondary Section
+              </option>
+            </select>
+          </div>
+
+          <button v-if="sectionData.sections[0].has_joint"
+                  id="toggle_joint"
+                  type="button" class=""
+                  aria-pressed="false"
+                  title="Show students from joint courses"
+          >
+            <i class="fa fa-square-o" aria-hidden="true" />
+            Joint Course Students
+          </button>
+        </div>
+
+        <div class="">
+          <a id="download_class_list" href="#" class="">
+            <i class="fa fa-download" />Download (CSV)
+          </a>
+
+          <a href="javascript:window.print()" class="">
+            <i class="fa fa-print" />Print
+          </a>
+        </div>
+      </div>
+
+      <uw-table-content :section="sectionData.sections[0]" />
     </template>
 
     <template v-if="noData" #card-error>
@@ -50,11 +124,13 @@
 import {mapGetters, mapState, mapActions} from 'vuex';
 import Card from '../_templates/card.vue';
 import CourseStats from './statistics.vue';
+import Content from './content.vue';
 
 export default {
   components: {
     'uw-card': Card,
     'uw-course-stats': CourseStats,
+    'uw-table-content': Content,
   },
   props: {
     sectionLabel: {
---
+++ myuw/static/vue/components/classlist/content.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="classlist_table_view"
+       aria-labelledby="list_view"
+       role="tabpanel" aria-hidden="false"
+  >
+    <h4 class="sr-only">
+      Table of Student Information
+    </h4>
+    <div class="">
+      <table id="student_list" class="">
+        <thead>
+          <tr>
+            <th>Student No.</th>
+            <th>UW NetID</th>
+            <th>Last Name</th>
+            <th>First Name</th>
+            <th v-if="section.has_joint" class="">
+              Joint Course
+            </th>
+            <th v-if="section.has_linked_sections">
+              Secondary Section
+            </th>
+            <th>Credits</th>
+            <th>Class</th>
+            <th>Major</th>
+            <th v-if="section.is_independent_start">
+              Start Date
+            </th>
+            <th v-if="section.is_independent_start">
+              End Date
+            </th>
+            <th><span class="sr-only">Email</span></th>
+          </tr>
+        </thead>
+        <tbody id="student_sort" />
+      </table>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    mobileOnly: {
+      type: Boolean,
+      default: false,
+    },
+    section: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+
+  },
+};
+</script>
