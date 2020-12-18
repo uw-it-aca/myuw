ae6a70a76c7764fe3f0aedae8b6e425af33c420c
Fang Lin <fanglin@uw.edu>
2020-12-17 16:52:59 -0800

    Add custom column of email

--- myuw/static/vue/components/classlist/table-view.vue
+++ myuw/static/vue/components/classlist/table-view.vue
@@ -6,130 +6,14 @@
       Table of Student Information
     </h4>
     <div class="">
-      <table id="student-list" class="">
-        <thead>
-          <tr>
-            <th id="student-number">
-              Student No.
-            </th>
-            <th id="uwnetid">
-              UW NetID
-            </th>
-            <th id="last-name">
-              Last Name
-            </th>
-            <th id="first-name">
-              First Name
-            </th>
-            <th v-if="section.has_joint" id="joint-section" class="">
-              Joint Course
-            </th>
-            <th v-if="section.has_linked_sections" id="linked-section">
-              Secondary Section
-            </th>
-            <th id="credits">
-              Credits
-            </th>
-            <th id="class-level">
-              Class
-            </th>
-            <th id="majors">
-              Majors
-            </th>
-            <th v-if="section.is_independent_start" id="start-date">
-              Start Date
-            </th>
-            <th v-if="section.is_independent_start" id="end-date">
-              End Date
-            </th>
-            <th id="email">
-              <span class="sr-only">Email</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody id="student-sort">
-          <tr v-for="(reg, i) in section.registrations"
-              :id="`student-${reg.regid}`"
-              :key="i"
-              :class="getClass(reg)"
-          >
-            <td headers="student-number">
-              {{ reg.student_number }}
-            </td>
-            <td headers="uwnetid">
-              {{ reg.netid }}
-            </td>
-            <td headers="last-name">
-              {{ reg.surname }}
-            </td>
-            <td headers="first-name">
-              {{ reg.first_name }}
-            </td>
-            <td v-if="section.has_joint"
-                headers="joint-section"
-                class="joint-col"
-            >
-              <span v-if="reg.is_joint">
-                {{ reg.joint_curric }}
-                {{ reg.joint_course_number }}
-                {{ reg.joint_section_id }}
-              </span>
-              <span v-else>
-                {{ section.curriculum_abbr }}
-                {{ section.course_number }}
-                {{ section.section_id }}
-              </span>
-            </td>
-            <td v-if="section.has_linked_sections"
-                headers="linked-section"
-            >
-              {{ reg.linked_sections }}
-            </td>
-            <td headers="credits">
-              <span v-if="reg.is_auditor">Audit</span>
-              <span v-else>{{ reg.credits }}</span>
-            </td>
-            <td headers="class-level">
-              {{ ucfirst(reg.class_level) }}
-            </td>
-            <td headers="majors">
-              <div v-for="(major, j) in reg.majors" :key="j">
-                <span v-if="major.name">
-                  {{ ucfirst(major.name) }}
-                </span>
-                <span v-if="(j < reg.majors.length - 1)">
-                  ,&nbsp;
-                </span>
-              </div>
-            </td>
-            <td v-if="section.is_independent_start"
-                headers="start-date"
-            >
-              {{ reg.start_date }}
-            </td>
-            <td v-if="section.is_independent_start"
-                headers="end-date"
-            >
-              {{ reg.end_date }}
-            </td>
-            <td headers="email">
-              <a href="`mailto:${reg.email}`"
-                 title="`Email ${reg.first_name} ${reg.surname}`"
-              >
-                <i class="fa fa-envelope-o" />
-                <span class="sr-only">{{ reg.email }}</span>
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <b-table striped hover :fields="fields" :items="items">
+      <b-table id="student-list"
+               hover head-variant="light"
+               :fields="fields" :items="items"
+      >
         <template #cell(email)="data">
-          <a href="`mailto:${data.item.email}`"
-             title="`Email ${data.item.firstName} ${data.item.surName}`"
-          >
+          <a :href="data.value.href" :title="data.value.title">
             <i class="fa fa-envelope-o" />
-            <span class="sr-only">{{ data.item.email }}</span>
+            <span class="sr-only">{{ data.value.email }}</span>
           </a>
         </template>
       </b-table>
@@ -154,7 +38,6 @@ export default {
         {
           key: 'studentNumber',
           label: 'Student No.',
-          sortable: true,
         },
         {
           key: 'netid',
@@ -204,7 +87,6 @@ export default {
         {
           key: 'majors',
           label: 'Majors',
-          sortable: true,
         }],
       );
       if (this.section.is_independent_start) {
@@ -212,17 +94,16 @@ export default {
           {
             key: 'startDate',
             label: 'Start Date',
-            sortable: true,
           },
           {
             key: 'endDate',
             label: 'End Date',
-            sortable: true,
           }],
         );
       }
       data.push(
           {
+            // class: "sr-only",
             key: 'email',
             label: 'Email',
           },
@@ -250,19 +131,27 @@ export default {
         }
         dataItem.credits = reg.is_auditor ? 'Audit' : reg.credits;
         dataItem.classLevel = this.ucfirst(reg.class_level);
-        let majors = '';
+        const majors = [];
         for (let j = 0; j < reg.majors.length; j++) {
-          if (j < reg.majors.length -1) {
-            majors = majors.concat(',&nbsp;');
+          const mj = reg.majors[j];
+          if (mj.name) {
+            majors.push(this.ucfirst(mj.name));
+            majors.push(',');
           }
-          majors = majors.concat(this.ucfirst(reg.majors[j].name));
         }
-        dataItem.majors = majors;
+        if (majors.length > 1) {
+          majors.pop(',');
+        }
+        dataItem.majors = majors.join('&nbsp;');
         if (this.section.is_independent_start) {
           dataItem.startDate = reg.start_date;
           dataItem.endDate = reg.end_date;
         }
-        dataItem.email = reg.email;
+        dataItem.email = {
+          email: reg.email,
+          href: 'mailto:' + reg.email,
+          title: 'Email ' + reg.first_name + ' ' + reg.surname,
+        };
         console.log(dataItem);
         data.push(dataItem);
       }
