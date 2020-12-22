d8fd959e1559fffcb4727afc9ad58bb70f985dff
Fang Lin <fanglin@uw.edu>
2020-12-21 18:08:39 -0800

    only show card to employee or student

--- myuw/static/vue/components/accounts/upass.vue
+++ myuw/static/vue/components/accounts/upass.vue
@@ -1,5 +1,6 @@
 <template>
-  <uw-card :loaded="isReady"
+  <uw-card v-if="showCard"
+           :loaded="isReady"
            :errored="isErrored"
            :errored-show="showError"
   >
@@ -17,64 +18,146 @@
       >UW Transportation page</a>.
     </template>
     <template #card-body>
-      <h4>
-        Status
-      </h4>
+      <h4>Status</h4>
       <span>{{ is_current ? 'Current' : 'Not current' }}</span>
 
-      <div v-if="is_current">
+      <div v-if="is_current" id="upass-notices">
         <p v-if="display_activation">
           Finalize activation by tapping your card on a reader.
         </p>
         <div v-if="!is_employee && in_summer && (pce || seattle)">
           <h4>Summer U-PASS Use</h4>
-          <p>Your U-PASS does not work during summer quarter unless you are registered for a class or are a <a href="https://facilities.uw.edu/transportation/employee-u-pass#8" data-linklabel="TEMP Pass">temporary employee</a>.</p>
+          <p>
+            Your U-PASS does not work during summer quarter unless you are registered
+            for a class or are a
+            <a href="https://facilities.uw.edu/transportation/employee-u-pass#8"
+               data-linklabel="TEMP Pass"
+            >
+              temporary employee
+            </a>.
+          </p>
         </div>
-        <ul>
-          <li>
-            <a v-if="is_employee" href="https://facilities.uw.edu/transportation/employee-u-pass#10" data-linklabel="Employee U-Pass">U-PASS not working?</a>
-            <a v-else-if="bothell" href="https://www.uwb.edu/facility/commuter-services/upass" data-linklabel="Bothell U-Pass">U-PASS not working?</a>
-            <a v-else-if="tacoma" href="https://www.tacoma.uw.edu/getting-campus/u-pass-orca" data-linklabel="Tacoma U-Pass and ORCA">U-PASS not working?</a>
-            <a v-else href="https://facilities.uw.edu/transportation/student-u-pass#3" data-linklabel="Student U-Pass">U-PASS not working?</a>
-          </li>
-        </ul>
+        <a v-if="is_employee"
+           href="https://facilities.uw.edu/transportation/employee-u-pass#10"
+           data-linklabel="Employee U-Pass"
+        >
+          U-PASS not working?
+        </a>
+        <a v-else-if="bothell"
+           href="https://www.uwb.edu/facility/commuter-services/upass"
+           data-linklabel="Bothell U-Pass"
+        >
+          U-PASS not working?
+        </a>
+        <a v-else-if="tacoma"
+           href="https://www.tacoma.uw.edu/getting-campus/u-pass-orca"
+           data-linklabel="Tacoma U-Pass and ORCA"
+        >
+          U-PASS not working?
+        </a>
+        <!-- PCE students and students without a campus (same link as Seattle's) -->
+        <a v-else
+             href="https://facilities.uw.edu/transportation/student-u-pass#3"
+           data-linklabel="Student U-Pass"
+        >
+          U-PASS not working?
+        </a>
       </div>
       <div v-else>
         <ul v-if="is_employee">
-          <li><a href="http://www.washington.edu/u-pass">What is the U-PASS?</a></li>
+          <li>
+            <a href="http://www.washington.edu/u-pass"
+               data-linklabel="Employee U-Pass"
+            >
+              What is the U-PASS?
+            </a>
+          </li>
         </ul>
         <div v-else-if="seattle">
           <p>
-            If you are registered for a quarter, your U-PASS
-            will work one week before the quarter starts.
+            If you are registered for a quarter, your U-PASS will work one week before the quarter starts.
           </p>
           <div v-if="in_summer">
             <h4>Summer U-PASS Use</h4>
-            <p>Your U-PASS does not work during summer quarter unless you are registered for a class or are a <a href="https://facilities.uw.edu/transportation/employee-u-pass#8" data-linklabel="Temporary Employee U-Pass">temporary employee</a>.</p>
+            <p>
+              Your U-PASS does not work during summer quarter unless you are registered
+              for a class or are a
+              <a href="https://facilities.uw.edu/transportation/employee-u-pass#8"
+                 data-linklabel="Temporary Employee U-PASS"
+              >
+                temporary employee
+              </a>.
+            </p>
           </div>
         </div>
         <div v-else-if="bothell">
-          <p>If you <a href="https://www.uwb.edu/facility/commuter-services/upass" data-linklabel="Purchase a U-Pass">purchase</a> a U-PASS for a quarter, your U-PASS will work one week before the quarter starts.</p>
+          <p>
+            If you
+            <a href="https://www.uwb.edu/facility/commuter-services/upass"
+               data-linklabel="Bothell U-PASS"
+            >
+              purchase
+            </a>
+            a U-PASS for a quarter, your U-PASS will work one week before the quarter starts.
+          </p>
         </div>
         <div v-else-if="tacoma">
-          <p>If you <a href="https://www.tacoma.uw.edu/getting-campus/students-purchasing-u-pass" data-linklabel="Purchase a U-Pass">purchase</a> a U-PASS for a quarter, your U-PASS will work one week before the quarter starts.</p>
+          <p>
+            If you
+            <a href="https://www.tacoma.uw.edu/getting-campus/students-purchasing-u-pass"
+               data-linklabel="Tacoma Student U-PASS"
+            >
+              purchase
+            </a>
+            a U-PASS for a quarter, your U-PASS will work one week before the quarter starts.
+          </p>
         </div>
 
         <ul v-if="pce">
-          <li><a href="https://facilities.uw.edu/transportation/student-u-pass#9">What is the U-PASS?</a></li>
-          <li><a href="https://facilities.uw.edu/transportation/student-purchased-u-pass">Purchasing a U-PASS</a></li>
+          <li>
+            <a href="https://facilities.uw.edu/transportation/student-u-pass#9"
+               data-linklabel="CC Student U-PASS"
+            >
+              What is the U-PASS?
+            </a>
+          </li>
+          <li>
+            <a href="https://facilities.uw.edu/transportation/student-purchased-u-pass"
+               data-linklabel="CC Student Purchase U-PASS"
+            >
+              Purchasing a U-PASS
+            </a>
+          </li>
         </ul>
 
         <ul v-else-if="seattle">
-          <li><a href="https://facilities.uw.edu/transportation/student-u-pass">What is the U-PASS?</a></li>
+          <li>
+            <a href="https://facilities.uw.edu/transportation/student-u-pass"
+               data-linklabel="Seattle Student U-PASS"
+            >
+              What is the U-PASS?
+            </a>
+          </li>
         </ul>
 
         <ul v-else-if="tacoma">
-          <li><a href="https://www.tacoma.uw.edu/getting-campus/what-u-pass">What is the U-PASS?</a></li>
+          <li>
+            <a href="https://www.tacoma.uw.edu/getting-campus/what-u-pass"
+               data-linklabel="Tacoma Student U-PASS"
+            >
+              What is the U-PASS?
+            </a>
+          </li>
         </ul>
 
         <ul v-else-if="!bothell">
-          <li><a href="http://www.washington.edu/u-pass/">What is the U-PASS?</a></li>
+          <li>
+            <a href="http://www.washington.edu/u-pass/"
+               data-linklabel="Student U-PASS"
+            >
+              What is the U-PASS?
+            </a>
+          </li>
         </ul>
       </div>
     </template>
@@ -94,6 +177,8 @@ export default {
       statusCode: 'statusCode',
     }),
     ...mapState({
+      employee: (state) => state.user.affiliations.employee,
+      student: (state) => state.user.affiliations.student,
       tacoma: (state) => state.user.affiliations.tacoma,
       bothell: (state) => state.user.affiliations.bothell,
       seattle: (state) => state.user.affiliations.seattle,
@@ -101,9 +186,11 @@ export default {
       display_activation: (state) => state.upass.value.display_activation,
       in_summer: (state) => state.upass.value.in_summer,
       is_current: (state) => state.upass.value.is_current,
-      is_employee: (state) => state.upass.value.is_employee,
     }),
-    showError: function() {
+    showCard() {
+      return this.employee || this.student;
+    },
+    showError() {
       return this.statusCode !== 404;
     },
   },
