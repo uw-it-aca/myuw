8ad0c19f5f3e13c5b29353c9ef90e6d2c32a0f20
Jonathan Beaubien <beaubj@uw.edu>
2020-12-23 11:50:48 -0800

    Fixed camel casing, found better computed values from profile api, cleaned up formatting.

--- myuw/static/vue/components/_common/medicine-account.vue
+++ myuw/static/vue/components/_common/medicine-account.vue
@@ -10,52 +10,49 @@
         <h3 class="h4 mb-3 text-dark-beige myuw-font-encode-sans">
           UW Medicine Account
         </h3>
-        <img :src="staticUrl+'images/UWMedicine_Logo_RGB@2x.png'" width="100px" alt="">
+        <img :src="staticUrl+'images/UWMedicine_Logo_RGB@2x.png'"
+             width="100px"
+             alt=""
+        >
       </template>
       <template #card-error>
-      An error occurred and MyUW cannot load your information right now.
-      In the meantime, try the
-      <a href="https://services.uwmedicine.org/passwordportal/login.htm"
-         data-linklabel="UW Medicine account site"
-         target="_blank"
-      >UW Medicine Account page</a>.
+        An error occurred and MyUW cannot load your information right now.
+        In the meantime, try the
+        <a href="https://services.uwmedicine.org/passwordportal/login.htm"
+           data-linklabel="UW Medicine account site"
+           target="_blank"
+        >UW Medicine Account page</a>.
       </template>
       <template #card-body>
         <p>
           This password is for UW Medicine applications
           like Epic, ORCA, MINDscape, AMC network, etc.
         </p>
-        <div v-if="med_pw_expired">
-          <!--Maybe add a b-alert instead..?-->
-          <p>Password expired on {{ toFriendlyDate(expires_med) }}.
-	          <a href="https://services.uwmedicine.org/passwordportal/login.htm" data-linklabel="Change UW Medicine Password" class="password-alert"><br>Change your password to regain access.</a>
-          </p>
+        <div v-if="medPwExpired">
+          <!--TODO: Maybe use a b-alert..?-->
+          <b-alert show variant="danger">
+            <p>
+              Password expired on {{ toFriendlyDate(expiresMed) }}.
+              <a :href="passwordChange"
+                 data-linklabel="Change UW Medicine Password"
+              >
+                <br>Change your password to regain access.
+              </a>
+            </p>
+          </b-alert>
         </div>
         <div v-else>
           <h4>
             Password expiration
           </h4>
-          <component :is="expires_30_days ? b-alert : div"
-                     :variant="expires_30_days ? danger : ''"
-                     show
-          >
-            <span>{{ toFriendlyDate(expires_med) }}</span>
-            <span>in {{ days_before_expires }} days*</span>
-          </component>
-          <!--
-          <b-alert v-if="expires_30_days" show variant="danger">
-            
-          </b-alert>
-          <div v-else>
-            <span>{{ toFriendlyDate(expires_med) }}</span>
-            <span>in {{ days_before_expires }} days*</span>
+          <div :class="expires30Days ? 'text-danger' : ''">
+            <span>{{ toFriendlyDate(expiresMed) }}</span>
+            <span>in {{ daysBeforeExpires }} days*</span>
           </div>
           <p>*Expiration date gets updated nightly.</p>
-          <a href="https://services.uwmedicine.org/passwordportal/login.htm">Change UW Medicine password</a>
-        -->
+          <a :href="passwordChange">Change UW Medicine password</a>
         </div>
       </template>
-
     </uw-card>
   </div>
 </template>
@@ -69,18 +66,18 @@ export default {
   components: {
     'uw-card': Card,
   },
+  data: function() {
+    return {
+      passwordChange: 'https://services.uwmedicine.org/passwordportal/login.htm',
+    };
+  },
   computed: {
     ...mapState({
-      has_active_med_pw: (state) => state.profile.value.password.has_active_med_pw,
-      med_pw_expired: (state) => state.profile.value.password.med_pw_expired,
-      expires_med: (state) => state.profile.value.password.expires_med,
-      time_stamp: (state) => state.profile.value.password.time_stamp,
-      days_before_expires: (state) => {
-        console.log(daysjs(time_stamp).diff(expires_med, 'day'));
-        return daysjs(time_stamp).diff(expires_med, 'day');
-      },
-      expires_30_days: (state) => {
-        return this.days_before_expires <= 30;
+      hasActiveMedPw: (state) => state.profile.value.password.has_active_med_pw,
+      medPwExpired: (state) => state.profile.value.password.med_pw_expired,
+      expiresMed: (state) => state.profile.value.password.expires_med,
+      daysBeforeExpires: (state) => {
+        return state.profile.value.password.days_before_med_pw_expires;
       },
       staticUrl: (state) => state.staticUrl,
     }),
@@ -93,11 +90,14 @@ export default {
       return this.statusCode !== 404;
     },
     showCard() {
-      return !this.isReady || (this.isReady && this.has_active_med_pw);
+      return !this.isReady || (this.isReady && this.hasActiveMedPw);
+    },
+    expires30Days() {
+      return this.daysBeforeExpires <= 30;
     },
   },
   mounted() {
-    this.fetch()
+    this.fetch();
   },
   methods: {
     toFriendlyDate(dateStr) {
