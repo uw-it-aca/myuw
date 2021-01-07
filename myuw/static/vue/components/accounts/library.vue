23bb0b2d63a4594a63d067f4456a29910b3c4511
Charlon Palacay <charlon@gmail.com>
2021-01-07 12:36:22 -0800

    Styles library card using new card components.

--- myuw/static/vue/components/accounts/library.vue
+++ myuw/static/vue/components/accounts/library.vue
@@ -1,80 +1,97 @@
 <template>
-  <uw-card :loaded="isReady"
-           :errored="isErrored"
-           :errored-show="showError"
-  >
+  <uw-card :loaded="isReady" :errored="isErrored" :errored-show="showError">
     <template #card-heading>
-      <h3 class="h4 mb-3 text-dark-beige myuw-font-encode-sans">
-        Library Account
-      </h3>
+      <h3 class="h4 mb-3 text-dark-beige myuw-font-encode-sans">Library Account</h3>
     </template>
     <template #card-error>
-      An error occurred and MyUW cannot load your library
-      account information right now. In the meantime, try the
-      <a href="http://www.lib.washington.edu/"
-         data-linklabel="UW Libraries"
-         target="_blank"
-      >UW Libraries page</a>.
+      An error occurred and MyUW cannot load your library account information right now. In the
+      meantime, try the
+      <a href="http://www.lib.washington.edu/" data-linklabel="UW Libraries" target="_blank"
+        >UW Libraries page</a
+      >.
     </template>
     <template #card-body>
       <b-alert v-if="holdsReady" show variant="info">
-        <!-- TODO: Add info-circle icon -->
-        <a href="https://search.lib.uw.edu/account"
-           target="_blank"
-           data-linklabel="Library Account Requests"
-        >{{ itemsRequestedText }}</a>
+        <div class="d-flex text-default m-0 myuw-text-md">
+          <div class="pr-2 flex-shrink-1">
+            <font-awesome-icon :icon="['fas', 'exclamation-triangle']" />
+          </div>
+          <div class="w-100">
+            <a
+              href="https://search.lib.uw.edu/account"
+              target="_blank"
+              data-linklabel="Library Account Requests"
+              >{{ itemsRequestedText }}</a
+            >
+          </div>
+        </div>
       </b-alert>
-      <ul>
+
+      <ul class="list-unstyled">
         <li>
-          <h4>
-            Items out
-          </h4>
-          <span>
-            {{ itemsLoaned + (isPlural(itemsLoaned) ? ' items' : ' item') }}
-          </span>
+          <uw-card-status>
+            <template #status-label>Items out</template>
+            <template #status-value>
+              {{ itemsLoaned + (isPlural(itemsLoaned) ? ' items' : ' item') }}
+            </template>
+          </uw-card-status>
         </li>
         <li v-if="nextDue">
-          <h4>
-            Next item due
-          </h4>
-          <span>
-            {{ toFromNowDate(nextDue) }}
-            &nbsp;
-            {{ toFriendlyDatetime(nextDue) }}
-          </span>
+          <uw-card-status>
+            <template #status-label>Next item due</template>
+            <template #status-value>
+              {{ toFromNowDate(nextDue) }}
+            </template>
+            <template #status-content>
+              <div class="myuw-text-sm text-muted text-right">
+                {{ toFriendlyDatetime(nextDue) }}
+              </div>
+            </template>
+          </uw-card-status>
         </li>
         <li v-if="fines">
-          <h4>
-            You owe
-          </h4>
-          <span class="text-danger">
-            {{ formatPrice(fines) }}
-          </span>
-          <a href="https://p.lib.washington.edu/payfines/"
-             target="_blank"
-             data-linklabel="Pay Library Fees"
-          >
-            Pay fees
-          </a>
+          <uw-card-status>
+            <template #status-label>You owe</template>
+            <template #status-value>
+              <span class="text-danger">
+                {{ formatPrice(fines) }}
+              </span>
+            </template>
+            <template #status-content>
+              <div class="myuw-text-sm text-muted text-right">
+                <a
+                  href="https://p.lib.washington.edu/payfines/"
+                  target="_blank"
+                  data-linklabel="Pay Library Fees"
+                >
+                  Pay fees
+                </a>
+              </div>
+            </template>
+          </uw-card-status>
         </li>
       </ul>
-      <a href="http://search.lib.uw.edu/account"
-         target="_blank"
-         aria-label="Your Library Account"
-      >
-        Access library account
-      </a>
+
+      <div class="text-right">
+        <uw-link-button href="http://search.lib.uw.edu/account" target="_blank">
+          Access library account
+        </uw-link-button>
+      </div>
     </template>
   </uw-card>
 </template>
 
 <script>
-import {mapGetters, mapState, mapActions} from 'vuex';
+import { mapGetters, mapState, mapActions } from 'vuex';
 import Card from '../_templates/card.vue';
+import CardStatus from '../_templates/card-status.vue';
+import LinkButton from '../_templates/link-button.vue';
 
 export default {
   components: {
     'uw-card': Card,
+    'uw-card-status': CardStatus,
+    'uw-link-button': LinkButton,
   },
   computed: {
     ...mapState({
@@ -92,8 +109,12 @@ export default {
       return this.statusCode !== 404;
     },
     itemsRequestedText() {
-      return this.holdsReady + ' requested ' +
-             (this.isPlural(this.holdsReady) ? 'items ' : 'item ') + 'ready';
+      return (
+        this.holdsReady +
+        ' requested ' +
+        (this.isPlural(this.holdsReady) ? 'items ' : 'item ') +
+        'ready'
+      );
     },
   },
   mounted() {
