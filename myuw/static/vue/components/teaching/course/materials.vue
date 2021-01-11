0c8be1986d4527f40486034cbdceacd1fe2326c4
Fang Lin <fanglin@uw.edu>
2021-01-11 14:46:47 -0800

    Add Course Canvas

--- myuw/static/vue/components/teaching/course/materials.vue
+++ myuw/static/vue/components/teaching/course/materials.vue
@@ -6,18 +6,21 @@
     <ul>
       <uw-website :section="section" />
       <uw-email-list :section="section" />
+      <uw-canvas :section="section" />
       <uw-textbook v-if="!section.mini_card" :section="section" />
     </ul>
   </div>
 </template>
 
 <script>
+import Canvas from './materials/canvas.vue';
 import Website from './materials/website.vue';
 import EmailList from './materials/email-list.vue';
 import Textbook from './materials/textbook.vue';
 
 export default {
   components: {
+    'uw-canvas': Canvas,
     'uw-website': Website,
     'uw-email-list': EmailList,
     'uw-textbook': Textbook,
