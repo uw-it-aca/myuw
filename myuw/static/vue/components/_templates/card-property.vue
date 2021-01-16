a0c65c4d916d714c5b1c956cd80273a7f459f8b6
Charlon Palacay <charlon@gmail.com>
2021-01-15 16:53:32 -0800

    Add reusable card-property component.

---
+++ myuw/static/vue/components/_templates/card-property.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="d-md-flex mb-3 myuw-text-md">
+    <div class="flex-md-fill mr-3 bd-highlight font-weight-bold property-label">
+      {{ title }}
+    </div>
+    <div class="flex-md-fill">
+      <slot name="content">
+        {{ content }}
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+  data: function () {
+    return {
+      titleWidth: 3,
+      contentWidth: 9,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@media (min-width: 768px) {
+  .property-label {
+    min-width: 25%;
+    max-width: 25%;
+  }
+}
+</style>
