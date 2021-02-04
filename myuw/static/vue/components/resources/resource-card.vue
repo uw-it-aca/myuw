e01c452bd31130bd2e6553117b107afdd10a183f
Fang Lin <fanglin@uw.edu>
2021-02-03 20:12:17 -0800

    Add scroll to top button. rename subcatRes.

--- myuw/static/vue/components/resources/resource-card.vue
+++ myuw/static/vue/components/resources/resource-card.vue
@@ -2,27 +2,27 @@
   <div>
     <b-card-group columns class="myuw-card-columns-2">
       <uw-card
-        v-for="(resourceCat, i) in Object.values(resource.subcategories)"
-        :id="resourceCat.subcat_id"
+        v-for="(subcatRes, i) in Object.values(resource.subcategories)"
+        :id="subcatRes.subcat_id"
         :key="i"
         loaded
       >
         <template #card-heading>
           <div>
             <h4>
-              {{resourceCat.subcat_name}}
+              {{subcatRes.subcat_name}}
             </h4>
-            <button v-if="!resourceCat.is_pinned" @click="pin(resourceCat)">
+            <button v-if="!subcatRes.is_pinned" @click="pin(subcatRes)">
               Pin to Home
             </button>
-            <button v-else @click="unpin(resourceCat)">
+            <button v-else @click="unpin(subcatRes)">
               Unpin
             </button>
           </div>
         </template>
         <template #card-body>
           <ul>
-            <li v-for="(link, j) in resourceCat.links" :key="j">
+            <li v-for="(link, j) in subcatRes.links" :key="j">
               <a :href="link.url">{{link.title}}</a>
             </li>
           </ul>
--- myuw/static/vue/components/resources/resources.vue
+++ myuw/static/vue/components/resources/resources.vue
@@ -14,6 +14,10 @@
       </h3>
       <uw-resource-card :resource="resource"/>
     </div>
+    <button type="button" id='scroll-to-top' class="myuw-back-to-top" title="Back to Top" data-spy="affix" data-offset-bottom="110">
+        <font-awesome-icon :icon="['fa', 'chevron-up']" />
+        <span><span class="sr-only">Back to</span> Top</span>
+    </button>
   </div>
 </template>
 
