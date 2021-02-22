328f7ffc9a851e36a8e08fffde6a05be2dff544c
Jonathan Beaubien <beaubj@uw.edu>
2021-02-22 14:19:00 -0800

    Styled the related help links

--- myuw/static/vue/components/profile/help-links.vue
+++ myuw/static/vue/components/profile/help-links.vue
@@ -1,42 +1,42 @@
 <template>
   <uw-panel :loaded="true">
     <template #panel-body>
-      <h4>
+      <h3 class="h5">
         Related Help
-      </h4>
-      <ul>
-        <li v-if="isBothell">
+      </h3>
+      <ul class="list-unstyled myuw-text-md mb-4">
+        <li v-if="isBothell" class="mb-1">
           <a href="https://www.uwb.edu/facility/mail-services">Mailing Services</a>
         </li>
-        <li v-else-if="!isTacoma">
+        <li v-else-if="!isTacoma" class="mb-1">
           <a href="http://www.washington.edu/about/addressing-letters-to-the-uw/">Addressing Letters to the UW</a>
         </li>
-        <li v-if="isTacoma">
+        <li v-if="isTacoma" class="mb-1">
           <a href="https://www.tacoma.uw.edu/office-registrar/preferred-names">Preferred Names</a>
         </li>
-        <li>
+        <li class="mb-1">
           <a :href="isTacoma ? prefNameLinkTac : prefNameLink">Preferred Names</a>
         </li>
         <template v-if="student">
-          <li v-if="isTacoma">
+          <li v-if="isTacoma" class="mb-1">
             <a href="http://www.tacoma.uw.edu/sites/default/files/sections/Registrar/Change-Student-Name.pdf">Change Your Legal Name</a>
           </li>
-          <li v-else-if="isBothell">
-            <a href="https://www.uwb.edu/registration/policies/name-change" class="student_legal_name">Name Change Policy</a>
+          <li v-else-if="isBothell" class="mb-1">
+            <a href="https://www.uwb.edu/registration/policies/name-change">Name Change Policy</a>
           </li>
           <template v-else>
-            <li>
-              <a href="https://registrar.washington.edu/enrollment-and-records/name-change-policy/" class="student_legal_name">Change Your Legal Name</a>
+            <li class="mb-1">
+              <a href="https://registrar.washington.edu/enrollment-and-records/name-change-policy/">Change Your Legal Name</a>
             </li>
-            <li>
+            <li class="mb-1">
               <a href="https://depts.washington.edu/qcenter/wordpress/changing-your-name-in-the-uw-student-database/">Changing Your Name and Gender</a>
             </li>
           </template>
         </template>
-        <li v-if="employee">
-          <a href="http://hr.uw.edu/benefits/life-events/change-your-address-name-gender-citizenship-or-birth-date/" class="employee_work_info">Change Your Personal Information</a>
+        <li v-if="employee" class="mb-1">
+          <a href="http://hr.uw.edu/benefits/life-events/change-your-address-name-gender-citizenship-or-birth-date/">Change Your Personal Information</a>
         </li>
-        <li v-if="student">
+        <li v-if="student" class="mb-1">
           <a href="https://registrar.washington.edu/enrollment-and-degree-verification/">Self-Service Enrollment Verification</a>
         </li>
       </ul>
