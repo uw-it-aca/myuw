8c3086ab13520ef5cf8f59e1963b24204a6edba1
Fang Lin <fanglin@uw.edu>
2021-03-01 20:29:46 -0800

    merge changes in vue/MUWM-4836

--- myuw/static/vue/components/teaching/course/materials/email-modals/request.vue
+++ myuw/static/vue/components/teaching/course/materials/email-modals/request.vue
@@ -1,15 +1,12 @@
 <template>
   <b-modal
     :id="`emaillist_request_${sln}`"
-    v-model="show"
+    ref="request-modal"
     title="Create Mailing List"
+    @hidden="onHide()"
   >
-    <b-alert variant="danger" :show="disableActions">
-      This action is disabled while overriding as another user.
-    </b-alert>
-
-    <div v-if="emailList.request_sent">
-      <b-alert variant="success" :show="disableActions">
+    <template v-if="emailList.request_sent || requestSuccess">
+      <b-alert variant="success" show>
         <font-awesome-icon :icon="faCheck" />
         Request submitted
       </b-alert>
@@ -18,37 +15,43 @@
         <li>An email confirmation will be sent to {{netid}}@uw.edu</li>
         <li>Mailing lists may take up to 24 hours to activate</li>
       </ul>
-    </div>
-    <div v-else>
+    </template>
+    <template v-else-if="!listView && !addError">
       <div v-if="!emailList.has_lists && emailList.has_joint">
-        <b-form-group title="Request a single email list for:">
-            <b-form-radio value="joint" checked>
-              {{emailList.course_abbr}}
-              {{emailList.course_number}}
-              {{emailList.section_id}}
-              <span v-for="(section, i) in emailList.joint_sections" :key="i">
-                +
-                {{section.course_abbr}}
-                {{section.course_number}}
-                {{section.section_id}}
-              </span>
-              <p>
-                Mailing list address:
-                {{emailList.joint_section_list.list_address}}@uw.edu
-              </p>
-            </b-form-radio>
-            <b-form-radio value="single" checked>
-              {{emailList.course_abbr}}
-              {{emailList.course_number}}
-              {{emailList.section_id}}
-              <p>
-                Mailing list address:
-                {{emailList.section_list.list_address}}@uw.edu
-              </p>
-            </b-form-radio>
+        <b-alert variant="danger" :show="disableActions">
+          This action is disabled while overriding as another user.
+        </b-alert>
+        <b-form-group label="Request a single email list for:">
+          <b-form-radio value="joint" checked>
+            {{emailList.course_abbr}}
+            {{emailList.course_number}}
+            {{emailList.section_id}}
+            <span v-for="(section, i) in emailList.joint_sections" :key="i">
+              +
+              {{section.course_abbr}}
+              {{section.course_number}}
+              {{section.section_id}}
+            </span>
+            <p>
+              Mailing list address:
+              {{emailList.joint_section_list.list_address}}@uw.edu
+            </p>
+          </b-form-radio>
+          <b-form-radio value="single" checked>
+            {{emailList.course_abbr}}
+            {{emailList.course_number}}
+            {{emailList.section_id}}
+            <p>
+              Mailing list address:
+              {{emailList.section_list.list_address}}@uw.edu
+            </p>
+          </b-form-radio>
         </b-form-group>
       </div>
       <div v-else-if="!emailList.has_lists && !emailList.section_list.list_exists">
+        <b-alert variant="danger" :show="disableActions">
+          This action is disabled while overriding as another user.
+        </b-alert>
         <p>
           <strong>
             Request a single email list for
@@ -62,10 +65,9 @@
             <br/>
             <span>
               Need more email lists for this class?
-              <!-- TODO: add button event here -->
-              <a href="#">
+              <b-link @click="listView = true">
                 Request multiple email lists.
-              </a>
+              </b-link>
             </span>
           </span>
         </p>
@@ -79,45 +81,64 @@
         </ul>
         <input type="hidden" :value="emailList.section_list.section_label"/>
       </div>
-      <div v-if="!emailList.no_secondary_section">
-        <div v-show="emailList.has_lists">
-          <b-form-group
-            v-slot="{ ariaDescribedby }"
-            title="Request multiple email lists, one for each section selected:"
-          >
-            <b-form-checkbox
-              v-model="allSelected"
-              :indeterminate="indeterminate"
-              aria-describedby="flavours"
-              aria-controls="flavours"
-              @change="toggleAll"
-            >
-              {{ allSelected ? 'Un-Select All' : 'Select All' }}
-            </b-form-checkbox>
-            <b-form-checkbox-group
-              v-model="selected"
-              :options="selectableEmailList"
-              :aria-describedby="ariaDescribedby"
-              stacked
-            ></b-form-checkbox-group>
-          </b-form-group>
-        </div>
-      </div>
-    </div>
-    <template #modal-footer>
+    </template>
+    <template v-else-if="!addError">
+      <uw-email-add-list v-model="selected" :email-list="emailList" />
+    </template>
+    <template v-else>
+      <b-alert show variant="danger">
+        An error has occurred. Please try again in a few minutes.
+      </b-alert>
+    </template>
+
+    <template v-if="emailList.request_sent || requestSuccess" #modal-footer>
       <a href="https://itconnect.uw.edu/connect/email/resources/mailman/"
-         rel="help" target="_blank" label="Mailman Help"
+         rel="help" target="_blank" data-linklabel="Mailman Help"
       >Mailman help</a>
-      <button v-if="!emailList.no_secondary_section && !emailList.has_lists">
+      <b-button variant="light" @click="$refs['request-modal'].hide()">
+        Close
+      </b-button>
+    </template>
+
+    <template v-else-if="!listView && !addError" #modal-footer>
+      <a href="https://itconnect.uw.edu/connect/email/resources/mailman/"
+         rel="help" target="_blank" data-linklabel="Mailman Help"
+      >Mailman help</a>
+      <b-button variant="light" @click="$refs['request-modal'].hide()">
+        Close
+      </b-button>
+      <b-button :disabled="disableActions" variant="primary" @click="requestSingle()">
+        Submit
+      </b-button>
+    </template>
+
+    <template v-else-if="!addError" #modal-footer>
+      <a href="https://itconnect.uw.edu/connect/email/resources/mailman/"
+         rel="help" target="_blank" data-linklabel="Mailman Help"
+      >Mailman help</a>
+      <b-button variant="outline-secondary" @click="listView = false">
         <font-awesome-icon :icon="faArrowLeft" />
-        Back: request a single mailing list
-      </button>
-      <button @click="show = false">Close</button>
-      <button v-if="!emailList.request_sent"
-              :disabled="selected.length === 0"
+        Back
+      </b-button>
+      <b-button variant="light" @click="$refs['request-modal'].hide()">
+        Close
+      </b-button>
+      <b-button
+        :disabled="selected.length === 0"
+        variant="primary"
+        @click="requestCreateEmail({list: selected, onSuccess, onError})"
       >
         Submit
-      </button>
+      </b-button>
+    </template>
+
+    <template v-else #modal-footer>
+      <a href="https://itconnect.uw.edu/connect/email/resources/mailman/"
+         rel="help" target="_blank" data-linklabel="Mailman Help"
+      >Mailman help</a>
+      <b-button variant="light" @click="$refs['request-modal'].hide()">
+        Close
+      </b-button>
     </template>
   </b-modal>
 </template>
@@ -128,8 +149,12 @@ import {
   faArrowLeft,
 } from '@fortawesome/free-solid-svg-icons';
 import {mapState, mapActions} from 'vuex';
+import EmailAddList from './add_list.vue';
 
 export default {
+  components: {
+    'uw-email-add-list': EmailAddList,
+  },
   props: {
     emailList: {
       type: Object,
@@ -142,12 +167,12 @@ export default {
   },
   data() {
     return {
-      show: false,
+      listView: false,
+      addError: false,
+      requestSuccess: false,
       faCheck,
       faArrowLeft,
       selected: [],
-      indeterminate: false,
-      allSelected: false,
     };
   },
   computed: {
@@ -155,68 +180,30 @@ export default {
       netid: (state) => state.user.netid,
       disableActions: (state) => state.disableActions,
     }),
-    selectableEmailList() {
-      const options = [];
-      if (this.emailList.section_list.list_exists) {
-        options.push({
-          text: `${this.emailList.course_abbr} ${
-            this.emailList.course_number
-          } ${this.emailList.section_list.section_id} - List already exists`,
-          value: `section_single_${this.emailList.section_list.section_id}`,
-          disabled: true,
-        });
-      } else {
-        options.push({
-          text: `${this.emailList.course_abbr} ${
-            this.emailList.course_number
-          } ${this.emailList.section_list.section_id}`,
-          value: `section_single_${this.emailList.section_list.section_id}`,
-        });
-      }
-
-      this.emailList.secondary_section_lists.forEach((section) => {
-        if (section.list_exists) {
-          options.push({
-            text: `${this.emailList.course_abbr} ${
-              this.emailList.course_number
-            } ${section.section_id} - List already exists`,
-            value: `secondary_single_${section.section_id}`,
-            disabled: true,
-          });
-        } else {
-          options.push({
-            text: `${this.emailList.course_abbr} ${
-              this.emailList.course_number
-            } ${section.section_id}`,
-            value: `secondary_single_${section.section_id}`,
-          });
-        }
-      });
-
-      return options;
-    }
-  },
-  watch: {
-    selected(newValue, oldValue) {
-      if (newValue.length === 0) {
-        this.indeterminate = false
-        this.allSelected = false
-      } else if (newValue.length === 
-        this.selectableEmailList.filter((o) => !o.disabled).length
-      ) {
-        this.indeterminate = false
-        this.allSelected = true
-      } else {
-        this.indeterminate = true
-        this.allSelected = false
-      }
-    }
   },
   methods: {
     ...mapActions('emaillist', ['requestCreateEmail']),
-    toggleAll(checked) {
-      this.selected = checked ? 
-        this.selectableEmailList.filter((o) => !o.disabled).map((o) => o.value) : [];
+    requestSingle() {
+      this.requestCreateEmail({
+        list: [{
+          key: `section_single_${this.emailList.section_list.section_id}`,
+          label: this.emailList.section_list.section_label,
+        }],
+        onSuccess: this.onSuccess,
+        onError: this.onError,
+      });
+    },
+    onSuccess() {
+      this.requestSuccess = true;
+    },
+    onError() {
+      this.addError = true;
+    },
+    onHide() {
+      this.listView = false;
+      this.addError = false;
+      this.requestSuccess = false;
+      this.selected = [];
     },
   },
 }
