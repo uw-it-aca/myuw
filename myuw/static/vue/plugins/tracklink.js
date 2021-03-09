5ccd3637e6bc4e7d7d41a42c4171482ed7595a49
Sarthak Singh <ss269@uw.edu>
2021-03-09 13:43:15 -0800

    Added internal link tracking

--- myuw/static/vue/plugins/tracklink.js
+++ myuw/static/vue/plugins/tracklink.js
@@ -1,4 +1,4 @@
-function linkClickHandler(event, binding, vnode) {
+function linkClickHandler(event, binding, vnode, out) {
   if (event.button > 1) return;
 
   let label = binding.value ? binding.value :
@@ -11,51 +11,54 @@ function linkClickHandler(event, binding, vnode) {
     instance,
     event.target.href,
     label,
-    true,
+    out,
   );
 
-  event.preventDefault();
-  // Creates a clone of the original <a> so that
-  // its attributes are not polluted
-  const newLink = event.target.cloneNode(true);
-  newLink.href = `${document.location.origin}/out?u=${
-    encodeURIComponent(event.target.href)
-  }&l=${encodeURIComponent(label)}`;
+  if (out) {
+    event.preventDefault();
+    // Creates a clone of the original <a> so that
+    // its attributes are not polluted
+    const newLink = event.target.cloneNode(true);
+    newLink.href = `${document.location.origin}/out?u=${
+      encodeURIComponent(event.target.href)
+    }&l=${encodeURIComponent(label)}`;
 
-  if (event.button === 1) {
-    newLink.target = "_blank";
+    if (event.button === 1) {
+      newLink.target = "_blank";
+    }
+
+    newLink.click();
   }
-
-  newLink.click();
 }
 
 export default function(Vue, _) {
   Vue.directive('out', {
     bind: (el, binding, vnode) => {
       let bindPoint = el.tagName === 'A' ? el : el.getElementsByTagName('a')[0];
-      bindPoint.addEventListener(
-        'click',
-        (evt) => linkClickHandler(evt, binding, vnode),
-      );
-      bindPoint.addEventListener(
-        'auxclick',
-        (evt) => linkClickHandler(evt, binding, vnode),
-      );
+      bindPoint.onclick = (evt) => linkClickHandler(evt, binding, vnode, true);
+      bindPoint.onauxclick = (evt) => linkClickHandler(evt, binding, vnode, true);
     },
   });
 
   Vue.directive('out-all', {
     update: (el, binding, vnode) => {
       el.getElementsByTagName('a').forEach((el) => {
-        el.addEventListener(
-          'click',
-          (evt) => linkClickHandler(evt, binding, vnode),
-        );
-        el.addEventListener(
-          'auxclick',
-          (evt) => linkClickHandler(evt, binding, vnode),
-        );
+        el.onclick = (evt) => linkClickHandler(evt, binding, vnode, true);
+        el.onauxclick = (evt) => linkClickHandler(evt, binding, vnode, true);
       });
     },
+  });
+
+  Vue.mixin({
+    updated() {
+      if (this.$el && this.$el.querySelectorAll) {
+        this.$el.querySelectorAll('a').forEach((el) => {
+          if (location.hostname === el.hostname) {
+            el.onclick = (evt) => linkClickHandler(evt, {}, {context: this}, false);
+            el.onauxclick = (evt) => linkClickHandler(evt, {}, {context: this}, false);
+          }
+        })
+      }
+    }
   });
 }
