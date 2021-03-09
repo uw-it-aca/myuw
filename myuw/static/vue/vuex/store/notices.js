8890c90961d9b5ba8ff299e0daaf38e1a7be7679
Fang Lin <fanglin@uw.edu>
2021-03-08 17:04:16 -0800

    Fix notice-boby: not just notice-body-with-title

--- myuw/static/vue/vuex/store/notices.js
+++ myuw/static/vue/vuex/store/notices.js
@@ -4,33 +4,30 @@ import {strToDate} from './common';  // when uw_sws  is 2.3.8
 import dayjs from 'dayjs';
 dayjs.extend(require('dayjs/plugin/timezone'))
 
-const postProcess = (response) => {
+const postProcess = (response, _, rootState) => {
   const notices = response.data;
 
   const parser = new DOMParser();
   return notices.map((notice) => {
-    // Split the notice_content into notice_body and notice_title
-    if (notice.notice_content.includes('&nbsp')) {
-      const parts = notice.notice_content.split('&nbsp');
-      notice.notice_title = parts[0];
-      notice.notice_body = parts[1];
-      if (notice.notice_body[0] === ';') {
-        notice.notice_body = notice.notice_body.slice(1);
-      }
-    } else {
-      const htmlDoc = parser.parseFromString(
-          notice.notice_content, 'text/html',
-      );
-      if (htmlDoc.getElementsByClassName('notice-title')[0] !== undefined) {
-        notice.notice_title = htmlDoc.getElementsByClassName(
-            'notice-title',
-        )[0].outerHTML;
-      }
-      if (htmlDoc.getElementsByClassName(
-          'notice-body-with-title')[0] !== undefined) {
-        notice.notice_body = htmlDoc.getElementsByClassName(
-            'notice-body-with-title',
-        )[0].outerHTML;
+    if (notice.notice_content.includes('notice-title')) {
+      const noticeContent = notice.notice_content;
+
+      // Split the notice_content into notice_body and notice_title
+      if (noticeContent.includes('&nbsp')) {
+        const parts = noticeContent.split('&nbsp');
+        notice.notice_title = parts[0];
+        notice.notice_body = parts[1];
+        if (notice.notice_body[0] === ';') {
+          notice.notice_body = notice.notice_body.slice(1);
+        }
+      } else {
+        const htmlDoc = parser.parseFromString(
+            noticeContent, 'text/html',
+        );
+
+        notice.notice_title = htmlDoc.getElementsByClassName('notice-title')[0].outerHTML;
+        htmlDoc.body.removeChild(htmlDoc.getElementsByClassName('notice-title')[0]);
+        notice.notice_body = htmlDoc.body.innerHTML;
       }
     }
 
