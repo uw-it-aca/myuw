8853164cf8adc15dc59fbf1750daea9aa5d9ed3e
Jonathan Beaubien <beaubj@uw.edu>
2021-03-23 14:28:27 -0700

    Textbooks

--- myuw/static/vue/components/textbooks/section.vue
+++ myuw/static/vue/components/textbooks/section.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-b-toggle="`books-${section.sln}`" :disabled="!collapsable">
-      <h3 class="h4">
+      <h2 class="h4">
         <font-awesome-icon
           :icon="['fas', 'square-full']"
           :class="`text-c${section.colorId}`"
@@ -9,7 +9,7 @@
         />
         {{ section.curriculum }}
         {{ section.courseNumber }}{{ section.sectionId }}
-      </h3>
+      </h2>
       <div v-if="collapsable && !isOpen">
         {{ section.books.length }}
         {{ section.books.length > 1 ? "textbooks" : "textbook" }}
