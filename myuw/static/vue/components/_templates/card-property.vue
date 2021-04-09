88a3a9fe1c9f36ff921f93307c424da8fc2d3e21
Fang Lin <fanglin@uw.edu>
2021-04-09 15:32:27 -0700

    eslint fix

--- myuw/static/vue/components/_templates/card-property.vue
+++ myuw/static/vue/components/_templates/card-property.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="d-md-flex mb-3 myuw-text-md">
     <slot name="title">
-      <div class="text-dark-beige flex-md-fill mr-3 bd-highlight myuw-font-encode-sans property-label">
+      <div class="text-dark-beige flex-md-fill mr-3 bd-highlight
+        myuw-font-encode-sans property-label">
         {{ title }}
       </div>
     </slot>
