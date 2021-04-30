34db9e3fe5a40f9bca7f6b486b79a0be7dfa053d
Fang Lin <fanglin@uw.edu>
2021-04-30 15:51:55 -0700

    process meeeting time

--- myuw/static/vue/vuex/store/myplan.js
+++ myuw/static/vue/vuex/store/myplan.js
@@ -1,4 +1,7 @@
 import { fetchBuilder, extractData, buildWith } from './model_builder';
+import {
+  tryConvertDayJS,
+} from './schedule/common';
 
 function postProcess(response, urlExtra) {
   let data = extractData(response);
@@ -7,6 +10,16 @@ function postProcess(response, urlExtra) {
     data = null;
   }
 
+  if (data.term.length && data.term[0].courses) {
+    data.term[0].courses.forEach((course) => {
+      course.sections.forEach((section) => {
+        section.section_data.meetings.forEach((meeting) => {
+          meeting.start_time = tryConvertDayJS(meeting.start_time, "hh:mm");
+          meeting.end_time = tryConvertDayJS(meeting.end_time, "hh:mm");
+        });
+      });
+    });
+  }
   return {[urlExtra]: data};
 }
 
