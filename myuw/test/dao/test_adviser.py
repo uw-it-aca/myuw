cf8561b908d096665610fc24ce030b217b5bd767
Fang Lin <fanglin@uw.edu>
2021-05-04 15:49:54 -0700

    MUWM-4599: enable advisers api

---
+++ myuw/test/dao/test_adviser.py
@@ -0,0 +1,25 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from django.test import TestCase
+from restclients_core.exceptions import DataFailureException
+from myuw.dao.adviser import get_academic_advisers
+from myuw.test import get_request_with_user
+
+
+class TestAdviserDao(TestCase):
+
+    def test_get_normal_case(self):
+        advisers = get_academic_advisers(
+            get_request_with_user('javergae')
+        )
+        self.assertEquals(len(advisers), 2)
+
+    def test_get_error_case(self):
+        self.assertRaises(DataFailureException,
+                          get_academic_advisers,
+                          get_request_with_user('jerror'))
+
+        self.assertRaises(DataFailureException,
+                          get_academic_advisers,
+                          get_request_with_user('jinter'))
