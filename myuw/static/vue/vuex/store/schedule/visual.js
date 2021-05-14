b055abe75ba7549fbf8286139b0a3828e4c39084
Sarthak Singh <ss269@uw.edu>
2021-05-14 15:00:13 -0700

    VS does not show tabs when there are no period and no meetings

--- myuw/static/vue/vuex/store/schedule/visual.js
+++ myuw/static/vue/vuex/store/schedule/visual.js
@@ -155,6 +155,12 @@ const postProcess = (response, urlExtra) => {
     period.latestMeetingTime = latestTime ? latestTime.clone() : null;
   });
 
+  let firstPeriod = schedule?.[urlExtra]?.periods?.[0];
+  if (firstPeriod && firstPeriod.id === 'finals') {
+    schedule[urlExtra].noPeriodsNoMeetings = firstPeriod.earliestMeetingTime === null &&
+      firstPeriod.latestMeetingTime === null;
+  }
+
   return schedule;
 }
 
