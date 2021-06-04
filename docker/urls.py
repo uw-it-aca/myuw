4dd5f089a80542b36ee171aec4649285298e23be
Fang Lin <fanglin@uw.edu>
2021-06-04 11:08:19 -0700

    moving the restclient custom forms back to myuw

--- docker/urls.py
+++ docker/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, re_path
 
 urlpatterns += [
     re_path(r'^support', include('userservice.urls')),
-    re_path(r'^restclients/', include('rc_django.urls')),
+#    re_path(r'^restclients/', include('rc_django.urls')),
     re_path(r'^logging/', include('django_client_logger.urls')),
     re_path(r'^', include('myuw.urls')),
 ]
