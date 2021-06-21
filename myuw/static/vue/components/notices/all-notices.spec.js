b017f2bd104296625cc582ef514665c8b9a6a31a
Sarthak Singh <ss269@uw.edu>
2021-06-21 16:19:07 -0700

    minor cleanup

--- myuw/static/vue/components/notices/all-notices.spec.js
+++ myuw/static/vue/components/notices/all-notices.spec.js
@@ -4,23 +4,21 @@ import Vuex from 'vuex';
 import notices from '../../vuex/store/notices';
 import AllNotices from './all-notices.vue';
 
-
-
 import mock_data from './mock.json';
 
 describe('<AllNotices />', () => {
   it('Mount', () => {
+    cy.intercept(
+      {method: 'GET', url: '/api/v1/notices/'},
+      mock_data
+    )
+
     cy.createLocalVue(Vuex).then((localVue) => {
       let store = new Vuex.Store({
         modules: {
           notices,
         }
       });
-
-      cy.intercept(
-        {method: 'GET', url: '/api/v1/notices/'},
-        mock_data
-      )
 
       mount(AllNotices, {store, localVue});
     });
