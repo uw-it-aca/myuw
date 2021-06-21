5547796276e503abca60a8969821058462ffcd3f
Sarthak Singh <ss269@uw.edu>
2021-06-21 16:40:59 -0700

    Expanded on the basic example

--- myuw/static/vue/components/notices/all-notices.spec.js
+++ myuw/static/vue/components/notices/all-notices.spec.js
@@ -4,14 +4,14 @@ import Vuex from 'vuex';
 import notices from '../../vuex/store/notices';
 import AllNotices from './all-notices.vue';
 
-import mock_data from './mock.json';
-
 describe('<AllNotices />', () => {
   it('Mount', () => {
-    cy.intercept(
-      {method: 'GET', url: '/api/v1/notices/'},
-      mock_data
-    )
+    // Can also use 
+    // cy.fixture('notices/javerage.json').then((javerageNotices) => {
+    //   cy.intercept({method: 'GET', url: '/api/v1/notices/'}, javerageNotices);
+    // });
+    cy.intercept('GET', '/api/v1/notices/', { fixture: 'notices/javerage.json' });
+    
 
     cy.createLocalVue(Vuex).then((localVue) => {
       let store = new Vuex.Store({
@@ -21,6 +21,10 @@ describe('<AllNotices />', () => {
       });
 
       mount(AllNotices, {store, localVue});
+
+      cy.intercept('PUT', '/api/v1/notices/', {times: 8}).as('openAllNotices');
+      cy.get('a').first().click();
+      cy.wait('@openAllNotices');
     });
   })
 })
