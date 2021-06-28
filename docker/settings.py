225ede8a0839c72cfcc72ea33b13ffb7a1a9e174
Fang Lin <fanglin@uw.edu>
2021-06-28 14:20:35 -0700

    comment context_processors.has_google_analytics

--- docker/settings.py
+++ docker/settings.py
@@ -125,7 +125,7 @@ TEMPLATES[0]['OPTIONS']['context_processors'] += [
     'django.template.context_processors.tz',
     'supporttools.context_processors.supportools_globals',
     #'supporttools.context_processors.has_less_compiled',
-    'supporttools.context_processors.has_google_analytics',
+    #'supporttools.context_processors.has_google_analytics',
     'myuw.context_processors.is_hybrid',
 ]
 
