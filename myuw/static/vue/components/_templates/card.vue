00b9371c701e184a7eb793ba3f4382eaf1575b78
Charlon Palacay <charlon@gmail.com>
2021-08-02 13:54:40 -0700

    Inital b-card conversion.

--- myuw/static/vue/components/_templates/card.vue
+++ myuw/static/vue/components/_templates/card.vue
@@ -1,60 +1,53 @@
 <template>
-  <b-card v-if="loaded"
-          v-visibility-change="loaded ? visibilityChanged : null"
-          :class="cardClasses"
-          :body-class="bodyClasses"
-          footer-class="border-0 px-3 py-2"
+  <b-card
+    v-if="loaded"
+    v-visibility-change="loaded ? visibilityChanged : null"
+    :class="cardClasses"
+    :body-class="bodyClasses"
+    footer-class="border-0 px-3 py-2"
   >
     <slot name="card-heading" />
     <div class="myuw-card-body">
       <slot name="card-body" />
     </div>
     <slot name="card-disclosure" />
-    <template v-if="!!$slots['card-footer']"
-              #footer
-              footer-tag="footer"
-    >
+    <template v-if="!!$slots['card-footer']" #footer footer-tag="footer">
       <slot name="card-footer" />
     </template>
   </b-card>
   <div v-else-if="errored">
-    <b-card v-if="erroredShow"
-            class="rounded-0 shadow-sm mb-3" tabindex="0" body-class="p-3"
-    >
-      <slot name="card-heading" />
-
-      <!-- default card error message -->
-      <div
-        class="alert alert-light p-0 m-0 border-0 bg-transparent"
-        role="alert"
-      >
-        <div class="d-flex text-danger mb-3 myuw-text-md">
-          <div class="pr-2 flex-shrink-1">
-            <font-awesome-icon :icon="faExclamationTriangle" />
-          </div>
-          <div class="w-100">
-            <slot name="card-error">
-              An error has occurred and we can't load this content right now.
-              Please try again later.
-            </slot>
+    <div v-if="erroredShow" class="card rounded-0 shadow-sm mb-3" tabindex="0">
+      <div class="card-body p-3">
+        <slot name="card-heading" />
+        <!-- default card error message -->
+        <div class="alert alert-light p-0 m-0 border-0 bg-transparent" role="alert">
+          <div class="d-flex text-danger mb-3 myuw-text-md">
+            <div class="pr-2 flex-shrink-1">
+              <font-awesome-icon :icon="faExclamationTriangle" />
+            </div>
+            <div class="w-100">
+              <slot name="card-error">
+                An error has occurred and we can't load this content right now. Please try again
+                later.
+              </slot>
+            </div>
           </div>
         </div>
+        <slot name="card-error-extra"></slot>
       </div>
-      <slot name="card-error-extra"></slot>
-    </b-card>
+    </div>
   </div>
-  <b-card v-else class="rounded-0 shadow-sm mb-3" body-class="p-3">
-    <b-card-text class="d-flex justify-content-center card-loading">
-      <!-- TODO: replace this with a cog -->
-      <b-spinner small variant="muted" class="my-auto" label="Loading..." />
-    </b-card-text>
-  </b-card>
+  <div v-else class="card rounded-0 shadow-sm mb-3">
+    <div class="card-body p-3">
+      <p class="card-text d-flex justify-content-center card-loading">
+        <b-spinner small variant="muted" class="my-auto" label="Loading..." />
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-import {
-  faExclamationTriangle,
-} from '@fortawesome/free-solid-svg-icons';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 export default {
   props: {
@@ -77,7 +70,7 @@ export default {
     noBottomMargin: {
       type: Boolean,
       default: false,
-    }
+    },
   },
   data: function() {
     return {
@@ -118,8 +111,8 @@ export default {
   methods: {
     visibilityChanged(entry) {
       this.$logger.visibilityChanged(this, entry);
-    }
-  }
+    },
+  },
 };
 </script>
 
