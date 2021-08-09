0d9b2e8e1481987ff440c4d2dd70476b3f178381
Fang Lin <fanglin@uw.edu>
2021-08-09 11:30:49 -0700

    use collapsible

--- myuw/static/vue/components/textbooks/section.vue
+++ myuw/static/vue/components/textbooks/section.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <div v-b-toggle="`books-${section.sln}`" :disabled="!collapsable">
-      <h2 class="h5 mb-3">
+    <div
+      v-if="collapsable"
+      data-toggle="collapse"
+      :data-target="`#books-${section.sln}`"
+      :aria-controls="`books-${section.sln}`"
+      aria-expanded="false"
+      aria-label="Toggle Course Textbooks"
+    >
+      <h2 class="h5">
         <font-awesome-icon
           :icon="faSquareFull"
           :class="`text-c${section.colorId}`"
@@ -10,34 +17,44 @@
         {{ section.curriculum }}
         {{ section.courseNumber }}{{ section.sectionId }}
       </h2>
-      <div v-if="collapsable && !isOpen">
+      <div v-if="!isOpen" class="mb-3">
         {{ section.books.length }}
         {{ section.books.length > 1 ? "textbooks" : "textbook" }}
       </div>
     </div>
-    <div v-if="section.hasBooks"
-      :id="`books-${section.sln}`"
-      v-model="isOpen"
-      class="collapse"
-    >
-      <uw-book
-        v-for="book in section.books"
-        :key="`books-${section.sln}-${book.isbn}`"
-        :book="book"
+
+    <h2 v-else class="h5">
+      <font-awesome-icon
+        :icon="faSquareFull"
+        :class="`text-c${section.colorId}`"
+        class="mr-1"
       />
-    </div>
-    <div v-else
-      :id="`books-${section.sln}`"
-      v-model="isOpen"
-      class="collapse myuw-text-md"
+      {{ section.curriculum }}
+      {{ section.courseNumber }}{{ section.sectionId }}
+    </h2>
+
+    <uw-collapse
+      :collapsable="collapsable"
+      :collapse-id="`books-${section.sln}`"
     >
-      <slot name="no-books">
-        No textbooks have been ordered for this course.
-        <a :href="teachingOrderBookUrl">
-          Order textbooks
-        </a>.
-      </slot>
-    </div>
+      <template #collapsible>
+        <div v-if="section.hasBooks" class="mb-3">
+          <uw-book
+            v-for="book in section.books"
+            :key="`books-${section.sln}-${book.isbn}`"
+            :book="book"
+          />
+        </div>
+        <div v-else class="myuw-text-md mb-3">
+          <slot name="no-books">
+            No textbooks have been ordered for this course.
+            <a :href="teachingOrderBookUrl">
+              Order textbooks
+            </a>.
+          </slot>
+        </div>
+      </template>
+    </uw-collapse>
 
     <hr v-if="!collapsable">
   </div>
@@ -48,10 +65,12 @@ import {
   faSquareFull,
 } from '@fortawesome/free-solid-svg-icons';
 import Book from './book.vue';
+import Collapsible from '../_templates/collapsible.vue';
 
 export default {
   components: {
     'uw-book': Book,
+    'uw-collapse': Collapsible,
   },
   props: {
     section: {
@@ -65,8 +84,8 @@ export default {
   },
   data() {
     return {
-      isOpen: !this.collapsable,
       faSquareFull,
+      isOpen: false,
     };
   },
   computed: {
