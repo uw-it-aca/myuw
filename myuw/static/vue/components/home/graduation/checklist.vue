c9477855e4b02e6ae736a7ea50e7e2eaa12723b9
Kathy Bui <kbui98@uw.edu>
2021-12-22 16:44:26 -0800

    Added columns to grad checklist

--- myuw/static/vue/components/home/graduation/checklist.vue
+++ myuw/static/vue/components/home/graduation/checklist.vue
@@ -4,11 +4,15 @@
       <h2 class="h4 mb-3 text-dark-beige myuw-font-encode-sans">Graduation: Checklist</h2>
     </template>
     <template #card-body>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dolorem labore a sit
-        placeat nisi iusto ullam, ipsam vitae numquam ad, magni at ex quas ut magnam dignissimos
-        incidunt nostrum.
-      </p>
+        <div class="row">
+          <div class="col-8">
+            <p>
+              <strong>To graduate, you’ll need apply.</strong> Don’t worry, it’s an easy process you
+              complete with your departmental adviser!
+            </p>
+          </div>
+          <div class="col-4">One of three columns</div>
+        </div>
     </template>
   </uw-card>
 </template>
@@ -20,9 +24,7 @@ export default {
   components: {
     'uw-card': Card,
   },
-  computed: {
-
-  },
+  computed: {},
 };
 </script>
 
