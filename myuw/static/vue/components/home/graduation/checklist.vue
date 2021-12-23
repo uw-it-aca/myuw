f745929278117fabc72cdbe5e836a6e8dfed66c3
Charlon Palacay <charlon@gmail.com>
2021-12-23 14:01:57 -0800

    Implements BsVue Collapse component.

--- myuw/static/vue/components/home/graduation/checklist.vue
+++ myuw/static/vue/components/home/graduation/checklist.vue
@@ -4,20 +4,21 @@
       <h2 class="h4 mb-3 text-dark-beige myuw-font-encode-sans">Graduation: Checklist</h2>
     </template>
     <template #card-body>
-        <div class="row">
-          <div class="col-8">
-            <p>
-              <strong>To graduate, you’ll need apply.</strong> Don’t worry, it’s an easy process you
-              complete with your departmental adviser!
-            </p>
-          </div>
-          <div class="col-4">One of three columns</div>
+      <div class="row">
+        <div class="col-8">
+          <p>
+            <strong>To graduate, you'll need apply.</strong> Dont worry, it's an easy process you
+            complete with your departmental adviser!
+          </p>
         </div>
+        <div class="col-4">One of three columns</div>
+      </div>
     </template>
   </uw-card>
 </template>
 
 <script>
+
 import Card from '../../_templates/card.vue';
 
 export default {
--- myuw/static/vue/components/home/graduation/pre-application.vue
+++ myuw/static/vue/components/home/graduation/pre-application.vue
@@ -10,18 +10,69 @@
         incidunt nostrum.
       </p>
     </template>
+    <template #card-disclosure>
+
+      <b-collapse
+        :id="`collapseGraduationContent`"
+        v-model="isOpen"
+      >
+        <p>this is the disclosure content</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dolorem labore a sit
+          placeat nisi iusto ullam, ipsam vitae numquam ad, magni at ex quas ut magnam dignissimos
+          incidunt nostrum.
+        </p>
+      </b-collapse>
+
+    </template>
+    <template #card-footer>
+       <button v-b-toggle="`collapseGraduationContent`"
+            type="button"
+            class="btn btn-link w-100 p-0 border-0 text-dark"
+          >
+            show more content
+            <font-awesome-icon v-if="!isOpen" :icon="faChevronDown" class="align-middle" />
+            <font-awesome-icon v-else :icon="faChevronUp" class="align-middle" />
+          </button>
+    </template>
   </uw-card>
 </template>
 
 <script>
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+
 import Card from '../../_templates/card.vue';
 
 export default {
   components: {
     'uw-card': Card,
   },
-  computed: {
+  data() {
+    return {
+      isOpen: false,
+      faChevronUp,
+      faChevronDown,
+    };
+  },
+  watch: {
 
+  },
+  computed: {},
+
+  mounted() {
+    this.getOpen();
+  },
+
+  methods: {
+    getOpen: function () {
+      let blah = document.querySelector('#collapseGraduationContent');
+      if (blah.classList.contains('show')) {
+        console.log('shown');
+      } else {
+        console.log('hidden');
+      }
+    },
   },
 };
 </script>
