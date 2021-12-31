d36f29df4295964ecc84b1b30fb2dfaa250afc92
Fang Lin <fanglin@uw.edu>
2021-12-30 17:34:44 -0800

    MUWM-5009: initial check in.

---
+++ myuw_vue/components/home/graduation/application-submitted.vue
@@ -0,0 +1,132 @@
+<template>
+  <uw-card v-if="showCard" :loaded="isReady">
+    <template #card-heading>
+      <h2 class="h4 mb-3 text-dark-beige myuw-font-encode-sans">Graduation: Checklist</h2>
+    </template>
+    <template #card-body>
+      <div class="row">
+        <div class="col-8">
+          <p>
+            <strong>You're on your way!</strong> We're here to help you get to the finish line!
+          </p>
+          <p class = myuw-text-lg>
+            Start with these Checklists
+          </p>
+          <ul class="list-style">
+            <li>Review the
+              <a href="https://www.washington.edu/students/graduation-checklist/">
+              UW Graduation checklist
+              </a> for an overview of tasks.
+            </li>
+            <li>International students, review the
+              <a href="https://iss.washington.edu/resources/final-checklist/">
+              ISS graduation checklist
+              </a> for additional guidance.
+            </li>
+          </ul>
+          <p class = myuw-text-lg>
+            Take part in the commencement ceremonies
+          </p>
+          <p class = myuw-text-lg>
+            Verify that your information and data will not be lost
+          </p>
+        </div>
+        <div class="col-4">
+          <h3 class="h6 text-dark-beige myuw-font-encode-sans">
+            Graduation application status
+          </h3>
+          <p v-if="isApproved(degrees[0].status)" class="myuw-text-md">
+            Approved for {{ titleCaseWord(degrees[0].quarter) }} {{ degrees[0].year }} graduation
+          </p>
+          <p v-else class="myuw-text-md">
+            There is an issue with your graduation status. Talk to your departmental advisor.
+          </p>
+          <h3 class="h6 text-dark-beige myuw-font-encode-sans">
+            Intended degree<span v-if="degrees.length > 1">s</span>
+          </h3>
+          <ul class="list-unstyled mb-0 myuw-text-md">
+            <li v-for="(degree, j) in degrees" :key="j" class="mb-1">
+              {{ degree.title }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </template>
+    <template #card-disclosure>
+      <b-collapse
+        id="collapseGradSupportAndHelp"
+        v-model="isOpen"
+      >
+        <p>this is the disclosure content</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dolorem labore a sit
+          placeat nisi iusto ullam, ipsam vitae numquam ad, magni at ex quas ut magnam dignissimos
+          incidunt nostrum.
+        </p>
+      </b-collapse>
+    </template>
+    <template #card-footer>
+      <button
+        v-b-toggle.collapseGradSupportAndHelp
+        type="button"
+        class="btn btn-link btn-sm w-100 p-0 text-dark"
+      >
+        Learn how to get support and help
+        <font-awesome-icon v-if="!isOpen" :icon="faChevronDown" class="align-middle" />
+        <font-awesome-icon v-else :icon="faChevronUp" class="align-middle" />
+      </button>
+    </template>
+  </uw-card>
+</template>
+
+<script>
+// MUWM-5009
+import { mapGetters, mapState, mapActions } from 'vuex';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import Card from '../../_templates/card.vue';
+
+export default {
+  components: {
+    'uw-card': Card,
+  },
+  data() {
+    return {
+      isOpen: false,
+      faChevronUp,
+      faChevronDown,
+    };
+  },
+  computed: {
+    ...mapGetters('profile', {
+      isReady: 'isReady',
+      isErrored: 'isErrored',
+      statusCode: 'statusCode',
+    }),
+    ...mapState({
+      intlStudent: (state) => state.user.affiliations.intl_stud,
+      classLevel: (state) => state.user.affiliations.class_level,
+    }),
+    ...mapState('profile', {
+      degreeStatus: (state) => state.value.degree_status,
+    }),
+    showCard() {
+      return (this.isReady && !this.degreeStatus.error_code);
+    },
+    degrees() {
+      return this.degreeStatus.degrees;
+    },
+  },
+  created() {
+    if (this.classLevel === 'SENIOR') this.fetch();
+  },
+  methods: {
+    ...mapActions('profile', ['fetch']),
+    isApproved(status) {
+      return (status >= 3 && status <= 5);
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ myuw_vue/components/home/graduation/pre-application.vue
@@ -0,0 +1,109 @@
+<template>
+  <uw-card
+    v-if="showCard"
+    v-meta="{term: term}"
+    :loaded="isReady">
+    <template #card-heading>
+      <h2 class="h4 mb-3 text-dark-beige myuw-font-encode-sans">
+        Graduation: Pre Application
+      </h2>
+    </template>
+    <template #card-body>
+      <p>
+        <strong>To graduate, you'll need apply.</strong> Dont worry, it's an easy
+        process you complete with your departmental adviser!
+      </p>
+    </template>
+    <template #card-disclosure>
+      <b-collapse
+        id="collapseGradAppDeadlineAndDetails"
+        v-model="isOpen"
+      >
+        <ul class="list-style">
+          <li>
+            <strong>Deadline:</strong> The third Friday of the quarter
+            in which you intend to graduate.
+          </li>
+          <li>
+            <strong>Submit early:</strong> If you apply 2-3 quarters before
+            graduation, you get Graduating Senior Priority for registration for 2 quarters.
+          </li>
+        </ul>
+        <p>
+          <strong>Get all the details:</strong> Follow the
+          <a href="https://www.washington.edu/students/graduation-checklist/">
+            UW Graduation checklist
+          </a>.
+        </p>
+        <p v-if="intlStudent">
+          International students, review the
+          <a href="https://iss.washington.edu/resources/final-checklist/">
+            ISS Graduation checklist
+          </a> for additional guidance.
+        </p>
+      </b-collapse>
+    </template>
+    <template #card-footer>
+      <button
+        v-b-toggle.collapseGradAppDeadlineAndDetails
+        type="button"
+        class="btn btn-link btn-sm w-100 p-0 text-dark"
+      >
+        Deadlines and details
+        <font-awesome-icon v-if="!isOpen" :icon="faChevronDown" class="align-middle" />
+        <font-awesome-icon v-else :icon="faChevronUp" class="align-middle" />
+      </button>
+    </template>
+  </uw-card>
+</template>
+
+<script>
+// MUWM-5009 Pre-application
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { mapGetters, mapState, mapActions } from 'vuex';
+import Card from '../../_templates/card.vue';
+
+export default {
+  components: {
+    'uw-card': Card,
+  },
+  data() {
+    return {
+      isOpen: false,
+      faChevronUp,
+      faChevronDown,
+    };
+  },
+  computed: {
+    ...mapGetters('profile', {
+      isReady: 'isReady',
+      isErrored: 'isErrored',
+      statusCode: 'statusCode',
+    }),
+    ...mapState({
+      quarter: (state) => state.termData.quarter,
+      year: (state) => state.termData.year,
+      intlStudent: (state) => state.user.affiliations.intl_stud,
+      classLevel: (state) => state.user.affiliations.class_level,
+    }),
+    ...mapState('profile', {
+      degreeStatus: (state) => state.value.degree_status,
+    }),
+    showCard() {
+      return (this.isReady && this.degreeStatus.error_code === 404);
+    },
+    term() {
+      return this.year + ',' + this.quarter;
+    }
+  },
+  created() {
+    if (this.classLevel === 'SENIOR') this.fetch();
+  },
+  methods: {
+    ...mapActions('profile', ['fetch']),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
