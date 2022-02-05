5c072f85432126748c10e9cc30789b0b0df46244
Fang Lin <fanglin@uw.edu>
2022-02-04 18:37:29 -0800

    investigate

--- myuw_vue/vuex/store/schedule/visual.js
+++ myuw_vue/vuex/store/schedule/visual.js
@@ -144,8 +144,16 @@ const postProcess = (response, urlExtra) => {
       // Generate dates if on a final period
       let refrenceDate = earliestFinalDate;  // MUWM-5001
 
-      if (refrenceDate.day() === 6) {
+      if (refrenceDate.day() === 7) {
+        period.daySlots['sunday'] = refrenceDate.clone();
+        period.daySlots['monday'] = refrenceDate.clone().day(8);
+        period.daySlots['tuesday'] = refrenceDate.clone().day(9);
+        period.daySlots['wednesday'] = refrenceDate.clone().day(10);
+        period.daySlots['thursday'] = refrenceDate.clone().day(11);
+        period.daySlots['friday'] = refrenceDate.clone().day(12);
+      } else if (refrenceDate.day() === 6) {
         period.daySlots['saturday'] = refrenceDate.clone();
+        period.daySlots['sunday'] = refrenceDate.clone().day(7);
         period.daySlots['monday'] = refrenceDate.clone().day(8);
         period.daySlots['tuesday'] = refrenceDate.clone().day(9);
         period.daySlots['wednesday'] = refrenceDate.clone().day(10);
