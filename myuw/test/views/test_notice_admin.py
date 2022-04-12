bee78f2b51118f0a583b2b0746538532193a8541
Fang Lin <fanglin@uw.edu>
2022-04-12 14:12:43 -0700

    inc test coverage

--- myuw/test/views/test_notice_admin.py
+++ myuw/test/views/test_notice_admin.py
@@ -1,15 +1,27 @@
 # Copyright 2022 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-import json
-from myuw.test.api import missing_url, MyuwApiTest
+from myuw.test.api import MyuwApiTest
 from django.test.client import RequestFactory
 from myuw.models.myuw_notice import MyuwNotice
-from myuw.views.notice_admin import _get_datetime, _save_notice
-from datetime import datetime
+from myuw.views.notice_admin import _get_datetime, _get_html, _save_notice
 from myuw.dao.myuw_notice import get_myuw_notices_for_user
 from myuw.test import get_request_with_user, get_request_with_date
-from myuw.test.dao.test_myuw_notice import get_datetime_with_tz
+
+CONTENT = (
+    "<p>Your Husky card is your key to efficiency on " +
+    "campus and serves as your University ID, providing access " +
+    "to eligible services such as dining plans, facilities, " +
+    "transportation, UW libraries, and so much more.</p>\n<p><a " +
+    "href=\"http://hfs.uw.edu/Husky-Card-Services/submit-a-photo" +
+    "\">Submit your photo online</a>\nto be able to have your " +
+    "Husky card printed and sent to you by mail. Students with " +
+    "an international address should submit a photo online and " +
+    "plan to pick up their Husky card on campus. Visit the <a " +
+    "href=\"https://hfs.uw.edu/Husky-Card-Services/Operations-" +
+    "Updates\" title=\"https://hfs.uw.edu/Husky-Card-Services/" +
+    "Operations-Updates\">Husky Card Operations</a> page for " +
+    "more information.\n</p>")
 
 
 class TestNoticeAdmin(MyuwApiTest):
@@ -24,6 +36,26 @@ class TestNoticeAdmin(MyuwApiTest):
         string = "2018-05-08T15:28"
         self.assertEqual(str(_get_datetime(string)),
                          "2018-05-08 15:28:00-07:00")
+        string = "2018-05-08T15:28:00-07:00"
+        self.assertEqual(str(_get_datetime(string)),
+                         "2018-05-08 15:28:00-07:00")
+
+    def test_get_html(self):
+        rf = RequestFactory()
+        notice_context = {
+            "title": "Test",
+            "content": CONTENT,
+            "notice_type": "Banner",
+            "notice_category": "MyUWNotice",
+            "is_critical": False,
+            "start": "2022-04-12T11:25:00-07:00",
+            "end": "2022-04-15T11:25:00-07:00",
+            "last_edit_by": "",
+            "last_edit_date": "2022-04-12T19:59:06.068510+00:00",
+            "target_group": ""}
+        request = rf.post('', notice_context)
+        self.assertIsNotNone(_get_html(request, 'content'))
+        self.assertFalse(_save_notice(request, {}))
 
     def test_save_new_notice(self):
         rf = RequestFactory()
@@ -101,7 +133,7 @@ class TestNoticeAdmin(MyuwApiTest):
         self.assertTrue(context['title_error'])
         self.assertTrue(context['content_error'])
 
-    def test_html_content(self):
+    def test_create_notice(self):
         notice_context = {
             'action': 'save',
             'title': '<b>The</b> Title',
