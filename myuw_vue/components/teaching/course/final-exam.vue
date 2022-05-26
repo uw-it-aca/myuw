0c71c0fcaa5b50b16dbf54d3ce0b9f8d13b36873
Fang Lin <fanglin@uw.edu>
2022-05-26 13:12:39 -0700

    Resolve MUWM-5145 (#2786)
    Resolve MUWM-5145

--- myuw_vue/components/teaching/course/final-exam.vue
+++ myuw_vue/components/teaching/course/final-exam.vue
@@ -79,7 +79,7 @@ export default {
       return ''.concat(
         'https://sdb.admin.uw.edu/sisMyUWClass/uwnetid/finalexam.aspx?quarter=',
         this.titleCaseWord(this.section.quarter), '+', this.section.year,
-        '&sln=', this.section.sln, '&chanid=');
+        '&sln=', this.section.sln);  // MUWM-5145
     }
   },
   mounted() {
