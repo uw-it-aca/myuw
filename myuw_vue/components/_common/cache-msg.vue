712176b390af973a9cb00ffe4b0bd51b3192a51a
Fang Lin <fanglin@uw.edu>
2022-07-05 14:07:34 -0700

    Set up component: cache-msg

---
+++ myuw_vue/components/_common/cache-msg.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="myuw-text-md">
+
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+
+export default {
+  computed: {
+    ...mapState({
+      pageLoadedDt: (state) => state.cardDisplayDates.comparison_date,
+    }),
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@use 'sass:map';
+@import '../../../../myuw/static/css/myuw/variables.scss';
+
+</style>
