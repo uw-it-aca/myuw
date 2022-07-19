bf0be41c39ac08935bb452a42a3a3ce4e77734dd
Charlon Palacay <charlon@gmail.com>
2022-07-19 15:35:43 -0700

    Enable bootstrap popover.

--- myuw_vue/components/_common/cache-msg.vue
+++ myuw_vue/components/_common/cache-msg.vue
@@ -1,41 +1,45 @@
 <template>
   <div class="myuw-text-md fst-italic text-end">
     Is something missing? Most data updates every 15 minutes.
-    <a data-bs-container="body" 
-      data-bs-toggle="popover" 
-      data-bs-placement="bottom" 
-      title="hello" 
-      data-bs-content="Bottom popover">
-      <font-awesome-icon :icon="faQuestionCircle" />Popover on bottom
+
+    <a
+      class="test-popover"
+      data-bs-container="body"
+      data-bs-toggle="popover"
+      data-bs-placement="bottom"
+      title="hello"
+      data-bs-content="Bottom popover"
+    >
+      <font-awesome-icon :icon="faQuestionCircle" />
     </a>
   </div>
 </template>
 
 <script>
-import {
-  faQuestionCircle,
-} from '@fortawesome/free-solid-svg-icons';
-import {mapState} from 'vuex';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { mapState } from 'vuex';
 import { Popover } from 'bootstrap';
 
 export default {
+  data() {
+    return {
+      faQuestionCircle,
+    };
+  },
   computed: {
     ...mapState({
       pageLoadedDt: (state) => state.cardDisplayDates.comparison_date,
     }),
   },
-  methods: {
+  mounted() {
+    var popover = new Popover(document.querySelector('.test-popover'));
   },
-    data() {
-    return {
-      faQuestionCircle,
-    }
-  },
+  methods: {},
+
 };
 </script>
 
 <style lang="scss" scoped>
 @use 'sass:map';
 @import '../../../myuw/static/css/myuw/variables.scss';
-
 </style>
