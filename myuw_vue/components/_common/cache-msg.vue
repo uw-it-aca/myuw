431b8c89cae6387e6a056c006c2a47ff3867b89f
William Washington <williamwashington@Williams-MacBook-Pro.local>
2022-08-02 14:09:53 -0700

    reformatting code with prettier

--- myuw_vue/components/_common/cache-msg.vue
+++ myuw_vue/components/_common/cache-msg.vue
@@ -1,20 +1,23 @@
 <template>
-    <div class="myuw-text-sm text-dark-gray fst-italic" :class="[$mq === 'mobile' ? 'text-center' : 'text-end']">
-      Most updates become available after 15 minutes.
+  <div
+    class="myuw-text-sm text-dark-gray fst-italic"
+    :class="[$mq === 'mobile' ? 'text-center' : 'text-end']"
+  >
+    Most updates become available after 15 minutes.
 
-      <a
-        role="button"
-        class="cache-msg"
-        data-bs-container="body"
-        data-bs-toggle="popover"
-        title="Data Update Policy"
-        data-bs-placement="bottom"
-        data-bs-html="true"
-        :data-bs-content="message"
-      >
-        <font-awesome-icon :icon="faQuestionCircle" class="text-dark-gray" />
-      </a>
-    </div>
+    <a
+      role="button"
+      class="cache-msg"
+      data-bs-container="body"
+      data-bs-toggle="popover"
+      title="Data Update Policy"
+      data-bs-placement="bottom"
+      data-bs-html="true"
+      :data-bs-content="message"
+    >
+      <font-awesome-icon :icon="faQuestionCircle" class="text-dark-gray" />
+    </a>
+  </div>
 </template>
 
 <script>
@@ -28,12 +31,14 @@ export default {
     };
   },
   computed: {
-    message () {
-      return "<p>Most data updates become available in MyUW every 15 minutes, but some can take up to four hours.</p>" +
-      "<div>If you’re not seeing information you’d expect, please refresh the page at " +
-      "a later time. If you're still having issues email <a href='mailto:help@uw.edu?subject=MyUW Question'>" +
-      "help@uw.edu</a>.</div>";
-    }
+    message() {
+      return (
+        '<p>Most data updates become available in MyUW every 15 minutes, but some can take up to four hours.</p>' +
+        '<div>If you’re not seeing information you’d expect, please refresh the page at ' +
+        "a later time. If you're still having issues email <a href='mailto:help@uw.edu?subject=MyUW Question'>" +
+        'help@uw.edu</a>.</div>'
+      );
+    },
   },
   mounted() {
     var popover = new Popover(document.querySelector('.cache-msg'));
