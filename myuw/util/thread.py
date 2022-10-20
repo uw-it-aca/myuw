d6d5045043fb694946a901b397a67288b9621c48
Jim Laney <jlaney@uw.edu>
2022-10-20 12:38:44 -0700

    refactor thread and add tests

--- myuw/util/thread.py
+++ myuw/util/thread.py
@@ -2,54 +2,64 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import sys
-import restclients_core.thread
+import threading
 from django.db import connection
 from django.conf import settings
 from userservice.user import UserServiceMiddleware
 
 
-class Thread(restclients_core.thread.Thread):
+class Thread(threading.Thread):
+    parent = None
+    _use_thread = False
+
     def __init__(self, *args, **kwargs):
-        # Only enable threading if forced.  Using some cache backends
-        # with sqlite doesn't work with threading.
-        self._use_thread = getattr(settings, "MYUW_PREFETCH_THREADING", True)
-        super(Thread, self).__init__(*args, **kwargs)
+        if getattr(settings, "MYUW_PREFETCH_THREADING", True):
+            self.parent = threading.currentThread()
+            self._use_thread = True
+
+        super().__init__(*args, **kwargs)
 
     def start(self):
         if self._use_thread:
-            super(Thread, self).start()
+            super().start()
         else:
             self.run()
 
     def join(self):
         if self._use_thread:
-            return super(Thread, self).join()
-
+            return super().join()
         return True
+
+    def close_connection(self):
+        if self._use_thread:
+            if not connection.in_atomic_block:
+                connection.close()
 
 
 class PrefetchThread(Thread):
+    method = None
+    request = None
 
     def run(self):
+        if self.method is None or self.request is None:
+            return
+
         try:
             UserServiceMiddleware().process_request(self.request)
             self.method(self.request)
         except Exception as ex:
             # We need to be sure that any prefetch errors don't crash the page!
+            print(ex)
             pass
 
-        if self._use_thread:
-            connection.close()
+        self.close_connection()
 
 
 class ThreadWithResponse(Thread):
-
-    def __init__(self, *args, **kwargs):
-        super(Thread, self).__init__(*args, **kwargs)
+    def run(self):
         self.response = None
         self.exception = None
 
-    def run(self):
         try:
             if sys.version_info[0] == 2:
                 if self._Thread__target is not None:
@@ -61,5 +71,4 @@ class ThreadWithResponse(Thread):
         except Exception as ex:
             self.exception = ex
 
-        if self._use_thread:
-            connection.close()
+        self.close_connection()
