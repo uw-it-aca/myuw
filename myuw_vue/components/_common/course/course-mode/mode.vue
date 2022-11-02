0dab947a3f45ffd01b617cd28c6e63fb1823b07e
Fang Lin <fanglin@uw.edu>
2022-11-01 17:38:01 -0700

    Create the common conponent for pop over

--- myuw_vue/components/_common/course/course-mode/mode.vue
+++ myuw_vue/components/_common/course/course-mode/mode.vue
@@ -2,105 +2,36 @@
   <div class="text-uppercase myuw-text-sm">
     <span v-if="section.is_asynchronous">
       Asynchronous Online (A)
-      <a
-        role="button"
-        class="async-msg"
-        data-bs-container="body"
-        data-bs-toggle="popover"
-        title="Asynchronous Online (A)"
-        data-bs-placement="bottom"
-        data-bs-html="true"
-        data-bs-content="<p>Students and instructors always interact with others and engage course materials 
-        asynchronously. While there are no regularly-scheduled, required synchronous class meetings, students 
-        are expected to meet all assignment deadlines and may be asked to schedule occasional, brief synchronous 
-        one-on-one check-in meetings with the instructors. <a href='https://registrar.washington.edu/classrooms/course-modes/'>
-        Learn more about course modes.</a></p>
-        <b>Incorrect course mode?</b>
-        <div>In case of an error, contact your department time schedule coordinator.</div>"
-    >
-      <font-awesome-icon :icon="faQuestionCircle" class="text-dark-gray" />
-    </a>
+      <uw-pop-over v-if="!hideInfoLink"
+        :selector="`${section.id}-async-msg`" title="Asynchronous Online (A)"
+        :content="asyncMsg"/>
     </span>
     <span v-else-if="section.is_synchronous">
       Synchronous Online (O)
-      <a
-        role="button"
-        class="sync-msg"
-        data-bs-container="body"
-        data-bs-toggle="popover"
-        title="Synchronous Online (O)"
-        data-bs-placement="bottom"
-        data-bs-html="true"
-        data-bs-content="<p>Students and instructors interact online synchronously through regularly-scheduled, 
-        required meetings using applications such as Zoom. Students should also expect to interact with 
-        others and engage in course materials asynchronously. The time schedule contains information about 
-        the course’s regular, required synchronous meeting time. <a href='https://registrar.washington.edu/classrooms/course-modes/'>
-        Learn more about course modes.</a></p>
-        <b>Incorrect course mode?</b>
-        <div>In case of an error, contact your department time schedule coordinator.</div>"
-    >
-      <font-awesome-icon :icon="faQuestionCircle" class="text-dark-gray" />
-    </a>
+      <uw-pop-over v-if="!hideInfoLink"
+        :selector="`${section.id}-sync-msg`" title="Synchronous Online (O)"
+        :content="syncMsg"/>
     </span>
     <span v-else-if="section.is_hybrid">
       Hybrid (B)
-      <a
-        role="button"
-        class="hybrid-msg"
-        data-bs-container="body"
-        data-bs-toggle="popover"
-        title="Hybrid (B)"
-        data-bs-placement="bottom"
-        data-bs-html="true"
-        data-bs-content="<p>Some, but not all, required class meetings occur in a physical classroom. When 
-        not meeting in the physical classroom, students and instructors will attend class online, either 
-        through required synchronous sessions or required asynchronous activities. The time schedule 
-        contains information about the course’s required weekly physical classroom and synchronous 
-        meeting time. <a href='https://registrar.washington.edu/classrooms/course-modes/'>Learn more about 
-        course modes.</a></p>
-        <b>Incorrect course mode?</b>
-        <div>In case of an error, contact your department time schedule coordinator.</div>
-"
-    >
-      <font-awesome-icon :icon="faQuestionCircle" class="text-dark-gray" />
-    </a>
+      <uw-pop-over v-if="!hideInfoLink"
+        :selector="`${section.id}-hybrid-msg`" title="Hybrid (B)" :content="hybMsg"/>
     </span>
     <span v-else>
       In Person
-      <a
-        role="button"
-        class="inperson-msg"
-        data-bs-container="body"
-        data-bs-toggle="popover"
-        title="In Person"
-        data-bs-placement="bottom"
-        data-bs-html="true"
-        data-bs-content="<p>All required class meetings occur in a physical classroom. The course may also have 
-        online content and activities that students engage in outside of class time as homework or study 
-        materials. The time schedule contains information about the course’s required weekly physical 
-        classroom meeting time. <a href='https://registrar.washington.edu/classrooms/course-modes/'>Learn more about course modes.</a></p>
-        <b>Incorrect course mode?</b>
-        <div>In case of an error, contact your department time schedule coordinator.</div>"
-    >
-      <font-awesome-icon :icon="faQuestionCircle" class="text-dark-gray" />
-    </a>
+      <uw-pop-over v-if="!hideInfoLink"
+        :selector="`${section.id}-inperson-msg`" title="In Person" :content="inPersonMsg"/>
     </span>
-    <a
-      v-if="!hideInfoLink"
-      v-out="'course-mode'"
-      href="https://registrar.washington.edu/classrooms/course-modes/"
-      :title="title"
-    ><font-awesome-icon :icon="faQuestionCircle" /></a>
   </div>
 </template>
 
 <script>
-import {
-  faQuestionCircle,
-} from '@fortawesome/free-solid-svg-icons';
-import { Popover } from 'bootstrap';
+import PopOver from '../../pop-over.vue';
 
 export default {
+  components: {
+    'uw-pop-over': PopOver,
+  },
   props: {
     section: {
       type: Object,
@@ -115,25 +46,53 @@ export default {
       default: false,
     },
   },
-  data() {
-    return {
-      faQuestionCircle,
-    }
-  },
   computed: {
-    title() {
-      return (
-        this.includeTSC ?
-        "Learn About Course Mode, in case of an error please" +
-        "contact your department Time Schedule coordinator." :
-        "Learn About Course Mode");
+    learnMoreMsg() {
+      return " <a href='https://registrar.washington.edu/classrooms/course-modes/'>" +
+        "Learn more about course modes.</a></p>";
     },
-  },
-  mounted() {
-    var popover = new Popover(document.querySelector('.async-msg'));
-    var popover = new Popover(document.querySelector('.inperson-msg'));
-    var popover = new Popover(document.querySelector('.sync-msg'));
-    var popover = new Popover(document.querySelector('.hybrid-msg'));
+    contactTsMsg() {
+      return "<b>Incorrect course mode?</b>" +
+        "<div>In case of an error, contact your department time schedule coordinator.</div>";
+    },
+    asyncMsg() {
+      const msg = (
+        "<p>Students and instructors always interact with others and engage course materials" +
+        " asynchronously. While there are no regularly-scheduled, required synchronous class" +
+        " meetings, students are expected to meet all assignment deadlines and may be asked " +
+        "to schedule occasional, brief synchronous one-on-one check-in meetings with the " +
+        "instructors.");
+      return (this.includeTSC
+        ? (msg + this.learnMoreMsg + this.contactTsMsg) : msg);
+    },
+    syncMsg() {
+      const msg = (
+        "Students and instructors interact online synchronously through regularly-scheduled, " +
+        "required meetings using applications such as Zoom. Students should also expect to " +
+        "interact with others and engage in course materials asynchronously. The time schedule" +
+        " contains information about the course’s regular, required synchronous meeting time.");
+      return (this.includeTSC
+        ? (msg + this.learnMoreMsg + this.contactTsMsg) : msg);
+    },
+    hybMsg() {
+      const msg = (
+        "Some, but not all, required class meetings occur in a physical classroom. When not " +
+        "meeting in the physical classroom, students and instructors will attend class online," +
+        " either through required synchronous sessions or required asynchronous activities. " +
+        "The time schedule contains information about the course’s required weekly physical " +
+        "classroom and synchronous meeting time.");
+      return (this.includeTSC
+        ? (msg + this.learnMoreMsg + this.contactTsMsg) : msg);
+    },
+    inPersonMsg() {
+      const msg = (
+        "All required class meetings occur in a physical classroom. The course may also have " +
+        "online content and activities that students engage in outside of class time as homework" +
+        " or study materials. The time schedule contains information about the course’s required" +
+        " weekly physical classroom meeting time.");
+      return (this.includeTSC
+        ? (msg + this.learnMoreMsg + this.contactTsMsg) : msg);
+    },
   },
 };
 </script>
---
+++ myuw_vue/components/_common/pop-over.vue
@@ -0,0 +1,47 @@
+<template>
+  <a
+    role="button"
+    :class="selector.toLowerCase()"
+    data-bs-container="body"
+    data-bs-toggle="popover"
+    :title="title"
+    data-bs-placement="bottom"
+    data-bs-html="true"
+    :data-bs-content="content"
+  >
+    <font-awesome-icon :icon="faQuestionCircle" class="text-dark-gray" />
+  </a>
+</template>
+
+<script>
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { Popover } from 'bootstrap';
+export default {
+  data() {
+    return {
+      faQuestionCircle,
+    };
+  },
+  props: {
+    selector: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  mounted() {
+    var popover = new Popover(document.querySelector(
+      "'." + this.selector.toLowerCase() + "'"));
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
