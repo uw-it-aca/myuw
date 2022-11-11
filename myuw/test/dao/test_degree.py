dd8655e702c38e123f9f3341407b5aa79f7518f1
Fang Lin <fanglin@uw.edu>
2022-11-10 22:26:23 -0800

    Resolve MUWM-5182

--- myuw/test/dao/test_degree.py
+++ myuw/test/dao/test_degree.py
@@ -18,6 +18,7 @@ DEGREE_DATA = {
         'is_degree_earned_term': False,
         'before_degree_earned_term': True,
         'during_april_may': True,
+        "four_wks_prior_last_day_inst": False,
         'level': 1,
         'name_on_diploma': 'John Joseph Average',
         'quarter': 'summer',
