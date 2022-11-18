c3a758cf6f6a638017f5a9ed8f65ebdb890a5572
Fang Lin <fanglin@uw.edu>
2022-11-17 17:05:43 -0800

    Resolve MUWM-5207

--- myuw_vue/components/_common/course/meeting/schedule.vue
+++ myuw_vue/components/_common/course/meeting/schedule.vue
@@ -36,14 +36,19 @@
             :headers="`days-${meeting.id}`"
             colspan="3" class="p-0"
           >
-            <span class="text-muted">Class does not meet</span>
+            <span v-if="isAsyHyb(meeting)" class="text-muted">
+              No meeting time (Online learning)
+            </span>
+            <span v-else class="text-muted">Class does not meet</span>
           </td>
 
           <td v-else-if="meeting.days_tbd"
             :headers="`days-${meeting.id}`"
             colspan="3" class="p-0"
           >
-            <span v-if="isAsyHyb(meeting)" class="text-muted">No meeting time</span>
+            <span v-if="isAsyHyb(meeting)" class="text-muted">
+              No meeting time (Online learning)
+            </span>
             <span v-else class="text-muted">Days and times to be arranged</span>
           </td>
 
@@ -51,7 +56,10 @@
             :headers="`days-${meeting.id}`"
             colspan="3" class="p-0"
           >
-            <span class="text-muted">No classroom meeting: online learning</span>
+            <span v-if="isAsyHyb(meeting)" class="text-muted">
+              No meeting time (Online learning)
+            </span>
+            <span v-else class="text-muted">No classroom meeting: Online learning</span>
           </td>
 
           <template v-else-if="meeting.start_time && meeting.end_time">
