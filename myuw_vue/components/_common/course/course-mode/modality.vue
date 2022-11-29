5decfd41f07fd0a56426cf961fa1a636786f98da
Fang Lin <fanglin@uw.edu>
2022-11-29 13:34:18 -0800

    Hide IN PERSON course mode (#2867)
    Resolve MUWM-5210

--- myuw_vue/components/_common/course/course-mode/modality.vue
+++ myuw_vue/components/_common/course/course-mode/modality.vue
@@ -1,5 +1,9 @@
 <template>
-  <uw-card-property title="Course Mode" :sr-only-title="!showRowHeading">
+  <uw-card-property
+    v-if="notInPerson"
+    title="Course Mode"
+    :sr-only-title="!showRowHeading"
+  >
     <uw-course-mode
       :section="section"
       :include-t-s-c="showRowHeading"
@@ -28,6 +32,15 @@ export default {
       type: Boolean,
       default: false,
     },
+  },
+  computed: {
+    notInPerson() {
+      // MUWM-5210
+      return (
+        this.section.is_asynchronous ||
+        this.section.is_synchronous ||
+        this.section.is_hybrid);
+    }
   },
 };
 </script>
