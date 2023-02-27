7f1748425f7a2be32d8e53609ac7dcc5e1101d82
Fang Lin <fanglin@uw.edu>
2023-02-26 21:50:48 -0800

    MUWM-5233: use a separate query for quarters completed

--- myuw/dao/pds.py
+++ myuw/dao/pds.py
@@ -9,6 +9,7 @@ import json
 import logging
 import traceback
 from memcached_clients import MemcacheError
+from sqlalchemy import and_
 from uw_person_client.clients.core_client import UWPersonClient
 from myuw.util.cache import MyUWMemcachedCache, ONE_DAY
 from myuw.logger.timer import Timer
@@ -55,38 +56,45 @@ class PdsClient(UWPersonClient):
     def load_cache(self):
         try:
             timer = Timer()
-            sqla_students = self.DB.session.query(
-                self.DB.Transcript).join(self.DB.Student).join(
-                    self.DB.Term, self.DB.Transcript.tran_term).filter(
-                    self.DB.Transcript.enoll_status == '12'
-                )  # enrolled in the term
+            Transcript = self.DB.Transcript
+            Student = self.DB.Student
+            Term = self.DB.Term
+            student_query = self.DB.session.query(Student).filter(
+                Student.enroll_status_code == '12').all()
 
-            for student in sqla_students.all():
+            for record in student_query:
+
+                term_query = self.DB.session.query(Transcript).join(
+                    Term, Transcript.tran_term_id == Term.id).filter(
+                        and_(Transcript.student_id == record.id,
+                             Transcript.enroll_status == 12)
+                        ).all()
                 quarters_completed = []
-                for transcript in student.transcript:
+                for term_completed in term_query:
                     quarters_completed.append(
                         json.dumps({
-                            "year": transcript.tran_term.year,
-                            "quarter": transcript.tran_term.quarter
+                            "year": term_completed.year,
+                            "quarter": term_completed.quarter
                         }))
+
                 set_cache_data(
-                    student.student_number,
+                    record.student_number,
                     json.dumps(
                         {
                             "application_status_code":
-                            student.application_type_code,
+                            record.application_type_code,
                             "total_deductible_credits":
-                                student.total_deductible_credits,
+                            record.total_deductible_credits,
                             "total_extension_credits":
-                                student.total_extension_credits,
+                            record.total_extension_credits,
                             "total_grade_attempted":
-                                student.total_grade_attempted,
+                            record.total_grade_attempted,
                             "total_lower_div_transfer_credits":
-                                student.total_lower_div_transfer_credits,
+                            record.total_lower_div_transfer_credits,
                             "total_upper_div_transfer_credits ":
-                                student.total_upper_div_transfer_credits,
+                            record.total_upper_div_transfer_credits,
                             "total_non_graded_credits":
-                                student.total_non_graded_credits,
+                            record.total_non_graded_credits,
                             "quarters_completed": quarters_completed
                         }
                     ))
