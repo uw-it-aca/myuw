60ec6bb979284feeb5d0de5807c2703418dabe8a
Cayla Snooke <csnooke@uw.edu>
2023-03-31 11:30:18 -0700

    Removed Online Teaching and Covid sections

--- myuw_vue/components/home/quicklinks/covid-links.vue
+++ myuw_vue/components/home/quicklinks/covid-links.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mx-n3 mb-3 p-3 myuw-covid-links">
     <div>
-      <h3 class="h6">
+      <!--<h3 class="h6">
         UW Coronavirus
       </h3>
       <ul class="list-unstyled myuw-text-md">
@@ -9,7 +9,7 @@
           <a href="https://www.washington.edu/coronavirus/">
             UW Coronavirus Home
           </a>
-        </li>
+        </li>-->
         <li v-if="student" class="mb-1">
           <a href="https://www.washington.edu/coronavirus/students/">
             Resources for Students
@@ -90,70 +90,10 @@
       </ul>
     </div>
     <div v-if="instructor">
-      <hr class="bg-secondary">
-      <h3 class="h6">
-        Online Teaching
-      </h3>
-      <ul class="list-unstyled mb-0 myuw-text-md">
-        <li class="mb-2">
-          <a href="https://teachingremotely.washington.edu/">
-            Teaching Remotely
-          </a>
-          <div class="fw-light myuw-text-sm">
-            Plan digital learning experiences for your remote and hybrid courses.
-          </div>
-        </li>
-        <li v-if="bothellEmp" class="mb-2">
-          <a href="https://www.uwb.edu/it/teaching">
-            UWB Teach Anywhere
-          </a>
-          <div class="fw-light myuw-text-sm">
-            UW Bothell resources for teaching online
-          </div>
-        </li>
-        <li v-if="tacomaEmp" class="mb-2">
-          <a href="https://www.tacoma.uw.edu/digital-learning/instructional-continuity">
-            UWT Instructional Continuity
-          </a>
-          <div class="fw-light myuw-text-sm">
-            UW Tacoma resources for teaching online
-          </div>
-        </li>
-        <li class="mb-2">
-          <a href="https://canvas.uw.edu/courses/1392969">
-            Teaching with UW Technologies
-          </a>
-          <div class="fw-light myuw-text-sm">
-            Learn about technology to support online instruction
-          </div>
-        </li>
-        <li class="mb-2">
-          <a href="https://teachingremotely.washington.edu/#getHelp">
-            Workshops and Office Hours
-          </a>
-          <div class="fw-light myuw-text-sm">
-            Calendars for workshops and office hours for all three campuses
-          </div>
-        </li>
-        <li class="mb-2">
-          <a href="https://washington.zoom.us/">Zoom</a>
-          <div class="fw-light myuw-text-sm">
-            Schedule and start online class sessions
-          </div>
-        </li>
-        <li class="mb-2">
-          <a href="https://uw.hosted.panopto.com/">Panopto</a>
-          <div class="fw-light myuw-text-sm">
-            Lecture capture – record, schedule, and review lectures
-          </div>
-        </li>
-        <li class="mb-2">
-          <a href="http://polleverywhere.com/">Poll Everywhere</a>
-          <div class="fw-light myuw-text-sm">
-            Create poll responses for your classes
-          </div>
-        </li>
-      </ul>
+    <p class="myuw-text-sm pt-2">
+      Zoom now has a permanent spot in the Quick Links above.
+      You can add other links by customizing your Quick Links,
+      including adding recently visited links from below.</p>
     </div>
   </div>
 </template>
