6c8ceb6c2b602592ebd2b593107052c45efd595a
Kathy Bui <kbui98@uw.edu>
2023-05-19 12:11:36 -0700

    Updated instructor of record italics text

--- myuw_vue/components/teaching/course/grading/instructors.vue
+++ myuw_vue/components/teaching/course/grading/instructors.vue
@@ -1,22 +1,22 @@
 <template>
-    <uw-card-property :title="`Instructor${instructorCount > 1 ? 's' : ''} of Record`">
-      <ul class="list-unstyled mb-0">
-        <li
-          v-for="(instructor, i) in section.instructors" :key="i" class="mb-1">
-            {{ titleCaseName(instructor.display_name) }}
-        </li>
-      </ul>
-      <div>
-        <span class="myuw-text-sm fst-italic">Only instructors of record can submit grades.</span>
-        <uw-collapsed-item
-          :part="submitGradesLearnMore" caller-id="InstructorsOfRecord">
-          <template #collapsed-body>
-            To add or remove an instructor of record, contact your departmental time schedule
-            coordinator.
-          </template>
-        </uw-collapsed-item>
-      </div>
-    </uw-card-property>
+  <uw-card-property :title="`Instructor${instructorCount > 1 ? 's' : ''} of Record`">
+    <ul class="list-unstyled mb-0">
+      <li v-for="(instructor, i) in section.instructors" :key="i" class="mb-1">
+        {{ titleCaseName(instructor.display_name) }}
+      </li>
+    </ul>
+    <div>
+      <span class="myuw-text-sm fst-italic"
+        >Only instructors of record have access to this course in MyUW and GradePage.
+      </span>
+      <uw-collapsed-item :part="submitGradesLearnMore" caller-id="InstructorsOfRecord">
+        <template #collapsed-body>
+          To add or remove an instructor of record, contact your departmental time schedule
+          coordinator.
+        </template>
+      </uw-collapsed-item>
+    </div>
+  </uw-card-property>
 </template>
 
 <script>
@@ -26,7 +26,7 @@ import CollapsedItem from '../../../_common/collapsed-part.vue';
 export default {
   components: {
     'uw-card-property': CardProperty,
-    'uw-collapsed-item': CollapsedItem
+    'uw-collapsed-item': CollapsedItem,
   },
   props: {
     section: {
@@ -43,8 +43,8 @@ export default {
     },
     submitGradesLearnMore() {
       return {
-        title: "Learn more",
-        id: "submitGradesLearnMore" + this.section.sln,
+        title: 'Learn more',
+        id: 'submitGradesLearnMore' + this.section.sln,
       };
     },
   },
