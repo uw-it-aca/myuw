40b76867c01a01b73c4d8ea4c3793cc07d587d71
Fang Lin <fanglin@uw.edu>
2023-05-19 17:45:16 -0700

    use collapsed-part component

--- myuw_vue/components/teaching/course/materials/futureq-textbook.vue
+++ myuw_vue/components/teaching/course/materials/futureq-textbook.vue
@@ -19,17 +19,8 @@
       <font-awesome-icon :icon="faExclamationTriangle" class="text-secondary" />
       Clarify course materials requirements in Time Schedule.
     </span>
-    <a
-      v-uw-collapse="`textbook-${section.anchor}-collapse-${$meta.uid}`"
-      v-no-track-collapse
-      class="p-0 border-0 mb-2 bg-transparent"
-    >How to fix this</a>
-    <uw-collapse
-      :id="`textbook-${section.anchor}-collapse-${$meta.uid}`"
-      v-model="collapseOpen"
-      class="myuw-fin-aid"
-    >
-      <div class="p-3 mt-2 bg-light text-dark notice-body">
+    <uw-collapsed-item :part="fixTextbook" caller-id="futureTextbook">
+      <template #collapsed-body>
         Use the
         <a href="https://www.ubookstore.com/faculty">
           UW Bookstore Course Materials Request Form </a>
@@ -38,8 +29,8 @@
           <br>To best accommodate students, list ALL the required reading materials for your 
           courses, regardless of whether you want UW Bookstore to supply the materials, or to 
           clarify if no texts are required for your course.
-      </div>
-    </uw-collapse>
+      </template>
+    </uw-collapsed-item>
   </span>
 </template>
 
@@ -49,10 +40,10 @@ import {mapGetters, mapState, mapActions} from 'vuex';
 import {
     faExclamationTriangle,
 } from '@fortawesome/free-solid-svg-icons';
-import Collapse from '../../../_templates/collapse.vue';
+import CollapsedItem from '../../_common/collapsed-part.vue';
 export default {
   components: {
-    'uw-collapse': Collapse,
+    'uw-collapsed-item': CollapsedItem,
   },
   props: {
     section: {
@@ -96,6 +87,12 @@ export default {
       return ("/textbooks/" + this.term + '#' + 
         this.section.curriculum_abbr +
         this.section.course_number + this.section.section_id);
+    },
+    fixTextbook() {
+      return {
+        title: 'How to fix this',
+        id: 'fixTextbook' + this.section.sln,
+      };
     }
   },
   created() {
