f113e4c2e6e6f140d5503c52f3abeed2ca4a8d61
Fang Lin <fanglin@uw.edu>
2023-05-23 16:30:47 -0700

    Resolve MUWM-5247

--- myuw_vue/components/home/quicklinks/covid-links.vue
+++ myuw_vue/components/home/quicklinks/covid-links.vue
@@ -1,30 +1,14 @@
 <template>
-  <div class="mx-n3 mb-3 p-3 myuw-covid-links">
-    <div v-if="instructor">
-      <p class="myuw-text-sm">
-        Zoom now has a permanent spot in the Quick Links above.
-        You can add other links by customizing your Quick Links,
-        including adding recently visited links from below.
-      </p>
-    </div>
+  <div class="mx-n3 mb-2 p-3 myuw-text-sm myuw-em-links">
+    Zoom now has a permanent spot in the Quick Links above.
+    You can add other links by customizing your Quick Links,
+    including adding recently visited links from below.
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 export default {
-  computed: {
-    ...mapState({
-      student: (state) => state.user.affiliations.student,
-      instructor: (state) => state.user.affiliations.instructor,
-      seattleStudent: (state) => state.user.affiliations.seattle,
-      bothellStudent: (state) => state.user.affiliations.bothell,
-      tacomaStudent: (state) => state.user.affiliations.tacoma,
-      seattleEmp: (state) => state.user.affiliations.official_seattle,
-      bothellEmp: (state) => state.user.affiliations.official_bothell,
-      tacomaEmp: (state) => state.user.affiliations.official_tacoma,
-    }),
-  },
 };
 </script>
 
@@ -32,7 +16,7 @@ export default {
 @use "sass:map";
 @import '../../../../myuw/static/css/myuw/variables.scss';
 
-.myuw-covid-links {
+.myuw-em-links {
   background-color: lighten(map.get($theme-colors, "beige"), 10%) !important;
 }
 
