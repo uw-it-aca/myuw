485c8d07b9c7845a4a62cf4bb36191c50497329a
Fang Lin <fanglin@uw.edu>
2023-06-02 12:57:02 -0700

    MUWM-5265: add checking

--- myuw/views/notice_admin.py
+++ myuw/views/notice_admin.py
@@ -209,20 +209,21 @@ def _save_notice(request, context, notice_id=None):
         notice.notice_type = notice_type
         notice.notice_category = notice_category
 
-        if start_date and start_date != notice.start:
-            notice.start = start_date
+        if start_date and end_date:
+            if start_date != notice.start:
+                notice.start = start_date
+            if end_date != notice.end:
+                notice.end = end_date
 
-        if end_date and end_date != notice.end:
-            notice.end = end_date
-
-        if start_week != notice.start_week:
-            notice.start_week = start_week
-
-        if duration != notice.duration:
-            notice.duration = duration
-
-        for term in terms:
-            setattr(notice, term, True)
+        if (start_week in start_week_range and
+                duration in duration_range and
+                terms and len(terms) > 0):
+            if start_week != notice.start_week:
+                notice.start_week = start_week
+            if duration != notice.duration:
+                notice.duration = duration
+            for term in terms:
+                setattr(notice, term, True)
 
         notice.target_group = target_group
 
