947549d798fac5c2aa346ea75f93a6da757efe13
Kathy Bui <kbui98@uw.edu>
2023-08-29 15:50:28 -0700

    Added new pronouns guidance banner to classlist

--- myuw_vue/components/teaching/classlist/class-student-list.vue
+++ myuw_vue/components/teaching/classlist/class-student-list.vue
@@ -1,33 +1,39 @@
 <template>
   <div>
-    <uw-card v-if="showCard"
-            :loaded="isReady"
-            :errored="isErrored"
-            no-bottom-margin
-            class="myuw-printable-card"
-            style="max-width: 100vw; overflow-x: hidden;"
+    <uw-card
+      v-if="showCard"
+      :loaded="isReady"
+      :errored="isErrored"
+      no-bottom-margin
+      class="myuw-printable-card"
+      style="max-width: 100vw; overflow-x: hidden"
     >
       <template #card-heading>
         <div v-if="sectionData" class="d-flex flex-wrap justify-content-between">
           <h2
             class="h3 text-dark-beige myuw-font-encode-sans"
             :class="[$mq === 'mobile' ? 'w-100' : '']"
-          >{{ cardHeading }}</h2>
-          <span v-if="sectionData.sln">
-            SLN {{ sectionData.sln }}
-          </span>
+          >
+            {{ cardHeading }}
+          </h2>
+          <span v-if="sectionData.sln"> SLN {{ sectionData.sln }} </span>
         </div>
       </template>
 
       <template #card-body>
+        <div class="alert alert-light border myuw-text-md">
+          Support students by reviewing the
+          <a href="#">Expectations and Guidance on Student Pronouns</a>. 
+          Pronouns are shared here in confidence for the purpose of supporting a respectful 
+          class environment.
+        </div>
         <uw-classlist-content
           :section="sectionData.sections[0]"
-          :is-joint-section-data-ready="isJointSectionDataReady" />
+          :is-joint-section-data-ready="isJointSectionDataReady"
+        />
       </template>
 
-      <template v-if="noData" #card-error>
-        No class information was found.
-      </template>
+      <template v-if="noData" #card-error> No class information was found. </template>
       <template v-else-if="noAccessPermission" #card-error>
         You need to be the class instructor to view student information.
       </template>
@@ -35,20 +41,17 @@
         The page you seek is for a past quarter and is no longer available.
       </template>
       <template v-else-if="dataError" #card-error>
-        An error occurred and MyUW cannot load the class student information
-        right now. Please try again later.
+        An error occurred and MyUW cannot load the class student information right now. Please try
+        again later.
       </template>
     </uw-card>
 
-    <uw-course-stats
-      :loaded="isReady"
-      :section-data="sectionData ? sectionData : {}"
-    />
+    <uw-course-stats :loaded="isReady" :section-data="sectionData ? sectionData : {}" />
   </div>
 </template>
 
 <script>
-import {mapGetters, mapState, mapActions} from 'vuex';
+import { mapGetters, mapState, mapActions } from 'vuex';
 import Card from '../../_templates/card.vue';
 import CourseStats from './statistics.vue';
 import Content from './content.vue';
@@ -65,7 +68,7 @@ export default {
       required: true,
     },
   },
-  data: function() {
+  data: function () {
     return {
       isJointSectionDataReady: false,
     };
@@ -95,12 +98,14 @@ export default {
       return this.isErroredTagged(this.getKey);
     },
     showContent() {
-      return this.sectionData && this.sectionData.sections.length &&
-        this.sectionData.sections[0].registrations.length;
+      return (
+        this.sectionData &&
+        this.sectionData.sections.length &&
+        this.sectionData.sections[0].registrations.length
+      );
     },
     showCard() {
-      return this.instructor &&
-        (!this.isReady || this.isErrored || this.showContent);
+      return this.instructor && (!this.isReady || this.isErrored || this.showContent);
     },
     getErrorCode() {
       return this.statusCodeTagged(this.getKey);
@@ -115,22 +120,27 @@ export default {
       return this.getErrorCode === 410;
     },
     dataError() {
-      return this.isErrored && !(this.noData ||
-        this.noAccessPermission || this.invalidCourse);
+      return this.isErrored && !(this.noData || this.noAccessPermission || this.invalidCourse);
     },
     jointSections() {
       return this.sectionData.sections[0].joint_sections;
     },
     cardHeading() {
       return (
-        this.sectionData.currAbbr + ' ' +
-        this.sectionData.courseNum + ' ' +
-        this.sectionData.sectionId + ', ' +
-        this.titleCaseWord(this.sectionData.quarter) + ' ' +
-        this.sectionData.year);
-    }
+        this.sectionData.currAbbr +
+        ' ' +
+        this.sectionData.courseNum +
+        ' ' +
+        this.sectionData.sectionId +
+        ', ' +
+        this.titleCaseWord(this.sectionData.quarter) +
+        ' ' +
+        this.sectionData.year
+      );
+    },
   },
-  watch: {  // MUWM-4385
+  watch: {
+    // MUWM-4385
     isReady: function (newValue, oldValue) {
       if (this.showContent && this.jointSections && this.jointSections.length) {
         this.loadJointRegLinkedSection();
@@ -146,16 +156,19 @@ export default {
     ...mapActions('classlist', {
       fetchClasslist: 'fetch',
     }),
-    loadJointRegLinkedSection() {  // MUWM-4385
+    loadJointRegLinkedSection() {
+      // MUWM-4385
       const fetches = [];
-      for(const section of this.jointSections) {
+      for (const section of this.jointSections) {
         fetches.push(this.fetchClasslist(section.url));
       }
-      Promise.all(fetches).then(results => {
-        this.isJointSectionDataReady = true;
-      }).catch(err => {
-        console.error(err);
-      })
+      Promise.all(fetches)
+        .then((results) => {
+          this.isJointSectionDataReady = true;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
     },
   },
 };
