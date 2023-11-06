2a1eaf3a4a0eb264430f0b6fdbfbda6f615890bd
Fang Lin <fanglin@uw.edu>
2023-11-06 14:30:36 -0800

    Add jest test for student course cards

---
+++ myuw_vue/tests/course-cards.test.js
@@ -0,0 +1,97 @@
+import axios from 'axios';
+import {createLocalVue} from './helper';
+import {mount} from '@vue/test-utils';
+import Vuex from 'vuex';
+
+import CourseCards from '../components/_common/course/student/course-cards.vue';
+import CourseCard from '../components/_common/course/student/course.vue';
+import MeetingInfo from '../components/_common/course/meeting/schedule.vue';
+import Instructors from '../components/_common/course/student/instructors.vue';
+import Resources from '../components/_common/course/student/resources.vue';
+import Textbooks from '../components/_common/textbooks.vue';
+
+import stud_schedule from '../vuex/store/schedule/student';
+import iasystem from '../vuex/store/iasystem';
+
+import mockStudCourses from
+  './mock_data/stud_schedule/javerage2013Spring.json';
+import mockCourseEval from
+  './mock_data/iasystem/javerage2013Spring.json';
+
+const localVue = createLocalVue(Vuex);
+
+jest.mock('axios');
+
+describe('Student Course cards', () => {
+  let store;
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      modules: {
+        stud_schedule,
+        iasystem,
+      },
+      state: {
+        user: {
+          affiliations: {
+            student: true,
+          }
+        },
+        termData: {
+          quarter: "spring",
+        },
+      }
+    });
+  });
+
+  it('Verify normal display with javerage', async () => {
+    axios.get.mockImplementation((url) => {
+      const urlData = {
+        '/api/v1/schedule/current': mockStudCourses,
+        '/api/v1/ias/': mockCourseEval,
+      };
+      return Promise.resolve({data: urlData[url]});
+    });
+
+    const wrapper = mount(CourseCards, {store, localVue,
+      propsData: {'term': 'current'}});
+    await new Promise(setImmediate);
+    expect(wrapper.vm.isCurrentTerm).toBe(true);
+    expect(wrapper.vm.quarter).toBe("spring");
+    expect(wrapper.vm.student ).toBe(true);
+    expect(wrapper.vm.course).toBeTruthy();
+    expect(wrapper.vm.isErrored).toBe(false);
+    expect(wrapper.findAllComponents(CourseCard).length).toBe(5);
+    expect(wrapper.findAllComponents(MeetingInfo).length).toBe(5);
+    expect(wrapper.findAllComponents(Instructors).length).toBe(3);
+    expect(wrapper.findAllComponents(Resources).length).toBe(5);
+    const section = wrapper.vm.course.sections[0];
+    wrapper = mount(Textbooks, {
+      store, localVue,
+      propsData: { 'term': 'current', 'section': section}
+    });
+    await new Promise(setImmediate);
+    expect(wrapper.vm.termId).toBe("2");
+    expect(wrapper.vm.uwtCourse).toBe(false);
+    expect(wrapper.vm.textbookPageUrl).toBeTruthy();
+    expect(wrapper.vm.uwtTextbookUrl).toBeTruthy();
+  });
+
+  it('Hide card if not student', () => {
+    store.state.user.affiliations.student = false;
+    const wrapper = mount(CourseCards, {store, localVue,
+      propsData: {'term': 'current'}});
+    expect(wrapper.findComponent(CourseCards).exists()).toBe(true);
+    expect(wrapper.findAllComponents(CourseCard).length).toBe(0);
+  });
+
+  it('Show error', async () => {
+    axios.get.mockImplementation((url) => {
+      return Promise.reject({response: {status: 543}});
+    });
+    const wrapper = mount(CourseCards, {store, localVue,
+      propsData: {'term': 'current'}});
+    await new Promise(setImmediate);
+    expect(wrapper.vm.isErrored).toBe(true);
+  });
+});
