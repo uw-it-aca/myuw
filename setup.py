32f2a5aae0cb7bc89019765cd81f3e08a6fa5510
Fang Lin <fanglin@uw.edu>
2024-02-22 21:08:16 -0800

    update lxml version

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
         'django_client_logger~=3.0',
         'UW-HX-Toolkit~=2.7',
         'django-blti~=2.2',
-        'lxml==4.9.4',
+        'lxml<5',
         'nh3',
         'pyyaml',
         'ua-parser',
