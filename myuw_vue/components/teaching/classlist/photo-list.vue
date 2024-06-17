4f89578a667989635a460a701cd8b12c3fbd621b
Fang Lin <fanglin@uw.edu>
2024-06-16 19:29:32 -0700

    Resolve MUWM-4369

--- myuw_vue/components/teaching/classlist/photo-list.vue
+++ myuw_vue/components/teaching/classlist/photo-list.vue
@@ -6,8 +6,12 @@
     <h3 class="visually-hidden">
       Grid of Student Photos
     </h3>
+    <div class="sort-buttons">
+      <button @click="setSortKey('first_name')">Sort by First Name</button>
+      <button @click="setSortKey('surname')">Sort by Surname</button>
+    </div>
     <ol class="list-unstyled d-flex flex-wrap">
-      <li v-for="(reg, i) in registrations"
+      <li v-for="(reg, i) in sortedRegistrations"
           :id="`student-photo-${reg.regid}`"
           :key="i"
           class="p-1 mb-1"
@@ -39,6 +43,25 @@ export default {
     showJointCourseStud: {
       type: Boolean,
       default: false,
+    },
+  },
+  data() {
+    return {
+      sortKey: 'first_name',
+    };
+  },
+  computed: {
+    sortedRegistrations() {
+      return [...this.registrations].sort((a, b) => {
+        if (a[this.sortKey] < b[this.sortKey]) return -1;
+        if (a[this.sortKey] > b[this.sortKey]) return 1;
+        return 0;
+      });
+    },
+  },
+  methods: {
+    setSortKey(key) {
+      this.sortKey = key;
     },
   },
 };
