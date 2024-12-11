26c9f4a44eb54bba0e066748c6a4646f4fb6d1c6
Fang Lin <fanglin@uw.edu>
2024-12-10 20:45:13 -0800

    MUWM-5352: _get_residency_change

--- myuw/dao/student_profile.py
+++ myuw/dao/student_profile.py
@@ -89,17 +89,8 @@ def get_academic_info(request, response):
             response['class_level'] = enrollment.class_level.upper()
             break
 
-    for term in terms:
-        if term in enrollments:
-            enrollment = enrollments[term]
-            if enrollment.has_pending_resident_change:
-                # MUWM-5352
-                response["pending_residency_change"] = {
-                    "pending_resident_code": enrollment.pending_resident_code,
-                    "pending_resident_desc": enrollment.pending_resident_desc,
-                    "term": {"year": term.year, "quarter": term.quarter}
-                }
-                break
+    response["pending_residency_change"] = _get_residency_change(
+        terms, enrollments, response['resident_code'])
 
     response['term_majors'] = _get_degrees_for_terms(terms, enrollments,
                                                      "majors")
@@ -177,3 +168,18 @@ def _has_only_dropped_degrees(first, second):
     has_dropped = len(set(first) - set(second)) > 0
     has_added = len(set(second) - set(first)) > 0
     return has_dropped and not has_added
+
+def _get_residency_change(terms, enrollments, current_resident_code):
+    # MUWM-5352
+    for term in terms:
+        if term in enrollments:
+            enrollment = enrollments[term]
+            if (enrollment.pending_resident_code != current_resident_code and
+                    enrollment.pending_resident_code != "0"):
+                # MUWM-5352
+                return {
+                    "pending_resident_code": enrollment.pending_resident_code,
+                    "pending_resident_desc": enrollment.pending_resident_desc,
+                    "term": {"year": term.year, "quarter": term.quarter}
+                }
+    return None
