07ef3827893a3113b1a64165b4eca2b348993ac4
Fang Lin <fanglin@uw.edu>
2025-02-27 10:18:36 -0800

    Revise the UI according to the input from Huksy office

--- myuw_vue/components/accounts/idcard.vue
+++ myuw_vue/components/accounts/idcard.vue
@@ -1,47 +1,38 @@
 <template>
   <uw-card v-if="showCard" :loaded="isReady" :errored="isErrored" :errored-show="showError">
     <template #card-heading>
-      <h2 class="h4 mb-3 text-dark-beige myuw-font-encode-sans">Husky Card</h2>
+      <h2 class="h4 mb-3 text-dark-beige myuw-font-encode-sans">Husky Card Eligibility</h2>
     </template>
 
     <template #card-body>
       <ul class="list-unstyled">
-        <li v-if="student" class="mb-1">
+        <li v-if="idcard.not_eligible" class="mb-1">
           <uw-card-status>
-            <template #status-label>Your Student Card</template>
-            <template v-if="idcard.student_eligible" #status-value>Eligible</template>
-            <template v-else #status-value>Not eligible</template>
+            <template #status-label>Status</template>
+            <template #status-value>Not Eligible</template>
             <template #status-content>
-              <div class="myuw-text-sm text-muted">
-                ......
+              <div v-if="hfs.student_husky_card.last_updated" class="myuw-text-sm text-muted">
+                No access into buildings, no UPASS membership, no parking permit, etc.
               </div>
             </template>
           </uw-card-status>
         </li>
-        <li v-if="employee || retiree" class="mb-1">
-          <uw-card-status v-if="retiree">
-            <template #status-label>Your Retiree Card</template>
-            <template v-if="idcard.retiree_eligible" #status-value>Eligible</template>
-            <template v-else #status-value>Not eligible</template>
-            <template #status-content>
-              <div class="myuw-text-sm text-muted">
-                ......
-              </div>
-            </template>
-          </uw-card-status>
-          <uw-card-status v-else>
-            <template #status-label>Your Employee Card</template>
-            <template v-if="idcard.employee_eligible" #status-value>Eligible</template>
-            <template v-else #status-value>Not eligible</template>
-            <template #status-content>
-              <div class="myuw-text-sm text-muted">
-                ......
-              </div>
+        <li v-else class="mb-1">
+          <uw-card-status>
+            <template #status-label>Status</template>
+            <template #status-value>
+              <ul class="list-unstyled">
+                <li v-if="idcard.student_eligible" class="mb-1">Student Eligible</li>
+                <li v-if="idcard.retiree_eligible" class="mb-1">Retiree Eligible</li>
+                <li v-if="idcard.employee_eligible" class="mb-1">Employee Eligible</li>
+              </ul>
             </template>
           </uw-card-status>
         </li>
       </ul>
-
+      <div class="myuw-text-sm text-muted">
+        <a href="/">About Husky Card</a>
+      </div>
     </template>
     <template #card-error>
       An error occurred and MyUW cannot load your ID card right now. In the meantime,
