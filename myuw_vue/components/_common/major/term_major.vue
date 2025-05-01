58453441b6d35a30a4cafd1697fb507b36328f0b
Fang Lin <fanglin@uw.edu>
2025-05-01 13:51:15 -0700

    Resolve MUWM-5399

---
+++ myuw_vue/components/_common/major/term_major.vue
@@ -0,0 +1,59 @@
+<template>
+  <li v-if="index == 0" class="mb-1" :key="index">
+    {{ degreeListString(termMajor.majors) }}
+    <span v-if="invalid_degree_name" :id="`major-${index}}`">
+      <font-awesome-icon :icon="faExclamationTriangle" class="me-1" />
+      <uw-tooltip :target="`major-${index}}`" :title="tooltipMessage" />
+    </span>
+  </li>
+  <li v-else-if="termMajor.degrees_modified" class="mb-1" :key="index">
+    Beginning {{ titleCaseWord(termMajor.quarter) }} {{ termMajor.year }}:
+    &nbsp;&nbsp;
+    <span v-if="termMajor.majors.length > 0" :id="`major-${index}}`">
+      {{ degreeListString(termMajor.majors) }}
+    </span>
+    <span v-if="invalid_degree_name">
+      <font-awesome-icon :icon="faExclamationTriangle" class="me-1" />
+      <uw-tooltip :target="`major-${index}}`" :title="tooltipMessage" />
+    </span>
+  </li>
+</template>
+
+<script>
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import Tooltip from '../../_templates/tooltip.vue';
+
+export default {
+  components: {
+    'uw-tooltip': Tooltip,
+  },
+  data() {
+    return {
+      faExclamationTriangle,
+    };
+  },
+  props: {
+    index: {
+      type: Number,
+      required: true,
+    },
+    termMajor: {
+      type: Object,
+      required: true
+    },
+  },
+  computed: {
+    invalid_degree_name () {
+      if (this.termMajor.majors.length == 0) return false;
+      const major = this.termMajor.majors[0];
+      return major.name === null && major.college_abbr.length > 0 && major.degree_level > 0;
+    },
+    tooltipMessage() {
+      return (
+        "The major name your have has been terminated. " +
+        "Please contact your department to update your major(s)."
+      );
+    }
+  },
+};
+</script>
--- myuw_vue/components/profile/student-profile.vue
+++ myuw_vue/components/profile/student-profile.vue
@@ -18,7 +18,7 @@
           {{ classStanding }}
         </uw-card-property>
         <uw-card-property title="Major">
-          <cur_majors :term-majors="termMajors"></cur_majors>
+          <major-list :term-majors="termMajors"></major-list>
         </uw-card-property>
         <uw-card-property v-if="hasMinors" title="Minor">
           <ul class="list-unstyled mb-0">
@@ -143,14 +143,14 @@ import { mapGetters, mapState, mapActions } from 'vuex';
 import Card from '../_templates/card.vue';
 import CardProperty from '../_templates/card-property.vue';
 import CardPropertyGroup from '../_templates/card-property-group.vue';
-import CurMajors from '../_common/cur_major.vue';
+import CurMajors from '../_common/major/cur_fut_majors.vue';
 
 export default {
   components: {
     'uw-card': Card,
     'uw-card-property': CardProperty,
     'uw-card-property-group': CardPropertyGroup,
-    'cur_majors': CurMajors,
+    'major-list': CurMajors,
   },
   computed: {
     ...mapState('profile', {
