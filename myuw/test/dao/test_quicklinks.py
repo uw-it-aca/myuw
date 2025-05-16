b3d20e7c59e868262bb990765e63a42efc762492
Fang Lin <fanglin@uw.edu>
2025-05-15 19:57:44 -0700

    MUWM-4955

--- myuw/test/dao/test_quicklinks.py
+++ myuw/test/dao/test_quicklinks.py
@@ -51,7 +51,7 @@ class TestQuickLinkDAO(TransactionTestCase):
         data = get_quicklink_data(req)
         recent = _get_recent(data)
 
-        self.assertEqual(len(recent), 1)
+        self.assertEqual(len(recent), 2)
         self.assertTrue(u1 in recent)
 
         CustomLink.objects.create(user=user, url=u1)
