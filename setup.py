4773a816f313213d9ecc335a560925cd21cb7fa6
Fang Lin <fanglin@uw.edu>
2025-09-22 12:22:06 -0700

    tidy

--- setup.py
+++ setup.py
@@ -11,17 +11,17 @@ See the README on `GitHub <https://github.com/uw-it-aca/myuw>`_.
 # The VERSION file is created by travis-ci, based on the tag name
 version_path = 'myuw/VERSION'
 VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
-VERSION = VERSION.replace('\n', '')
+VERSION = VERSION.replace("\n", "")
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-url = 'https://github.com/uw-it-aca/myuw'
+url = "https://github.com/uw-it-aca/myuw"
 setup(
     name='MyUW',
     version=VERSION,
     packages=['myuw'],
-    author='UW-IT Student & Educational Technology Services',
+    author="UW-IT Student & Educational Technology Services",
     author_email='aca-it@uw.edu',
     include_package_data=True,
     install_requires=[
@@ -31,7 +31,7 @@ setup(
         'urllib3',
         'uw-memcached-clients~=1.1',
         'UW-Django-SAML2~=1.8',
-        'django-userservice~=3.2.2',
+        'django-userservice~=3.2',
         'aws-message-client~=1.6',
         'UW-RestClients-Core~=1.4',
         'UW-RestClients-Bookstore~=1.4',
@@ -56,7 +56,7 @@ setup(
         'Django-SupportTools~=3.6',
         'Django-Persistent-Message~=1.3',
         'UW-RestClients-Django-Utils~=2.3',
-        'Django-Safe-EmailBackend~=1.2.',
+        'Django-Safe-EmailBackend~=1.2',
         'django_client_logger~=3.1',
         'UW-HX-Toolkit~=2.7',
         'django-blti~=3.0',
