ebe0e21c5bf2275670c35b813f8b341c0a9ed54a
Fang Lin <fanglin@uw.edu>
2026-01-22 10:44:18 -0800

    MUWM-5214

---
+++ myuw_vue/components/husky_experience/husky-exp.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="row" :class="[$mq === 'mobile' ? 'px-3' : 'px-0']">
+    <p class="myuw-text-lg">
+      Looking for the Husky Experience Toolkit? You can now find the helpful toolkit articles
+      on Undergraduate Academic Affairs’
+      <a href="https://sas.uaa.uw.edu/husky-experience/">Husky Experience Toolkit</a> site.
+      Bookmark their site for future reference!
+    </p>
+  </div>
+</template>
